17a8b4292346f0aa7d8eaa9a168b2c6de22e2bb9
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-03-31 21:27:40 +0200

    feat: Add new built-in block selector menus by Hasrat

---
+++ app/src/main/java/dev/aldi/sayuti/block/ExtraMenuBlock.java
@@ -0,0 +1,110 @@
+package dev.aldi.sayuti.block;
+
+import android.net.Uri;
+
+import com.besome.sketch.editor.LogicEditorActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import a.a.a.Ss;
+import a.a.a.jC;
+import mod.agus.jcoderz.lib.FileUtil;
+import mod.hilal.saif.asd.asdforall.AsdAll;
+import mod.hilal.saif.blocks.BlocksMenu;
+import mod.w3wide.menu.MenuBean;
+
+public class ExtraMenuBlock {
+
+    private final LogicEditorActivity a;
+    private final MenuBean menuBean;
+
+    public ExtraMenuBlock(LogicEditorActivity logicEditorActivity) {
+        this.a = logicEditorActivity;
+        this.menuBean = new MenuBean(logicEditorActivity);
+    }
+
+    public void a(Ss ss, AsdAll asdAll, ArrayList<String> arrayList) {
+        BlocksMenu.extraBlockMenu(ss, asdAll, arrayList);
+        this.menuBean.projectMenu(ss, asdAll, arrayList);
+        String menuName = ss.getMenuName();
+        try {
+            JSONArray jSONArray = new JSONArray(ExtraBlockFile.getMenuBlockFile());
+            for (int i = 0; i < jSONArray.length(); i++) {
+                JSONObject jSONObject = jSONArray.getJSONObject(i);
+                if (!jSONObject.isNull("id") && menuName.equals(jSONObject.getString("id"))) {
+                    arrayList.clear();
+                    b(jSONObject, asdAll, arrayList, this.a.B);
+                    try {
+                        JSONArray jSONArray2 = new JSONObject(ExtraBlockFile.getMenuDataFile()).getJSONArray(jSONObject.getString("id"));
+                        if (jSONArray2.length() > 0) {
+                            for (int i2 = 0; i2 < jSONArray2.length(); i2++) {
+                                JSONObject jSONObject2 = jSONArray2.getJSONObject(i2);
+                                asdAll.b(jSONObject2.getString("title"));
+                                ArrayList<String> arrayList2 = new ArrayList<>(Arrays.asList(jSONObject2.getString("value").split("\\+")));
+                                arrayList.addAll(arrayList2);
+                            }
+                        }
+                    } catch (JSONException ignored) {
+                    }
+                }
+            }
+        } catch (JSONException ignored) {
+        }
+    }
+
+    public final void b(JSONObject json, AsdAll asdAll, ArrayList<String> selectableItems, String sc_id) {
+        ArrayList<String> menus = new ArrayList<>();
+        ArrayList<String> layouts = new ArrayList<>();
+        ArrayList<String> animations = new ArrayList<>();
+        ArrayList<String> drawables = new ArrayList<>();
+        ArrayList<String> drawables_xhdpi = new ArrayList<>();
+        FileUtil.listDir(FileUtil.getExternalStorageDir().concat("/.sketchware/data/" + sc_id + "/files/resource/menu/"), menus);
+        FileUtil.listDir(FileUtil.getExternalStorageDir().concat("/.sketchware/data/" + sc_id + "/files/resource/layout/"), layouts);
+        FileUtil.listDir(FileUtil.getExternalStorageDir().concat("/.sketchware/data/" + sc_id + "/files/resource/anim/"), animations);
+        FileUtil.listDir(FileUtil.getExternalStorageDir().concat("/.sketchware/data/" + sc_id + "/files/resource/drawable/"), drawables);
+        FileUtil.listDir(FileUtil.getExternalStorageDir().concat("/.sketchware/data/" + sc_id + "/files/resource/drawable-xhdpi/"), drawables_xhdpi);
+        try {
+            if (json.getString("id").equals("menu")) {
+                asdAll.b("Select a menu");
+                for (String menu : menus) {
+                    selectableItems.add(Uri.parse(menu).getLastPathSegment().substring(0, Uri.parse(menu).getLastPathSegment().indexOf(".xml")));
+                }
+            } else if (json.getString("id").equals("layout")) {
+                asdAll.b("Select a layout");
+                for (String layout : layouts) {
+                    selectableItems.add(Uri.parse(layout).getLastPathSegment().substring(0, Uri.parse(layout).getLastPathSegment().indexOf(".xml")));
+                }
+                for (String str4 : jC.b(a.B).e()) {
+                    selectableItems.add(str4.substring(0, str4.indexOf(".xml")));
+                }
+            } else if (json.getString("id").equals("anim")) {
+                asdAll.b("Select an animation");
+                for (String animation : animations) {
+                    selectableItems.add(Uri.parse(animation).getLastPathSegment().substring(0, Uri.parse(animation).getLastPathSegment().indexOf(".xml")));
+                }
+            } else if (json.getString("id").equals("drawable")) {
+                asdAll.b("Select a drawable");
+                for (String drawable : drawables) {
+                    selectableItems.add(Uri.parse(drawable).getLastPathSegment().substring(0, Uri.parse(drawable).getLastPathSegment().indexOf(".xml")));
+                }
+            } else if (json.getString("id").equals("image")) {
+                asdAll.b("Select an image");
+                for (String drawable_xhdpi : drawables_xhdpi) {
+                    if (drawable_xhdpi.contains(".png") || drawable_xhdpi.contains(".jpg")) {
+                        if (drawable_xhdpi.contains(".png")) {
+                            selectableItems.add(Uri.parse(drawable_xhdpi).getLastPathSegment().substring(0, Uri.parse(drawable_xhdpi).getLastPathSegment().indexOf(".png")));
+                        } else {
+                            selectableItems.add(Uri.parse(drawable_xhdpi).getLastPathSegment().substring(0, Uri.parse(drawable_xhdpi).getLastPathSegment().indexOf(".jpg")));
+                        }
+                    }
+                }
+            }
+        } catch (JSONException ignored) {
+        }
+    }
+}
