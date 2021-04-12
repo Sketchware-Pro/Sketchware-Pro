76f11b09b612e856e96da8c0898e4504735724b8
Hosni Fraj <82328277+hosni-fraj@users.noreply.github.com>
2021-04-12 08:04:33 +0100

    Update CompileLogSaver.java
    Sketchware can't access to this method without static keyword
    added a `static` keyword to all methods

--- app/src/main/java/mod/hosni/fraj/compilerlog/CompileLogSaver.java
+++ app/src/main/java/mod/hosni/fraj/compilerlog/CompileLogSaver.java
@@ -7,17 +7,17 @@ public class CompileLogSaver {
 
     public static String sc_id;
     public static FileUtil fileUtil;
-
+    public static FilePathUtil filePathUtil;
     public CompileLogSaver(String sc_id) {
         this.sc_id = sc_id;
     }
 
     public static void clearLastLog() {
-        FileUtil.deleteFile(getLastLogPath());
+        fileUtil.deleteFile(getLastLogPath());
     }
 
     public static String getLastLog() {
-        String readFile = FileUtil.readFile(getLastLogPath());
+        String readFile = fileUtil.readFile(getLastLogPath());
         if (readFile.equals("")) {
             readFile = "No errors have occurred during the last build.";
         }
@@ -25,10 +25,10 @@ public class CompileLogSaver {
     }
 
     public static void setLastLog(String log) {
-        FileUtil.writeFile(getLastLogPath(), log);
+        fileUtil.writeFile(getLastLogPath(), log);
     }
 
     private static String getLastLogPath() {
-        return FilePathUtil.getLastCompileLogPath(sc_id);
+        return filePathUtil.getLastCompileLogPath(sc_id);
     }
 }
