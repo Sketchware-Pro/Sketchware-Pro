19a119f7151b8079a002fde5d78418b98f41c672
Hosni Fraj <82328277+hosni-fraj@users.noreply.github.com>
2021-04-12 08:01:18 +0100

    Update CompileLogSaver.java

--- app/src/main/java/mod/hosni/fraj/compilerlog/CompileLogSaver.java
+++ app/src/main/java/mod/hosni/fraj/compilerlog/CompileLogSaver.java
@@ -5,17 +5,18 @@ import mod.agus.jcoderz.lib.FileUtil;
 
 public class CompileLogSaver {
 
-    public String sc_id;
+    public static String sc_id;
+    public static FileUtil fileUtil;
 
     public CompileLogSaver(String sc_id) {
         this.sc_id = sc_id;
     }
 
-    public void clearLastLog() {
+    public static void clearLastLog() {
         FileUtil.deleteFile(getLastLogPath());
     }
 
-    public String getLastLog() {
+    public static String getLastLog() {
         String readFile = FileUtil.readFile(getLastLogPath());
         if (readFile.equals("")) {
             readFile = "No errors have occurred during the last build.";
@@ -23,11 +24,11 @@ public class CompileLogSaver {
         return readFile;
     }
 
-    public void setLastLog(String log) {
+    public static void setLastLog(String log) {
         FileUtil.writeFile(getLastLogPath(), log);
     }
 
-    private String getLastLogPath() {
+    private static String getLastLogPath() {
         return FilePathUtil.getLastCompileLogPath(sc_id);
     }
 }
