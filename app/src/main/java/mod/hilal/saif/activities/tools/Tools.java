56ff620988f761935ae91bed7815535aa50151e6
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-12 12:36:15 +0200

    feat: Add (still) unfinished APK Signer in Tools.java, add APK Signer helper class

--- app/src/main/java/mod/hilal/saif/activities/tools/Tools.java
+++ app/src/main/java/mod/hilal/saif/activities/tools/Tools.java
@@ -10,27 +10,42 @@ import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.RippleDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.besome.sketch.lib.ui.EasyDeleteEditText;
 import com.github.angads25.filepicker.controller.DialogSelectionListener;
 import com.github.angads25.filepicker.model.DialogConfigs;
 import com.github.angads25.filepicker.model.DialogProperties;
 import com.github.angads25.filepicker.view.FilePickerDialog;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 
 import dev.aldi.sayuti.editor.manage.ManageLocalLibraryActivity;
 import mod.SketchwareUtil;
+import mod.agus.jcoderz.lib.FileUtil;
+import mod.alucard.tn.apksigner.ApkSigner;
 import mod.hey.studios.code.SrcCodeEditor;
 import mod.hey.studios.util.Helper;
-import mod.hilal.saif.lib.FileUtil;
 
 public class Tools extends Activity {
 
@@ -40,7 +55,7 @@ public class Tools extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(2131427775);
+        setContentView(0x7f0b01bf);
         dump = findViewById(2131231449);
         base = (ViewGroup) dump.getParent();
         base.removeView(dump);
@@ -105,7 +120,7 @@ public class Tools extends Activity {
         ((ViewGroup) parent).addView(inflate, 0);
     }
 
-    public void openWorkingDirectory() {
+    private void openWorkingDirectory() {
         DialogProperties properties = new DialogProperties();
         properties.selection_mode = DialogConfigs.MULTI_MODE;
         properties.selection_type = DialogConfigs.FILE_AND_DIR_SELECT;
@@ -133,6 +148,7 @@ public class Tools extends Activity {
                                                 public void onClick(DialogInterface dialog, int which) {
                                                     for (String file : files) {
                                                         FileUtil.deleteFile(file);
+                                                        dialog.dismiss();
                                                     }
                                                 }
                                             })
@@ -279,5 +295,303 @@ public class Tools extends Activity {
                 openWorkingDirectory();
             }
         });
+        CardView signApkFile = newCard(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 0.0f);
+        LinearLayout signApkFileLayout = newLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 0.0f);
+        signApkFile.addView(signApkFileLayout);
+        makeup(signApkFileLayout, 0x7f0701f8, "Sign an APK file with testkey", "Sign an already existing APK file with testkey and signature schemes up to V4");
+        base.addView(signApkFile);
+        signApkFileLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signApkFileDialog();
+            }
+        });
+    }
+
+    private void signApkFileDialog() {
+        class DialogCancelButtonOnClickListener implements View.OnClickListener {
+
+            private final AlertDialog mDialog;
+
+            public DialogCancelButtonOnClickListener(AlertDialog dialog) {
+                mDialog = dialog;
+            }
+
+            @Override
+            public void onClick(View v) {
+                mDialog.dismiss();
+            }
+        }
+
+        View dialog_root = getLayoutInflater().inflate(0x7f0b0052, null, false);
+        ImageView dialog_img = dialog_root.findViewById(0x7f0800fe);
+        TextView dialog_title = dialog_root.findViewById(0x7f080100);
+        TextView dialog_msg = dialog_root.findViewById(0x7f0800ff);
+        FrameLayout custom_view = dialog_root.findViewById(0x7f0800dd);
+        TextView dialog_btn_no = dialog_root.findViewById(0x7f0800fc);
+        TextView dialog_btn_yes = dialog_root.findViewById(0x7f0800fd);
+
+        AlertDialog dialog_key_type = new AlertDialog.Builder(this)
+                .setView(dialog_root)
+                .create();
+
+        RadioGroup radio_group = new RadioGroup(this);
+
+        RadioButton radio_testkey = new RadioButton(this);
+        radio_testkey.setText("Debug key (testkey)");
+        radio_group.addView(radio_testkey);
+
+        RadioButton radio_own_jks = new RadioButton(this);
+        radio_own_jks.setText("Custom Java keystore (.jks)");
+        radio_group.addView(radio_own_jks);
+
+        RadioButton radio_own_key = new RadioButton(this);
+        radio_own_key.setText("Custom key files (.pk8 and .x509.pem)");
+        radio_group.addView(radio_own_key);
+
+        custom_view.addView(radio_group);
+
+        /* ic_apk_color_96dp */
+        dialog_img.setImageResource(0x7f0701f8);
+        dialog_title.setText("Sign an APK");
+        dialog_msg.setText("To sign an APK file, a key is required. What type is the key to sign the APK with?");
+        dialog_btn_no.setText("Cancel");
+        dialog_btn_no.setOnClickListener(new DialogCancelButtonOnClickListener(dialog_key_type));
+        dialog_btn_yes.setText("Next");
+        dialog_btn_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* Check if user has selected a key type */
+                if (radio_group.getCheckedRadioButtonId() == -1) {
+                    SketchwareUtil.toast("Select a key type");
+                    return;
+                }
+
+                /* We recycle dialog_root, so we need to remove it from its parent first */
+                ((ViewGroup) dialog_root.getParent()).removeView(dialog_root);
+
+                if (radio_testkey.isChecked()) {
+                    View testkey_root = getLayoutInflater().inflate(0x7f0b00c1, null, false);
+                    TextView font_preview = testkey_root.findViewById(0x7f08015d);
+                    EasyDeleteEditText ed_input = testkey_root.findViewById(0x7f08010e);
+                    ImageView select_file = testkey_root.findViewById(0x7f0803de);
+                    TextView tv_collection = testkey_root.findViewById(0x7f0804a9);
+                    CheckBox chk_collection = testkey_root.findViewById(0x7f0800a7);
+
+                    select_file.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            DialogProperties properties = new DialogProperties();
+                            properties.selection_mode = DialogConfigs.SINGLE_MODE;
+                            properties.selection_type = DialogConfigs.FILE_SELECT;
+                            properties.extensions = new String[]{"apk"};
+                            FilePickerDialog dialog = new FilePickerDialog(Tools.this, properties);
+                            dialog.setDialogSelectionListener(new DialogSelectionListener() {
+                                @Override
+                                public void onSelectedFilePaths(String[] files) {
+                                    ed_input.getEditText().setText(files[0]);
+                                }
+                            });
+                            dialog.setTitle("Select the APK to sign");
+                            dialog.show();
+                        }
+                    });
+
+                    dialog_title.setText("Input APK");
+                    /* ic_apk_color_96dp */
+                    dialog_img.setImageResource(0x7f0701f8);
+                    dialog_msg.setVisibility(View.GONE);
+                    font_preview.setText("Path of APK to sign");
+                    font_preview.setVisibility(View.VISIBLE);
+                    tv_collection.setVisibility(View.GONE);
+                    chk_collection.setVisibility(View.GONE);
+
+                    custom_view.removeAllViews();
+                    custom_view.addView(testkey_root);
+
+                    AlertDialog dialog_testkey = new AlertDialog.Builder(Tools.this)
+                            .setView(dialog_root)
+                            .create();
+
+                    dialog_btn_no.setOnClickListener(new DialogCancelButtonOnClickListener(dialog_testkey));
+
+                    dialog_btn_yes.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog_testkey.dismiss();
+
+                            String input_apk_path = ed_input.getEditText().getText().toString();
+                            String output_apk_file_name = Uri.fromFile(new File(input_apk_path)).getLastPathSegment();
+                            String output_apk_path = new File(FileUtil.getExternalStorageDir(),
+                                    "sketchware/signed_apk/" + output_apk_file_name).getAbsolutePath();
+
+                            if (new File(output_apk_path).exists()) {
+                                /* We recycle dialog_root, so we need to remove it from its parent first */
+                                ((ViewGroup) dialog_root.getParent()).removeView(dialog_root);
+
+                                /* As we're recycling dialog_root, we need to remove the checkboxes from earlier */
+                                if (custom_view.getParent() != null) {
+                                    ((ViewGroup) custom_view.getParent()).removeView(custom_view);
+                                }
+
+                                AlertDialog dialog_confirm_overwrite = new AlertDialog.Builder(Tools.this)
+                                        .setView(dialog_root)
+                                        .create();
+
+                                /* color_save_as_new_96 */
+                                dialog_img.setImageResource(0x7f070121);
+                                dialog_title.setText("File exists");
+                                dialog_msg.setText("An APK named " + output_apk_file_name
+                                        + " exists already in /Internal storage/sketchware/signed_apk/. Overwrite it?");
+                                dialog_msg.setVisibility(View.VISIBLE);
+                                dialog_btn_no.setText("Cancel");
+                                dialog_btn_no.setOnClickListener(new DialogCancelButtonOnClickListener(dialog_confirm_overwrite));
+                                dialog_btn_yes.setText("Overwrite");
+                                dialog_btn_yes.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        dialog_confirm_overwrite.dismiss();
+                                        signApkFileDebugWithDialog(input_apk_path, output_apk_path);
+                                    }
+                                });
+
+                                dialog_confirm_overwrite.show();
+                                return;
+                            }
+
+                            signApkFileDebugWithDialog(input_apk_path, output_apk_path);
+                        }
+                    });
+
+                    dialog_testkey.show();
+                } else if (radio_own_jks.isChecked()) {
+                    View own_jks_root = getLayoutInflater().inflate(0x7f0b00c1, null, false);
+                    TextView font_preview = own_jks_root.findViewById(0x7f08015d);
+                    EasyDeleteEditText ed_input = own_jks_root.findViewById(0x7f08010e);
+                    ImageView select_file = own_jks_root.findViewById(0x7f0803de);
+                    TextView tv_collection = own_jks_root.findViewById(0x7f0804a9);
+                    CheckBox chk_collection = own_jks_root.findViewById(0x7f0800a7);
+
+                    select_file.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            DialogProperties properties = new DialogProperties();
+                            properties.selection_mode = DialogConfigs.SINGLE_MODE;
+                            properties.selection_type = DialogConfigs.FILE_SELECT;
+                            properties.extensions = new String[]{"jks", "keystore"};
+                            FilePickerDialog dialog = new FilePickerDialog(Tools.this, properties);
+                            dialog.setDialogSelectionListener(new DialogSelectionListener() {
+                                @Override
+                                public void onSelectedFilePaths(String[] files) {
+                                    ed_input.getEditText().setText(files[0]);
+                                }
+                            });
+                            dialog.setTitle("Select a keystore");
+                            dialog.show();
+                        }
+                    });
+
+                    dialog_title.setText("Sign with custom keystore");
+                    /* ic_renew_48dp */
+                    dialog_img.setImageResource(0x7f070289);
+                    dialog_msg.setVisibility(View.GONE);
+                    font_preview.setText("Custom keystore path");
+                    font_preview.setVisibility(View.VISIBLE);
+                    tv_collection.setVisibility(View.GONE);
+                    chk_collection.setVisibility(View.GONE);
+
+                    custom_view.removeAllViews();
+                    custom_view.addView(own_jks_root);
+
+                    AlertDialog dialog_own_jks_path = new AlertDialog.Builder(Tools.this)
+                            .setView(dialog_root)
+                            .create();
+
+                    dialog_btn_no.setOnClickListener(new DialogCancelButtonOnClickListener(dialog_own_jks_path));
+
+                    dialog_btn_yes.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (ed_input.getEditText().getText().toString().isEmpty()) {
+                                SketchwareUtil.toast("Select a keystore or enter its path");
+                                return;
+                            }
+
+                            File keystore_file = new File(ed_input.getEditText().getText().toString());
+                            if (keystore_file.exists()) {
+                                try {
+                                    KeyStore key_store = KeyStore.getInstance("JKS");
+                                    key_store.load(new FileInputStream(ed_input.getEditText().getText().toString()), "".toCharArray());
+                                } catch (KeyStoreException | CertificateException | IOException | NoSuchAlgorithmException e) {
+                                    SketchwareUtil.toastError("An error occurred while operating with the keystore: " + e.getMessage());
+                                    Log.e(Tools.class.getSimpleName(), "Error while handling keystore "
+                                            + ed_input.getEditText().getText().toString() + ": " + e.getMessage(), e);
+                                }
+                                //TODO: Verify Keystore and list possible aliases
+                                //TODO: Use 0x7f070250 as keystore password icon (ic_key_48dp)
+                            } else {
+                                SketchwareUtil.toastError("Check your keystore file path");
+                            }
+                        }
+                    });
+
+                    dialog_own_jks_path.show();
+                } else if (radio_own_key.isChecked()) {
+                    SketchwareUtil.toast("Coming soon");
+                }
+                dialog_key_type.dismiss();
+            }
+        });
+
+        dialog_key_type.show();
+    }
+
+    private void signApkFileDebugWithDialog(String input_apk_path, String output_apk_path) {
+        View building_root = getLayoutInflater().inflate(0x7f0b002c, null, false);
+        LinearLayout layout_quiz = building_root.findViewById(0x7f0802a1);
+        TextView tv_progress = building_root.findViewById(0x7f080557);
+
+        ScrollView scroll_view = new ScrollView(Tools.this);
+        TextView tv_log = new TextView(Tools.this);
+        scroll_view.addView(tv_log);
+        layout_quiz.addView(scroll_view);
+
+        tv_progress.setText("Signing APK...");
+
+        AlertDialog building_dialog = new AlertDialog.Builder(Tools.this)
+                .setView(building_root)
+                .create();
+
+        ApkSigner signer = new ApkSigner(Tools.this);
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                signer.signWithTestKey(input_apk_path, output_apk_path, new ApkSigner.LogCallback() {
+                    @Override
+                    public void onNewLineLogged(String line) {
+                        Log.d("Tools", "New System.out line: " + line);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tv_log.setText(tv_log.getText().toString() + line);
+                            }
+                        });
+                    }
+                });
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv_progress.setText("Signed APK file! You can close the dialog now.");
+                        SketchwareUtil.toast("Successfully saved signed APK to: /Internal storage/sketchware/signed_apk/"
+                                        + Uri.fromFile(new File(output_apk_path)).getLastPathSegment(),
+                                Toast.LENGTH_LONG);
+                    }
+                });
+            }
+        }.start();
+
+        building_dialog.show();
     }
 }
