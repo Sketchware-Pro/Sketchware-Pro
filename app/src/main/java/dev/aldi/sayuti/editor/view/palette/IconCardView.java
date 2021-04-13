e107e2cc0f89496e8b995c95391983e7c9c7c51c
Rayen Mark <73752904+MarkDroid@users.noreply.github.com>
2021-04-13 15:41:01 +0200

    add : all aldi sayuti view classes.

---
+++ app/src/main/java/dev/aldi/sayuti/editor/view/palette/IconCardView.java
@@ -0,0 +1,33 @@
+package dev.aldi.sayuti.editor.view.palette;
+
+import android.content.Context;
+import com.besome.sketch.beans.LayoutBean;
+import com.besome.sketch.beans.ViewBean;
+import com.besome.sketch.editor.view.palette.IconBase;
+
+public class IconCardView extends IconBase {
+    public IconCardView(Context context) {
+        super(context);
+    }
+
+    public void a(Context context) {
+        IconCardView.super.a(context);
+        setWidgetImage(2131166299);
+        setWidgetName("CardView");
+    }
+
+    public ViewBean getBean() {
+        ViewBean viewBean = new ViewBean();
+        viewBean.type = 36;
+        LayoutBean layoutBean = viewBean.layout;
+        layoutBean.orientation = 1;
+        layoutBean.width = -1;
+        layoutBean.paddingLeft = 8;
+        layoutBean.paddingTop = 8;
+        layoutBean.paddingRight = 8;
+        layoutBean.paddingBottom = 8;
+        viewBean.convert = "androidx.cardview.widget.CardView";
+        viewBean.inject = "app:cardElevation=\"10dp\"\napp:cardCornerRadius=\"20dp\"";
+        return viewBean;
+    }
+}
