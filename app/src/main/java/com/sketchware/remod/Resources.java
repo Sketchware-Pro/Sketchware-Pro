c36110fb6fb1b902f9d18d26f84ed2bfef810df1
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-19 19:35:42 +0200

    feat: Create class Resources
    This class is like R generated by AAPT/AAPT2, it contains constants for resources. This will benefit of no lint errors and (often) instant knowing what resource is being used.

---
+++ app/src/main/java/com/sketchware/remod/Resources.java
@@ -0,0 +1,212 @@
+package com.sketchware.remod;
+
+public class Resources {
+    public static final class anim {
+        public static final int abc_fade_in = 0x7f010000;
+    }
+
+    public static final class animator {
+        public static final int flip_bottom_in = 0x7f020003;
+        public static final int flip_bottom_out = 0x7f020004;
+        public static final int flip_top_in = 0x7f020009;
+        public static final int flip_top_out = 0x7f02000a;
+    }
+
+    public static final class drawable {
+        public static final int arrow_back_white_48dp = 0x7f070089;
+        public static final int collect_48 = 0x7f07010b;
+        public static final int color_more_96 = 0x7f07011c;
+        public static final int color_palette_48 = 0x7f070120;
+        public static final int expand_48 = 0x7f0701a5;
+        public static final int ic_discord = 0x7f070141;
+        public static final int swimming_girl_flat = 0x7f070142;
+        public static final int ic_user = 0x7f070143;
+        public static final int delete_96 = 0x7f070154;
+        public static final int facebook_50 = 0x7f0701ac;
+        public static final int google_96 = 0x7f0701d3;
+        public static final int gravity_96 = 0x7f0701dc;
+        public static final int ic_bookmark_red_48dp = 0x7f070204;
+        public static final int ic_detail_setting_48dp = 0x7f070225;
+        public static final int ic_export_his_white_48dp = 0x7f070236;
+        public static final int ic_reset_color_32dp = 0x7f07028a;
+        public static final int icon_file_white_96 = 0x7f0702cb;
+        public static final int insert_white_space_48 = 0x7f0702e4;
+        public static final int language_48 = 0x7f0702eb;
+        public static final int side_menu_info_icon_over_white = 0x7f0703c5;
+        public static final int side_menu_setting_icon_over_white = 0x7f0703c9;
+        public static final int style_48dp = 0x7f0703e3;
+    }
+
+    public static final class id {
+        public static final int ALT = 0x7f080000;
+        public static final int CTRL = 0x7f080001;
+        public static final int FUNCTION = 0x7f080002;
+        public static final int META = 0x7f080003;
+        public static final int add_button = 0x7f080023;
+        public static final int btn_editproperties = 0x7f080065;
+        public static final int btn_left = 0x7f080073;
+        public static final int btn_right = 0x7f080080;
+        public static final int chk_bottom = 0x7f0800a6;
+        public static final int chk_hcenter = 0x7f0800a8;
+        public static final int chk_left = 0x7f0800a9;
+        public static final int chk_pty_all = 0x7f0800aa;
+        public static final int chk_right = 0x7f0800ab;
+        public static final int chk_top = 0x7f0800ae;
+        public static final int chk_vcenter = 0x7f0800af;
+        public static final int common_dialog_cancel_button = 0x7f0800bd;
+        public static final int common_dialog_ok_button = 0x7f0800c2;
+        public static final int components_list = 0x7f0800c7;
+        public static final int confirm_layout = 0x7f0800ca;
+        public static final int confirm_no = 0x7f0800cb;
+        public static final int confirm_no_text = 0x7f0800cd;
+        public static final int confirm_yes = 0x7f0800cf;
+        public static final int confirm_yes_text = 0x7f0800d1;
+        public static final int desc = 0x7f0800f3;
+        public static final int docs_button = 0x7f080104;
+        public static final int drawer_layout = 0x7f080105;
+        public static final int ed_input = 0x7f08010e;
+        public static final int ed_input_file_picker = 0x7f080111;
+        public static final int ed_input_filename = 0x7f080112;
+        public static final int ed_input_firebase_path = 0x7f080113;
+        public static final int et_all = 0x7f08012f;
+        public static final int et_bottom = 0x7f080131;
+        public static final int et_left = 0x7f08013e;
+        public static final int et_right = 0x7f080142;
+        public static final int et_top = 0x7f080144;
+        public static final int fab = 0x7f08014e;
+        public static final int hcv_property = 0x7f080165;
+        public static final int icon = 0x7f080172;
+        public static final int img_activity = 0x7f080180;
+        public static final int img_back = 0x7f080188;
+        public static final int img_button = 0x7f080190;
+        public static final int img_file_picker = 0x7f0801a9;
+        public static final int img_icon = 0x7f0801af;
+        public static final int img_left_icon = 0x7f0801b3;
+        public static final int img_save = 0x7f0801c9;
+        public static final int img_title_logo = 0x7f0801d9;
+        public static final int img_user_icon = 0x7f0801df;
+        public static final int layout_button = 0x7f080258;
+        public static final int layout_coordinator = 0x7f080267;
+        public static final int layout_description = 0x7f08026a;
+        public static final int layout_img_icon = 0x7f080279;
+        public static final int layout_input_file_picker = 0x7f08027d;
+        public static final int layout_inputs = 0x7f08027e;
+        public static final int layout_main_logo = 0x7f08028a;
+        public static final int layout_property_group = 0x7f08029c;
+        public static final int layout_property_see_all = 0x7f08029d;
+        public static final int layout_qna_bottom = 0x7f08029f;
+        public static final int left_drawer = 0x7f0802c2;
+        public static final int ll_warning = 0x7f0802f7;
+        public static final int makeblock_view = 0x7f080300;
+        public static final int menu_list = 0x7f08031f;
+        public static final int menu_login = 0x7f080322;
+        public static final int menu_mypage = 0x7f080326;
+        public static final int moreblock_create = 0x7f08033a;
+        public static final int name = 0x7f080349;
+        public static final int pane = 0x7f080368;
+        public static final int project_buttons = 0x7f08037c;
+        public static final int property_contents = 0x7f080387;
+        public static final int property_group_item = 0x7f080388;
+        public static final int property_item = 0x7f08038a;
+        public static final int property_layout = 0x7f08038b;
+        public static final int property_menu_item = 0x7f08038c;
+        public static final int rg_content = 0x7f0803b4;
+        public static final int save_button = 0x7f0803c1;
+        public static final int scroll_view = 0x7f0803cc;
+        public static final int social_fb = 0x7f0803ff;
+        public static final int social_medium = 0x7f080400;
+        public static final int social_slack = 0x7f080401;
+        public static final int spn_widget = 0x7f08040c;
+        public static final int tab_layout = 0x7f080425;
+        public static final int ti_all = 0x7f08043a;
+        public static final int ti_bottom = 0x7f08043e;
+        public static final int ti_input = 0x7f080448;
+        public static final int ti_input_file_picker = 0x7f080449;
+        public static final int ti_input_filename = 0x7f08044a;
+        public static final int ti_input_firebase_path = 0x7f08044b;
+        public static final int ti_left = 0x7f080450;
+        public static final int ti_right = 0x7f080455;
+        public static final int ti_top = 0x7f080457;
+        public static final int toolbar = 0x7f080467;
+        public static final int tv_activity_name = 0x7f080477;
+        public static final int tv_component_title = 0x7f0804b5;
+        public static final int tv_desc_file_picker = 0x7f0804cc;
+        public static final int tv_desc_firebase_path = 0x7f0804cd;
+        public static final int tv_description = 0x7f0804d2;
+        public static final int tv_expire_date = 0x7f0804e5;
+        public static final int tv_favorites_guide = 0x7f0804e9;
+        public static final int tv_login_id = 0x7f080524;
+        public static final int tv_menu_name = 0x7f08052c;
+        public static final int tv_name = 0x7f080537;
+        public static final int tv_sub_title = 0x7f0805b6;
+        public static final int tv_title = 0x7f0805c3;
+        public static final int tv_value = 0x7f08060e;
+        public static final int tv_warning = 0x7f08061d;
+        public static final int tv_warning_message = 0x7f08061f;
+        public static final int view_color = 0x7f08063e;
+        public static final int view_event = 0x7f080640;
+        public static final int viewpager = 0x7f080648;
+    }
+
+    public static final class layout {
+        public static final int color_picker = 0x7f0b002d;
+        public static final int fr_logic_list_item_button = 0x7f0b0067;
+        public static final int fr_logic_list_item_buttons = 0x7f0b0068;
+        public static final int logic_popup_add_component_temp = 0x7f0b00a7;
+        public static final int main = 0x7f0b00ad;
+        public static final int main_drawer_header = 0x7f0b00af;
+        public static final int main_drawer_item = 0x7f0b00b0;
+        public static final int make_block = 0x7f0b00b1;
+        public static final int manage_collection_show_widget = 0x7f0b00b9;
+        public static final int manage_screen_activity_add_view_preset_setting = 0x7f0b00e9;
+        public static final int property_grid_item = 0x7f0b012e;
+        public static final int property_group_item = 0x7f0b012f;
+        public static final int property_input_item = 0x7f0b0131;
+        public static final int property_popup_input_indent = 0x7f0b0133;
+        public static final int property_popup_input_size = 0x7f0b0135;
+        public static final int property_popup_input_text = 0x7f0b0136;
+        public static final int property_popup_save_to_favorite = 0x7f0b0138;
+        public static final int property_popup_selector_gravity = 0x7f0b013a;
+        public static final int property_popup_selector_single = 0x7f0b013b;
+        public static final int property_selector_item = 0x7f0b0140;
+        public static final int view_properties = 0x7f0b01c0;
+        public static final int view_property = 0x7f0b01c1;
+        public static final int about = 0x7f0b01cf;
+    }
+
+    public static final class menu {
+        public static final int main_menu = 0x7f0c0009;
+        public static final int moreblock_menu = 0x7f0c0013;
+    }
+
+    public static final class string {
+        public static final int besome_blog_url = 0x7f0e0044;
+        public static final int common_message_check_network = 0x7f0e03e4;
+        public static final int common_message_complete_save = 0x7f0e03ea;
+        public static final int common_message_confirm = 0x7f0e03ed;
+        public static final int common_word_all = 0x7f0e040c;
+        public static final int common_word_cancel = 0x7f0e040e;
+        public static final int common_word_choose = 0x7f0e0410;
+        public static final int common_word_continue = 0x7f0e0414;
+        public static final int common_word_delete = 0x7f0e041a;
+        public static final int common_word_reset = 0x7f0e0443;
+        public static final int common_word_save = 0x7f0e0447;
+        public static final int common_word_see_all = 0x7f0e0449;
+        public static final int common_word_select = 0x7f0e044b;
+        public static final int design_button_properties = 0x7f0e04b4;
+        public static final int facebook_url = 0x7f0e05b5;
+        public static final int logic_list_menu_add_to_collection = 0x7f0e0668;
+        public static final int main_drawer_title_export_urls = 0x7f0e067d;
+        public static final int main_drawer_title_language_settings = 0x7f0e067e;
+        public static final int main_drawer_title_program_information = 0x7f0e067f;
+        public static final int main_drawer_title_system_settings = 0x7f0e0686;
+        public static final int property_group_basic = 0x7f0e0760;
+        public static final int property_group_event = 0x7f0e0761;
+        public static final int property_group_recent = 0x7f0e0762;
+        public static final int property_description_edittext_ime_options = 0x7f0e075b;
+        public static final int slack_url_primary = 0x7f0e0981;
+        public static final int slack_url_secondary = 0x7f0e0982;
+        public static final int view_widget_favorites_save_guide_new = 0x7f0e09e9;
+        public static final int view_widget_favorites_save_title = 0x7f0e09ea;
+    }
+}
