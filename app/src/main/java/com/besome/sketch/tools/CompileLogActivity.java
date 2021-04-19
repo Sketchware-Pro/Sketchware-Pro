1047a91d91771a737b8edcfac6532cdf9b20569e
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-19 19:47:32 +0200

    feat, style: Remove usage log, format file

--- app/src/main/java/com/besome/sketch/tools/CompileLogActivity.java
+++ app/src/main/java/com/besome/sketch/tools/CompileLogActivity.java
@@ -5,33 +5,24 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.besome.sketch.lib.base.BaseActivity;
-import com.google.android.gms.analytics.HitBuilders;
+import com.sketchware.remod.Resources;
 
 import mod.hey.studios.util.CompileLogHelper;
 
 public class CompileLogActivity extends BaseActivity {
 
-    private TextView e;
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        a.setScreenName(CompileLogActivity.class.getSimpleName());
-        a.send(new HitBuilders.ScreenViewBuilder().build());
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(2131427388);
-        e = findViewById(2131231922);
+        setContentView(Resources.layout.compile_log);
+        TextView tv_compile_log = findViewById(Resources.id.tv_compile_log);
 
         Intent intent = getIntent();
         if (intent == null) {
             finish();
         } else {
-            e.setText(CompileLogHelper.colorErrsAndWarnings(intent.getStringExtra("error")));
-            e.setTextIsSelectable(true);
+            tv_compile_log.setText(CompileLogHelper.colorErrsAndWarnings(intent.getStringExtra("error")));
+            tv_compile_log.setTextIsSelectable(true);
         }
     }
 }
