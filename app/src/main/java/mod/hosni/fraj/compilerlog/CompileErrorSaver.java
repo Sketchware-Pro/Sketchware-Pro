c6c92dee9f63d640408dad9c40828b325d631b49
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-22 15:43:07 +0200

    style, refactor: Reformat file, rename to clearer method name

--- app/src/main/java/mod/hosni/fraj/compilerlog/CompileErrorSaver.java
+++ app/src/main/java/mod/hosni/fraj/compilerlog/CompileErrorSaver.java
@@ -3,73 +3,60 @@ package mod.hosni.fraj.compilerlog;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.os.Environment;
-import java.io.File;
 
+import com.sketchware.remod.Resources;
+
+import mod.SketchwareUtil;
+import mod.agus.jcoderz.lib.FilePathUtil;
 import mod.agus.jcoderz.lib.FileUtil;
 import mod.hey.studios.util.CompileLogHelper;
-import mod.agus.jcoderz.lib.FilePathUtil;
-
-import a.a.a.bB;
 
 public class CompileErrorSaver {
 
-	public String sc_id;
-	public Activity activity;
-	public FilePathUtil filePathUtil = new FilePathUtil();
-        public String path;
+    public String sc_id;
+    public Activity activity;
+    public FilePathUtil filePathUtil = new FilePathUtil();
+    public String path;
 
-	public CompileErrorSaver(Activity act, String str) {
-		sc_id = str ;
-		activity = act;
-                path = filePathUtil.getPathErrorLog(str);
-		check();
-	}
+    public CompileErrorSaver(Activity activity, String sc_id) {
+        this.sc_id = sc_id;
+        this.activity = activity;
+        path = FilePathUtil.getLastCompileLogPath(sc_id);
+        check();
+    }
 
-	public CompileErrorSaver() {}
+    public void setErrorText(String errorText) {
+        FileUtil.deleteFile(path);
+        FileUtil.writeFile(path, errorText);
+    }
 
-	public void setErrorText(String str) {
-		FileUtil.deleteFile(path);
-		FileUtil.writeFile(path, str);
-	}
-
-	public void show() {
-		AlertDialog dialog = new AlertDialog.Builder(activity)
-			.setTitle("Compile Log:")
-			.setMessage(CompileLogHelper.colorErrsAndWarnings(getLog()))
-
-			.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-
-				@Override
-				public void onClick(DialogInterface dia, int which) {
-
-				}
-			})
-            .setNeutralButton("Clear", new DialogInterface.OnClickListener() {
-
-                @Override
-                public void onClick(DialogInterface dia, int which) {
-					bB.a(activity, "Cleared", 0).show();
-					clear();
-                }
-			})
-			.setNegativeButton("Cancel", null)
-			.create();
-		dialog.show();
-	}
+    public void show() {
+        AlertDialog dialog = new AlertDialog.Builder(activity)
+                .setTitle("Last compile log")
+                .setMessage(CompileLogHelper.colorErrsAndWarnings(getLog()))
+                .setPositiveButton(Resources.string.common_word_ok, null)
+                .setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SketchwareUtil.toast("Cleared log");
+                        clear();
+                    }
+                })
+                .create();
+        dialog.show();
+    }
 
     public void clear() {
         FileUtil.deleteFile(path);
     }
 
-	public String getLog() {
-		return FileUtil.readFile(path);
-	}
-
-	public void check() {
-		if (!FileUtil.isExistFile(path)) {
-			FileUtil.writeFile(path, "No error detected in the last compile !");
-		}
+    public String getLog() {
+        return FileUtil.readFile(path);
     }
 
+    public void check() {
+        if (!FileUtil.isExistFile(path)) {
+            FileUtil.writeFile(path, "No compile errors have been saved yet.");
+        }
+    }
 }
