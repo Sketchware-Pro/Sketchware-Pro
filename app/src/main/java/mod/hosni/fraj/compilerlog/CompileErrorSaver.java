18ca22a5b9e3a96b0047acb66d323f25e6cb9d09
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-25 11:02:44 +0200

    refactor, docs: Require Activity in needed method, add Javadocs

--- app/src/main/java/mod/hosni/fraj/compilerlog/CompileErrorSaver.java
+++ app/src/main/java/mod/hosni/fraj/compilerlog/CompileErrorSaver.java
@@ -1,7 +1,7 @@
 package mod.hosni.fraj.compilerlog;
 
-import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 
 import com.sketchware.remod.Resources;
@@ -14,24 +14,37 @@ import mod.hey.studios.util.CompileLogHelper;
 public class CompileErrorSaver {
 
     public String sc_id;
-    public Activity activity;
     public FilePathUtil filePathUtil = new FilePathUtil();
     public String path;
 
-    public CompileErrorSaver(Activity activity, String sc_id) {
+    /**
+     * Create this helper class for saving compile errors.
+     *
+     * @param sc_id The Sketchware project ID for the project to operate on, like 605
+     */
+    public CompileErrorSaver(String sc_id) {
         this.sc_id = sc_id;
-        this.activity = activity;
         path = FilePathUtil.getLastCompileLogPath(sc_id);
         check();
     }
 
+    /**
+     * Save a compile error in the project's last compile error file.
+     *
+     * @param errorText The text to save, if possible, with detailed messages
+     */
     public void setErrorText(String errorText) {
         FileUtil.deleteFile(path);
         FileUtil.writeFile(path, errorText);
     }
 
-    public void show() {
-        AlertDialog dialog = new AlertDialog.Builder(activity)
+    /**
+     * Show an {@link AlertDialog} that displays the last saved error to the user.
+     *
+     * @param context The context to show the dialog on
+     */
+    public void showDialog(Context context) {
+        AlertDialog dialog = new AlertDialog.Builder(context)
                 .setTitle("Last compile log")
                 .setMessage(CompileLogHelper.colorErrsAndWarnings(getLog()))
                 .setPositiveButton(Resources.string.common_word_ok, null)
@@ -46,14 +59,24 @@ public class CompileErrorSaver {
         dialog.show();
     }
 
+    /**
+     * Clear the last saved error text.
+     */
     public void clear() {
         FileUtil.deleteFile(path);
     }
 
+    /**
+     * @return The last saved error text
+     */
     public String getLog() {
         return FileUtil.readFile(path);
     }
 
+    /**
+     * Check if the last saved error text file exists, if not, it'll get created and
+     * "No compile errors have been saved yet." will be written to it.
+     */
     public void check() {
         if (!FileUtil.isExistFile(path)) {
             FileUtil.writeFile(path, "No compile errors have been saved yet.");
