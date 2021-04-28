f107267789ef9f353a86e08013ae8a1a7071d1f7
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-28 13:47:56 +0200

    fix, style: Inline obf. anon. classes, format file

--- app/src/main/java/com/besome/sketch/projects/MyProjectButtonLayout.java
+++ app/src/main/java/com/besome/sketch/projects/MyProjectButtonLayout.java
@@ -1,10 +1,8 @@
 package com.besome.sketch.projects;
 
-import a.a.a.mC;
-import a.a.a.nC;
-import a.a.a.wB;
-import a.a.a.xB;
+import android.animation.Animator;
 import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.content.Context;
 import android.view.View;
@@ -12,7 +10,13 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.sketchware.remod.Resources;
+
+import a.a.a.wB;
+import a.a.a.xB;
+
 public class MyProjectButtonLayout extends FrameLayout {
+
     public Context a;
     public View b;
     public TextView c;
@@ -37,60 +41,84 @@ public class MyProjectButtonLayout extends FrameLayout {
     }
 
     public void setButtonOnClickListener(View.OnClickListener onClickListener) {
-        this.d.setOnClickListener(onClickListener);
-        this.e.setOnClickListener(onClickListener);
-        this.m.setOnClickListener(onClickListener);
-        this.n.setOnClickListener(onClickListener);
-        this.o.setOnClickListener(onClickListener);
-        this.p.setOnClickListener(onClickListener);
-        this.q.setOnClickListener(onClickListener);
+        d.setOnClickListener(onClickListener);
+        e.setOnClickListener(onClickListener);
+        m.setOnClickListener(onClickListener);
+        n.setOnClickListener(onClickListener);
+        o.setOnClickListener(onClickListener);
+        p.setOnClickListener(onClickListener);
+        q.setOnClickListener(onClickListener);
     }
 
     public final void a(Context context) {
-        this.a = context;
-        wB.a(context, this, 2131427581);
-        this.h = (LinearLayout) findViewById(2131231612);
-        this.b = findViewById(2131230922);
-        this.c = (TextView) findViewById(2131230926);
-        this.c.setText(xB.b().a(getContext(), 2131625466));
-        this.d = findViewById(2131230927);
-        this.e = findViewById(2131230923);
-        this.f = (TextView) findViewById(2131230929);
-        this.f.setText(xB.b().a(getContext(), 2131624986));
-        this.g = (TextView) findViewById(2131230925);
-        this.g.setText(xB.b().a(getContext(), 2131624974));
-        this.c.setText(xB.b().a(context, 2131625636));
-        this.f.setText(xB.b().a(context, 2131624986));
-        this.g.setText(xB.b().a(context, 2131624974));
-        this.b.setVisibility(4);
-        this.m = a(0, 2131166124, xB.b().a(context, 2131625666));
-        xB.b().a(context, 2131625665);
-        this.n = a(1, 2131166383, "Backup");
-        this.o = a(2, 2131165749, xB.b().a(context, 2131625669));
-        this.p = a(3, 2131165728, xB.b().a(context, 2131625663));
-        this.q = a(4, 2131166124, "Config");
-        this.h.addView(this.m);
-        this.h.addView(this.n);
-        this.h.addView(this.o);
-        this.h.addView(this.p);
-        this.h.addView(this.q);
-        this.i = (AnimatorSet) AnimatorInflater.loadAnimator(context, 2130837513);
-        this.j = (AnimatorSet) AnimatorInflater.loadAnimator(context, 2130837514);
-        this.k = (AnimatorSet) AnimatorInflater.loadAnimator(context, 2130837507);
-        this.l = (AnimatorSet) AnimatorInflater.loadAnimator(context, 2130837508);
+        a = context;
+        wB.a(context, this, Resources.layout.myproject_buttons);
+        h = findViewById(Resources.id.project_buttons);
+        b = findViewById(Resources.id.confirm_layout);
+        c = findViewById(Resources.id.confirm_title);
+        c.setText(xB.b().a(getContext(), Resources.string.language_message_confirm_delete));
+        d = findViewById(Resources.id.confirm_yes);
+        e = findViewById(Resources.id.confirm_no);
+        f = findViewById(Resources.id.confirm_yes_text);
+        f.setText(xB.b().a(getContext(), Resources.string.common_word_delete));
+        g = findViewById(Resources.id.confirm_no_text);
+        g.setText(xB.b().a(getContext(), Resources.string.common_word_cancel));
+        c.setText(xB.b().a(context, Resources.string.myprojects_confirm_project_delete));
+        f.setText(xB.b().a(context, Resources.string.common_word_delete));
+        g.setText(xB.b().a(context, Resources.string.common_word_cancel));
+        b.setVisibility(INVISIBLE);
+        m = a(0,
+                Resources.drawable.settings_96,
+                xB.b().a(context, Resources.string.myprojects_list_menu_title_settings));
+        n = a(1,
+                Resources.drawable.ic_backup,
+                "Back up");
+        o = a(2,
+                Resources.drawable.ic_export_grey_48dp,
+                xB.b().a(context, Resources.string.myprojects_list_menu_title_sign_export));
+        p = a(3,
+                Resources.drawable.ic_delete_grey_48dp,
+                xB.b().a(context, Resources.string.myprojects_list_menu_title_delete));
+        q = a(4,
+                Resources.drawable.settings_96,
+                "Config");
+        h.addView(m);
+        h.addView(n);
+        h.addView(o);
+        h.addView(p);
+        h.addView(q);
+        i = (AnimatorSet) AnimatorInflater.loadAnimator(context, Resources.animator.flip_top_in);
+        j = (AnimatorSet) AnimatorInflater.loadAnimator(context, Resources.animator.flip_top_out);
+        k = (AnimatorSet) AnimatorInflater.loadAnimator(context, Resources.animator.flip_bottom_in);
+        l = (AnimatorSet) AnimatorInflater.loadAnimator(context, Resources.animator.flip_bottom_out);
     }
 
     public void b() {
-        this.j.setTarget(this.h);
-        this.i.setTarget(this.b);
+        j.setTarget(h);
+        i.setTarget(b);
         AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.play(this.j).with(this.i);
-        animatorSet.addListener(new mC(this));
+        animatorSet.play(j).with(i);
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                p.setEnabled(false);
+                e.setEnabled(true);
+                h.setVisibility(INVISIBLE);
+            }
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                b.setVisibility(VISIBLE);
+                p.setEnabled(false);
+                e.setEnabled(false);
+            }
+        });
         animatorSet.start();
     }
 
     public final MyProjectButton a(int i2, int i3, String str) {
-        MyProjectButton myProjectButton = new MyProjectButton(this.a);
+        MyProjectButton myProjectButton = new MyProjectButton(a);
         myProjectButton.b = i2;
         myProjectButton.d.setImageResource(i3);
         myProjectButton.e.setText(str);
@@ -98,11 +126,26 @@ public class MyProjectButtonLayout extends FrameLayout {
     }
 
     public void a() {
-        this.k.setTarget(this.h);
-        this.l.setTarget(this.b);
+        k.setTarget(h);
+        l.setTarget(b);
         AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.play(this.k).with(this.l);
-        animatorSet.addListener(new nC(this));
+        animatorSet.play(k).with(l);
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                p.setEnabled(true);
+                e.setEnabled(false);
+                b.setVisibility(INVISIBLE);
+            }
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                h.setVisibility(VISIBLE);
+                p.setEnabled(false);
+                e.setEnabled(false);
+            }
+        });
         animatorSet.start();
     }
 }
