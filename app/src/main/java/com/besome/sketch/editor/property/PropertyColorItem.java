37c88328620c21f09e092a02aaa3f301a9ab2d60
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-29 09:29:18 +0200

    fix, style: Inline obf. anon. class, format file

--- app/src/main/java/com/besome/sketch/editor/property/PropertyColorItem.java
+++ app/src/main/java/com/besome/sketch/editor/property/PropertyColorItem.java
@@ -1,22 +1,24 @@
 package com.besome.sketch.editor.property;
 
-import a.a.a.Kw;
-import a.a.a.Mw;
-import a.a.a.Zx;
-import a.a.a.mB;
-import a.a.a.wB;
-import a.a.a.xB;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.view.Gravity;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.sketchware.remod.Resources;
+
+import a.a.a.Kw;
+import a.a.a.Zx;
+import a.a.a.mB;
+import a.a.a.wB;
+import a.a.a.xB;
+
 public class PropertyColorItem extends RelativeLayout implements View.OnClickListener {
+
     public Context a;
     public String b;
     public int c;
@@ -35,11 +37,40 @@ public class PropertyColorItem extends RelativeLayout implements View.OnClickLis
     }
 
     public String getKey() {
-        return this.b;
+        return b;
+    }
+
+    public void setKey(String str) {
+        b = str;
+        int identifier = getResources().getIdentifier(str, "string", getContext().getPackageName());
+        if (identifier > 0) {
+            d.setText(xB.b().a(getResources(), identifier));
+            h = Resources.drawable.color_palette_48;
+            if (j.getVisibility() == VISIBLE) {
+                ((ImageView) findViewById(Resources.id.img_icon)).setImageResource(h);
+                ((TextView) findViewById(Resources.id.tv_title)).setText(xB.b().a(getContext(), identifier));
+                return;
+            }
+            g.setImageResource(h);
+        }
     }
 
     public int getValue() {
-        return this.c;
+        return c;
+    }
+
+    public void setValue(int value) {
+        c = value;
+        if (value == 0) {
+            e.setText("TRANSPARENT");
+            f.setBackgroundColor(value);
+        } else if (value == 0xffffff) {
+            e.setText("NONE");
+            f.setBackgroundColor(value);
+        } else {
+            e.setText(String.format("#%08X", value));
+            f.setBackgroundColor(value);
+        }
     }
 
     public void onClick(View view) {
@@ -48,83 +79,58 @@ public class PropertyColorItem extends RelativeLayout implements View.OnClickLis
         }
     }
 
-    @SuppressLint("ResourceType")
-    public void setKey(String str) {
-        this.b = str;
-        int identifier = getResources().getIdentifier(str, "string", getContext().getPackageName());
-        if (identifier > 0) {
-            this.d.setText(xB.b().a(getResources(), identifier));
-            this.h = 2131165472;
-            if (this.j.getVisibility() == 0) {
-                ((ImageView) findViewById(2131231151)).setImageResource(this.h);
-                ((TextView) findViewById(2131232195)).setText(xB.b().a(getContext(), identifier));
-                return;
-            }
-            this.g.setImageResource(this.h);
-        }
-    }
-
     public void setOnPropertyValueChangeListener(Kw kw) {
-        this.k = kw;
+        k = kw;
     }
 
-    @SuppressLint("WrongConstant")
-    public void setOrientationItem(int i2) {
-        if (i2 == 0) {
-            this.i.setVisibility(8);
-            this.j.setVisibility(0);
+    public void setOrientationItem(int orientationItem) {
+        if (orientationItem == 0) {
+            i.setVisibility(GONE);
+            j.setVisibility(VISIBLE);
             return;
         }
-        this.i.setVisibility(0);
-        this.j.setVisibility(8);
+        i.setVisibility(VISIBLE);
+        j.setVisibility(GONE);
     }
 
-    public void setValue(int i2) {
-        this.c = i2;
-        if (i2 == 0) {
-            this.e.setText("TRANSPARENT");
-            this.f.setBackgroundColor(i2);
-        } else if (i2 == 16777215) {
-            this.e.setText("NONE");
-            this.f.setBackgroundColor(i2);
-        } else {
-            this.e.setText(String.format("#%08X", Integer.valueOf(i2 & -1)));
-            this.f.setBackgroundColor(i2);
-        }
-    }
-
-    @SuppressLint("ResourceType")
     public final void a(Context context, boolean z) {
-        this.a = context;
-        wB.a(context, this, 2131427628);
-        this.d = (TextView) findViewById(2131232055);
-        this.e = (TextView) findViewById(2131232270);
-        this.f = findViewById(2131232318);
-        this.g = (ImageView) findViewById(2131231155);
-        this.i = findViewById(2131231626);
-        this.j = findViewById(2131231628);
+        a = context;
+        wB.a(context, this, Resources.layout.property_color_item);
+        d = findViewById(Resources.id.tv_name);
+        e = findViewById(Resources.id.tv_value);
+        f = findViewById(Resources.id.view_color);
+        g = findViewById(Resources.id.img_left_icon);
+        i = findViewById(Resources.id.property_item);
+        j = findViewById(Resources.id.property_menu_item);
         if (z) {
             setOnClickListener(this);
             setSoundEffectsEnabled(true);
         }
     }
 
-    @SuppressLint("ResourceType")
     public final void a() {
         boolean z;
         boolean z2;
-        View a2 = wB.a(this.a, 2131427373);
-        a2.setAnimation(AnimationUtils.loadAnimation(this.a, 2130771968));
-        if (this.b == "property_background_color") {
+        View view = wB.a(a, Resources.layout.color_picker);
+        view.setAnimation(AnimationUtils.loadAnimation(a, Resources.anim.abc_fade_in));
+        if (b.equals("property_background_color")) {
             z2 = true;
             z = true;
         } else {
             z2 = false;
             z = false;
         }
-        Zx zx = new Zx(a2, (Activity) this.a, this.c, z2, z);
-        zx.a(new Mw(this));
-        zx.setAnimationStyle(2130771968);
-        zx.showAtLocation(a2, 17, 0, 0);
+        Zx zx = new Zx(view, (Activity) a, c, z2, z);
+        zx.a(new Zx.b() {
+            @Override
+            public void a(int i) {
+                setValue(i);
+                if (k != null) {
+                    k.a(b, c);
+                }
+            }
+        });
+        zx.setAnimationStyle(Resources.anim.abc_fade_in);
+        zx.showAtLocation(view, Gravity.CENTER, 0, 0);
     }
 }
