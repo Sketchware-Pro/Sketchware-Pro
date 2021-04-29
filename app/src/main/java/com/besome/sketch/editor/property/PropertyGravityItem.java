a42788a8f840ac1b3f868594b89ee5ffda73ea4d
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-29 13:52:32 +0200

    fix, style: Inline obf. anon. class, format file

--- app/src/main/java/com/besome/sketch/editor/property/PropertyGravityItem.java
+++ app/src/main/java/com/besome/sketch/editor/property/PropertyGravityItem.java
@@ -1,15 +1,5 @@
 package com.besome.sketch.editor.property;
 
-import a.a.a.Kw;
-import a.a.a.Ww;
-import a.a.a.Xw;
-import a.a.a.aB;
-import a.a.a.mB;
-import a.a.a.sq;
-import a.a.a.wB;
-import a.a.a.xB;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.view.View;
@@ -18,7 +8,17 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.sketchware.remod.Resources;
+
+import a.a.a.Kw;
+import a.a.a.aB;
+import a.a.a.mB;
+import a.a.a.sq;
+import a.a.a.wB;
+import a.a.a.xB;
+
 public class PropertyGravityItem extends RelativeLayout implements View.OnClickListener {
+
     public String a = "";
     public int b = -1;
     public TextView c;
@@ -35,24 +35,44 @@ public class PropertyGravityItem extends RelativeLayout implements View.OnClickL
     }
 
     public String getKey() {
-        return this.a;
+        return a;
+    }
+
+    public void setKey(String str) {
+        a = str;
+        int identifier = getResources().getIdentifier(str, "string", getContext().getPackageName());
+        if (identifier > 0) {
+            c.setText(xB.b().a(getResources(), identifier));
+            f = Resources.drawable.gravity_96;
+            if (h.getVisibility() == VISIBLE) {
+                ((ImageView) findViewById(Resources.id.img_icon)).setImageResource(f);
+                ((TextView) findViewById(Resources.id.tv_title)).setText(xB.b().a(getContext(), identifier));
+                return;
+            }
+            e.setImageResource(f);
+        }
     }
 
     public int getValue() {
-        return this.b;
+        return b;
     }
 
-    public void onClick(View view) {
+    public void setValue(int value) {
+        b = value;
+        d.setText(sq.a(value));
+    }
+
+    public void onClick(View v) {
         if (!mB.a()) {
-            String str = this.a;
             char c2 = 65535;
-            int hashCode = str.hashCode();
-            if (hashCode != -1474767389) {
-                if (hashCode == -1244048924 && str.equals("property_gravity")) {
+            switch (a) {
+                case "property_gravity":
                     c2 = 0;
-                }
-            } else if (str.equals("property_layout_gravity")) {
-                c2 = 1;
+                    break;
+
+                case "property_layout_gravity":
+                    c2 = 1;
+                    break;
             }
             if (c2 == 0 || c2 == 1) {
                 a();
@@ -60,94 +80,99 @@ public class PropertyGravityItem extends RelativeLayout implements View.OnClickL
         }
     }
 
-    @SuppressLint("ResourceType")
-    public void setKey(String str) {
-        this.a = str;
-        int identifier = getResources().getIdentifier(str, "string", getContext().getPackageName());
-        if (identifier > 0) {
-            this.c.setText(xB.b().a(getResources(), identifier));
-            this.f = 2131165660;
-            if (this.h.getVisibility() == 0) {
-                ((ImageView) findViewById(2131231151)).setImageResource(this.f);
-                ((TextView) findViewById(2131232195)).setText(xB.b().a(getContext(), identifier));
-                return;
-            }
-            this.e.setImageResource(this.f);
-        }
-    }
-
     public void setOnPropertyValueChangeListener(Kw kw) {
-        this.i = kw;
+        i = kw;
     }
 
-    @SuppressLint("WrongConstant")
-    public void setOrientationItem(int i2) {
-        if (i2 == 0) {
-            this.g.setVisibility(8);
-            this.h.setVisibility(0);
+    public void setOrientationItem(int orientationItem) {
+        if (orientationItem == 0) {
+            g.setVisibility(GONE);
+            h.setVisibility(VISIBLE);
             return;
         }
-        this.g.setVisibility(0);
-        this.h.setVisibility(8);
+        g.setVisibility(VISIBLE);
+        h.setVisibility(GONE);
     }
 
-    public void setValue(int i2) {
-        this.b = i2;
-        this.d.setText(sq.a(i2));
-    }
-
-    @SuppressLint("ResourceType")
     public final void a(Context context, boolean z) {
-        wB.a(context, this, 2131427648);
-        this.c = (TextView) findViewById(2131232055);
-        this.d = (TextView) findViewById(2131232270);
-        this.e = (ImageView) findViewById(2131231155);
-        this.g = findViewById(2131231626);
-        this.h = findViewById(2131231628);
+        wB.a(context, this, Resources.layout.property_selector_item);
+        c = findViewById(Resources.id.tv_name);
+        d = findViewById(Resources.id.tv_value);
+        e = findViewById(Resources.id.img_left_icon);
+        g = findViewById(Resources.id.property_item);
+        h = findViewById(Resources.id.property_menu_item);
         if (z) {
             setOnClickListener(this);
             setSoundEffectsEnabled(true);
         }
     }
 
-    @SuppressLint("ResourceType")
     public final void a() {
-        aB aBVar = new aB((Activity) getContext());
-        aBVar.b(this.c.getText().toString());
-        aBVar.a(this.f);
-        View a2 = wB.a(getContext(), 2131427642);
-        CheckBox checkBox = (CheckBox) a2.findViewById(2131230889);
-        CheckBox checkBox2 = (CheckBox) a2.findViewById(2131230891);
-        CheckBox checkBox3 = (CheckBox) a2.findViewById(2131230888);
-        CheckBox checkBox4 = (CheckBox) a2.findViewById(2131230894);
-        CheckBox checkBox5 = (CheckBox) a2.findViewById(2131230886);
-        CheckBox checkBox6 = (CheckBox) a2.findViewById(2131230895);
-        int i2 = this.b;
-        int i3 = i2 & 112;
-        int i4 = i2 & 7;
-        if (i4 == 1) {
-            checkBox3.setChecked(true);
+        aB dialog = new aB((Activity) getContext());
+        dialog.b(c.getText().toString());
+        dialog.a(f);
+        View view = wB.a(getContext(), Resources.layout.property_popup_selector_gravity);
+        CheckBox chk_left = view.findViewById(Resources.id.chk_left);
+        CheckBox chk_right = view.findViewById(Resources.id.chk_right);
+        CheckBox chk_hcenter = view.findViewById(Resources.id.chk_hcenter);
+        CheckBox chk_top = view.findViewById(Resources.id.chk_top);
+        CheckBox chk_bottom = view.findViewById(Resources.id.chk_bottom);
+        CheckBox chk_vcenter = view.findViewById(Resources.id.chk_vcenter);
+        int i2 = b & 112;
+        int i3 = b & 7;
+        if (i3 == 1) {
+            chk_hcenter.setChecked(true);
         } else {
-            if ((i4 & 3) == 3) {
-                checkBox.setChecked(true);
+            if ((i3 & 3) == 3) {
+                chk_left.setChecked(true);
             }
-            if ((i4 & 5) == 5) {
-                checkBox2.setChecked(true);
+            if ((i3 & 5) == 5) {
+                chk_right.setChecked(true);
             }
         }
-        if (i3 == 16) {
-            checkBox6.setChecked(true);
+        if (i2 == 16) {
+            chk_vcenter.setChecked(true);
         } else {
-            if ((i3 & 48) == 48) {
-                checkBox4.setChecked(true);
+            if ((i2 & 48) == 48) {
+                chk_top.setChecked(true);
             }
-            if ((i3 & 80) == 80) {
-                checkBox5.setChecked(true);
+            if ((i2 & 80) == 80) {
+                chk_bottom.setChecked(true);
             }
         }
-        aBVar.a(a2);
-        aBVar.b(xB.b().a(getContext(), 2131625035), new Ww(this, checkBox, checkBox2, checkBox3, checkBox4, checkBox5, checkBox6, aBVar));
-        aBVar.a(xB.b().a(getContext(), 2131624974), new Xw(this, aBVar));
-        aBVar.show();
+        dialog.a(view);
+        dialog.b(xB.b().a(getContext(), Resources.string.common_word_select), new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int value = chk_left.isChecked() ? 3 : 0;
+                if (chk_right.isChecked()) {
+                    value |= 5;
+                }
+                if (chk_hcenter.isChecked()) {
+                    value |= 1;
+                }
+                if (chk_top.isChecked()) {
+                    value |= 48;
+                }
+                if (chk_bottom.isChecked()) {
+                    value |= 80;
+                }
+                if (chk_vcenter.isChecked()) {
+                    value |= 16;
+                }
+                setValue(value);
+                if (i != null) {
+                    i.a(a, b);
+                }
+                dialog.dismiss();
+            }
+        });
+        dialog.a(xB.b().a(getContext(), Resources.string.common_word_cancel), new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
