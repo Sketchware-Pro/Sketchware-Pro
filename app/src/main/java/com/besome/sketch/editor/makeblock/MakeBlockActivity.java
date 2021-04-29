d057875f4dddeebb55c49d336fed9977624c77a1
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-29 15:20:48 +0200

    fix, style: Inline obf. anon. classes, format file

--- app/src/main/java/com/besome/sketch/editor/makeblock/MakeBlockActivity.java
+++ app/src/main/java/com/besome/sketch/editor/makeblock/MakeBlockActivity.java
@@ -1,112 +1,118 @@
 package com.besome.sketch.editor.makeblock;
 
-import a.a.a.Xs;
-import a.a.a.Ys;
-import a.a.a.Zs;
-import a.a.a.aB;
-import a.a.a.dt;
-import a.a.a.jC;
-import a.a.a.xB;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
+
 import androidx.appcompat.widget.Toolbar;
+
 import com.besome.sketch.beans.ProjectFileBean;
 import com.besome.sketch.lib.base.BaseAppCompatActivity;
+import com.sketchware.remod.Resources;
+
+import a.a.a.aB;
+import a.a.a.dt;
+import a.a.a.jC;
+import a.a.a.mB;
+import a.a.a.xB;
+import mod.hey.studios.util.Helper;
 
 public class MakeBlockActivity extends BaseAppCompatActivity {
+
     public Toolbar k;
     public String l;
     public ProjectFileBean m;
     public LinearLayout n;
     public dt o;
 
-    /* JADX DEBUG: Multi-variable search result rejected for r4v0, resolved type: com.besome.sketch.editor.makeblock.MakeBlockActivity */
-    /* JADX WARN: Multi-variable type inference failed */
     public final void l() {
         aB aBVar = new aB(this);
-        aBVar.b(xB.b().a(getApplicationContext(), 2131625501));
-        aBVar.a(2131165604);
-        aBVar.a(xB.b().a(getApplicationContext(), 2131625500));
-        aBVar.b(xB.b().a(getApplicationContext(), 2131625000), new Ys(this, aBVar));
-        aBVar.a(xB.b().a(getApplicationContext(), 2131624974), new Zs(this, aBVar));
+        aBVar.b(xB.b().a(getApplicationContext(), Resources.string.logic_editor_more_block_dialog_message_confirm_goback));
+        aBVar.a(Resources.drawable.exit_96);
+        aBVar.a(xB.b().a(getApplicationContext(), Resources.string.logic_editor_more_block_dialog_description_goback));
+        aBVar.b(xB.b().a(getApplicationContext(), Resources.string.common_word_goback), new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mB.a()) {
+                    aBVar.dismiss();
+                    finish();
+                }
+            }
+        });
+        aBVar.a(xB.b().a(getApplicationContext(), Resources.string.common_word_cancel),
+                Helper.getSketchwareDialogDismissOnClickListener(aBVar));
         aBVar.show();
     }
 
+    @Override
     public void onBackPressed() {
-        if (this.o.a()) {
-            MakeBlockActivity.super.onBackPressed();
+        if (o.a()) {
+            super.onBackPressed();
         } else {
             l();
         }
     }
 
-    /* JADX DEBUG: Multi-variable search result rejected for r3v0, resolved type: com.besome.sketch.editor.makeblock.MakeBlockActivity */
-    /* JADX WARN: Multi-variable type inference failed */
-    @SuppressLint("ResourceType")
     @Override // com.besome.sketch.lib.base.BaseAppCompatActivity
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         if (!super.j()) {
             finish();
         }
-        setContentView(2131427505);
+        setContentView(Resources.layout.make_block);
         if (bundle == null) {
-            this.l = getIntent().getStringExtra("sc_id");
-            this.m = (ProjectFileBean) getIntent().getParcelableExtra("project_file");
+            l = getIntent().getStringExtra("sc_id");
+            m = getIntent().getParcelableExtra("project_file");
         } else {
-            this.l = bundle.getString("sc_id");
-            this.m = (ProjectFileBean) bundle.getParcelable("project_file");
+            l = bundle.getString("sc_id");
+            m = bundle.getParcelable("project_file");
         }
-        this.k = findViewById(2131231847);
-        a(this.k);
-        findViewById(2131231370).setVisibility(8);
-        d().a(xB.b().a(getApplicationContext(), 2131625498));
+        k = findViewById(Resources.id.toolbar);
+        a(k);
+        findViewById(Resources.id.layout_main_logo).setVisibility(View.GONE);
+        d().a(xB.b().a(getApplicationContext(),
+                Resources.string.logic_editor_more_block_actionbar_title_create_more_block));
         d().e(true);
         d().d(true);
-        this.k.setNavigationOnClickListener(new Xs(this));
-        this.o = new dt(this);
-        this.o.setFuncNameValidator(jC.a(this.l).a(this.m));
-        this.n = (LinearLayout) findViewById(2131231488);
-        this.n.addView(this.o);
+        /* To save an anonymous class */
+        k.setNavigationOnClickListener(Helper.getBackPressedClickListener(this));
+        o = new dt(this);
+        o.setFuncNameValidator(jC.a(l).a(m));
+        n = findViewById(Resources.id.makeblock_view);
+        n.addView(o);
     }
 
-    /* JADX DEBUG: Multi-variable search result rejected for r3v0, resolved type: com.besome.sketch.editor.makeblock.MakeBlockActivity */
-    /* JADX WARN: Multi-variable type inference failed */
-    @SuppressLint("ResourceType")
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(2131492883, menu);
-        menu.findItem(2131231546).setTitle(xB.b().a(getApplicationContext(), 2131624983));
+        getMenuInflater().inflate(Resources.menu.moreblock_menu, menu);
+        menu.findItem(Resources.id.moreblock_create)
+                .setTitle(xB.b().a(getApplicationContext(), Resources.string.common_word_create));
         return true;
     }
 
-    /* JADX DEBUG: Multi-variable search result rejected for r3v0, resolved type: com.besome.sketch.editor.makeblock.MakeBlockActivity */
-    /* JADX WARN: Multi-variable type inference failed */
+    @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
-        if (menuItem.getItemId() == 2131231546) {
-            if (this.o.a()) {
+        if (menuItem.getItemId() == Resources.id.moreblock_create) {
+            if (o.a()) {
                 return false;
             }
-            if (this.o.b()) {
+            if (o.b()) {
                 Intent intent = new Intent();
-                Pair blockInformation = this.o.getBlockInformation();
-                intent.putExtra("block_name", (String) blockInformation.first);
-                intent.putExtra("block_spec", (String) blockInformation.second);
+                Pair<String, String> blockInformation = o.getBlockInformation();
+                intent.putExtra("block_name", blockInformation.first);
+                intent.putExtra("block_spec", blockInformation.second);
                 setResult(-1, intent);
                 finish();
                 return true;
             }
         }
-        return MakeBlockActivity.super.onOptionsItemSelected(menuItem);
+        return super.onOptionsItemSelected(menuItem);
     }
 
-    /* JADX DEBUG: Multi-variable search result rejected for r2v0, resolved type: com.besome.sketch.editor.makeblock.MakeBlockActivity */
-    /* JADX WARN: Multi-variable type inference failed */
     @Override // com.besome.sketch.lib.base.BaseAppCompatActivity
     public void onResume() {
         super.onResume();
@@ -115,9 +121,10 @@ public class MakeBlockActivity extends BaseAppCompatActivity {
         }
     }
 
-    public void onSaveInstanceState(Bundle bundle) {
-        bundle.putString("sc_id", this.l);
-        bundle.putParcelable("project_file", this.m);
-        MakeBlockActivity.super.onSaveInstanceState(bundle);
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putString("sc_id", l);
+        savedInstanceState.putParcelable("project_file", m);
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
