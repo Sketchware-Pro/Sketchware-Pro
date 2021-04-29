c4b4056f6315785b31f236eae5d60e8f62dce825
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-29 14:59:14 +0200

    fix, style: Inline obf. anon. classes, format files

--- app/src/main/java/com/besome/sketch/editor/manage/ShowWidgetCollectionActivity.java
+++ app/src/main/java/com/besome/sketch/editor/manage/ShowWidgetCollectionActivity.java
@@ -1,6 +1,25 @@
 package com.besome.sketch.editor.manage;
 
-import a.a.a.Ft;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.besome.sketch.beans.ViewBean;
+import com.besome.sketch.editor.view.ViewPane;
+import com.besome.sketch.lib.base.BaseAppCompatActivity;
+import com.besome.sketch.lib.ui.EasyDeleteEditText;
+import com.sketchware.remod.Resources;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import a.a.a.GB;
 import a.a.a.NB;
 import a.a.a.Op;
@@ -10,24 +29,10 @@ import a.a.a.kC;
 import a.a.a.sy;
 import a.a.a.wq;
 import a.a.a.xB;
-
-import android.annotation.SuppressLint;
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import androidx.appcompat.widget.Toolbar;
-import com.besome.sketch.beans.ViewBean;
-import com.besome.sketch.editor.view.ViewPane;
-import com.besome.sketch.lib.base.BaseAppCompatActivity;
-import com.besome.sketch.lib.ui.EasyDeleteEditText;
-import java.util.ArrayList;
-import java.util.Iterator;
+import mod.hey.studios.util.Helper;
 
 public class ShowWidgetCollectionActivity extends BaseAppCompatActivity implements View.OnClickListener {
+
     public Toolbar k;
     public String l;
     public ViewPane m;
@@ -57,77 +62,76 @@ public class ShowWidgetCollectionActivity extends BaseAppCompatActivity implemen
     }
 
     public void l() {
-        int i = getResources().getDisplayMetrics().heightPixels;
-        this.r.measure(0, 0);
-        this.n.setLayoutParams(new LinearLayout.LayoutParams(-1, ((i - GB.a(this.e)) - GB.f(this.e)) - this.r.getMeasuredHeight()));
-        this.n.requestLayout();
+        r.measure(0, 0);
+        n.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ((getResources().getDisplayMetrics().heightPixels
+                        - GB.a(e)) - GB.f(e)) - r.getMeasuredHeight()
+        ));
+        n.requestLayout();
     }
 
-    /* JADX DEBUG: Multi-variable search result rejected for r3v0, resolved type: com.besome.sketch.editor.manage.ShowWidgetCollectionActivity */
-    /* JADX WARN: Multi-variable type inference failed */
+    @Override
     public void onClick(View view) {
         int id = view.getId();
-        if (id == 2131231112) {
+        if (id == Resources.id.img_back) {
             onBackPressed();
-        } else if (id == 2131231681 && this.s.b()) {
-            Rp.h().a(this.l, this.o.getText().toString(), true);
-            bB.a(getApplicationContext(), xB.b().a(getApplicationContext(), 2131625279), 0).show();
+        } else if (id == Resources.id.save_button && s.b()) {
+            Rp.h().a(l, o.getText().toString(), true);
+            bB.a(getApplicationContext(),
+                    xB.b().a(getApplicationContext(),
+                            Resources.string.design_manager_message_edit_complete),
+                    0).show();
             finish();
         }
     }
 
+    @Override
     public void onConfigurationChanged(Configuration configuration) {
-        ShowWidgetCollectionActivity.super.onConfigurationChanged(configuration);
+        super.onConfigurationChanged(configuration);
         l();
     }
 
-    /* JADX DEBUG: Multi-variable search result rejected for r3v0, resolved type: com.besome.sketch.editor.manage.ShowWidgetCollectionActivity */
-    /* JADX WARN: Multi-variable type inference failed */
-    @SuppressLint("ResourceType")
     @Override // com.besome.sketch.lib.base.BaseAppCompatActivity
-    public void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
-        setContentView(2131427513);
-        this.k = findViewById(2131231847);
-        a(this.k);
-        findViewById(2131231370).setVisibility(8);
-        d().a(xB.b().a(getApplicationContext(), 2131625306));
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(Resources.layout.manage_collection_show_widget);
+        k = findViewById(Resources.id.toolbar);
+        a(k);
+        findViewById(Resources.id.layout_main_logo).setVisibility(View.GONE);
+        d().a(xB.b().a(getApplicationContext(), Resources.string.design_manager_widget_title_actionbar_title));
         d().e(true);
         d().d(true);
-        this.k.setNavigationOnClickListener(new Ft(this));
-        this.l = getIntent().getStringExtra("widget_name");
-        this.m = (ViewPane) findViewById(2131231592);
-        this.m.setVerticalScrollBarEnabled(true);
+        k.setNavigationOnClickListener(Helper.getBackPressedClickListener(this));
+        l = getIntent().getStringExtra("widget_name");
+        m = findViewById(Resources.id.pane);
+        m.setVerticalScrollBarEnabled(true);
         kC kCVar = new kC("", wq.a() + "/image/data/", "", "");
         kCVar.b(Op.g().f());
-        this.m.setResourceManager(kCVar);
-        this.p = (EasyDeleteEditText) findViewById(2131230990);
-        this.o = this.p.getEditText();
-        this.o.setPrivateImeOptions("defaultInputmode=english;");
-        this.o.setText(this.l);
-        this.p.setHint(xB.b().a(this, 2131625305));
-        this.q = (Button) findViewById(2131231681);
-        this.q.setText(xB.b().a(getApplicationContext(), 2131625031));
-        this.q.setOnClickListener(this);
-        this.s = new NB(this, this.p.getTextInputLayout(), Rp.h().g());
-        this.r = (LinearLayout) findViewById(2131231320);
-        this.n = (ScrollView) findViewById(2131231692);
+        m.setResourceManager(kCVar);
+        p = findViewById(Resources.id.ed_input);
+        o = p.getEditText();
+        o.setPrivateImeOptions("defaultInputmode=english;");
+        o.setText(l);
+        p.setHint(xB.b().a(this, Resources.string.design_manager_widget_hint_enter_widget_name));
+        q = findViewById(Resources.id.save_button);
+        q.setText(xB.b().a(getApplicationContext(), Resources.string.common_word_save));
+        q.setOnClickListener(this);
+        s = new NB(this, p.getTextInputLayout(), Rp.h().g());
+        r = findViewById(Resources.id.layout_button);
+        n = findViewById(Resources.id.scroll_view);
     }
 
-    public void onPostCreate(Bundle bundle) {
-        ShowWidgetCollectionActivity.super.onPostCreate(bundle);
-        a(Rp.h().a(this.l).widgets);
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        a(Rp.h().a(l).widgets);
         l();
-    }
-
-    @Override // com.besome.sketch.lib.base.BaseAppCompatActivity
-    public void onResume() {
-        super.onResume();
     }
 
     public sy a(ViewBean viewBean) {
-        sy b = (sy) this.m.b(viewBean);
-        this.m.a((View) b);
+        sy b = (sy) m.b(viewBean);
+        m.a((View) b);
         return b;
     }
 }
