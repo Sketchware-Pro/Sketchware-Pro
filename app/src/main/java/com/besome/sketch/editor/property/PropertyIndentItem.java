338e8d1813771ce07788f08cf3474f2f86bc7bf0
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-29 16:03:44 +0200

    fix, style: Inline obf. anon. classes, format file

--- app/src/main/java/com/besome/sketch/editor/property/PropertyIndentItem.java
+++ app/src/main/java/com/besome/sketch/editor/property/PropertyIndentItem.java
@@ -1,19 +1,9 @@
 package com.besome.sketch.editor.property;
 
-import a.a.a.Kw;
-import a.a.a.TB;
-import a.a.a.Yw;
-import a.a.a.Zw;
-import a.a.a._w;
-import a.a.a.aB;
-import a.a.a.ax;
-import a.a.a.mB;
-import a.a.a.wB;
-import a.a.a.xB;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -21,7 +11,18 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.sketchware.remod.Resources;
+
+import a.a.a.Kw;
+import a.a.a.TB;
+import a.a.a.aB;
+import a.a.a.mB;
+import a.a.a.wB;
+import a.a.a.xB;
+import mod.hey.studios.util.Helper;
+
 public class PropertyIndentItem extends RelativeLayout implements View.OnClickListener {
+
     public Context a;
     public String b = "";
     public String c = "";
@@ -43,24 +44,54 @@ public class PropertyIndentItem extends RelativeLayout implements View.OnClickLi
     }
 
     public String getKey() {
-        return this.b;
+        return b;
+    }
+
+    public void setKey(String str) {
+        b = str;
+        int identifier = getResources().getIdentifier(str, "string", getContext().getPackageName());
+        if (identifier > 0) {
+            h.setText(xB.b().a(getResources(), identifier));
+            char c2 = 65535;
+            switch (b) {
+                case "property_padding":
+                    c2 = 1;
+                    break;
+
+                case "property_margin":
+                    c2 = 0;
+                    break;
+            }
+            if (c2 == 0) {
+                g = Resources.drawable.insert_white_space_48;
+            } else if (c2 == 1) {
+                g = Resources.drawable.collect_48;
+            }
+            if (e.getVisibility() == VISIBLE) {
+                ((ImageView) findViewById(Resources.id.img_icon)).setImageResource(g);
+                ((TextView) findViewById(Resources.id.tv_title))
+                        .setText(xB.b().a(getContext(), identifier));
+                return;
+            }
+            f.setImageResource(g);
+        }
     }
 
     public String getValue() {
-        return this.c;
+        return c;
     }
 
-    public void onClick(View view) {
+    public void onClick(View v) {
         if (!mB.a()) {
-            String str = this.b;
             char c2 = 65535;
-            int hashCode = str.hashCode();
-            if (hashCode != -576300200) {
-                if (hashCode == 1964055463 && str.equals("property_padding")) {
+            switch (b) {
+                case "property_padding":
                     c2 = 1;
-                }
-            } else if (str.equals("property_margin")) {
-                c2 = 0;
+                    break;
+
+                case "property_margin":
+                    c2 = 0;
+                    break;
             }
             if (c2 == 0 || c2 == 1) {
                 a();
@@ -68,124 +99,154 @@ public class PropertyIndentItem extends RelativeLayout implements View.OnClickLi
         }
     }
 
-    @SuppressLint("ResourceType")
-    public void setKey(String str) {
-        this.b = str;
-        int identifier = getResources().getIdentifier(str, "string", getContext().getPackageName());
-        if (identifier > 0) {
-            this.h.setText(xB.b().a(getResources(), identifier));
-            String str2 = this.b;
-            char c2 = 65535;
-            int hashCode = str2.hashCode();
-            if (hashCode != -576300200) {
-                if (hashCode == 1964055463 && str2.equals("property_padding")) {
-                    c2 = 1;
-                }
-            } else if (str2.equals("property_margin")) {
-                c2 = 0;
-            }
-            if (c2 == 0) {
-                this.g = 2131165924;
-            } else if (c2 == 1) {
-                this.g = 2131165451;
-            }
-            if (this.e.getVisibility() == 0) {
-                ((ImageView) findViewById(2131231151)).setImageResource(this.g);
-                ((TextView) findViewById(2131232195)).setText(xB.b().a(getContext(), identifier));
-                return;
-            }
-            this.f.setImageResource(this.g);
-        }
-    }
-
     public void setOnPropertyValueChangeListener(Kw kw) {
-        this.n = kw;
+        n = kw;
     }
 
-    @SuppressLint("WrongConstant")
-    public void setOrientationItem(int i2) {
-        if (i2 == 0) {
-            this.d.setVisibility(8);
-            this.e.setVisibility(0);
-            return;
+    public void setOrientationItem(int orientationItem) {
+        if (orientationItem == 0) {
+            d.setVisibility(GONE);
+            e.setVisibility(VISIBLE);
+        } else {
+            d.setVisibility(VISIBLE);
+            e.setVisibility(GONE);
         }
-        this.d.setVisibility(0);
-        this.e.setVisibility(8);
     }
 
-    @SuppressLint("ResourceType")
     public final void a(Context context, boolean z) {
-        this.a = context;
-        wB.a(context, this, 2131427633);
-        this.h = (TextView) findViewById(2131232055);
-        this.i = (TextView) findViewById(2131232270);
-        this.f = (ImageView) findViewById(2131231155);
-        this.d = findViewById(2131231626);
-        this.e = findViewById(2131231628);
+        a = context;
+        wB.a(context, this, Resources.layout.property_input_item);
+        h = findViewById(Resources.id.tv_name);
+        i = findViewById(Resources.id.tv_value);
+        f = findViewById(Resources.id.img_left_icon);
+        d = findViewById(Resources.id.property_item);
+        e = findViewById(Resources.id.property_menu_item);
         if (z) {
             setSoundEffectsEnabled(true);
             setOnClickListener(this);
         }
     }
 
-    public void a(int i2, int i3, int i4, int i5) {
-        this.j = i2;
-        this.k = i3;
-        this.l = i4;
-        this.m = i5;
-        TextView textView = this.i;
-        textView.setText("left: " + this.j + ", top: " + this.k + ", right: " + this.l + ", bottom: " + this.m);
+    public void a(int left, int top, int right, int bottom) {
+        j = left;
+        k = top;
+        l = right;
+        m = bottom;
+        i.setText("left: " + j + ", top: " + k + ", right: " + l + ", bottom: " + m);
     }
 
     public final void a() {
-        int i2;
         aB aBVar = new aB((Activity) getContext());
-        aBVar.b(this.h.getText().toString());
-        aBVar.a(this.g);
-        View a2 = wB.a(getContext(), 2131427635);
-        @SuppressLint("ResourceType") CheckBox checkBox = (CheckBox) a2.findViewById(2131230890);
-        checkBox.setText(xB.b().a(getContext(), 2131624972));
-        EditText editText = (EditText) a2.findViewById(2131231023);
-        EditText editText2 = (EditText) a2.findViewById(2131231038);
-        EditText editText3 = (EditText) a2.findViewById(2131231044);
-        EditText editText4 = (EditText) a2.findViewById(2131231042);
-        EditText editText5 = (EditText) a2.findViewById(2131231025);
-        TB tb = new TB(this.a, a2.findViewById(2131231802), 0, 999);
-        TB tb2 = new TB(this.a, a2.findViewById(2131231824), 0, 999);
-        @SuppressLint("ResourceType") TB tb3 = new TB(this.a, a2.findViewById(2131231829), 0, 999);
-        TB tb4 = new TB(this.a, a2.findViewById(2131231831), 0, 999);
-        TB tb5 = new TB(this.a, a2.findViewById(2131231806), 0, 999);
-        tb2.a(String.valueOf(this.j));
-        tb4.a(String.valueOf(this.k));
-        tb3.a(String.valueOf(this.l));
-        tb5.a(String.valueOf(this.m));
-        int i3 = this.j;
-        int i4 = this.k;
-        if (i3 == i4 && i4 == (i2 = this.l) && i2 == this.m) {
-            tb.a(String.valueOf(i3));
-            checkBox.setChecked(true);
-            editText.setEnabled(true);
-            editText2.clearFocus();
-            editText3.clearFocus();
-            editText4.clearFocus();
-            editText5.clearFocus();
-            editText2.setEnabled(false);
-            editText3.setEnabled(false);
-            editText4.setEnabled(false);
-            editText5.setEnabled(false);
+        aBVar.b(h.getText().toString());
+        aBVar.a(g);
+        View view = wB.a(getContext(), Resources.layout.property_popup_input_indent);
+        CheckBox chk_pty_all = view.findViewById(Resources.id.chk_pty_all);
+        chk_pty_all.setText(xB.b().a(getContext(), Resources.string.common_word_all));
+        EditText et_all = view.findViewById(Resources.id.et_all);
+        EditText et_left = view.findViewById(Resources.id.et_left);
+        EditText et_top = view.findViewById(Resources.id.et_top);
+        EditText et_right = view.findViewById(Resources.id.et_right);
+        EditText et_bottom = view.findViewById(Resources.id.et_bottom);
+        TB ti_all = new TB(a, view.findViewById(Resources.id.ti_all), 0, 999);
+        TB ti_left = new TB(a, view.findViewById(Resources.id.ti_left), 0, 999);
+        TB ti_right = new TB(a, view.findViewById(Resources.id.ti_right), 0, 999);
+        TB ti_top = new TB(a, view.findViewById(Resources.id.ti_top), 0, 999);
+        TB ti_bottom = new TB(a, view.findViewById(Resources.id.ti_bottom), 0, 999);
+        ti_left.a(String.valueOf(j));
+        ti_top.a(String.valueOf(k));
+        ti_right.a(String.valueOf(l));
+        ti_bottom.a(String.valueOf(m));
+        if (j == k && k == l && l == m) {
+            ti_all.a(String.valueOf(j));
+            chk_pty_all.setChecked(true);
+            et_all.setEnabled(true);
+            et_left.clearFocus();
+            et_top.clearFocus();
+            et_right.clearFocus();
+            et_bottom.clearFocus();
+            et_left.setEnabled(false);
+            et_top.setEnabled(false);
+            et_right.setEnabled(false);
+            et_bottom.setEnabled(false);
         } else {
-            editText.clearFocus();
-            editText.setEnabled(false);
-            editText2.setEnabled(true);
-            editText3.setEnabled(true);
-            editText4.setEnabled(true);
-            editText5.setEnabled(true);
+            et_all.clearFocus();
+            et_all.setEnabled(false);
+            et_left.setEnabled(true);
+            et_top.setEnabled(true);
+            et_right.setEnabled(true);
+            et_bottom.setEnabled(true);
         }
-        checkBox.setOnClickListener(new Yw(this, checkBox, editText, editText2, editText3, editText4, editText5));
-        editText.addTextChangedListener(new Zw(this, tb2, editText, tb4, tb3, tb5));
-        aBVar.a(a2);
-        aBVar.b(xB.b().a(getContext(), 2131625031), new _w(this, checkBox, tb, tb2, tb3, tb4, tb5, editText2, editText3, editText4, editText5, aBVar));
-        aBVar.a(xB.b().a(this.a, 2131624974), new ax(this, aBVar));
+        chk_pty_all.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chk_pty_all.isChecked()) {
+                    et_all.setEnabled(true);
+                    et_left.clearFocus();
+                    et_top.clearFocus();
+                    et_right.clearFocus();
+                    et_bottom.clearFocus();
+                    et_left.setEnabled(false);
+                    et_top.setEnabled(false);
+                    et_right.setEnabled(false);
+                    et_bottom.setEnabled(false);
+                } else {
+                    et_all.clearFocus();
+                    et_all.setEnabled(false);
+                    et_left.setEnabled(true);
+                    et_top.setEnabled(true);
+                    et_right.setEnabled(true);
+                    et_bottom.setEnabled(true);
+                }
+            }
+        });
+        et_all.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                ti_left.a(et_all.getText().toString());
+                ti_top.a(et_all.getText().toString());
+                ti_right.a(et_all.getText().toString());
+                ti_bottom.a(et_all.getText().toString());
+            }
+        });
+        aBVar.a(view);
+        aBVar.b(xB.b().a(getContext(), Resources.string.common_word_save), new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chk_pty_all.isChecked()) {
+                    if (ti_all.b() && ti_left.b() && ti_right.b() && ti_top.b() && ti_bottom.b()) {
+                        int left = Integer.parseInt(et_left.getText().toString());
+                        int top = Integer.parseInt(et_top.getText().toString());
+                        int right = Integer.parseInt(et_right.getText().toString());
+                        int bottom = Integer.parseInt(et_bottom.getText().toString());
+                        a(left, top, right, bottom);
+                        if (n != null) {
+                            n.a(b, new int[]{left, top, right, bottom});
+                            aBVar.dismiss();
+                        }
+                    }
+                } else if (ti_left.b() && ti_right.b() && ti_top.b() && ti_bottom.b()) {
+                    int left = Integer.parseInt(et_left.getText().toString());
+                    int top = Integer.parseInt(et_top.getText().toString());
+                    int right = Integer.parseInt(et_right.getText().toString());
+                    int bottom = Integer.parseInt(et_bottom.getText().toString());
+                    a(left, top, right, bottom);
+                    if (n != null) {
+                        n.a(b, new int[]{left, top, right, bottom});
+                        aBVar.dismiss();
+                    }
+                }
+            }
+        });
+        aBVar.a(xB.b().a(getContext(), Resources.string.common_word_cancel),
+                Helper.getSketchwareDialogDismissOnClickListener(aBVar));
         aBVar.show();
     }
 }
