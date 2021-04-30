d2e4112c5cd3a36fc88e8bcaa380257edb68684a
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-04-30 15:08:52 +0200

    feat, style: Add & use more Resource ID constants, format file

--- app/src/main/java/mod/hilal/saif/xml/BaseLayout.java
+++ app/src/main/java/mod/hilal/saif/xml/BaseLayout.java
@@ -11,184 +11,135 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.TextView;
+
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.sketchware.remod.Resources;
+
 import java.util.HashMap;
-import mod.hilal.saif.lib.FileUtil;
+
+import mod.agus.jcoderz.lib.FileUtil;
+import mod.hey.studios.util.Helper;
 
 public class BaseLayout {
-    public static String defaultValue(String str, String str2, boolean z) {
-        if (z) {
-            HashMap hashMap = new HashMap();
+
+    public static String defaultValue(String path, String type, boolean write) {
+        if (write) {
+            HashMap<String, String> hashMap = new HashMap<>();
             hashMap.put("toolbar", "androidx.appcompat.widget.Toolbar");
             hashMap.put("appbarlayout", "com.google.android.material.appbar.AppBarLayout");
             hashMap.put("coordinatorlayout", "androidx.coordinatorlayout.widget.CoordinatorLayout");
             hashMap.put("drawerlayout", "androidx.drawerlayout.widget.DrawerLayout");
             hashMap.put("navigationdrawer", "LinearLayout");
             hashMap.put("floatingactionbutton", "com.google.android.material.floatingactionbutton.FloatingActionButton");
-            FileUtil.writeFile(str.concat("-convert"), new Gson().toJson(hashMap));
+            FileUtil.writeFile(path.concat("-convert"), new Gson().toJson(hashMap));
         }
-        switch (str2.hashCode()) {
-            case -1140094085:
-                if (str2.equals("toolbar")) {
-                    return "androidx.appcompat.widget.Toolbar";
-                }
-                break;
-            case -301919654:
-                if (str2.equals("coordinatorlayout")) {
-                    return "androidx.coordinatorlayout.widget.CoordinatorLayout";
-                }
-                break;
-            case 320322395:
-                if (str2.equals("drawerlayout")) {
-                    return "androidx.drawerlayout.widget.DrawerLayout";
-                }
-                break;
-            case 1499805692:
-                if (str2.equals("appbarlayout")) {
-                    return "com.google.android.material.appbar.AppBarLayout";
-                }
-                break;
-            case 1654772718:
-                if (str2.equals("floatingactionbutton")) {
-                    return "com.google.android.material.floatingactionbutton.FloatingActionButton";
-                }
-                break;
-            case 1676420101:
-                if (str2.equals("navigationdrawer")) {
-                    return "LinearLayout";
-                }
-                break;
-        }
-        return "LinearLayout";
-    }
 
-    public static String getPreValue(String str, String str2) {
-        String obj;
-        if (!FileUtil.isExistFile(String.valueOf(str) + "-convert") || FileUtil.readFile(String.valueOf(str) + "-convert").equals("") || FileUtil.readFile(String.valueOf(str) + "-convert").equals("[]")) {
-            return defaultValue(str, str2, true);
-        }
-        HashMap hashMap = (HashMap) new Gson().fromJson(FileUtil.readFile(String.valueOf(str) + "-convert"), new TypeToken<HashMap<String, Object>>() {
-        }.getType());
-        switch (str2.hashCode()) {
-            case -1140094085:
-                if (str2.equals("toolbar")) {
-                    obj = hashMap.get("toolbar").toString();
-                    break;
-                } else {
-                    obj = "";
-                    break;
-                }
-            case -301919654:
-                if (str2.equals("coordinatorlayout")) {
-                    obj = hashMap.get("coordinatorlayout").toString();
-                    break;
-                } else {
-                    obj = "";
-                    break;
-                }
-            case 320322395:
-                if (str2.equals("drawerlayout")) {
-                    obj = hashMap.get("drawerlayout").toString();
-                    break;
-                } else {
-                    obj = "";
-                    break;
-                }
-            case 1499805692:
-                if (str2.equals("appbarlayout")) {
-                    obj = hashMap.get("appbarlayout").toString();
-                    break;
-                } else {
-                    obj = "";
-                    break;
-                }
-            case 1654772718:
-                if (str2.equals("floatingactionbutton")) {
-                    obj = hashMap.get("floatingactionbutton").toString();
-                    break;
-                } else {
-                    obj = "";
-                    break;
-                }
-            case 1676420101:
-                if (str2.equals("navigationdrawer")) {
-                    obj = hashMap.get("navigationdrawer").toString();
-                    break;
-                } else {
-                    obj = "";
-                    break;
-                }
+        switch (type) {
+            case "toolbar":
+                return "androidx.appcompat.widget.Toolbar";
+
+            case "coordinatorlayout":
+                return "androidx.coordinatorlayout.widget.CoordinatorLayout";
+
+            case "drawerlayout":
+                return "androidx.drawerlayout.widget.DrawerLayout";
+
+            case "appbarlayout":
+                return "com.google.android.material.appbar.AppBarLayout";
+
+            case "floatingactionbutton":
+                return "com.google.android.material.floatingactionbutton.FloatingActionButton";
+
+            case "navigationdrawer":
             default:
-                obj = "";
-                break;
+                return "LinearLayout";
         }
-        if (obj.equals("")) {
-            return defaultValue(str, str2, false);
-        }
-        return obj;
     }
 
-    public static void showDialog(Context context, String str, final String str2, final String str3) {
-        final AlertDialog create = new AlertDialog.Builder(context).create();
-        View inflate = ((LayoutInflater) context.getSystemService("layout_inflater")).inflate(2131427797, (ViewGroup) null);
-        create.setView(inflate);
-        create.setCanceledOnTouchOutside(true);
-        create.getWindow().setBackgroundDrawableResource(17170445);
-        ((EditText) inflate.findViewById(2131232446)).setVisibility(8);
-        EditText editText = (EditText) inflate.findViewById(2131232443);
-        editText.setVisibility(View.GONE);
-        final EditText editText2 = (EditText) inflate.findViewById(2131232447);
-        ((TextView) ((ViewGroup) editText.getParent()).getChildAt(0)).setText("Convert");
-        editText2.setText(str);
-        editText2.setHint("type");
-        ((TextView) inflate.findViewById(2131232445)).setOnClickListener(new View.OnClickListener() {
+    public static String getPreValue(String path, String type) {
+        String value = "";
+        if (!FileUtil.isExistFile(path + "-convert") || FileUtil.readFile(path + "-convert").equals("") || FileUtil.readFile(path + "-convert").equals("[]")) {
+            return defaultValue(path, type, true);
+        }
+        HashMap<String, Object> hashMap = (HashMap<String, Object>) new Gson().fromJson(FileUtil.readFile(path + "-convert"), Helper.TYPE_MAP_LIST);
+        if (hashMap.containsKey(type) && hashMap.get(type) != null) {
+            switch (type) {
+                case "toolbar":
+                    value = hashMap.get("toolbar").toString();
+                    break;
 
+                case "coordinatorlayout":
+                    value = hashMap.get("coordinatorlayout").toString();
+                    break;
+
+                case "drawerlayout":
+                    value = hashMap.get("drawerlayout").toString();
+                    break;
+
+                case "appbarlayout":
+                    value = hashMap.get("appbarlayout").toString();
+                    break;
+
+                case "floatingactionbutton":
+                    value = hashMap.get("floatingactionbutton").toString();
+                    break;
+
+                case "navigationdrawer":
+                    value = hashMap.get("navigationdrawer").toString();
+                    break;
+            }
+        }
+
+        if (value.equals("")) {
+            return defaultValue(path, type, false);
+        }
+        return value;
+    }
+
+    public static void showDialog(Context context, String pre, final String type, final String path) {
+        final AlertDialog dialog = new AlertDialog.Builder(context).create();
+        View inflate = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(Resources.layout.custom_dialog_attribute, null);
+        dialog.setView(inflate);
+        dialog.setCanceledOnTouchOutside(true);
+        /* what */
+        /* dialog.getWindow().setBackgroundDrawableResource(17170445); */
+        ((EditText) inflate.findViewById(Resources.id.dialog_input_res)).setVisibility(View.GONE);
+        EditText dialog_input_attr = (EditText) inflate.findViewById(Resources.id.dialog_input_attr);
+        dialog_input_attr.setVisibility(View.GONE);
+        final EditText dialog_input_value = (EditText) inflate.findViewById(Resources.id.dialog_input_value);
+        ((TextView) ((ViewGroup) dialog_input_attr.getParent()).getChildAt(0)).setText("Convert");
+        dialog_input_value.setText(pre);
+        dialog_input_value.setHint("type");
+        ((TextView) inflate.findViewById(Resources.id.dialog_btn_save)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if (editText2.getText().toString().trim().equals("")) {
-                    create.dismiss();
+                if (dialog_input_value.getText().toString().trim().equals("")) {
+                    dialog.dismiss();
                     return;
                 }
-                ((HashMap) new Gson().fromJson(FileUtil.readFile(String.valueOf(str3) + "-convert"), new TypeToken<HashMap<String, Object>>() {
-                }.getType())).put(str2, editText2.getText().toString().trim());
-                create.dismiss();
+                ((HashMap<String, Object>) new Gson().fromJson(FileUtil.readFile(path + "-convert"), Helper.TYPE_MAP_LIST))
+                        .put(type, dialog_input_value.getText().toString().trim());
+                dialog.dismiss();
             }
         });
-        ((TextView) inflate.findViewById(2131232444)).setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
-                create.dismiss();
-            }
-        });
-        create.show();
+        ((TextView) inflate.findViewById(Resources.id.dialog_btn_cancel)).setOnClickListener(Helper.getDialogDismissListener(dialog));
+        dialog.show();
     }
 
-    public static void visuals(final Context context, ImageView imageView, final String str, String str2, final String str3) {
-        final ImageView imageView2 = (ImageView) ((LinearLayout) imageView.getParent()).findViewById(2131232459);
-        imageView2.setVisibility(View.VISIBLE);
-        imageView2.setImageResource(2131165788);
-        imageView2.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
-                PopupMenu popupMenu = new PopupMenu(context, imageView2);
+    public static void visuals(final Context context, ImageView bc, final String fileName, String sc_id, final String type) {
+        final ImageView ig_toolbar_load_file = ((LinearLayout) bc.getParent()).findViewById(Resources.id.ig_toolbar_load_file);
+        ig_toolbar_load_file.setVisibility(View.VISIBLE);
+        ig_toolbar_load_file.setImageResource(Resources.drawable.ic_menu_white_24dp);
+        ig_toolbar_load_file.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(context, ig_toolbar_load_file);
                 popupMenu.getMenu().add("Convert");
-                final Context Context = context;
-                final String Str = str;
-                final String str2 = str3;
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-
-                    public boolean onMenuItemClick(MenuItem menuItem) {
-                        String charSequence = menuItem.getTitle().toString();
-                        switch (charSequence.hashCode()) {
-                            case -1678723693:
-                                if (!charSequence.equals("Convert")) {
-                                    return true;
-                                }
-                                BaseLayout.showDialog(context, BaseLayout.getPreValue(str, str2), str2, str);
-                                return true;
-                            default:
-                                return true;
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if (item.getTitle().toString().equals("Convert")) {
+                            showDialog(context, getPreValue(fileName, type), type, fileName);
                         }
+
+                        return true;
                     }
                 });
                 popupMenu.show();
