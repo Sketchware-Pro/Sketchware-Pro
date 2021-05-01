e239b65fbb4791e382ecccd16a3629228af7dccf
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-05-01 13:24:28 +0200

    feat: Add Stringfog related classes

---
+++ app/src/main/java/mod/hey/studios/project/stringfog/ManageStringfogActivity.java
@@ -0,0 +1,49 @@
+package mod.hey.studios.project.stringfog;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.sketchware.remod.Resources;
+
+import mod.hey.studios.util.Helper;
+
+public class ManageStringfogActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
+
+    private StringfogHandler stringfogHandler;
+    private Switch sw_pg_enabled;
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        stringfogHandler.setStringfogEnabled(isChecked);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(Resources.layout.manage_stringfog);
+        initialize();
+        initializeLogic();
+    }
+
+    private void initialize() {
+        sw_pg_enabled = (Switch) findViewById(Resources.id.sw_pg_enabled);
+        sw_pg_enabled.setOnCheckedChangeListener(this);
+    }
+
+    private void initializeLogic() {
+        initToolbar();
+        stringfogHandler = new StringfogHandler(getIntent().getStringExtra("sc_id"));
+        sw_pg_enabled.setChecked(stringfogHandler.isStringfogEnabled());
+    }
+
+    private void initToolbar() {
+        ((TextView) findViewById(Resources.id.tx_toolbar_title)).setText("StringFog Manager");
+        ImageView ig_toolbar_back = findViewById(Resources.id.ig_toolbar_back);
+        ig_toolbar_back.setOnClickListener(Helper.getBackPressedClickListener(this));
+        Helper.applyRippleToToolbarView(ig_toolbar_back);
+    }
+}
---
+++ app/src/main/java/mod/hey/studios/project/stringfog/StringfogHandler.java
@@ -0,0 +1,69 @@
+package mod.hey.studios.project.stringfog;
+
+import com.besome.sketch.design.DesignActivity.a;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+
+import a.a.a.Dp;
+import mod.agus.jcoderz.lib.FileUtil;
+import mod.hey.studios.project.proguard.ProguardHandler;
+
+public class StringfogHandler {
+
+    private final String config_path;
+    private final String sc_id;
+
+    public StringfogHandler(String sc_id) {
+        this.sc_id = sc_id;
+        config_path = FileUtil.getExternalStorageDir().concat("/.sketchware/data/" + sc_id + "/stringfog");
+        if (!FileUtil.isExistFile(config_path)) {
+            FileUtil.writeFile(config_path, getDefaultConfig());
+        }
+    }
+
+    private static String getDefaultConfig() {
+        HashMap<String, String> config = new HashMap<>();
+        config.put("enabled", "false");
+        return new Gson().toJson(config);
+    }
+
+    public boolean isStringfogEnabled() {
+        boolean enabled;
+
+        if (FileUtil.isExistFile(config_path)) {
+            HashMap<String, String> config = null;
+            try {
+                config = new Gson().fromJson(FileUtil.readFile(config_path), ProguardHandler.type);
+            } finally {
+                Object enabledValue;
+
+                enabled = (config != null)
+                        && (enabledValue = config.get("enabled")) != null
+                        && enabledValue.equals("true");
+            }
+            return enabled;
+        }
+
+        return false;
+    }
+
+    public void setStringfogEnabled(boolean enabled) {
+        HashMap<String, String> config = new Gson().fromJson(FileUtil.readFile(config_path), ProguardHandler.type);
+        config.put("enabled", Boolean.valueOf(enabled).toString());
+        FileUtil.writeFile(config_path, new Gson().toJson(config));
+    }
+
+    /**
+     * Check if Stringfog is enabled for the project, and run it if it is.
+     *
+     * @param dialog An optional {@link a} object, to update progress to the user
+     * @param dp     The {@link Dp} object that's building the project
+     */
+    public void start(a dialog, Dp dp) {
+        if (isStringfogEnabled()) {
+            if (dialog != null) dialog.c("Running StringFog...");
+            dp.runStringfog();
+        }
+    }
+}
