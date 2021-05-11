b1f03a1fe004dca786845ce61d62a54fd217a48a
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-05-11 12:35:58 +0200

    style: Format files

--- app/src/main/java/mod/agus/jcoderz/editor/event/ManageDefineBlockComponent.java
+++ app/src/main/java/mod/agus/jcoderz/editor/event/ManageDefineBlockComponent.java
@@ -2,32 +2,31 @@ package mod.agus.jcoderz.editor.event;
 
 public class ManageDefineBlockComponent {
 
-  public static String a(String name) {
-    switch (name) {
-    case "PhoneAuthCredential":
-      return "PhoneAuthCredential";
-      
-    case "GoogleSignInAccount":
-      return "GoogleSignInAccount";
+    public static String a(String name) {
+        switch (name) {
+            case "PhoneAuthCredential":
+                return "PhoneAuthCredential";
 
-    case "FirebasePhoneAuth":
-      return "FirebasePhoneAuth";
+            case "GoogleSignInAccount":
+                return "GoogleSignInAccount";
 
-    default:
-      return name;
+            case "FirebasePhoneAuth":
+                return "FirebasePhoneAuth";
+
+            default:
+                return name;
+        }
     }
-  }
-  
-  public static String b(String name) {
-    switch (name) {
-      case "PhoneAuthCredential":
-      case "GoogleSignInAccount":
-      case "FirebasePhoneAuth":
-        return "p";
-       
-      default:
-        return "v"; 
-      }
-  }
-  
+
+    public static String b(String name) {
+        switch (name) {
+            case "PhoneAuthCredential":
+            case "GoogleSignInAccount":
+            case "FirebasePhoneAuth":
+                return "p";
+
+            default:
+                return "v";
+        }
+    }
 }
