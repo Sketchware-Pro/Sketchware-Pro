e6b9e9aeff5b9e9843848b78e1e7352449a5a2a4
tyron12233 <74818961+tyron12233@users.noreply.github.com>
2021-05-12 17:15:18 +0800

    fix: revert changes and removed extra colon
    this should fix missing libraries

--- app/src/main/java/mod/tyron/compiler/IncrementalJavaCompiler.java
+++ app/src/main/java/mod/tyron/compiler/IncrementalJavaCompiler.java
@@ -341,7 +341,6 @@ public class IncrementalJavaCompiler extends Compiler {
         StringBuilder sb = new StringBuilder();
 
         for (String library : getBuiltInLibraries()) {
-            sb.append(":");
             sb.append(library).append("/").append("classes.jar");
         }
 
@@ -379,40 +378,39 @@ public class IncrementalJavaCompiler extends Compiler {
     
         String path = getContext().getFilesDir() + "/libs/libs/";
         ArrayList<String> arrayList = new ArrayList<>();
-        String classString = "/classes.jar";
 
         if (projectConfig.N.g) {
-            arrayList.add(":" + path + "appcompat-1.0.0" + classString);
-            arrayList.add(":" + path + "coordinatorlayout-1.0.0" + classString);
-            arrayList.add(":" + path + "material-1.0.0" + classString);
+            arrayList.add(":" + path + "appcompat-1.0.0");
+            arrayList.add(":" + path + "coordinatorlayout-1.0.0");
+            arrayList.add(":" + path + "material-1.0.0");
         }
         if (projectConfig.N.h) {
-            arrayList.add(":" + path + "firebase-common-19.0.0" + classString);
+            arrayList.add(":" + path + "firebase-common-19.0.0");
         }
         if (projectConfig.N.i) {
-            arrayList.add(":" + path + "firebase-auth-19.0.0" + classString);
+            arrayList.add(":" + path + "firebase-auth-19.0.0");
         }
         if (projectConfig.N.j) {
-            arrayList.add(":" + path + "firebase-database-19.0.0" + classString);
+            arrayList.add(":" + path + "firebase-database-19.0.0");
         }
         if (projectConfig.N.k) {
-            arrayList.add(":" + path + "firebase-storage-19.0.0" + classString);
+            arrayList.add(":" + path + "firebase-storage-19.0.0");
         }
         if (projectConfig.N.m) {
-            arrayList.add(":" + path + "play-services-maps-17.0.0" + classString);
+            arrayList.add(":" + path + "play-services-maps-17.0.0");
         }
         if (projectConfig.N.l) {
-            arrayList.add(":" + path + "play-services-ads-18.2.0" + classString);
+            arrayList.add(":" + path + "play-services-ads-18.2.0");
         }
 
         if (projectConfig.N.n) {
-            arrayList.add(":" + path + "glide-4.11.0" + classString);
+            arrayList.add(":" + path + "glide-4.11.0");
         }
         if (projectConfig.N.p) {
-            arrayList.add(":" + path + "okhttp-3.9.1" + classString);
+            arrayList.add(":" + path + "okhttp-3.9.1");
         }
         if (projectConfig.N.o) {
-            arrayList.add(":" + path + "gson-2.8.0" + classString);
+            arrayList.add(":" + path + "gson-2.8.0");
         }
         
         return arrayList;
