6e127240336abf20d6264071572d3cd9e9b512e1
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-05-14 22:05:53 +0200

    feat, style: Improve new VCS dialog
    Reformat file(s), remove XML layouts and generate dialog's content at runtime.

---
+++ app/src/main/java/mod/w3wide/validator/VersionNamePostfixValidator.java
@@ -0,0 +1,39 @@
+package mod.w3wide.validator;
+
+import android.content.Context;
+import android.text.Spanned;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Pattern;
+
+import a.a.a.MB;
+
+public class VersionNamePostfixValidator extends MB {
+
+    private static final Pattern VERSION_NAME_POSTFIX_PATTERN = Pattern.compile("^[a-zA-Z0-9_]*");
+
+    public VersionNamePostfixValidator(Context context, TextInputLayout textInputLayout) {
+        super(context, textInputLayout);
+    }
+
+    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+        return null;
+    }
+
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        String se = s.toString();
+        if (VERSION_NAME_POSTFIX_PATTERN.matcher(se).matches()) {
+            b.setErrorEnabled(false);
+            d = true;
+        } else if (se.contains(" ")) {
+            b.setErrorEnabled(true);
+            b.setError("Spaces aren't allowed to prevent crashes");
+            d = false;
+        } else {
+            b.setErrorEnabled(true);
+            b.setError("Only use letters (a-zA-Z), numbers and Special characters (_)");
+            d = false;
+        }
+    }
+}
