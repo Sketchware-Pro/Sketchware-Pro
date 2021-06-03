544c9bde19371b2c3500bc29d27e61bec5ec22f4
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-06-03 20:27:34 +0200

    feat: Add some Hey! Studios DEV's sources

---
+++ app/src/main/java/mod/hey/studios/project/ProjectSettingsDialog.java
@@ -0,0 +1,186 @@
+package mod.hey.studios.project;
+
+import static mod.SketchwareUtil.getDip;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.sketchware.remod.Resources;
+
+import mod.hey.studios.util.Helper;
+
+public class ProjectSettingsDialog {
+
+    private final Activity activity;
+    private final String sc_id;
+    private final ProjectSettings settings;
+
+    public ProjectSettingsDialog(Activity activity, String sc_id) {
+        this.activity = activity;
+        this.sc_id = sc_id;
+        this.settings = new ProjectSettings(sc_id);
+    }
+
+    public void show() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View view = inflater.inflate(Resources.layout.project_config_layout, null);
+
+
+        final TextView text_cancel = view.findViewById(Resources.id.text_cancel);
+        final TextView text_save = view.findViewById(Resources.id.text_save);
+
+        final LinearLayout prefs = view.findViewById(Resources.id.project_config_pref_layout);
+
+        int numberType = InputType.TYPE_CLASS_NUMBER;
+
+        EditText et_minsdk = addInputPref("min_sdk",
+                "21",
+                "Minimum SDK version",
+                numberType, prefs);
+
+        EditText et_targetsdk = addInputPref("target_sdk",
+                "28",
+                "Target SDK version",
+                numberType,
+                prefs);
+
+        EditText et_app_class = addInputPref("app_class",
+                ".SketchApplication",
+                "Application class name",
+                InputType.TYPE_CLASS_TEXT,
+                prefs);
+
+        CheckBox cb_oldmethods = addTogglePref("disable_old_methods",
+                false,
+                "Remove old deprecated methods in files, like showMessage, getDip, etc.",
+                prefs);
+
+        CheckBox cb_largeheap = addTogglePref("disable_large_heap",
+                false,
+                "Disable large heap for App",
+                prefs);
+
+        CheckBox cb_appthemeparent = addTogglePref("enable_bridgeless_themes",
+                false,
+                "Use new MaterialComponents AppTheme (will replace e.g. Button with MaterialButton, be careful!)",
+                prefs);
+
+        // hmm, seem interesting!
+
+        //EditText et_java_comp
+
+        //CheckBox cb_ovrrd = addTogglePref("override_src_files", false, "Override existing Java/Res files with your files in case they co-exist. This option is especially useful if you want to edit a file that Sketchware generates automatically.", prefs);
+
+        //CheckBox cb_block_opt = addTogglePref("block_opt", false, "Apply extra block optimization methods in logic editor", prefs);
+
+        final View[] pref_views = {
+                et_minsdk,
+                et_targetsdk,
+                et_app_class,
+                cb_oldmethods,
+                cb_largeheap,
+                cb_appthemeparent
+        };
+
+        builder.setView(view);
+
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+
+        text_cancel.setOnClickListener(Helper.getDialogDismissListener(dialog));
+
+        text_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settings.setValues(pref_views);
+
+                dialog.dismiss();
+            }
+        });
+
+    }
+
+    private CheckBox addTogglePref(String key, boolean defaultState, String hint, LinearLayout layout) {
+        CheckBox i = new CheckBox(activity);
+        LinearLayout.LayoutParams l = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        l.setMargins(
+                0,
+                (int) getDip(10),
+                0,
+                0
+        );
+        i.setLayoutParams(l);
+        layout.addView(i);
+
+        String v = settings.getValue(key, defaultState ? "true" : "false");
+
+        i.setText(hint);
+        i.setChecked(v.equals("true"));
+        i.setTextColor(Color.BLACK);
+        i.setPadding(
+                (int) getDip(4),
+                (int) getDip(8),
+                (int) getDip(8),
+                (int) getDip(8)
+        );
+		
+		/*i.setTag(0, key);
+		i.setTag(1, v);*/
+
+        i.setTag(key);
+
+        return i;
+    }
+
+    private EditText addInputPref(String key, String defaultValue, String hint, int inputType, LinearLayout layout) {
+        TextInputLayout i = new TextInputLayout(activity);
+        LinearLayout.LayoutParams l = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        l.setMargins(
+                0,
+                (int) getDip(10),
+                0,
+                0
+        );
+        i.setLayoutParams(l);
+        layout.addView(i);
+
+        EditText e = new EditText(activity);
+        e.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        e.setPadding(
+                (int) getDip(4),
+                (int) getDip(8),
+                (int) getDip(8),
+                (int) getDip(8)
+        );
+        e.setTextSize(16);
+        e.setTextColor(Color.BLACK);
+        e.setHint(hint);
+        e.setHintTextColor(Color.parseColor("#607D8B"));
+        e.setText(settings.getValue(key, defaultValue));
+		/*e.setTag(0, key);
+		e.setTag(1, e.getText().toString());*/
+        e.setTag(key);
+        e.setInputType(inputType);
+        i.addView(e);
+
+        return e;
+    }
+}
