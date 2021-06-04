4449e69249b53ceeb154b6abf5b8962eba3d19ed
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-06-04 23:12:54 +0200

    refactor, feat: Work with TextInputLayouts, several improvements
     - Adds notices to `TextInputLayout`s if any important field is empty
     - Fixes `<type> <name> = ;` instead of `<type> <name>;` when adding custom Variable without static initializer
     - Use of own `View$OnClickListener` class as previous one was an obfuscated anonymous class
     - Code reformats

--- app/src/main/java/mod/w3wide/variable/DialogCustomVariable.java
+++ app/src/main/java/mod/w3wide/variable/DialogCustomVariable.java
@@ -1,42 +1,65 @@
 package mod.w3wide.variable;
 
-import a.a.a.ZB;
-import a.a.a.aB;
-import a.a.a.mB;
-
 import android.text.TextUtils;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.EditText;
 
 import com.besome.sketch.editor.LogicEditorActivity;
+import com.google.android.material.textfield.TextInputLayout;
+
+import a.a.a.ZB;
+import a.a.a.aB;
+import mod.SketchwareUtil;
 
 public class DialogCustomVariable implements View.OnClickListener {
+
     public final aB dialog;
-    public final EditText editText;
-    public final EditText edittext2;
-    public final EditText edittext3;
     public final LogicEditorActivity logicEditor;
+    public final TextInputLayout tilType;
+    public final TextInputLayout tilName;
+    public final TextInputLayout tilInitializer;
     public final ZB validator;
 
-    public DialogCustomVariable(LogicEditorActivity logicEditorActivity, EditText varType, EditText varName, EditText varExtra, ZB zb, aB aBVar) {
-        this.dialog = aBVar;
-        this.editText = varName;
-        this.edittext2 = varType;
-        this.edittext3 = varExtra;
-        this.logicEditor = logicEditorActivity;
-        this.validator = zb;
+    public DialogCustomVariable(LogicEditorActivity logicEditorActivity, TextInputLayout varType, TextInputLayout varName, TextInputLayout varExtra, ZB zb, aB aB) {
+        dialog = aB;
+        tilName = varName;
+        tilType = varType;
+        tilInitializer = varExtra;
+        logicEditor = logicEditorActivity;
+        validator = zb;
     }
 
-    public void onClick(View view) {
-        String variableName = editText.getText().toString();
-        String variableType = edittext2.getText().toString();
-        String variableExtra = TextUtils.isEmpty(edittext3.getText().toString()) ? "" : edittext3.getText().toString();
-        
-        if (!TextUtils.isEmpty(variableName) && validator.b()) {
-            logicEditor.b(5, variableType + " " + variableName + " = " + variableExtra );
-            mB.a(logicEditor.getApplicationContext(), this.editText);
-            this.dialog.dismiss();
+    @Override
+    public void onClick(View v) {
+        String variableType = tilType.getEditText().getText().toString();
+        String variableName = tilName.getEditText().getText().toString();
+        String variableInitializer = TextUtils.isEmpty(tilInitializer.getEditText().getText().toString()) ? "" : tilInitializer.getEditText().getText().toString();
+
+        boolean validType = !TextUtils.isEmpty(variableType);
+        boolean validName = !TextUtils.isEmpty(variableName);
+        boolean getsInitialized = !TextUtils.isEmpty(variableInitializer);
+
+        if (validType) {
+            tilType.setError(null);
+        } else {
+            if (validName) tilType.requestFocus();
+            tilType.setError("Type can't be empty");
+        }
+
+        if (validName) {
+            tilName.setError(null);
+        } else {
+            tilName.requestFocus();
+            tilName.setError("Name can't be empty");
+        }
+
+        if (validName && validType && validator.b()) {
+            String toAdd = variableType + " " + variableName;
+            if (getsInitialized) {
+                toAdd += " = " + variableInitializer;
+            }
+            logicEditor.b(5, toAdd);
+            SketchwareUtil.hideKeyboard(tilName);
+            dialog.dismiss();
         }
     }
 }
