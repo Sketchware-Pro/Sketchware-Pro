dc6b16ca4d74ded633d9b071bb815b4586859ee7
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-06-08 23:13:00 +0200

    style: Format files

--- app/src/main/java/mod/hey/studios/editor/view/IdGenerator.java
+++ app/src/main/java/mod/hey/studios/editor/view/IdGenerator.java
@@ -4,6 +4,7 @@ import com.besome.sketch.beans.ViewBean;
 import com.besome.sketch.editor.view.ViewEditor;
 
 public class IdGenerator {
+
     public static String getId(ViewEditor viewEditor, int i, ViewBean viewBean) {
         String lastPath = getLastPath(viewBean.convert);
         switch (lastPath) {
