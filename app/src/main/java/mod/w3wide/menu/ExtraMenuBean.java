3acbe2da7872082bfee68c8edd33366eb8701a2d
Hasrat Ali <42956453+hasrat-ali@users.noreply.github.com>
2021-06-10 14:16:18 +0530

    feat: added code editor menu

--- app/src/main/java/mod/w3wide/menu/ExtraMenuBean.java
+++ app/src/main/java/mod/w3wide/menu/ExtraMenuBean.java
@@ -10,26 +10,152 @@ import java.util.ArrayList;
 
 import a.a.a.Ss;
 import mod.agus.jcoderz.lib.FileUtil;
+import mod.hilal.saif.activities.tools.ConfigActivity;
+import mod.hilal.saif.asd.AsdDialog;
+import mod.hilal.saif.asd.old.AsdOldDialog;
 
 public class ExtraMenuBean {
 
-    private static final String ASSETS_PATH = FileUtil.getExternalStorageDir() + "/.sketchware/data/%s/files/assets/";
-    private static final String NATIVE_PATH = FileUtil.getExternalStorageDir() + "/.sketchware/data/%s/files/native_libs/";
+    private final String ASSETS_PATH = FileUtil.getExternalStorageDir() + "/.sketchware/data/%s/files/assets/";
+    private final String NATIVE_PATH = FileUtil.getExternalStorageDir() + "/.sketchware/data/%s/files/native_libs/";
 
     private final FilePickerDialog fpd;
     private final LogicEditorActivity logicEditor;
     private final DialogProperties mProperty = new DialogProperties();
     private final String sc_id;
+    
     private String path;
     private String splitter;
 
-    public ExtraMenuBean(LogicEditorActivity logicA, String sc_id) {
-        this.fpd = new FilePickerDialog(logicA);
-        this.logicEditor = logicA;
-        this.sc_id = sc_id;
+    public ExtraMenuBean(LogicEditorActivity logicA) {
+        fpd = new FilePickerDialog(logicA);
+        logicEditor = logicA;
+        sc_id = logicA.B;
     }
+    
+    private void codeMenu(Ss menu) {
+        if (ConfigActivity.isLegacyCeEnabled()) {
+            AsdOldDialog asdOldDialog = new AsdOldDialog(logicEditor);
+            asdOldDialog.setCon(menu.getArgValue().toString());
+            asdOldDialog.show();
+            asdOldDialog.saveLis(logicEditor, false/*'true' is for number.*/, menu, asdOldDialog);
+            asdOldDialog.cancelLis(logicEditor, asdOldDialog);
+        } else {
+            AsdDialog asdDialog = new AsdDialog(logicEditor);
+            asdDialog.setCon(menu.getArgValue().toString());
+            asdDialog.show();
+            asdDialog.saveLis(logicEditor, false/*'true' is for number.*/, menu, asdDialog);
+            asdDialog.cancelLis(logicEditor, asdDialog);
+        }
+    }
+    
+    public void defineMenuSelector(Ss ss) {
+        String menuType = ss.b;
+        String menuName = ss.getMenuName();
 
-    public void pathSelectorMenu(final Ss ss) {
+        if (menuType.equals("d")) {
+            logicEditor.a(ss, true);
+        } else if (menuType.equals("s")) {
+            switch (menuName) {
+                case "intentData":
+                    logicEditor.e(ss);
+                    return;
+
+                case "url":
+                    logicEditor.c(ss);
+                    return;
+
+                case "inputCode":
+                    codeMenu(ss);
+                    return;
+
+                default:
+                    logicEditor.cusA(ss, false);
+                    return;
+            }
+        } else if (menuType.equals("m")) {            
+            switch(menuName) {
+                case "resource":
+                    logicEditor.a(ss, "property_image");
+                    return;
+                    
+                case "resource_bg":
+                    logicEditor.a(ss, "property_background_resource");
+                    return;
+
+                case "sound":
+                    logicEditor.h(ss);
+                    return;
+
+                case "font":
+                    logicEditor.d(ss);
+                    return;
+
+                case "typeface":
+                    logicEditor.i(ss);
+                    return;
+
+                case "color":
+                    logicEditor.b(ss);
+                    return;
+
+                case "view":
+                case "textview":
+                case "edittext":
+                case "imageview":
+                case "listview":
+                case "spinner":
+                case "listSpn":
+                case "webview":
+                case "checkbox":
+                case "switch":
+                case "seekbar":
+                case "calendarview":
+                case "compoundButton":
+                case "adview":
+                case "progressbar":
+                case "mapview":
+                case "radiobutton":
+                case "ratingbar":
+                case "searchview":
+                case "videoview":
+                case "gridview":
+                case "actv":
+                case "mactv":
+                case "tablayout":
+                case "viewpager":
+                case "bottomnavigation":
+                case "badgeview":
+                case "patternview":
+                case "sidebar":
+                case "recyclerview":
+                case "cardview":
+                case "collapsingtoolbar":
+                case "textinputlayout":
+                case "swiperefreshlayout":
+                case "radiogroup":
+                case "lottie":
+                case "otpview":
+                case "youtubeview":
+                case "codeview":
+                case "datepicker":
+                case "timepicker":
+                    logicEditor.f(ss);
+                    return;
+
+                case "Assets":
+                case "NativeLib":
+                    pathSelectorMenu(ss);
+                    return;
+
+                default:
+                    logicEditor.g(ss);
+                    return;
+            }
+        }
+    }
+    
+    private void pathSelectorMenu(final Ss ss) {
         String menuName = ss.getMenuName();
         ArrayList<String> markedPath = new ArrayList<>();
 
