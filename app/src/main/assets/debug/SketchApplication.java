63c19ffcd9d121b075da3b52d6f56f54b2e8012d
Hasrat Ali <42956453+hasrat-ali@users.noreply.github.com>
2021-06-28 10:16:36 +0530

    Create SketchApplication.java

---
+++ app/src/main/assets/debug/SketchApplication.java
@@ -0,0 +1,52 @@
+package <?package_name?>;
+
+import android.app.AlarmManager;
+import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Process;
+import android.util.Log;
+
+public class SketchApplication extends Application {
+
+    private static Context mApplicationContext;
+    private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
+
+    public static Context getContext() {
+        return mApplicationContext;
+    }
+
+    @Override
+    public void onCreate() {
+        mApplicationContext = getApplicationContext();
+        this.uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+
+        Thread.setDefaultUncaughtExceptionHandler(
+            new Thread.UncaughtExceptionHandler() {
+                @Override
+                public void uncaughtException(Thread thread, Throwable throwable) {
+                    Intent intent = new Intent(getApplicationContext(), DebugActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.putExtra("error", Log.getStackTraceString(throwable));
+
+                    PendingIntent pendingIntent =
+                        PendingIntent.getActivity(
+                            getApplicationContext(),
+                            11111,
+                            intent,
+                            PendingIntent.FLAG_ONE_SHOT
+                        );
+
+                    AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                    am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, pendingIntent);
+
+                    Process.killProcess(Process.myPid());
+                    System.exit(1);
+
+                    uncaughtExceptionHandler.uncaughtException(thread, throwable);
+                }
+            });
+        super.onCreate();
+    }
+}
