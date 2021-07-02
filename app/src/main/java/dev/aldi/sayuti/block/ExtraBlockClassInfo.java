2cf16e9546f3e5f67ff6ed5ce0167851db0e9785
Hasrat Ali <42956453+hasrat-ali@users.noreply.github.com>
2021-07-02 13:53:55 +0530

    feat: add block typename

--- app/src/main/java/dev/aldi/sayuti/block/ExtraBlockClassInfo.java
+++ app/src/main/java/dev/aldi/sayuti/block/ExtraBlockClassInfo.java
@@ -84,7 +84,7 @@ public class ExtraBlockClassInfo {
                 return "AsyncTask";
 
             case "activity":
-                return "Class";
+                return "Context";
 
             case "otpview":
                 return "OTPView";
@@ -148,6 +148,12 @@ public class ExtraBlockClassInfo {
 
             case "swiperefreshlayout":
                 return "SwipeRefreshLayout";
+
+            case "pageadapter":
+                return "PageAdapter";
+
+            case "signinbutton":
+                return "SignInButton";
 
             default:
                 return str;
