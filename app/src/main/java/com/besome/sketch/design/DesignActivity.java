2bb1873dba09dcb234dcad16be1ac6d8294ae33e
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-07-19 23:01:48 +0200

    style: Format file, minor logic change
     - Formats lines, changes some strings
     - Only adds "Install last built APK" to PopupMenu if the file exists

--- app/src/main/java/com/besome/sketch/design/DesignActivity.java
+++ app/src/main/java/com/besome/sketch/design/DesignActivity.java
@@ -396,7 +396,9 @@ public class DesignActivity extends BaseAppCompatActivity implements OnClickList
                 menu.add(Menu.NONE, 1, Menu.NONE, "Build Settings");
                 menu.add(Menu.NONE, 2, Menu.NONE, "Clean temporary files");
                 menu.add(Menu.NONE, 3, Menu.NONE, "Show last compile error");
-                menu.add(Menu.NONE, 4, Menu.NONE, "Install last built APK");
+		if (FileUtil.isExistFile(q.H)) {
+                    menu.add(Menu.NONE, 4, Menu.NONE, "Install last built APK");
+		}
 
                 popupMenu.setOnMenuItemClickListener(item -> {
                     switch (item.getItemId()) {
@@ -411,13 +413,15 @@ public class DesignActivity extends BaseAppCompatActivity implements OnClickList
                         case 3:
                             new CompileErrorSaver(l).showDialog(DesignActivity.this);
                             break;
+
                         case 4:
-                       if (FileUtil.isExistFile(q.H)) {
-			o();
-		} else {
-			SketchwareUtil.toast("Apk not found!");
-		}
-                     break;
+                            if (FileUtil.isExistFile(q.H)) {
+			        o();
+		            } else {
+			        SketchwareUtil.toast("APK doesn't exist anymore");
+		            }
+                            break;
+
                         default:
                             return false;
                     }
