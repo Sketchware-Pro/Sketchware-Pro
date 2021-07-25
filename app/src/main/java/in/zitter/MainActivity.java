d57a059e11935d04b4b391e93076cfab8e3f49b3
Zymate Studio <77316475+SaiKiranSparkApps@users.noreply.github.com>
2021-07-25 22:01:03 +0530

    Update and rename Main.java to MainActivity.java

---
+++ app/src/main/java/in/zitter/MainActivity.java
@@ -0,0 +1,17 @@
+package in.zitter;
+
+
+package in.zitter;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends AppCompatActivity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
