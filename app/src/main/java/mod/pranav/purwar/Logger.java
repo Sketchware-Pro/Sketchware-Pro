1258a338d07c3fc3d3ac30cc4a4213f602d22724
Pranav <purwarpranav80@gmail.com>
2021-07-27 16:41:07 +0530

    Update and rename Custom logger.java to Logger.java

---
+++ app/src/main/java/mod/pranav/purwar/Logger.java
@@ -0,0 +1,18 @@
+package mod.pranav.purwar;
+
+import java.io.File;
+import java.lang.StringBuilder;
+
+public class Logger {
+
+    static String logPath = "/storage/emulated/0/.sketchware/"+"logs.txt";
+    public static void logExcept(String info, Exception e) {
+        try {
+            String except = info+e.toString();
+            e.printStackTrace();
+            FileUtil.writeFile(logPath, s);
+        } catch(Exception exception) {
+              e.printStackTrace();
+        }
+    }
+}
