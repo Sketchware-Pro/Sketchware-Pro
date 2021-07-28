c03d34b23d99b12eff954f4f0b9eebd46c7ebd8a
Zymate Studio <77316475+SaiKiranSparkApps@users.noreply.github.com>
2021-07-28 11:15:21 +0530

    Update Kotlinc.java

--- app/src/main/java/in/zitter/kotlinc/Kotlinc.java
+++ app/src/main/java/in/zitter/kotlinc/Kotlinc.java
@@ -1,1 +1,42 @@
+package in.zitter.kotlinc;
 
+import in.zitter.kotlinc.Main;
+import java.util.*;
+
+public class Kotlinc
+{
+	Main main;
+	String[] cmd;
+	ArrayList<String> flag;
+	private Boolean nowarn , verbose , progressive , includeruntime , javaparameters , noreflect , nostdlib;
+	String argsfile , cp;
+	Float jvmtarget;
+	public void main(){
+		flag.add(argsfile);
+		flag.add(cp);
+		if(nowarn= true){
+			flag.add("-nowarn");
+		}
+		if(verbose= true){
+			flag.add("-verbose");
+		}
+		if(progressive= true){
+			flag.add("-progressive");
+		}
+		if(includeruntime=true){
+			flag.add("-include-runtime");
+		}
+		if(javaparameters=true){
+			flag.add("-java-parameters");
+		}
+		if(noreflect=true){
+			flag.add("-no-reflect");
+		}
+		if(nostdlib=true){
+			flag.add("-no-stdlib");
+		}
+		
+		main.args = flag.toArray(new String[flag.size()]);
+		main.kotlinc();
+	}
+}
