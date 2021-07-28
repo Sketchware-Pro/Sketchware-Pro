3889e7fa07f202141462d04f9d1fc45559442d7c
Rakshit2 <88087043+Rakshit2@users.noreply.github.com>
2021-07-28 16:04:56 +0530

    changed debug actiity

--- app/src/main/java/com/besome/sketch/DebugActivity.java
+++ app/src/main/java/com/besome/sketch/DebugActivity.java
@@ -16,18 +16,12 @@ public class DebugActivity extends Activity {
             String error = intent.getStringExtra("error");
 
             if (error != null) {
-                AlertDialog dialog = new AlertDialog.Builder(this)
-                        .setTitle("An error occurred")
-                        .setMessage(error)
-                        .setPositiveButton("End Application", (dialog1, which) -> finish())
-                        .create();
-
-                TextView messageView = dialog.findViewById(android.R.id.message);
-                if (messageView != null) {
-                    messageView.setTextIsSelectable(true);
+                Intent intent=new Intent(context,Debug4Activty.class);
+                startActivity(intent);
+                finish();
                 }
 
-                dialog.show();
+
             }
         }
     }
