651d94ba1b42e8eb574ea2ed8369b6b8cfbb9e88
Zymate Studio <77316475+SaiKiranSparkApps@users.noreply.github.com>
2021-07-28 09:56:32 +0530

    Update Main.java

--- app/src/main/java/in/zitter/kotlinc/Main.java
+++ app/src/main/java/in/zitter/kotlinc/Main.java
@@ -2,11 +2,11 @@ package in.zitter.kotlinc;
 import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler;
 public class Main{
   K2JVMCompiler ktc;
-
+  String[] args;
 
   public void Kotlinc (String KotlinHome , String cp , String output , String TargetVersion ,String argsFile){
 
-    String[] args = {"-kotlin-home" , KotlinHome , "-cp" , cp , "-d" , output , "-jvm-target" , TargetVersion , "@argfile" , argsFile};
-    ktc.main(args);
+
+    ktc.main(this.args);
   }
 }
