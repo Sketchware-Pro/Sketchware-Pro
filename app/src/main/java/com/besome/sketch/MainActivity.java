7bb2614c3c727c6f821129f286bba7c37e149406
Pranav <purwarpranav80@gmail.com>
2021-07-29 13:24:06 +0530

    feat: remove tablayout reference in code

--- app/src/main/java/com/besome/sketch/MainActivity.java
+++ app/src/main/java/com/besome/sketch/MainActivity.java
@@ -36,7 +36,6 @@ import com.besome.sketch.shared.project.SharedProjectDetailActivity;
 import com.google.ads.consent.ConsentForm;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.sketchware.remod.Resources;
 
@@ -75,7 +74,6 @@ public class MainActivity extends BasePermissionAppCompatActivity implements Vie
     public l n;
     public MainDrawer o;
     public ViewPager p;
-    public TabLayout q;
     public String[] r;
     public int[] s = {Resources.drawable.android_os_96, Resources.drawable.ic_class_48, Resources.drawable.globe_96};
     public DB t;
@@ -109,7 +107,7 @@ public class MainActivity extends BasePermissionAppCompatActivity implements Vie
     }
 
     private void k(int i) {
-        for (int counter = 0; counter < r.length; counter++) {
+        /*for (int counter = 0; counter < r.length; counter++) {
             TabLayout.f c = q.c(counter);
             View childAt = ((ViewGroup) q.getChildAt(0)).getChildAt(counter);
             LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) childAt.getLayoutParams();
@@ -124,7 +122,7 @@ public class MainActivity extends BasePermissionAppCompatActivity implements Vie
                 layoutParams.weight = 1.0f;
             }
             childAt.setLayoutParams(layoutParams);
-        }
+        */}
     }
 
     @Override // com.besome.sketch.lib.base.BasePermissionAppCompatActivity
@@ -237,8 +235,8 @@ public class MainActivity extends BasePermissionAppCompatActivity implements Vie
         p = findViewById(Resources.id.viewpager);
         p.setOffscreenPageLimit(2);
         p.setAdapter(new PagerAdapter(getSupportFragmentManager()));
-        q = findViewById(Resources.id.tab_layout);
-        q.setupWithViewPager(p);
+        /*q = findViewById(Resources.id.tab_layout);
+        q.setupWithViewPager(p);*/
         p.a(this);
         E = findViewById(Resources.id.layout_qna_bottom);
         F = findViewById(Resources.id.fab);
