a5769d7f47d344e4c529e626cd0ea49e81a56b82
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-08-01 16:10:24 +0200

    refactor: Make AsyncTasks static, combine listener classes into 1
     - Makes 2 anonymous AsyncTask classes static inner classes
     - Fixes some errors from the previous commit
     - Doesn't center CheckBox labels in new backup dialog

--- app/src/main/java/mod/hey/studios/project/backup/BackupRestoreManager.java
+++ app/src/main/java/mod/hey/studios/project/backup/BackupRestoreManager.java
@@ -5,27 +5,25 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Environment;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.view.Gravity;
-import mod.SketchwareUtil;
-import mod.hey.studios.util.Helper;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.github.angads25.filepicker.model.DialogProperties;
 import com.github.angads25.filepicker.view.FilePickerDialog;
 import com.sketchware.remod.Resources;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
 import java.util.HashMap;
 
 import a.a.a.GC;
-import a.a.a.bB;
-import a.a.a.lC;
 import a.a.a.aB;
+import a.a.a.lC;
+import a.a.a.xB;
+import mod.SketchwareUtil;
+import mod.hey.studios.util.Helper;
 
 public class BackupRestoreManager {
 
@@ -34,7 +32,6 @@ public class BackupRestoreManager {
     // Needed to refresh the project list after restoring
     private GC gc;
 
-    //private AlertDialog backup_dlg;
     private HashMap<Integer, Boolean> bckpDialogStates;
 
     public BackupRestoreManager(Activity act) {
@@ -47,95 +44,75 @@ public class BackupRestoreManager {
     }
 
     public void backup(final String sc_id, final String project_name) {
+        final String localLibrariesTag = "local libraries";
+        final String customBlocksTag = "Custom Blocks";
         bckpDialogStates = new HashMap<>();
         bckpDialogStates.put(0, false);
         bckpDialogStates.put(1, false);
 
-        aB dialog = new aB(this);
+        aB dialog = new aB(act);
         dialog.a(Resources.drawable.ic_backup);
         dialog.b("Backup Options");
-        
-        LinearLayout linearLayout = new LinearLayout(act);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+
+        LinearLayout checkboxContainer = new LinearLayout(act);
+        checkboxContainer.setOrientation(LinearLayout.VERTICAL);
+        checkboxContainer.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
         int dip = (int) SketchwareUtil.getDip(8);
-        linearLayout.setPadding(dip, dip, dip, dip);
-        CheckBox checkBox = new CheckBox(act);
-        checkBox.setText("Include used local libs");
-        checkBox.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        checkBox.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-        checkBox.setOnCheckedChangeListener(new new CompoundButton.OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(CompoundButton cb, boolean isChecked)  {
-				bckpDialogStates.put(0, isChecked);
-			}
-		});
-        linearLayout.addView(checkBox);
-        CheckBox checkBox2 = new CheckBox(act);
-        checkBox2.setText("Include used custom blocks");
-        checkBox2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        checkBox2.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-        checkBox2.setOnCheckedChangeListener(new new CompoundButton.OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(CompoundButton cb, boolean isChecked)  {
-				bckpDialogStates.put(1, isChecked);
-			}
-		});
-        linearLayout.addView(checkBox2);
-        
-        dialog.a(linearLayout);
-        dialog.a("Backup", new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				doBackup(sc_id, project_name);
-			}
-		});
-        dialog.b("Cancel", Helper.getDialogDismissListener(dialog));
+        checkboxContainer.setPadding(dip, dip, dip, dip);
+
+        CompoundButton.OnCheckedChangeListener listener = (buttonView, isChecked) -> {
+            int index;
+            Object tag = buttonView.getTag();
+            if (tag instanceof String) {
+                switch ((String) tag) {
+                    case localLibrariesTag:
+                        index = 0;
+                        break;
+
+                    case customBlocksTag:
+                        index = 1;
+                        break;
+
+                    default:
+                        return;
+                }
+                bckpDialogStates.put(index, isChecked);
+            }
+        };
+
+        CheckBox includeLocalLibraries = new CheckBox(act);
+        includeLocalLibraries.setTag(localLibrariesTag);
+        includeLocalLibraries.setText("Include used Local libraries");
+        includeLocalLibraries.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        includeLocalLibraries.setOnCheckedChangeListener(listener);
+        checkboxContainer.addView(includeLocalLibraries);
+
+        CheckBox includeUsedCustomBlocks = new CheckBox(act);
+        includeUsedCustomBlocks.setTag(customBlocksTag);
+        includeUsedCustomBlocks.setText("Include used Custom Blocks");
+        includeUsedCustomBlocks.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        includeUsedCustomBlocks.setOnCheckedChangeListener(listener);
+        checkboxContainer.addView(includeUsedCustomBlocks);
+
+        dialog.a(checkboxContainer);
+        dialog.b("Back up", v -> {
+            dialog.dismiss();
+            doBackup(sc_id, project_name);
+        });
+        dialog.a(xB.b().a(act.getApplicationContext(), Resources.string.common_word_cancel),
+                Helper.getDialogDismissListener(dialog));
         dialog.show();
     }
 
     private void doBackup(final String sc_id, final String project_name /*, final SparseBooleanArray arr*/) {
-        new AsyncTask<String, Integer, String>() {
-            final boolean error = false;
-            BackupFactory bm;
-            ProgressDialog dlg;
-
-            @Override
-            protected void onPreExecute() {
-                dlg = new ProgressDialog(act);
-                dlg.setMessage("Creating backup...");
-                dlg.setCancelable(false);
-                dlg.show();
-            }
-
-            @Override
-            protected String doInBackground(String... params) {
-                bm = new BackupFactory(sc_id);
-                bm.setBackupLocalLibs(bckpDialogStates.get(0));
-                bm.setBackupCustomBlocks(bckpDialogStates.get(1));
-
-                //     try {
-                bm.backup(project_name);
-                /* } catch(Exception e) {
-                 bm.error = e.toString();
-                 error = true;
-                 }*/
-
-                return "";
-            }
-
-            @Override
-            protected void onPostExecute(String _result) {
-
-                dlg.dismiss();
-
-                if (bm.getOutFile() != null && !error) {
-                    bB.a(act, "Created backup successfully to " + bm.getOutFile().getAbsolutePath(), 0).show();
-                } else {
-                    bB.a(act, "Error: " + bm.error, 0).show();
-                }
-            }
-        }.execute("");
+        new BackupAsyncTask(new WeakReference<>(act), sc_id, project_name, bckpDialogStates)
+                .execute("");
     }
 
     /*** Restore ***/
@@ -154,7 +131,6 @@ public class BackupRestoreManager {
         fpd.setDialogSelectionListener(files -> {
             final String file = files[0];
 
-            //boolean custom_blocks = BackupFactory.zipContainsFile(file, "custom_blocks");
             final boolean local_libs = BackupFactory.zipContainsFile(file, "local_libs");
 
             if (local_libs) {
@@ -173,54 +149,110 @@ public class BackupRestoreManager {
         fpd.show();
     }
 
-
     public void doRestore(final String file, final boolean restoreLocalLibs) {
-        new AsyncTask<String, Integer, String>() {
-            BackupFactory bm;
-            boolean error = false;
-            ProgressDialog dlg;
+        new RestoreAsyncTask(new WeakReference<>(act), file, restoreLocalLibs, gc).execute("");
+    }
 
-            @Override
-            protected void onPreExecute() {
-                dlg = new ProgressDialog(act);
-                dlg.setMessage("Restoring...");
-                dlg.setCancelable(false);
-                dlg.show();
+    private static class BackupAsyncTask extends AsyncTask<String, Integer, String> {
+
+        private final String sc_id;
+        private final String project_name;
+        private final HashMap<Integer, Boolean> options;
+        private final WeakReference<Activity> activityWeakReference;
+        private BackupFactory bm;
+        private ProgressDialog dlg;
+
+        BackupAsyncTask(WeakReference<Activity> activityWeakReference, String sc_id, String project_name, HashMap<Integer, Boolean> options) {
+            this.activityWeakReference = activityWeakReference;
+            this.sc_id = sc_id;
+            this.project_name = project_name;
+            this.options = options;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dlg = new ProgressDialog(activityWeakReference.get());
+            dlg.setMessage("Creating backup...");
+            dlg.setCancelable(false);
+            dlg.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            bm = new BackupFactory(sc_id);
+            bm.setBackupLocalLibs(options.get(0));
+            bm.setBackupCustomBlocks(options.get(1));
+
+            bm.backup(project_name);
+
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String _result) {
+            dlg.dismiss();
+
+            if (bm.getOutFile() != null) {
+                SketchwareUtil.toast("Successfully created backup to: " + bm.getOutFile().getAbsolutePath());
+            } else {
+                SketchwareUtil.toastError("Error: " + bm.error, Toast.LENGTH_LONG);
+            }
+        }
+    }
+
+    private static class RestoreAsyncTask extends AsyncTask<String, Integer, String> {
+
+        private final WeakReference<Activity> activityWeakReference;
+        private final String file;
+        private final GC gc;
+        private final boolean restoreLocalLibs;
+        private BackupFactory bm;
+        private ProgressDialog dlg;
+        private boolean error = false;
+
+        RestoreAsyncTask(WeakReference<Activity> activityWeakReference, String file, boolean restoreLocalLibraries, GC gc) {
+            this.activityWeakReference = activityWeakReference;
+            this.file = file;
+            this.gc = gc;
+            this.restoreLocalLibs = restoreLocalLibraries;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dlg = new ProgressDialog(activityWeakReference.get());
+            dlg.setMessage("Restoring...");
+            dlg.setCancelable(false);
+            dlg.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            bm = new BackupFactory(lC.b());
+            bm.setBackupLocalLibs(restoreLocalLibs);
+
+            try {
+                bm.restore(new File(file));
+            } catch (Exception e) {
+                bm.error = e.getMessage();
+                error = true;
             }
 
-            @Override
-            protected String doInBackground(String... params) {
-                //changed in 6.3.0 fix1
-                bm = new BackupFactory(lC.b()/*BackupFactory.getNewScId()*/);
-                bm.setBackupLocalLibs(restoreLocalLibs);
+            return "";
+        }
 
-                try {
-                    bm.restore(new File(file/*files[0]*/));
-                } catch (Exception e) {
-                    bm.error = e.getMessage();
-                    error = true;
-                }
+        @Override
+        protected void onPostExecute(String _result) {
+            dlg.dismiss();
 
-                return "";
+            if (!bm.isRestoreSuccess() || error) {
+                SketchwareUtil.toastError("Couldn't restore: " + bm.error, Toast.LENGTH_LONG);
+            } else if (gc != null) {
+                // Refreshes the main list
+                gc.g();
+                SketchwareUtil.toast("Restored successfully");
+            } else {
+                SketchwareUtil.toast("Restored successfully. Refresh to see the project", Toast.LENGTH_LONG);
             }
-
-            @Override
-            protected void onPostExecute(String _result) {
-                dlg.dismiss();
-
-                if (!bm.isRestoreSuccess() || error) {
-                    bB.a(act, "Couldn't restore: " + bm.error, 0).show();
-                    return;
-                }
-
-                if (gc != null) {
-                    // Refreshes the main list
-                    gc.g();
-                    bB.a(act, "Restored successfully", 0).show();
-                } else {
-                    bB.a(act, "Restored successfully. You can refresh the list", 1).show();
-                }
-            }
-        }.execute("");
+        }
     }
 }
