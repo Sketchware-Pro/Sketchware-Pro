17e497d2a7ecbef96d21525fe94d8389d1fa4fdf
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-08-07 16:41:04 +0200

    feat: Add known View type constants, make use of those constants
     - Adds constants of View types to the class to be similar to com.besome.sketch.beans.ViewBean
     - Makes use of those new constants

--- app/src/main/java/mod/agus/jcoderz/beans/ViewBeans.java
+++ app/src/main/java/mod/agus/jcoderz/beans/ViewBeans.java
@@ -5,116 +5,169 @@ import com.google.common.collect.ImmutableBiMap;
 
 public class ViewBeans {
 
-    // Id : ViewName
-    static BiMap<Integer, String> views;
+    public static final int VIEW_TYPE_WIDGET_RADIOBUTTON = 19;
+    public static final int VIEW_TYPE_WIDGET_RATINGBAR = 20;
+    public static final int VIEW_TYPE_WIDGET_VIDEOVIEW = 21;
+    public static final int VIEW_TYPE_WIDGET_SEARCHVIEW = 22;
+    public static final int VIEW_TYPE_WIDGET_AUTOCOMPLETETEXTVIEW = 23;
+    public static final int VIEW_TYPE_WIDGET_MULTIAUTOCOMPLETETEXTVIEW = 24;
+    public static final int VIEW_TYPE_WIDGET_GRIDVIEW = 25;
+    public static final int VIEW_TYPE_WIDGET_ANALOGCLOCK = 26;
+    public static final int VIEW_TYPE_WIDGET_DATEPICKER = 27;
+    public static final int VIEW_TYPE_WIDGET_TIMEPICKER = 28;
+    public static final int VIEW_TYPE_WIDGET_DIGITALCLOCK = 29;
+    public static final int VIEW_TYPE_LAYOUT_TABLAYOUT = 30;
+    public static final int VIEW_TYPE_LAYOUT_VIEWPAGER = 31;
+    public static final int VIEW_TYPE_LAYOUT_BOTTOMNAVIGATIONVIEW = 32;
+    public static final int VIEW_TYPE_WIDGET_BADGEVIEW = 33;
+    public static final int VIEW_TYPE_WIDGET_PATTERNLOCKVIEW = 34;
+    public static final int VIEW_TYPE_WIDGET_WAVESIDEBAR = 35;
+    public static final int VIEW_TYPE_LAYOUT_CARDVIEW = 36;
+    public static final int VIEW_TYPE_LAYOUT_COLLAPSINGTOOLBARLAYOUT = 37;
+    public static final int VIEW_TYPE_LAYOUT_TEXTINPUTLAYOUT = 38;
+    public static final int VIEW_TYPE_LAYOUT_SWIPEREFRESHLAYOUT = 39;
+    public static final int VIEW_TYPE_LAYOUT_RADIOGROUP = 40;
+    public static final int VIEW_TYPE_WIDGET_MATERIALBUTTON = 41;
+    public static final int VIEW_TYPE_WIDGET_SIGNINBUTTON = 42;
+    public static final int VIEW_TYPE_WIDGET_CIRCLEIMAGEVIEW = 43;
+    public static final int VIEW_TYPE_WIDGET_LOTTIEANIMATIONVIEW = 44;
+    public static final int VIEW_TYPE_WIDGET_YOUTUBEPLAYERVIEW = 45;
+    public static final int VIEW_TYPE_WIDGET_OTPVIEW = 46;
+    public static final int VIEW_TYPE_WIDGET_CODEVIEW = 47;
+    public static final int VIEW_TYPE_WIDGET_RECYCLERVIEW = 48;
 
-    static {
-        // Initialize the views variable
-        views = new ImmutableBiMap.Builder<Integer, String>()
-                .put(19, "RadioButton")
-                .put(20, "RatingBar")
-                .put(21, "VideoView")
-                .put(22, "SearchView")
-                .put(23, "AutoCompleteTextView")
-                .put(24, "MultiAutoCompleteTextView")
-                .put(25, "GridView")
-                .put(26, "AnalogClock")
-                .put(27, "DatePicker")
-                .put(28, "TimePicker")
-                .put(29, "DigitalClock")
-                .put(30, "TabLayout")
-                .put(31, "ViewPager")
-                .put(32, "BottomNavigationView")
-                .put(33, "BadgeView")
-                .put(34, "PatternLockView")
-                .put(35, "WaveSideBar")
-                .put(36, "CardView")
-                .put(37, "CollapsingToolbarLayout")
-                .put(38, "TextInputLayout")
-                .put(39, "SwipeRefreshLayout")
-                .put(40, "RadioGroup")
-                .put(41, "MaterialButton")
-                .put(42, "SignInButton")
-                .put(43, "CircleImageView")
-                .put(44, "LottieAnimationView")
-                .put(45, "YoutubePlayerView")
-                .put(46, "OTPView")
-                .put(47, "CodeView")
-                .put(48, "RecyclerView")
-                .build();
+    /**
+     * Map that stores both a view's type and type name.
+     */
+    static BiMap<Integer, String> views = new ImmutableBiMap.Builder<Integer, String>()
+            .put(VIEW_TYPE_WIDGET_RADIOBUTTON, "RadioButton")
+            .put(VIEW_TYPE_WIDGET_RATINGBAR, "RatingBar")
+            .put(VIEW_TYPE_WIDGET_VIDEOVIEW, "VideoView")
+            .put(VIEW_TYPE_WIDGET_SEARCHVIEW, "SearchView")
+            .put(VIEW_TYPE_WIDGET_AUTOCOMPLETETEXTVIEW, "AutoCompleteTextView")
+            .put(VIEW_TYPE_WIDGET_MULTIAUTOCOMPLETETEXTVIEW, "MultiAutoCompleteTextView")
+            .put(VIEW_TYPE_WIDGET_GRIDVIEW, "GridView")
+            .put(VIEW_TYPE_WIDGET_ANALOGCLOCK, "AnalogClock")
+            .put(VIEW_TYPE_WIDGET_DATEPICKER, "DatePicker")
+            .put(VIEW_TYPE_WIDGET_TIMEPICKER, "TimePicker")
+            .put(VIEW_TYPE_WIDGET_DIGITALCLOCK, "DigitalClock")
+            .put(VIEW_TYPE_LAYOUT_TABLAYOUT, "TabLayout")
+            .put(VIEW_TYPE_LAYOUT_VIEWPAGER, "ViewPager")
+            .put(VIEW_TYPE_LAYOUT_BOTTOMNAVIGATIONVIEW, "BottomNavigationView")
+            .put(VIEW_TYPE_WIDGET_BADGEVIEW, "BadgeView")
+            .put(VIEW_TYPE_WIDGET_PATTERNLOCKVIEW, "PatternLockView")
+            .put(VIEW_TYPE_WIDGET_WAVESIDEBAR, "WaveSideBar")
+            .put(VIEW_TYPE_LAYOUT_CARDVIEW, "CardView")
+            .put(VIEW_TYPE_LAYOUT_COLLAPSINGTOOLBARLAYOUT, "CollapsingToolbarLayout")
+            .put(VIEW_TYPE_LAYOUT_TEXTINPUTLAYOUT, "TextInputLayout")
+            .put(VIEW_TYPE_LAYOUT_SWIPEREFRESHLAYOUT, "SwipeRefreshLayout")
+            .put(VIEW_TYPE_LAYOUT_RADIOGROUP, "RadioGroup")
+            .put(VIEW_TYPE_WIDGET_MATERIALBUTTON, "MaterialButton")
+            .put(VIEW_TYPE_WIDGET_SIGNINBUTTON, "SignInButton")
+            .put(VIEW_TYPE_WIDGET_CIRCLEIMAGEVIEW, "CircleImageView")
+            .put(VIEW_TYPE_WIDGET_LOTTIEANIMATIONVIEW, "LottieAnimationView")
+            .put(VIEW_TYPE_WIDGET_YOUTUBEPLAYERVIEW, "YoutubePlayerView")
+            .put(VIEW_TYPE_WIDGET_OTPVIEW, "OTPView")
+            .put(VIEW_TYPE_WIDGET_CODEVIEW, "CodeView")
+            .put(VIEW_TYPE_WIDGET_RECYCLERVIEW, "RecyclerView")
+            .build();
+
+    public static String buildClassInfo(int id) {
+        return getViewTypeName(id);
     }
 
-    public static String buildClassInfo(int i) {
-        return getViewTypeName(i);
+    public static int getViewTypeByTypeName(String typeName) {
+        return views.inverse().containsKey(typeName) ? views.inverse().get(typeName) : 0;
     }
 
-    public static int getViewTypeByTypeName(String str) {
-        return views.inverse().containsKey(str) ? views.inverse().get(str) : 0;
+    public static String getViewTypeName(int id) {
+        return views.containsKey(id) ? views.get(id) : "";
     }
 
-    public static String getViewTypeName(int i) {
-        return views.containsKey(i) ? views.get(i) : "";
-    }
-
-    public static int getViewTypeResId(int i) {
-        switch (i) {
-            case 19:
+    public static int getViewTypeResId(int id) {
+        switch (id) {
+            case VIEW_TYPE_WIDGET_RADIOBUTTON:
                 return 2131166264;
-            case 20:
+
+            case VIEW_TYPE_WIDGET_RATINGBAR:
                 return 2131165475;
-            case 21:
+
+            case VIEW_TYPE_WIDGET_VIDEOVIEW:
                 return 2131166259;
-            case 22:
+
+            case VIEW_TYPE_WIDGET_SEARCHVIEW:
                 return 2131165849;
-            case 23:
-            case 24:
-            case 38:
+
+            case VIEW_TYPE_WIDGET_AUTOCOMPLETETEXTVIEW:
+            case VIEW_TYPE_WIDGET_MULTIAUTOCOMPLETETEXTVIEW:
+            case VIEW_TYPE_LAYOUT_TEXTINPUTLAYOUT:
                 return 2131166242;
-            case 25:
-            case 48:
+
+            case VIEW_TYPE_WIDGET_GRIDVIEW:
+            case VIEW_TYPE_WIDGET_RECYCLERVIEW:
                 return 2131165662;
-            case 26:
-            case 28:
-            case 29:
+
+            case VIEW_TYPE_WIDGET_ANALOGCLOCK:
+            case VIEW_TYPE_WIDGET_TIMEPICKER:
+            case VIEW_TYPE_WIDGET_DIGITALCLOCK:
                 return 2131166276;
-            case 27:
+
+            case VIEW_TYPE_WIDGET_DATEPICKER:
                 return 2131165519;
-            case 30:
+
+            case VIEW_TYPE_LAYOUT_TABLAYOUT:
                 return 2131166303;
-            case 31:
+
+            case VIEW_TYPE_LAYOUT_VIEWPAGER:
                 return 2131166352;
-            case 32:
+
+            case VIEW_TYPE_LAYOUT_BOTTOMNAVIGATIONVIEW:
                 return 2131166314;
-            case 33:
+
+            case VIEW_TYPE_WIDGET_BADGEVIEW:
                 return 2131166257;
-            case 34:
+
+            case VIEW_TYPE_WIDGET_PATTERNLOCKVIEW:
                 return 2131166308;
-            case 35:
+
+            case VIEW_TYPE_WIDGET_WAVESIDEBAR:
                 return 2131166312;
-            case 36:
+
+            case VIEW_TYPE_LAYOUT_CARDVIEW:
                 return 2131166299;
-            case 37:
+
+            case VIEW_TYPE_LAYOUT_COLLAPSINGTOOLBARLAYOUT:
                 return 2131166351;
-            case 39:
+
+            case VIEW_TYPE_LAYOUT_SWIPEREFRESHLAYOUT:
                 return 2131166320;
-            case 40:
+
+            case VIEW_TYPE_LAYOUT_RADIOGROUP:
                 return 2131166321;
-            case 41:
+
+            case VIEW_TYPE_WIDGET_MATERIALBUTTON:
                 return 2131166353;
-            case 42:
+
+            case VIEW_TYPE_WIDGET_SIGNINBUTTON:
                 return 2131165650;
-            case 43:
+
+            case VIEW_TYPE_WIDGET_CIRCLEIMAGEVIEW:
                 return 2131166354;
-            case 44:
+
+            case VIEW_TYPE_WIDGET_LOTTIEANIMATIONVIEW:
                 return 2131166355;
-            case 45:
+
+            case VIEW_TYPE_WIDGET_YOUTUBEPLAYERVIEW:
                 return 2131166356;
-            case 46:
+
+            case VIEW_TYPE_WIDGET_OTPVIEW:
                 return 2131166346;
-            case 47:
+
+            case VIEW_TYPE_WIDGET_CODEVIEW:
                 return 2131166357;
+
             default:
-                return i;
+                return id;
         }
     }
 }
