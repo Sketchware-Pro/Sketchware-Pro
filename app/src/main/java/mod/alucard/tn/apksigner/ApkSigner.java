2e00142808bc39560d04bf08669a86a1f366462c
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-08-08 22:30:28 +0200

    refactor: Use simple method provided by `System`
     - Replaces complicated reflection with `System#setOut(PrintStream)`

--- app/src/main/java/mod/alucard/tn/apksigner/ApkSigner.java
+++ app/src/main/java/mod/alucard/tn/apksigner/ApkSigner.java
@@ -10,7 +10,6 @@ import com.android.apksigner.ApkSignerTool;
 import java.io.File;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class ApkSigner {
@@ -30,11 +29,9 @@ public class ApkSigner {
      * @param callback   Callback for System.out during signing. May be null
      */
     public void signWithTestKey(@NonNull String inputPath, @NonNull String outputPath, @Nullable LogCallback callback) {
-        Object savedAccessFlags = null;
-        Object savedOut = null;
-
         LogWriter logger = new LogWriter(callback);
         long savedTimeMillis = System.currentTimeMillis();
+        PrintStream oldOut = System.out;
 
         ArrayList<String> args = new ArrayList<String>() {{
             add("sign");
@@ -48,69 +45,26 @@ public class ApkSigner {
             add(new File(context.getFilesDir(), TESTKEY_DIR_IN_FILES + "testkey.x509.pem").getAbsolutePath());
         }};
 
-        logger.write("Signing an APK file with these arguments: " + args.toString());
+        logger.write("Signing an APK file with these arguments: " + args);
 
         /* If the signing has a callback, we need to change System.out to our logger */
         if (callback != null) {
-            try {
-                Field outField = System.class.getField("out");
-                Field accessFlagsField = Field.class.getField("accessFlags");
-
-                accessFlagsField.setAccessible(true);
-                savedAccessFlags = accessFlagsField.get(null);
-                accessFlagsField.set(outField, outField.getModifiers() & -17);
-                outField.setAccessible(true);
-                savedOut = outField.get(null);
-                outField.set(null, new PrintStream(logger));
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                logger.write("Failed to get/set field out in System: " + e.getMessage());
-            }
+            System.setOut(new PrintStream(logger));
         }
 
         try {
             ApkSignerTool.main(args.toArray(new String[0]));
         } catch (Exception e) {
-            logger.write("An error occurred while trying to sign the APK file " + inputPath
-                    + " and outputting it to " + outputPath
-                    + ": " + e.getMessage() + "\nStack trace: " + Log.getStackTraceString(e));
+            logger.write("An error occurred while trying to sign the APK file " + inputPath +
+                    " and outputting it to " + outputPath + ": " + e.getMessage() + "\n" +
+                    "Stack trace: " + Log.getStackTraceString(e));
         }
 
         logger.write("Signing an APK file took " + (System.currentTimeMillis() - savedTimeMillis) + " ms");
 
         /* Try to revert System.out */
-        if (callback == null) {
-            return;
-        }
-
-        try {
-            Field outField = System.class.getField("out");
-            Field accessFlagsField = Field.class.getField("accessFlags");
-
-            if (savedOut == null) {
-                logger.write("savedOut is null, skipping reverting field out in System");
-                return;
-            }
-
-            if (savedAccessFlags == null) {
-                logger.write("savedAccessFlags is null, skipping reverting field out in System");
-                return;
-            }
-
-            if (!outField.isAccessible()) {
-                logger.write("outField isn't accessible anymore, skipping reverting field out in System");
-                return;
-            }
-
-            if (!accessFlagsField.isAccessible()) {
-                logger.write("accessFlagsField isn't accessible anymore, skipping reverting field out in System");
-                return;
-            }
-
-            outField.set(null, savedOut);
-            accessFlagsField.set(null, savedAccessFlags);
-
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            logger.write("Failed to revert field out in System: " + e.getMessage());
+        if (callback != null) {
+            System.setOut(oldOut);
         }
     }
 
