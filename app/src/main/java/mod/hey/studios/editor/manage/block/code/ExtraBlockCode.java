33566418698c556cb8e958f33b1680e06e7228b4
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-08-13 19:38:05 +0200

    chore: Clean up last commit
     - Removes unnecessary casts, renames some local variables
     - Reverts to single `String#format` call
     - Lets parameter get saved to local variable instead of retrieving it every single time from the ArrayList

--- app/src/main/java/mod/hey/studios/editor/manage/block/code/ExtraBlockCode.java
+++ app/src/main/java/mod/hey/studios/editor/manage/block/code/ExtraBlockCode.java
@@ -16,96 +16,98 @@ import mod.hey.studios.editor.manage.block.v2.BlockLoader;
  * 6.3.0
  */
 public class ExtraBlockCode {
-	public Fx fx;
-	
-	public ExtraBlockCode(Fx mfx) {
-		fx = mfx;
-	}
-	
-	public int getBlockType(BlockBean blockBean, int i) {
-		int blockType;
-		if (blockBean.getParamClassInfo().get(i).b("boolean")) {
-			blockType = 0;
-		} else if (blockBean.getParamClassInfo().get(i).b("double")) {
-			blockType = 1;
-		} else if (blockBean.getParamClassInfo().get(i).b("String")) {
-			blockType = 2;
-		} else {
-			blockType = 3;
-		}
-		
-		return blockType;
-	}
-	
-	//changed 6.3.0
-	public String getCodeExtraBlock(BlockBean blockBean, String str) {
-		try {
-			ArrayList<String> arrayList = new ArrayList<>();
-			
-			for (int i = 0; i < blockBean.parameters.size(); i++) {
-				int blockType = getBlockType(blockBean, i);
-				
-				switch (blockType) {
-					case 0:
-					if (((String) blockBean.parameters.get(i)).isEmpty()) {
-						arrayList.add("true");
-					} else {
-						arrayList.add(fx.a((String) blockBean.parameters.get(i), getBlockType(blockBean, i), blockBean.opCode));
-					}
-					break;
-					case 1:
-					if (((String) blockBean.parameters.get(i)).isEmpty()) {
-						arrayList.add("0");
-					} else {
-						arrayList.add(fx.a((String) blockBean.parameters.get(i), getBlockType(blockBean, i), blockBean.opCode));
-					}
-					break;
-					case 2:
-					if (((String) blockBean.parameters.get(i)).isEmpty()) {
-						arrayList.add("\"\"");
-					} else {
-						arrayList.add(fx.a((String) blockBean.parameters.get(i), getBlockType(blockBean, i), blockBean.opCode));
-					}
-					break;
-					default:
-					if (((String) blockBean.parameters.get(i)).isEmpty()) {
-						arrayList.add("");
-					} else {
-						arrayList.add(fx.a((String) blockBean.parameters.get(i), getBlockType(blockBean, i), blockBean.opCode));
-					}
-				}
-			}
-			
-			if (blockBean.subStack1 >= 0) {
-				arrayList.add(fx.a(String.valueOf(blockBean.subStack1), str));
-			} else {
-				arrayList.add(" ");
-			}
-			if (blockBean.subStack2 >= 0) {
-				arrayList.add(fx.a(String.valueOf(blockBean.subStack2), str));
-			} else {
-				arrayList.add(" ");
-			}
-			
-			ExtraBlockInfo blockInfo = BlockLoader.getBlockInfo(blockBean.opCode);
-			
-			//6.3.0
-			if (blockInfo.isMissing) {
-				blockInfo = BlockLoader.getBlockFromProject(fx.e.sc_id, blockBean.opCode);
-			}
-			
-			String code = blockInfo.getCode();
-			for (int i = 0; i < arrayList.size(); i++) {
-				String parameter = (String) arrayList.get(i);
-				code = code.replace("%".concat(String.valueOf(i + 1).concat("$s")), parameter);
-				code = code.replaceFirst("%s", parameter);
-			}
-			return code;
-		} catch (Exception error) {
-			StringBuilder sb = new StringBuilder("/* Error: ");
-			sb.append(error.toString());
-			sb.append("*/");
-			return sb.toString();
-		}
-	}
+
+    public Fx fx;
+
+    public ExtraBlockCode(Fx fxVar) {
+        fx = fxVar;
+    }
+
+    public int getBlockType(BlockBean blockBean, int parameterIndex) {
+        int blockType;
+
+        if (blockBean.getParamClassInfo().get(parameterIndex).b("boolean")) {
+            blockType = 0;
+        } else if (blockBean.getParamClassInfo().get(parameterIndex).b("double")) {
+            blockType = 1;
+        } else if (blockBean.getParamClassInfo().get(parameterIndex).b("String")) {
+            blockType = 2;
+        } else {
+            blockType = 3;
+        }
+
+        return blockType;
+    }
+
+    public String getCodeExtraBlock(BlockBean blockBean, String var2) {
+        ArrayList<String> parameters = new ArrayList<>();
+
+        for (int i = 0; i < blockBean.parameters.size(); i++) {
+            String parameterValue = blockBean.parameters.get(i);
+
+            switch (getBlockType(blockBean, i)) {
+                case 0:
+                    if (parameterValue.isEmpty()) {
+                        parameters.add("true");
+                    } else {
+                        parameters.add(fx.a(parameterValue, getBlockType(blockBean, i), blockBean.opCode));
+                    }
+                    break;
+
+                case 1:
+                    if (parameterValue.isEmpty()) {
+                        parameters.add("0");
+                    } else {
+                        parameters.add(fx.a(parameterValue, getBlockType(blockBean, i), blockBean.opCode));
+                    }
+                    break;
+
+                case 2:
+                    if (parameterValue.isEmpty()) {
+                        parameters.add("\"\"");
+                    } else {
+                        parameters.add(fx.a(parameterValue, getBlockType(blockBean, i), blockBean.opCode));
+                    }
+                    break;
+
+                default:
+                    if (parameterValue.isEmpty()) {
+                        parameters.add("");
+                    } else {
+                        parameters.add(fx.a(parameterValue, getBlockType(blockBean, i), blockBean.opCode));
+                    }
+            }
+        }
+
+        if (blockBean.subStack1 >= 0) {
+            parameters.add(fx.a(String.valueOf(blockBean.subStack1), var2));
+        } else {
+            parameters.add(" ");
+        }
+
+        if (blockBean.subStack2 >= 0) {
+            parameters.add(fx.a(String.valueOf(blockBean.subStack2), var2));
+        } else {
+            parameters.add(" ");
+        }
+
+        ExtraBlockInfo blockInfo = BlockLoader.getBlockInfo(blockBean.opCode);
+
+        if (blockInfo.isMissing) {
+            blockInfo = BlockLoader.getBlockFromProject(fx.e.sc_id, blockBean.opCode);
+        }
+
+        String formattedCode;
+        if (parameters.size() > 0) {
+            try {
+                formattedCode = String.format(blockInfo.getCode(), parameters.toArray(new Object[0]));
+            } catch (Exception e) {
+                formattedCode = "/* Failed to resolve Custom Block's code: " + e + " */";
+            }
+        } else {
+            formattedCode = blockInfo.getCode();
+        }
+
+        return formattedCode;
+    }
 }
