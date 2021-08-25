728bc8b42e48fe0467e3ede98ca843252ff77ed2
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-08-25 14:39:44 +0200

    chore: Clean up code, remove overscroll effect

--- app/src/main/java/mod/hey/studios/logic/SourceCodeDialog.java
+++ app/src/main/java/mod/hey/studios/logic/SourceCodeDialog.java
@@ -1,25 +1,23 @@
 package mod.hey.studios.logic;
 
-import static mod.SketchwareUtil.getDip;
-
 import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Typeface;
+
+import com.sketchware.remod.Resources;
+
 import io.github.rosemoe.editor.langs.java.JavaLanguage;
 import io.github.rosemoe.editor.widget.CodeEditor;
 import io.github.rosemoe.editor.widget.EditorColorScheme;
-
-import com.sketchware.remod.Resources;
 
 public class SourceCodeDialog {
 
     public static void show(Context context, String code) {
         CodeEditor codeEditor = new CodeEditor(context);
         codeEditor.setTypefaceText(Typeface.MONOSPACE);
-        codeEditor.setOverScrollEnabled(true);
         codeEditor.setEditorLanguage(new JavaLanguage());
         codeEditor.setText(code);
-        codeEditor.setTextSize((float) 14);
+        codeEditor.setTextSize(14);
         codeEditor.setWordwrap(false);
         codeEditor.setAutoCompletionEnabled(false);
         codeEditor.setEditable(false);
