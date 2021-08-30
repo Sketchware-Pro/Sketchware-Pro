dd4d3ffcaa50975a7ab23bb8f252679d376df515
Hasrat <hasratali2880@gmail.com>
2021-08-30 23:46:31 +0530

    refactor: Parameterized local variable, rename method

--- app/src/main/java/com/besome/sketch/editor/logic/PaletteSelector.java
+++ app/src/main/java/com/besome/sketch/editor/logic/PaletteSelector.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import java.util.HashMap;
-import java.util.Iterator;
 
 import a.a.a.Vs;
 import a.a.a.Ws;
@@ -30,12 +29,18 @@ public class PaletteSelector extends LinearLayout implements View.OnClickListene
 
     @SuppressLint("WrongConstant")
     public final void a(Context context) {
-        this.a = context;
-        setOrientation(1);
-        setLayoutParams(new LinearLayout.LayoutParams(-1, -2));
-        int a2 = (int) wB.a(context, 8.0f);
-        int a3 = (int) wB.a(context, 4.0f);
-        setPadding(a2, a3, a2, a3);
+        a = context;
+        setOrientation(LinearLayout.VERTICAL);
+        setLayoutParams(new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        setPadding(
+            (int) wB.a(context, 8.0f),
+            (int) wB.a(context, 4.0f),
+            (int) wB.a(context, 8.0f),
+            (int) wB.a(context, 4.0f)
+        );
         a();
     }
 
@@ -51,40 +56,42 @@ public class PaletteSelector extends LinearLayout implements View.OnClickListene
     public void onClick(View view) {
         if (view instanceof Ws) {
             b();
-            Ws ws = (Ws) view;
-            ws.setSelected(true);
-            this.b.a(ws.getId(), ws.getColor());
+            Ws paletteView = (Ws) view;
+            paletteView.setSelected(true);
+            b.a(paletteView.getId(), paletteView.getColor());
         }
     }
 
     public void setOnBlockCategorySelectListener(Vs vs) {
-        this.b = vs;
+        b = vs;
     }
 
     public final void a() {
-        a(0, xB.b().a(getResources(), 2131624105), -1147626);
-        a(1, xB.b().a(getResources(), 2131624101), -3384542);
-        a(2, xB.b().a(getResources(), 2131624099), -1988310);
-        a(3, xB.b().a(getResources(), 2131624104), -10701022);
-        a(4, xB.b().a(getResources(), 2131624102), -14435927);
-        a(5, xB.b().a(getResources(), 2131624100), -6190977);
-        a(6, xB.b().a(getResources(), 2131624106), -11899692);
-        a(7, xB.b().a(getResources(), 2131624098), -13851166);
-        a(8, xB.b().a(getResources(), 2131624103), -7711273);
-        Iterator it = new mod.agus.jcoderz.editor.manage.block.palette.PaletteSelector().getPaletteSelector().iterator();
-        while (it.hasNext()) {
-            HashMap hashMap = (HashMap) it.next();
-            a(((Integer) hashMap.get("index")).intValue(), (String) hashMap.get("text"), ((Integer) hashMap.get("color")).intValue());
+        addPalette(0, xB.b().a(getResources(), 2131624105), 0xffee7d16);
+        addPalette(1, xB.b().a(getResources(), 2131624101), 0xffcc5b22);
+        addPalette(2, xB.b().a(getResources(), 2131624099), 0xffe1a92a);
+        addPalette(3, xB.b().a(getResources(), 2131624104), 0xff5cb722);
+        addPalette(4, xB.b().a(getResources(), 2131624102), 0xff23b9a9);
+        addPalette(5, xB.b().a(getResources(), 2131624100), 0xffa1887f);
+        addPalette(6, xB.b().a(getResources(), 2131624106), 0xff4a6cd4);
+        addPalette(7, xB.b().a(getResources(), 2131624098), 0xff2ca5e2);
+        addPalette(8, xB.b().a(getResources(), 2131624103), 0xff8a55d7);
+        for (HashMap<String, Object> palette : new mod.agus.jcoderz.editor.manage.block.palette.PaletteSelector().getPaletteSelector()) {
+            addPalette(
+                Integer.parseInt(palette.get("index").toString()),
+                palette.get("text").toString(),
+                Integer.parseInt(palette.get("color").toString())
+            );
         }
     }
 
-    public final void a(int i, String str, int i2) {
-        Ws ws = new Ws(this.a, i, str, i2);
-        ws.setTag(String.valueOf(i));
-        ws.setOnClickListener(this);
-        addView(ws);
-        if (i == 0) {
-            ws.setSelected(true);
+    private void addPalette(int id, String title, int color) {
+        Ws paletteView = new Ws(a, id, title, color);
+        paletteView.setTag(String.valueOf(id));
+        paletteView.setOnClickListener(this);
+        addView(paletteView);
+        if (id == 0) {
+            paletteView.setSelected(true);
         }
     }
 }
