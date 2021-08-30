cd6bca6ff168cbc61b77ba307cb946d1dca0240e
Hasrat <hasratali2880@gmail.com>
2021-08-31 00:25:46 +0530

    feat: add sketchware's toast class La/a/a/bB; in java

---
+++ app/src/main/java/a/a/a/bB.java
@@ -0,0 +1,74 @@
+package a.a.a;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sketchware.remod.Resources;
+
+public class bB {
+
+    public static final int TOAST_NORMAL = 0;
+    public static final int TOAST_WARNING = 1;
+
+    public static Toast a(Context context, int resString, int duration) {
+        return a(context, xB.b().a(context, resString), duration, 48, 0.0f, 64.0f, TOAST_NORMAL);
+    }
+
+    public static Toast a(Context context, CharSequence charSequence, int duration) {
+        return a(context, charSequence, duration, 48, 0.0f, 64.0f, TOAST_NORMAL);
+    }
+
+    public static Toast a(Context context, CharSequence charSequence, int duration, int gravity, float xOffset, float yOffset) {
+        View inflate = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(Resources.layout.custom_toast, null);
+        LinearLayout linearLayout = (LinearLayout) inflate.findViewById(Resources.id.custom_toast_container);
+        ((TextView) inflate.findViewById(Resources.id.tv_stoast)).setText(charSequence.toString());
+        Toast toast = new Toast(context);
+        toast.setDuration(duration);
+        toast.setGravity(
+            gravity,
+            (int) xOffset,
+            (int) yOffset
+        );
+        toast.setView(inflate);
+        return toast;
+    }
+
+    public static Toast a(Context context, CharSequence charSequence, int duration, int gravity, float xOffset, float yOffset, int toastType) {
+        try {
+            View inflate = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(Resources.layout.custom_toast, null);
+            LinearLayout linearLayout = (LinearLayout) inflate.findViewById(Resources.id.custom_toast_container);
+            TextView textView = (TextView) inflate.findViewById(Resources.id.tv_stoast);
+            if (toastType != TOAST_WARNING) {
+                linearLayout.setBackgroundResource(Resources.drawable.bg_toast_normal);
+                textView.setTextColor(context.getResources().getColor(Resources.color.scolor_black_01));
+            } else {
+                linearLayout.setBackgroundResource(Resources.drawable.bg_toast_warning);
+                textView.setTextColor(context.getResources().getColor(Resources.color.scolor_red_02));
+            }
+            textView.setText(charSequence.toString());
+            Toast toast = new Toast(context);
+            toast.setDuration(duration);
+            toast.setGravity(
+                gravity,
+                (int) wB.a(context, xOffset),
+                (int) wB.a(context, yOffset)
+            );
+            toast.setView(inflate);
+            return toast;
+        } catch (Exception e) {
+            return Toast.makeText(context, charSequence, duration);
+        }
+    }
+
+    public static Toast b(Context context, int resString, int duration) {
+        return a(context, xB.b().a(context, resString), duration, 48, 0.0f, 64.0f, TOAST_WARNING);
+    }
+
+    public static Toast b(Context context, CharSequence charSequence, int duration) {
+        return a(context, charSequence, duration, 48, 0.0f, 64.0f, TOAST_WARNING);
+    }
+}
