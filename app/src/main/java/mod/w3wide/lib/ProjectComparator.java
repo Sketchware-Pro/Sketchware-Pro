37ebfc82aa8fdcb95c9f2ca0e0d2a0f12218f90d
Hasrat <hasratali2880@gmail.com>
2021-09-19 22:51:06 +0530

    feat: add ProjectComparator for Library Activity
    used in com.besome.sketch.editor.manage.library.*;

---
+++ app/src/main/java/mod/w3wide/lib/ProjectComparator.java
@@ -0,0 +1,15 @@
+package mod.w3wide.lib;
+
+import java.util.Comparator;
+import java.util.HashMap;
+
+import a.a.a.yB;
+
+public class ProjectComparator implements Comparator<HashMap<String, Object>> {
+
+    public ProjectComparator() {}
+
+    public int compare(HashMap<String, Object> map1, HashMap<String, Object> map2) {
+        return yB.c(map1, "sc_id").compareTo(yB.c(map2, "sc_id")) * -1;
+    }
+}
