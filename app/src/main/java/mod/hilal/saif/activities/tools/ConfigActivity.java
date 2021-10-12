f83adbffc4e92a1c8cf2dfffd1441ca32cc2199d
Pranav <75154889+PranavPurwar@users.noreply.github.com>
2021-10-12 10:19:45 +0530

    feat: add scrollview to mod settings activity

--- app/src/main/java/mod/hilal/saif/activities/tools/ConfigActivity.java
+++ app/src/main/java/mod/hilal/saif/activities/tools/ConfigActivity.java
@@ -11,6 +11,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
@@ -125,7 +126,9 @@ public class ConfigActivity extends Activity {
         root = new LinearLayout(this);
         root.setBackgroundColor(DEFAULT_BACKGROUND_COLOR);
         root.setOrientation(LinearLayout.VERTICAL);
-        setContentView(root);
+        ScrollView scrollView = new ScrollView(this);
+        scrollView.addView(root);
+        setContentView(scrollView);
 
         View toolbar = getLayoutInflater().inflate(Resources.layout.toolbar_improved, root, false);
         root.addView(toolbar);
