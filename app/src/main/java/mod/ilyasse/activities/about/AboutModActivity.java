bc7dedced11d0a7028b3d6633a2865883db3f8fd
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-11-12 07:37:51 +0100

    feat: Apply ripple effect on views, make left line rounded
     - Adds ripple effects show when touching the log background and the arrow icon
     - Makes the left line rounded

--- app/src/main/java/mod/ilyasse/activities/about/AboutModActivity.java
+++ app/src/main/java/mod/ilyasse/activities/about/AboutModActivity.java
@@ -618,6 +618,8 @@ public class AboutModActivity extends AppCompatActivity {
             view_additional_info.setVisibility(showingAdditionalInfo ? View.VISIBLE : View.GONE);
             arrow.setRotation(showingAdditionalInfo ? 0 : 180);
 
+            rippleRound(log_background, "#ffffff", "#848484", 0);
+            rippleRound(arrow, "#ffffff", "#848484", 90);
             arrow.setOnClickListener(v -> log_background.performClick());
 
             log_background.setOnClickListener(v -> {
@@ -636,6 +638,8 @@ public class AboutModActivity extends AppCompatActivity {
                 // RecyclerView$a<VH extends RecyclerView.v>.c(int)
                 c(position);
             });
+
+            advancedCorners(leftLine, "#008dcd");
         }
 
         // RecyclerView$Adapter<T extends RecyclerView.ViewHolder>.getItemCount() got obfuscated
