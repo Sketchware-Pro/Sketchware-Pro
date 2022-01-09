2ba0f41dacf13b870f411fa5ded9cbf6fb4799f1
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-01-09 15:32:18 +0000

    refactor: Rename fields, make methods private

--- app/src/main/java/com/besome/sketch/editor/view/ColorGroupItem.java
+++ app/src/main/java/com/besome/sketch/editor/view/ColorGroupItem.java
@@ -1,6 +1,5 @@
 package com.besome.sketch.editor.view;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -9,21 +8,16 @@ import android.widget.TextView;
 import a.a.a.wB;
 
 public class ColorGroupItem extends RelativeLayout {
-    public Context a;
-    public TextView b;
-    public ImageView c;
+
+    public Context context;
+    public TextView tvColorName;
+    public ImageView imgSelector;
 
     public ColorGroupItem(Context context) {
         super(context);
-        a(context);
-    }
-
-    @SuppressLint("ResourceType")
-    public final void a(Context context) {
-        this.a = context;
         wB.a(context, this, 2131427374);
-        this.b = (TextView) findViewById(2131231916);
-        this.c = (ImageView) findViewById(2131231182);
+        tvColorName = (TextView) findViewById(2131231916);
+        imgSelector = (ImageView) findViewById(2131231182);
         setPadding(0, 0, 4, 0);
     }
 }
