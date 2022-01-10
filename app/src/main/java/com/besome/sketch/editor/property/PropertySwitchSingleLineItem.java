9b774d9b4c15ca4f3d6828ea901306d397239b85
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-01-10 02:39:57 +0000

    refactor, chore: Rename fields, make methods private

--- app/src/main/java/com/besome/sketch/editor/property/PropertySwitchSingleLineItem.java
+++ app/src/main/java/com/besome/sketch/editor/property/PropertySwitchSingleLineItem.java
@@ -16,28 +16,28 @@ import a.a.a.xB;
 
 public class PropertySwitchSingleLineItem extends LinearLayout implements View.OnClickListener {
 
-    public String a = "";
-    public boolean b = false;
-    public TextView c;
-    public Switch d;
-    public ImageView e;
-    public int f;
-    public View g;
-    public View h;
-    public Kw i;
+    public String key = "";
+    public boolean value = false;
+    public TextView tvName;
+    public Switch switchValue;
+    public ImageView imgLeftIcon;
+    public int icon;
+    public View propertyItem;
+    public View propertyMenuItem;
+    public Kw valueChangeListener;
 
     public PropertySwitchSingleLineItem(Context context, boolean z) {
         super(context);
-        a(context, z);
+        initialize(context, z);
     }
 
-    public final void a(Context context, boolean z) {
+    private void initialize(Context context, boolean z) {
         wB.a(context, this, Resources.layout.property_switch_item_singleline);
-        c = findViewById(Resources.id.tv_name);
-        d = findViewById(Resources.id.switch_value);
-        e = findViewById(Resources.id.img_left_icon);
-        g = findViewById(Resources.id.property_item);
-        h = findViewById(Resources.id.property_menu_item);
+        tvName = findViewById(Resources.id.tv_name);
+        switchValue = findViewById(Resources.id.switch_value);
+        imgLeftIcon = findViewById(Resources.id.img_left_icon);
+        propertyItem = findViewById(Resources.id.property_item);
+        propertyMenuItem = findViewById(Resources.id.property_menu_item);
         if (z) {
             setOnClickListener(this);
             setSoundEffectsEnabled(true);
@@ -45,77 +45,67 @@ public class PropertySwitchSingleLineItem extends LinearLayout implements View.O
     }
 
     public String getKey() {
-        return a;
+        return key;
     }
 
-    public void setKey(String str) {
+    public void setKey(String key) {
         mB.a(this);
-        a = str;
-        int identifier = getResources().getIdentifier(str, "string", getContext().getPackageName());
+        this.key = key;
+        int identifier = getResources().getIdentifier(key, "string", getContext().getPackageName());
         if (identifier > 0) {
-            c.setText(xB.b().a(getResources(), identifier));
-            char type = 65535;
-            switch (a) {
+            tvName.setText(xB.b().a(getResources(), identifier));
+            switch (this.key) {
                 case "property_checked":
-                    type = 3;
+                    icon = Resources.drawable.ok_48;
                     break;
 
                 case "property_single_line":
-                    type = 0;
+                    icon = Resources.drawable.horizontal_line_48;
                     break;
 
                 case "property_enabled":
-                    type = 1;
+                    icon = Resources.drawable.light_on_48;
                     break;
 
                 case "property_clickable":
-                    type = 2;
+                    icon = Resources.drawable.natural_user_interface2_48;
             }
-            if (type == 0) {
-                f = Resources.drawable.horizontal_line_48;
-            } else if (type == 1) {
-                f = Resources.drawable.light_on_48;
-            } else if (type == 2) {
-                f = Resources.drawable.natural_user_interface2_48;
-            } else if (type == 3) {
-                f = Resources.drawable.ok_48;
-            }
-            if (h.getVisibility() == VISIBLE) {
-                ((ImageView) findViewById(Resources.id.img_icon)).setImageResource(f);
+            if (propertyMenuItem.getVisibility() == VISIBLE) {
+                ((ImageView) findViewById(Resources.id.img_icon)).setImageResource(icon);
                 ((TextView) findViewById(Resources.id.tv_title)).setText(xB.b().a(getContext(), identifier));
                 return;
             }
-            e.setImageResource(f);
+            imgLeftIcon.setImageResource(icon);
         }
     }
 
     public boolean getValue() {
-        return b;
+        return value;
     }
 
     public void setValue(boolean value) {
-        b = value;
-        d.setChecked(value);
+        this.value = value;
+        switchValue.setChecked(value);
     }
 
     public void onClick(View view) {
-        setValue(!b);
-        if (i != null) {
-            i.a(a, b);
+        setValue(!value);
+        if (valueChangeListener != null) {
+            valueChangeListener.a(key, value);
         }
     }
 
-    public void setOnPropertyValueChangeListener(Kw kw) {
-        i = kw;
+    public void setOnPropertyValueChangeListener(Kw onPropertyValueChangeListener) {
+        valueChangeListener = onPropertyValueChangeListener;
     }
 
     public void setOrientationItem(int orientationItem) {
         if (orientationItem == 0) {
-            g.setVisibility(GONE);
-            h.setVisibility(VISIBLE);
+            propertyItem.setVisibility(GONE);
+            propertyMenuItem.setVisibility(VISIBLE);
         } else {
-            g.setVisibility(VISIBLE);
-            h.setVisibility(GONE);
+            propertyItem.setVisibility(VISIBLE);
+            propertyMenuItem.setVisibility(GONE);
         }
     }
 }
