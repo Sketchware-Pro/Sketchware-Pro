e63e4731581e0abf54f5e2c2a82a77cb68222d1a
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-01-10 15:09:40 +0000

    refactor: Rename fields and some local variables

--- app/src/main/java/com/besome/sketch/editor/property/PropertyColorItem.java
+++ app/src/main/java/com/besome/sketch/editor/property/PropertyColorItem.java
@@ -19,115 +19,116 @@ import a.a.a.xB;
 
 public class PropertyColorItem extends RelativeLayout implements View.OnClickListener {
 
-    public Context a;
-    public String b;
-    public int c;
-    public TextView d;
-    public TextView e;
-    public View f;
-    public ImageView g;
-    public int h;
-    public View i;
-    public View j;
-    public Kw k;
+    public Context context;
+    public String key;
+    public int value;
+    public TextView tvName;
+    public TextView tvValue;
+    public View viewColor;
+    public ImageView imgLeftIcon;
+    public int icon;
+    public View propertyItem;
+    public View propertyMenuItem;
+    public Kw valueChangeListener;
 
     public PropertyColorItem(Context context, boolean z) {
         super(context);
-        a(context, z);
+        initialize(context, z);
     }
 
     public String getKey() {
-        return b;
+        return key;
     }
 
-    public void setKey(String str) {
-        b = str;
-        int identifier = getResources().getIdentifier(str, "string", getContext().getPackageName());
+    public void setKey(String key) {
+        this.key = key;
+        int identifier = getResources().getIdentifier(key, "string", getContext().getPackageName());
         if (identifier > 0) {
-            d.setText(xB.b().a(getResources(), identifier));
-            h = Resources.drawable.color_palette_48;
-            if (j.getVisibility() == VISIBLE) {
-                ((ImageView) findViewById(Resources.id.img_icon)).setImageResource(h);
+            tvName.setText(xB.b().a(getResources(), identifier));
+            icon = Resources.drawable.color_palette_48;
+            if (propertyMenuItem.getVisibility() == VISIBLE) {
+                ((ImageView) findViewById(Resources.id.img_icon)).setImageResource(icon);
                 ((TextView) findViewById(Resources.id.tv_title)).setText(xB.b().a(getContext(), identifier));
                 return;
             }
-            g.setImageResource(h);
+            imgLeftIcon.setImageResource(icon);
         }
     }
 
     public int getValue() {
-        return c;
+        return value;
     }
 
     public void setValue(int value) {
-        c = value;
+        this.value = value;
         if (value == 0) {
-            e.setText("TRANSPARENT");
-            f.setBackgroundColor(value);
+            tvValue.setText("TRANSPARENT");
+            viewColor.setBackgroundColor(value);
         } else if (value == 0xffffff) {
-            e.setText("NONE");
-            f.setBackgroundColor(value);
+            tvValue.setText("NONE");
+            viewColor.setBackgroundColor(value);
         } else {
-            e.setText(String.format("#%08X", value));
-            f.setBackgroundColor(value);
+            tvValue.setText(String.format("#%08X", value));
+            viewColor.setBackgroundColor(value);
         }
     }
 
+    @Override
     public void onClick(View view) {
         if (!mB.a()) {
-            a();
+            showColorPicker();
         }
     }
 
-    public void setOnPropertyValueChangeListener(Kw kw) {
-        k = kw;
+    public void setOnPropertyValueChangeListener(Kw onPropertyValueChangeListener) {
+        valueChangeListener = onPropertyValueChangeListener;
     }
 
     public void setOrientationItem(int orientationItem) {
         if (orientationItem == 0) {
-            i.setVisibility(GONE);
-            j.setVisibility(VISIBLE);
+            propertyItem.setVisibility(GONE);
+            propertyMenuItem.setVisibility(VISIBLE);
         } else {
-            i.setVisibility(VISIBLE);
-            j.setVisibility(GONE);
+            propertyItem.setVisibility(VISIBLE);
+            propertyMenuItem.setVisibility(GONE);
         }
     }
 
-    public final void a(Context context, boolean z) {
-        a = context;
+    private void initialize(Context context, boolean z) {
+        this.context = context;
         wB.a(context, this, Resources.layout.property_color_item);
-        d = findViewById(Resources.id.tv_name);
-        e = findViewById(Resources.id.tv_value);
-        f = findViewById(Resources.id.view_color);
-        g = findViewById(Resources.id.img_left_icon);
-        i = findViewById(Resources.id.property_item);
-        j = findViewById(Resources.id.property_menu_item);
+        tvName = findViewById(Resources.id.tv_name);
+        tvValue = findViewById(Resources.id.tv_value);
+        viewColor = findViewById(Resources.id.view_color);
+        imgLeftIcon = findViewById(Resources.id.img_left_icon);
+        propertyItem = findViewById(Resources.id.property_item);
+        propertyMenuItem = findViewById(Resources.id.property_menu_item);
         if (z) {
             setOnClickListener(this);
             setSoundEffectsEnabled(true);
         }
     }
 
-    public final void a() {
+    private void showColorPicker() {
         boolean z;
         boolean z2;
-        View view = wB.a(a, Resources.layout.color_picker);
-        view.setAnimation(AnimationUtils.loadAnimation(a, Resources.anim.abc_fade_in));
-        if (b.equals("property_background_color")) {
+        View view = wB.a(context, Resources.layout.color_picker);
+        view.setAnimation(AnimationUtils.loadAnimation(context, Resources.anim.abc_fade_in));
+        if (key.equals("property_background_color")) {
             z2 = true;
             z = true;
         } else {
             z2 = false;
             z = false;
         }
-        Zx zx = new Zx(view, (Activity) a, c, z2, z);
-        zx.a(i -> {
+        Zx colorPicker = new Zx(view, (Activity) context, value, z2, z);
+        colorPicker.a(i -> {
             setValue(i);
-            if (k != null) {
-                k.a(b, c);
+            if (valueChangeListener != null) {
+                valueChangeListener.a(key, value);
             }
         });
-        zx.setAnimationStyle(Resources.anim.abc_fade_in);
-        zx.showAtLocation(view, Gravity.CENTER, 0, 0);
+        colorPicker.setAnimationStyle(Resources.anim.abc_fade_in);
+        colorPicker.showAtLocation(view, Gravity.CENTER, 0, 0);
     }
 }
