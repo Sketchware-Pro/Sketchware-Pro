7851ebde533a5938bec20318c01b875324407c7d
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-01-17 05:10:33 +0000

    feat: Add remove list/variable click listener

---
+++ app/src/main/java/mod/w3wide/variable/DialogRemoveVariable.java
@@ -0,0 +1,52 @@
+package mod.w3wide.variable;
+
+import static com.besome.sketch.SketchApplication.getContext;
+
+import a.a.a.eC;
+import a.a.a.jC;
+import a.a.a.xB;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.Toast;
+import com.besome.sketch.editor.LogicEditorActivity;
+import mod.w3wide.dialog.SketchDialog;
+
+public class DialogRemoveVariable implements OnClickListener {
+	public final ViewGroup viewGroup;
+	public final SketchDialog dialog;
+	public final LogicEditorActivity logicEditor;
+	
+	public DialogRemoveVariable(LogicEditorActivity logicEditorActivity, ViewGroup viewGroup, SketchDialog dialog) {
+		logicEditor = logicEditorActivity;
+		this.viewGroup = viewGroup;
+		this.dialog = dialog;
+	}
+
+	public void onClick(View view) {
+		int childCount = viewGroup.getChildCount();
+		String eventName = logicEditor.C + "_" + logicEditor.D;
+		String javaName = logicEditor.M.getJavaName();
+
+		for (int i = 0; i < childCount; i++) {
+			if (viewGroup.getChildAt(i) instanceof CheckBox) {
+				CheckBox checkBox = (CheckBox) viewGroup.getChildAt(i);
+				String variable = checkBox.getText().toString();
+				if (checkBox.isChecked()) {
+					if (!logicEditor.o.c(variable)) {
+						if (!jC.a(logicEditor.B).c(javaName, variable, eventName)) {
+							logicEditor.m(variable);
+							dialog.dismiss();
+						}
+					} else {
+						Toast.makeText(getContext(), xB.b().a(getContext(), 2131625493), Toast.LENGTH_LONG).show();
+						return;
+					}
+				}
+			}
+		}
+		dialog.dismiss();
+	}
+}
