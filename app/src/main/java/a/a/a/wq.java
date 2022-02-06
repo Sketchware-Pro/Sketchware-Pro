3a116b3e3a261894fb6dcf50fb8378c7aaa8f759
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-02-06 05:28:39 +0000

    refactor: Inline `TypeWidgetSelector#a(int)` method into `wq#b(int)`

--- app/src/main/java/a/a/a/wq.java
+++ app/src/main/java/a/a/a/wq.java
@@ -5,8 +5,6 @@ import android.os.Environment;
 
 import java.io.File;
 
-import mod.agus.jcoderz.lib.TypeWidgetSelector;
-
 public class wq {
 
     public static final String A = "sketchware" + File.separator + "localization" + File.separator + "strings_provided.xml";
@@ -72,7 +70,7 @@ public class wq {
 
             case 4:
                 return "SL-04";
-                
+
             default:
                 return "";
         }
@@ -106,74 +104,108 @@ public class wq {
     }
 
     public static String b(int type) {
-        String selectorType;
         switch (type) {
             case 0:
-                selectorType = "linear";
-                break;
-
+                return "linear";
             case 1:
-            case 16:
-            default:
-                selectorType = TypeWidgetSelector.a(type);
-                break;
-
+                return "relativelayout";
             case 2:
-                selectorType = "hscroll";
-                break;
-
+                return "hscroll";
             case 3:
-                selectorType = "button";
-                break;
-
+                return "button";
             case 4:
-                selectorType = "textview";
-                break;
-
+                return "textview";
             case 5:
-                selectorType = "edittext";
-                break;
+                return "edittext";
             case 6:
-                selectorType = "imageview";
-                break;
+                return "imageview";
             case 7:
-                selectorType = "webview";
-                break;
+                return "webview";
             case 8:
-                selectorType = "progressbar";
-                break;
+                return "progressbar";
             case 9:
-                selectorType = "listview";
-                break;
+                return "listview";
             case 10:
-                selectorType = "spinner";
-                break;
+                return "spinner";
             case 11:
-                selectorType = "checkbox";
-                break;
+                return "checkbox";
             case 12:
-                selectorType = "vscroll";
-                break;
+                return "vscroll";
             case 13:
-                selectorType = "switch";
-                break;
+                return "switch";
             case 14:
-                selectorType = "seekbar";
-                break;
+                return "seekbar";
             case 15:
-                selectorType = "calendarview";
-                break;
+                return "calendarview";
+            case 16:
+                return "fab";
             case 17:
-                selectorType = "adview";
-                break;
+                return "adview";
             case 18:
-                selectorType = "mapview";
-                break;
+                return "mapview";
             case 19:
-                selectorType = "radiobutton";
+                return "radiobutton";
+            case 20:
+                return "ratingbar";
+            case 21:
+                return "videoview";
+            case 22:
+                return "searchview";
+            case 23:
+                return "autocomplete";
+            case 24:
+                return "multiautocomplete";
+            case 25:
+                return "gridview";
+            case 26:
+                return "analogclock";
+            case 27:
+                return "datepicker";
+            case 28:
+                return "timepicker";
+            case 29:
+                return "digitalclock";
+            case 30:
+                return "tablayout";
+            case 31:
+                return "viewpager";
+            case 32:
+                return "bottomnavigation";
+            case 33:
+                return "badgeview";
+            case 34:
+                return "patternlockview";
+            case 35:
+                return "wavesidebar";
+            case 36:
+                return "cardview";
+            case 37:
+                return "collapsingtoolbar";
+            case 38:
+                return "textinputlayout";
+            case 39:
+                return "swiperefreshlayout";
+            case 40:
+                return "radiogroup";
+            case 41:
+                return "materialbutton";
+            case 42:
+                return "signinbutton";
+            case 43:
+                return "circleimageview";
+            case 44:
+                return "lottie";
+            case 45:
+                return "youtube";
+            case 46:
+                return "otpview";
+            case 47:
+                return "codeview";
+            case 48:
+                return "recyclerview";
+            default:
+                return "widget";
         }
-
-        return selectorType;
     }
 
     public static String b(String sc_id) {
