3c2685706aaa1f04b62305be8576c177d64dfb44
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2022-02-23 23:10:29 +0100

    refactor: Inline `ConstVarWidget#a` in `Lx#a`

--- app/src/main/java/mod/agus/jcoderz/handle/component/ConstVarWidget.java
+++ app/src/main/java/mod/agus/jcoderz/handle/component/ConstVarWidget.java
@@ -1,22 +1,6 @@
 package mod.agus.jcoderz.handle.component;
 
-import a.a.a.Lx;
-
 public class ConstVarWidget {
-
-    /**
-     * @return Field declaration for FirebaseCloudMessage (that's just <code>""</code>)
-     * or a component with an empty <code>componentNameId</code>
-     */
-    public static String a(String fieldDeclaration, String componentNameId, String componentName) {
-        if ("FirebaseCloudMessage".equals(componentNameId)) {
-            return "";
-        } else if ("FragmentStatePagerAdapter".equals(componentNameId)) {
-            return fieldDeclaration + " " + Lx.a(componentName + "Fragment") + " " + componentName + ";";
-        } else {
-            return fieldDeclaration + " " + componentNameId + " " + componentName + ";";
-        }
-    }
 
     /**
      * @return Field type for the Videos component, or the parameter if it's not <code>"Videos"</code>
