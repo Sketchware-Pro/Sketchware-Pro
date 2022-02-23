fafa9633d0ac25e1514ba2671c4caa7672af08f4
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2022-02-23 17:05:34 +0100

    fix: Rename fields back to original names, remove unused field
     - Renames 2 fields back to their names `La/a/a/Zx;` knows about, as that smali-class accesses those fields.

--- app/src/main/java/com/besome/sketch/editor/view/ColorGroupItem.java
+++ app/src/main/java/com/besome/sketch/editor/view/ColorGroupItem.java
@@ -11,15 +11,14 @@ import a.a.a.wB;
 
 public class ColorGroupItem extends RelativeLayout {
 
-    public Context context;
-    public TextView tvColorName;
-    public ImageView imgSelector;
+    public TextView b;
+    public ImageView c;
 
     public ColorGroupItem(Context context) {
         super(context);
         wB.a(context, this, Resources.layout.color_picker_grid_item);
-        tvColorName = findViewById(Resources.id.tv_color_name);
-        imgSelector = findViewById(Resources.id.img_selector);
+        b = findViewById(Resources.id.tv_color_name);
+        c = findViewById(Resources.id.img_selector);
         setPadding(0, 0, 4, 0);
     }
 }
