78bb7c3cc21187b25a0d6c8389eafee6af18af3f
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2022-02-27 11:18:08 +0100

    chore, refactor: Clean up file, rename fields to senseful names
     - Removes one & renames some fields to senseful names
     - Replaces resource ID magic numbers with constants, adds a new layout ID constant

--- app/src/main/java/com/besome/sketch/projects/MyProjectButton.java
+++ app/src/main/java/com/besome/sketch/projects/MyProjectButton.java
@@ -1,32 +1,35 @@
 package com.besome.sketch.projects;
 
 import android.content.Context;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.sketchware.remod.Resources;
+
 import a.a.a.wB;
 
 public class MyProjectButton extends LinearLayout {
-    public Context a;
+
+    /* Referenced by a.a.a.IC */
     public int b;
-    public LinearLayout c;
-    public ImageView d;
-    public TextView e;
+    public ImageView icon;
+    public TextView name;
 
     public MyProjectButton(Context context) {
         super(context);
-        a(context);
+        initialize(context);
     }
 
-    public final void a(Context context) {
-        this.a = context;
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(0, -2);
-        layoutParams.weight = 1.0f;
-        setLayoutParams(layoutParams);
-        wB.a(context, this, 2131427580);
-        this.c = findViewById(2131231120);
-        this.d = findViewById(2131231090);
-        this.e = findViewById(2131231561);
+    private void initialize(Context context) {
+        setLayoutParams(new LinearLayout.LayoutParams(
+                0,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                1.0f));
+
+        wB.a(context, this, Resources.layout.myproject_button);
+        icon = findViewById(Resources.id.icon);
+        name = findViewById(Resources.id.name);
     }
 }
