86f6fb49e8279defc1f7c933f5113e289b9ce81a
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2022-03-13 12:09:17 +0100

    chore: Clean up DebugActivity.java in assets

--- app/src/main/assets/debug/DebugActivity.java
+++ app/src/main/assets/debug/DebugActivity.java
@@ -6,56 +6,52 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-
 import android.widget.TextView;
 
-import java.io.InputStream;
-
 public class DebugActivity extends Activity {
-    
+
     private String[] exceptionTypes = {
-        "StringIndexOutOfBoundsException",
-        "IndexOutOfBoundsException",
-        "ArithmeticException",
-        "NumberFormatException",
-        "ActivityNotFoundException"
+            "StringIndexOutOfBoundsException",
+            "IndexOutOfBoundsException",
+            "ArithmeticException",
+            "NumberFormatException",
+            "ActivityNotFoundException"
     };
-    
+
     private String[] exceptionMessages = {
-        "Invalid string operation\n",
-        "Invalid list operation\n",
-        "Invalid arithmetical operation\n",
-        "Invalid toNumber block operation\n",
-        "Invalid intent operation"
+            "Invalid string operation\n",
+            "Invalid list operation\n",
+            "Invalid arithmetical operation\n",
+            "Invalid toNumber block operation\n",
+            "Invalid intent operation"
     };
-    
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         Intent intent = getIntent();
         String errorMessage = "";
         String madeErrorMessage = "";
-        
+
         if (intent != null) {
             errorMessage = intent.getStringExtra("error");
-            
+
             String[] split = errorMessage.split("\n");
             //errorMessage = split[0];
             try {
                 for (int j = 0; j < exceptionTypes.length; j++) {
                     if (split[0].contains(exceptionTypes[j])) {
                         madeErrorMessage = exceptionMessages[j];
-                        
+
                         int addIndex = split[0].indexOf(exceptionTypes[j]) + exceptionTypes[j].length();
-                        
+
                         madeErrorMessage += split[0].substring(addIndex, split[0].length());
                         madeErrorMessage += "\n\nDetailed error message:\n" + errorMessage;
                         break;
                     }
                 }
-                
+
                 if (madeErrorMessage.isEmpty()) {
                     madeErrorMessage = errorMessage;
                 }
@@ -63,22 +59,18 @@ public class DebugActivity extends Activity {
                 madeErrorMessage = madeErrorMessage + "\n\nError while getting error: " + Log.getStackTraceString(e);
             }
         }
-        
-        AlertDialog builder = new AlertDialog.Builder(this)
-            .setTitle("An error occurred")
-            .setMessage(madeErrorMessage)
-            .setPositiveButton("End Application", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    finish();
-                }
-            })
-        .create();
-        builder.show();
-        //Note: Now debug info is selectable.
-        TextView messageView=(TextView) builder.findViewById(android.R.id.message);
-        if (messageView != null) {
-            messageView.setTextIsSelectable(true);
-        }
+
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("An error occurred")
+                .setMessage(madeErrorMessage)
+                .setPositiveButton("End Application", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .create();
+        dialog.show();
+        ((TextView) dialog.findViewById(android.R.id.message)).setTextIsSelectable(true);
     }
 }
