285e8f7420a9fb455612bb46ca599063b584753b
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-03-14 13:44:20 +0000

    chore: Remove unnecessary/unused parameters

--- app/src/main/java/mod/hilal/saif/asd/AsdHandler.java
+++ app/src/main/java/mod/hilal/saif/asd/AsdHandler.java
@@ -10,16 +10,14 @@ import mod.SketchwareUtil;
 
 public class AsdHandler implements View.OnClickListener {
 
-    public final boolean b;
     public final AsdOrigin asdOrigin;
     public final LogicEditorActivity logicEditorActivity;
     public final EditText editText;
     public final Ss ss;
 
-    public AsdHandler(LogicEditorActivity logicEditorActivity, EditText editText, boolean z, Ss ss, AsdOrigin asdOrigin) {
+    public AsdHandler(LogicEditorActivity logicEditorActivity, EditText editText, Ss ss, AsdOrigin asdOrigin) {
         this.logicEditorActivity = logicEditorActivity;
         this.editText = editText;
-        b = z;
         this.ss = ss;
         this.asdOrigin = asdOrigin;
     }
@@ -27,17 +25,7 @@ public class AsdHandler implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         String content = editText.getText().toString();
-        if (b) {
-            try {
-                double editableDouble = Double.parseDouble(content);
-                if (Double.isNaN(editableDouble) || Double.isInfinite(editableDouble)) {
-                    content = "";
-                }
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-                content = "";
-            }
-        } else if (content.length() > 0 && content.charAt(0) == '@') {
+        if (content.length() > 0 && content.charAt(0) == '@') {
             content = " " + content;
         }
         logicEditorActivity.a(ss, (Object) content);
