390ed037c90c39c6b81b054b9fcb32ea2b4ac771
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-04-03 10:39:18 +0100

    feat: Improve launcher activity, Use theme style for splash screen

--- app/src/main/java/com/besome/sketch/InitActivity.java
+++ app/src/main/java/com/besome/sketch/InitActivity.java
@@ -1,13 +1,15 @@
 package com.besome.sketch;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
-import com.besome.sketch.lib.base.BaseAppCompatActivity;
+public class InitActivity extends Activity implements Runnable {
 
-public class InitActivity extends BaseAppCompatActivity {
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
 
-    @Override
+    /*@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (!isTaskRoot()) {
@@ -19,5 +21,25 @@ public class InitActivity extends BaseAppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
         finish();
+    }*/
+
+    @Override
+    protected void onStop() {
+        mHandler.removeCallbacks(this);
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mHandler.postDelayed(this, 2000);
+    }
+
+    @Override
+    public void run() {
+        if (!isFinishing()) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
     }
 }
