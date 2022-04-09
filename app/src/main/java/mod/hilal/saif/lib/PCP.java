a3c2ddf201f5cc4136f72d78f715ba41a1fce406
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-04-09 05:46:55 +0100

    chore: Remove unnecessary boxing

--- app/src/main/java/mod/hilal/saif/lib/PCP.java
+++ app/src/main/java/mod/hilal/saif/lib/PCP.java
@@ -10,29 +10,31 @@ public class PCP implements Zx.b {
 
 
     public final BlocksManager a;
-    public final AlertDialog d;
+    public final AlertDialog dialog;
     public final EditText e;
     public boolean ii = false;
 
     public PCP(BlocksManager blocksManager, EditText editText, AlertDialog alertDialog) {
         this.a = blocksManager;
         this.e = editText;
-        this.d = alertDialog;
+        dialog = alertDialog;
     }
 
     public PCP(EditText editText) {
         this.e = editText;
         this.ii = true;
-        this.d = null;
+        dialog = null;
         this.a = null;
     }
 
     public void a(int i) {
-        if (this.ii) {
-            this.e.setText(String.format("#%08X", Integer.valueOf(i & -1)));
+        if (ii) {
+            e.setText(String.format("#%08X", i));
             return;
         }
-        this.d.show();
-        this.e.setText(String.format("#%08X", Integer.valueOf(i & -1)));
+        if (dialog != null) {
+            dialog.show();
+        }
+        e.setText(String.format("#%08X", i));
     }
 }
