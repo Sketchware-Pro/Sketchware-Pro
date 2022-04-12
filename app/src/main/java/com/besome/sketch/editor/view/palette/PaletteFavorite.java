a2cdd6d1319c4d0e312051086f45b4bffd74dd30
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-04-12 16:40:30 +0100

    feat: Add `PaletteFavorite` class in java

---
+++ app/src/main/java/com/besome/sketch/editor/view/palette/PaletteFavorite.java
@@ -0,0 +1,59 @@
+package com.besome.sketch.editor.view.palette;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+
+import com.besome.sketch.beans.ViewBean;
+import com.besome.sketch.lib.ui.CustomScrollView;
+
+import java.util.ArrayList;
+
+import a.a.a.uy;
+import a.a.a.wB;
+import a.a.a.xB;
+
+public class PaletteFavorite extends LinearLayout {
+    public LinearLayout a;
+    public CustomScrollView b;
+    public CardView c;
+
+    public PaletteFavorite(Context context) {
+        super(context);
+        initialize(context);
+    }
+
+    public PaletteFavorite(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        initialize(context);
+    }
+
+    public View a(String name, ArrayList<ViewBean> viewBeans) {
+        uy favoriteWidget = new uy(getContext(), name, viewBeans);
+        a.addView(favoriteWidget);
+        return favoriteWidget;
+    }
+
+    public void a() {
+        a.removeAllViews();
+    }
+
+    private void initialize(Context context) {
+        wB.a(context, this, 2131427607);
+        a = findViewById(2131232333);
+        b = findViewById(2131231695);
+        ((TextView) findViewById(2131232162)).setText(xB.b().a(getContext(), 2131626360));
+    }
+
+    public void setScrollEnabled(boolean scrollEnabled) {
+        if (scrollEnabled) {
+            b.b();
+        } else {
+            b.a();
+        }
+    }
+}
