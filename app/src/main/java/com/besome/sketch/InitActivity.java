d2ab95c58973d7f6144a6cab959111986392dad7
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-04-22 14:42:47 +0100

    chore: Remove useless comment

--- app/src/main/java/com/besome/sketch/InitActivity.java
+++ app/src/main/java/com/besome/sketch/InitActivity.java
@@ -9,20 +9,6 @@ public class InitActivity extends Activity implements Runnable {
 
     private final Handler mHandler = new Handler(Looper.getMainLooper());
 
-    /*@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (!isTaskRoot()) {
-            finish();
-            return;
-        }
-        //We don't need to do anything here, so let's move on to MainActivity.
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
-        finish();
-    }*/
-
     @Override
     protected void onStop() {
         mHandler.removeCallbacks(this);
