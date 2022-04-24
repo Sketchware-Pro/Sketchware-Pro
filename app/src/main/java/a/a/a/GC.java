7f9a15979b90f83edbe8dc59fbcec30254aeeead
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-04-24 14:52:23 +0100

    fix: Restore mistakenly deleted method

--- app/src/main/java/a/a/a/GC.java
+++ app/src/main/java/a/a/a/GC.java
@@ -181,6 +181,25 @@ public class GC extends DA implements View.OnClickListener {
         startActivityForResult(intent, requestCode);
     }
 
+    public void c(String var1) {
+        int var2 = 0;
+
+        while (true) {
+            if (var2 >= projectsList.size()) {
+                var2 = 0;
+                break;
+            }
+
+            if (yB.c(projectsList.get(var2), "sc_id").equals(var1)) {
+                break;
+            }
+
+            ++var2;
+        }
+
+        toProjectSettingOrRequestPermission(var2);
+    }
+
     public void d() {
         if (getActivity() instanceof MainActivity) {
             ((MainActivity) getActivity()).s();
