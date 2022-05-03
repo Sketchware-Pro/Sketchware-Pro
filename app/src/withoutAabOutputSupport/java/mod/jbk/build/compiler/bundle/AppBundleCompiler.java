d537a3ff616db11dbd1e88cc8e79f22c859bc864
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-05-03 20:50:59 +0200

    chore: Remove dead code, make all fields private

--- app/src/withoutAabOutputSupport/java/mod/jbk/build/compiler/bundle/AppBundleCompiler.java
+++ app/src/withoutAabOutputSupport/java/mod/jbk/build/compiler/bundle/AppBundleCompiler.java
@@ -5,37 +5,13 @@ import android.content.Context;
 import com.besome.sketch.design.DesignActivity.BuildAsyncTask;
 
 import java.io.File;
-import java.io.IOException;
 
 import a.a.a.Dp;
 import a.a.a.yq;
 
 public class AppBundleCompiler {
 
-    public static final String MODULE_ARCHIVE_FILE_NAME = "module-main.zip";
-    public static final String MODULE_ASSETS = "assets";
-    public static final String MODULE_DEX = "dex";
-    public static final String MODULE_LIB = "lib";
-    public static final String MODULE_RES = "res";
-    public static final String MODULE_ROOT = "root";
-    public static final String MODULE_MANIFEST = "manifest";
-
-    public File mainModuleArchive;
-    public File appBundle;
-    public File apkSet;
-
     public AppBundleCompiler(Dp dp, BuildAsyncTask designActivityBuildAsyncTask) {
-        mainModuleArchive = new File(dp.yq.t, MODULE_ARCHIVE_FILE_NAME);
-        appBundle = new File(dp.yq.t, getBundleFilename(dp.yq.d));
-        apkSet = new File(dp.yq.t, getApkSetFilename(dp.yq.d));
-    }
-
-    public static String getApkSetFilename(String sc_id) {
-        return sc_id + ".apks";
-    }
-
-    public static String getBundleFilename(String sc_id) {
-        return sc_id + ".aab";
     }
 
     public static File getDefaultAppBundleOutputFile(Context context, String sc_id) {
@@ -49,10 +25,8 @@ public class AppBundleCompiler {
 
     /**
      * Re-compresses &lt;project name&gt;.apk.res to module-main.zip in the right format.
-     *
-     * @throws IOException Thrown if any I/O exception occurs while creating the archive
      */
-    public void createModuleMainArchive() throws IOException {
+    public void createModuleMainArchive() {
         throw new UnsupportedOperationException("Not built to support exporting AABs!");
     }
 }
