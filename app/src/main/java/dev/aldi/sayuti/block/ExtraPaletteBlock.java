8d94797a21cf884ccb86d6a6e36eafc3ebfe52f6
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-05-19 18:50:47 +0100

    fix: Don't show modifier as variable id

--- app/src/main/java/dev/aldi/sayuti/block/ExtraPaletteBlock.java
+++ app/src/main/java/dev/aldi/sayuti/block/ExtraPaletteBlock.java
@@ -216,8 +216,10 @@ public class ExtraPaletteBlock {
             if (i == 0) logicEditor.a("Custom Variable", 0xff555555);
 
             String[] split = customVariables2.get(i).split(" ");
-            if (split.length > 1) {
+            if (split.length == 2) {
                 logicEditor.a(split[1], "v", split[0], "getVar").setTag(customVariables2.get(i));
+            } else if (split.length > 2) {
+                logicEditor.a(split[2], "v", split[1], "getVar").setTag(customVariables2.get(i));
             } else {
                 SketchwareUtil.toastError("Found invalid data of Custom Variable #" + (i + 1) + ": \"" + customVariables2.get(i) + "\"");
             }
