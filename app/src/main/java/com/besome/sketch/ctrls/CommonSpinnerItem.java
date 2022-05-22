0f469ce49c521b628c42b2ec79a643d64f989630
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-05-22 11:59:17 +0100

    feat: Add `CommonSpinnerItem` and `ViewIdSpinnerItem` in java

---
+++ app/src/main/java/com/besome/sketch/ctrls/CommonSpinnerItem.java
@@ -0,0 +1,43 @@
+package com.besome.sketch.ctrls;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.sketchware.remod.R;
+
+import a.a.a.wB;
+
+public class CommonSpinnerItem extends LinearLayout {
+
+    public ImageView a;
+    public TextView b;
+    public Context c;
+
+    public CommonSpinnerItem(Context context) {
+        super(context);
+        initialize(context);
+    }
+
+    private void initialize(Context context) {
+        c = context;
+        wB.a(context, this, R.layout.common_spinner_item);
+        b = (TextView) findViewById(R.id.tv_spn_name);
+        a = (ImageView) findViewById(R.id.imgv_selected);
+    }
+
+    public void a(String name, boolean isVisible) {
+        b.setText(name);
+        if (isVisible) {
+            a.setVisibility(View.VISIBLE);
+        } else {
+            a.setVisibility(View.GONE);
+        }
+
+    }
+
+    public void setTextSize(int textSize) {
+    }
+}
---
+++ app/src/main/java/com/besome/sketch/ctrls/ViewIdSpinnerItem.java
@@ -0,0 +1,71 @@
+package com.besome.sketch.ctrls;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.sketchware.remod.R;
+
+import a.a.a.wB;
+
+public class ViewIdSpinnerItem extends LinearLayout {
+
+    public ImageView a;
+    public TextView b;
+    public ImageView c;
+    public Context d;
+    public boolean e;
+
+    public ViewIdSpinnerItem(Context context) {
+        super(context);
+        initialize(context);
+    }
+
+    public void a(int icon, String text, boolean selected) {
+        if (selected) {
+            c.setVisibility(View.VISIBLE);
+        } else {
+            c.setVisibility(View.GONE);
+        }
+
+        if (text.charAt(0) == '_') {
+            b.setText(text.substring(1));
+            a(false, 0xffff5555, 0xfff8f820);
+        } else {
+            b.setText(text);
+            a(true, 0xff757575, 0xffffffff);
+        }
+
+        a.setImageResource(icon);
+    }
+
+    private void initialize(Context context) {
+        d = context;
+        wB.a(context, this, R.layout.var_id_spinner_item);
+        a = (ImageView) findViewById(R.id.icon);
+        b = (TextView) findViewById(R.id.name);
+        c = (ImageView) findViewById(R.id.imgv_selected);
+    }
+
+    public void a(boolean notSelected, int color, int var3) {
+        if (notSelected) {
+            if (!e) color = var3;
+            b.setTextColor(color);
+            b.setTypeface((Typeface) null, Typeface.NORMAL);
+        } else {
+            if (!e) color = var3;
+            b.setTextColor(color);
+            b.setTypeface((Typeface) null, Typeface.BOLD_ITALIC);
+        }
+    }
+
+    public void setDropDown(boolean isDropDown) {
+        e = isDropDown;
+    }
+
+    public void setTextSize(int textSize) {
+    }
+}
