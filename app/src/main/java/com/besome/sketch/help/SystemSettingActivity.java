a4cf6c637c0c5eeae5745a3b8b22c94852441acd
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-05-24 11:57:24 +0100

    chore: Replace magic numbers with constants

--- app/src/main/java/com/besome/sketch/help/SystemSettingActivity.java
+++ app/src/main/java/com/besome/sketch/help/SystemSettingActivity.java
@@ -1,5 +1,21 @@
 package com.besome.sketch.help;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.besome.sketch.editor.property.PropertySwitchItem;
+import com.besome.sketch.lib.base.BaseAdsAppCompatActivity;
+import com.sketchware.remod.R;
+
+import java.util.HashMap;
+import java.util.List;
+
 import a.a.a.DB;
 import a.a.a.MA;
 import a.a.a.bB;
@@ -8,27 +24,16 @@ import a.a.a.uz;
 import a.a.a.vz;
 import a.a.a.xB;
 import a.a.a.yB;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-import androidx.appcompat.widget.Toolbar;
-import com.besome.sketch.editor.property.PropertySwitchItem;
-import com.besome.sketch.lib.base.BaseAdsAppCompatActivity;
-import java.util.HashMap;
-import java.util.List;
 
 public class SystemSettingActivity extends BaseAdsAppCompatActivity {
 
-    public Toolbar n;
-    public LinearLayout o;
     public final int p = 0;
     public final int q = 1;
     public final int r = 2;
     public final int s = 3;
     public final int t = 5;
+    public Toolbar n;
+    public LinearLayout o;
     public DB u;
     public Intent v = new Intent();
     public HashMap<String, Object> w;
@@ -60,7 +65,7 @@ public class SystemSettingActivity extends BaseAdsAppCompatActivity {
         if (key == 3 && !super.i.a()) {
             switchItem.setEnabled(false);
             switchItem.setValue(false);
-            switchItem.setTextColor(-3750202);
+            switchItem.setTextColor(0xffc6c6c6);
         }
 
     }
@@ -90,20 +95,31 @@ public class SystemSettingActivity extends BaseAdsAppCompatActivity {
 
     public void onCreate(Bundle var1) {
         super.onCreate(var1);
-        setContentView(2131427742);
-        n = (Toolbar)findViewById(2131231847);
+        setContentView(R.layout.system_settings);
+        n = (Toolbar) findViewById(R.id.toolbar);
         a(n);
         d().d(true);
         d().e(true);
-        findViewById(2131231370).setVisibility(8);
-        d().a(xB.b().a(this, 2131625606));
+        findViewById(R.id.layout_main_logo).setVisibility(View.GONE);
+        d().a(xB.b().a(this, R.string.main_drawer_title_system_settings));
         n.setNavigationOnClickListener(new uz(this));
-        o = (LinearLayout)findViewById(2131230932);
+        o = (LinearLayout) findViewById(R.id.content);
         u = new DB(getApplicationContext(), "P12");
-        a(0, 2131626402, 2131626397, u.a("P12I0", true));
-        a(1, 2131626400, 2131626396, u.a("P12I2", false));
-        a(2, 2131626404, 2131626399, u.a("P12I4", true));
-        a(3, 2131626401, 2131626399, u.a("P12I5", true));
+        a(0, R.string.system_settings_title_setting_vibration,
+                R.string.system_settings_description_setting_vibration,
+                u.a("P12I0", true));
+
+        a(1, R.string.system_settings_title_automatically_save,
+                R.string.system_settings_description_automatically_save
+                , u.a("P12I2", false));
+
+        a(2, R.string.system_settings_title_use_push_notification,
+                R.string.system_settings_description_use_push_notification,
+                u.a("P12I4", true));
+
+        a(3, R.string.system_settings_title_enable_alerts,
+                R.string.system_settings_description_use_push_notification,
+                u.a("P12I5", true));
         if (super.i.a() && super.i.p()) {
             (new SystemSettingActivity.a(this, getApplicationContext())).execute();
         }
@@ -121,11 +137,11 @@ public class SystemSettingActivity extends BaseAdsAppCompatActivity {
 
         boolean var2;
         boolean var4;
-        for(var2 = false; var1 < o.getChildCount(); var2 = var4) {
+        for (var2 = false; var1 < o.getChildCount(); var2 = var4) {
             View var3 = o.getChildAt(var1);
             var4 = var2;
             if (var3 instanceof PropertySwitchItem) {
-                PropertySwitchItem var6 = (PropertySwitchItem)var3;
+                PropertySwitchItem var6 = (PropertySwitchItem) var3;
                 int var5 = var6.getKey();
                 if (var5 != 0) {
                     if (var5 != 1) {
@@ -172,8 +188,8 @@ public class SystemSettingActivity extends BaseAdsAppCompatActivity {
     }
 
     public class a extends MA {
-        public List<Object> c;
         public final SystemSettingActivity d;
+        public List<Object> c;
 
         public a(SystemSettingActivity var1, Context var2) {
             super(var2);
@@ -197,7 +213,7 @@ public class SystemSettingActivity extends BaseAdsAppCompatActivity {
             }
 
             if (var3 & var2) {
-                d.w = c.get(0);
+                d.w = (HashMap<String, Object>) c.get(0);
                 String var4 = xB.b().a(d.getApplicationContext(), 2131626403);
                 String var6 = xB.b().a(d.getApplicationContext(), 2131626398);
                 boolean var5 = "Y".equals(yB.c(d.w, "subscribe"));
@@ -224,9 +240,9 @@ public class SystemSettingActivity extends BaseAdsAppCompatActivity {
     }
 
     public class b extends MA {
+        public final SystemSettingActivity e;
         public String c;
         public String d;
-        public final SystemSettingActivity e;
 
         public b(SystemSettingActivity var1, Context var2, String var3) {
             super(var2);
