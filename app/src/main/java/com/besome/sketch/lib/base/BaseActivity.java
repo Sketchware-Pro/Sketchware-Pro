6e2f020864dac4ecd89dc021f3c5cdfa5b6495bb
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-06-01 10:39:33 +0530

    chore: Remove analytics, deprecated context, make fields private

--- app/src/main/java/com/besome/sketch/lib/base/BaseActivity.java
+++ app/src/main/java/com/besome/sketch/lib/base/BaseActivity.java
@@ -1,13 +1,9 @@
 package com.besome.sketch.lib.base;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.AsyncTask.Status;
 import android.os.Bundle;
 import android.os.Handler;
-
-import com.besome.sketch.SketchApplication;
-import com.google.android.gms.analytics.Tracker;
 
 import java.util.ArrayList;
 
@@ -15,47 +11,41 @@ import a.a.a.MA;
 import a.a.a.ZA;
 
 public class BaseActivity extends Activity {
-    public Tracker a;
-    @Deprecated
-    public Context b;
-    public ZA c;
-    public ArrayList<MA> d;
+
+    private ZA dialog;
+    private ArrayList<MA> taskList;
 
     public void a() {
-        if (c != null && c.isShowing()) {
-            c.dismiss();
+        if (dialog != null && dialog.isShowing()) {
+            dialog.dismiss();
         }
     }
 
     public void a(MA task) {
-        d.add(task);
+        taskList.add(task);
     }
 
     public void b() {
-        if (c != null && !c.isShowing()) {
-            c.show();
-            new Handler().postDelayed(() -> c.d(), 5000L);
+        if (dialog != null && !dialog.isShowing()) {
+            dialog.show();
+            new Handler().postDelayed(() -> dialog.d(), 5000L);
         }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        d = new ArrayList<>();
-        b = getApplicationContext();
-        c = new ZA(this);
-        a = ((SketchApplication) getApplication()).a();
-        a.enableAdvertisingIdCollection(true);
-        a.enableExceptionReporting(true);
+        taskList = new ArrayList<>();
+        dialog = new ZA(this);
     }
 
     @Override
     public void onDestroy() {
-        if (c != null && c.isShowing()) {
-            c.a();
+        if (dialog != null && dialog.isShowing()) {
+            dialog.a();
         }
 
-        for (MA task : d) {
+        for (MA task : taskList) {
             if (task.getStatus() != Status.FINISHED) {
                 task.cancel(false);
             }
@@ -65,8 +55,8 @@ public class BaseActivity extends Activity {
 
     @Override
     public void onPause() {
-        if (c != null && c.isShowing()) {
-            c.b();
+        if (dialog != null && dialog.isShowing()) {
+            dialog.b();
         }
         super.onPause();
     }
@@ -74,8 +64,8 @@ public class BaseActivity extends Activity {
     @Override
     public void onResume() {
         super.onResume();
-        if (c != null && c.isShowing()) {
-            c.c();
+        if (dialog != null && dialog.isShowing()) {
+            dialog.c();
         }
     }
 }
