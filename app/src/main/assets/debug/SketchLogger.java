9991c9a1215490b9e957d4892856b60ed85ede4a
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-06-03 21:19:18 +0200

    fix: Don't allow SketchLogger running more than once

--- app/src/main/assets/debug/SketchLogger.java
+++ app/src/main/assets/debug/SketchLogger.java
@@ -19,55 +19,56 @@ public class SketchLogger {
      * Or, Use "SketchLogger.broadcastLog(String)" directly to Send A Log.
      * Send Context along when logging directly when "startLogging(Context)" hasn't been called yet
      * Use "SketchLogger.stopLogging()" to stop from anywhere
-     * <p>
-     * *DO NOT CALL THIS MORE THAN ONCE WITHOUT STOPPING THE PREVIOUS**
-     * *OTHERWISE IT WILL RESULT IN DUPLICATE LOGS**
      */
 
-    private static Thread loggerThread;
+    private static Thread loggerThread = new Thread() {
+        @Override
+        public void run() {
+            isRunning = true;
+            BufferedReader bufferedReader = null;
+            try {
+                Runtime.getRuntime().exec("logcat -c");
+                Process process = Runtime.getRuntime().exec("logcat");
+                bufferedReader = new BufferedReader(
+                        new InputStreamReader(process.getInputStream()));
+                String logTxt = bufferedReader.readLine();
+                do {
+                    broadcastLog(logTxt);
+                } while (isRunning && ((logTxt = bufferedReader.readLine()) != null));
+                //Thread Stopped, Restarting If Not Stopped Manually
+                if (isRunning) {
+                    broadcastLog("Logger Got Killed. Restarting Complete");
+                    startLogging();
+                } else {
+                    broadcastLog("Logger Stopped");
+                    return;
+                }
+            } catch (Exception e) {
+                broadcastLog(e.toString());
+                //Auto Restart Logger When Crashed
+                startLogging();
+            } finally {
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        /*I don't wanna get Haunted for Keeping Open InputStream*/
+                        broadcastLog("Oh No! Failed to Close BufferedReader " + e.toString());
+                    }
+                }
+            }
+        }
+    };
+
     private static boolean isRunning = false;
     private static String packageName = "Undefined";
 
     public static void startLogging() {
-        loggerThread = new Thread() {
-            @Override
-            public void run() {
-                isRunning = true;
-                BufferedReader bufferedReader = null;
-                try {
-                    Runtime.getRuntime().exec("logcat -c");
-                    Process process = Runtime.getRuntime().exec("logcat");
-                    bufferedReader = new BufferedReader(
-                            new InputStreamReader(process.getInputStream()));
-                    String logTxt = bufferedReader.readLine();
-                    do {
-                        broadcastLog(logTxt);
-                    } while (isRunning && ((logTxt = bufferedReader.readLine()) != null));
-                    //Thread Stopped, Restarting If Not Stopped Manually
-                    if (isRunning) {
-                        broadcastLog("Logger Got Killed. Restarting Complete");
-                        startLogging();
-                    } else {
-                        broadcastLog("Logger Stopped");
-                        return;
-                    }
-                } catch (Exception e) {
-                    broadcastLog(e.toString());
-                    //Auto Restart Logger When Crashed
-                    startLogging();
-                } finally {
-                    if (bufferedReader != null) {
-                        try {
-                            bufferedReader.close();
-                        } catch (IOException e) {
-                            /*I don't wanna get Haunted for Keeping Open InputStream*/
-                            broadcastLog("Oh No! Failed to Close BufferedReader " + e.toString());
-                        }
-                    }
-                }
-            }
-        };
-        loggerThread.start();
+        if (!isRunning) {
+            loggerThread.start();
+        } else {
+            throw new IllegalStateException("Logger already running");
+        }
     }
 
     public static void broadcastLog(String log) {
@@ -79,10 +80,11 @@ public class SketchLogger {
     }
 
     public static void stopLogging() {
-        if (loggerThread != null) {
+        if (isRunning) {
             isRunning = false;
             Log.i("stopLogging()", "Stopping Logger By User Request");
-            loggerThread = null;
+        } else {
+            throw new IllegalStateException("Logger not running");
         }
     }
 }
