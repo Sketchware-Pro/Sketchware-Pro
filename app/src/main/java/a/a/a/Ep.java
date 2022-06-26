3e05c55e7bb9509bb5852d57cbe1f5cf142d0572
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-06-26 14:34:48 +0530

    feat: Add `Ep` and `ZA` class in java

---
+++ app/src/main/java/a/a/a/Ep.java
@@ -0,0 +1,57 @@
+package a.a.a;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.besome.sketch.tools.QuizBoard;
+import com.sketchware.remod.R;
+
+public class Ep extends Dialog {
+
+    private final TextView tvProgress;
+    private final QuizBoard quizBoard;
+    private boolean cancelOnBackPressed;
+
+    public Ep(Context context) {
+        super(context, R.style.progress);
+        requestWindowFeature(1);
+        setContentView(R.layout.build_progress_msg_box);
+        LinearLayout quizLayout = (LinearLayout) findViewById(R.id.layout_quiz);
+        quizBoard = new QuizBoard(getContext());
+        quizLayout.addView(quizBoard);
+        ((LottieAnimationView) findViewById(R.id.animation_view)).setScale(2.0F);
+        setTitle(xB.b().a(context, R.string.common_message_progress));
+        tvProgress = (TextView) findViewById(R.id.tv_progress);
+        tvProgress.setText(xB.b().a(context, R.id.common_message_loading));
+        super.setCanceledOnTouchOutside(false);
+        super.setCancelable(true);
+    }
+
+    public void a(String text) {
+        tvProgress.setText(text);
+    }
+
+    public void a(boolean cancellable) {
+        cancelOnBackPressed = cancellable;
+    }
+
+    public boolean a() {
+        return cancelOnBackPressed;
+    }
+
+    @Override
+    public void dismiss() {
+        quizBoard.a();
+        super.dismiss();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!cancelOnBackPressed) {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/a/a/a/ZA.java
@@ -0,0 +1,48 @@
+package a.a.a;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.sketchware.remod.R;
+
+public class ZA extends Dialog {
+
+    private final LottieAnimationView animationView;
+
+    public ZA(Context context) {
+        super(context, R.style.progress);
+        setContentView(R.layout.progress);
+        animationView = (LottieAnimationView) findViewById(R.id.anim_sketchware);
+        TextView tvProgress = (TextView) findViewById(R.id.tv_progress);
+        tvProgress.setText(xB.b().a(context, R.string.common_message_loading));
+        super.setCancelable(false);
+    }
+
+    public void a() {
+        if (animationView != null & animationView.h()) {
+            animationView.e();
+        }
+    }
+
+    public void b() {
+        if (animationView != null & animationView.h()) {
+            animationView.i();
+        }
+    }
+
+    public void c() {
+        if (animationView != null & !animationView.h()) {
+            animationView.l();
+        }
+    }
+
+    public void d() {
+        super.setCancelable(true);
+    }
+
+    public void show() {
+        super.show();
+    }
+}
