4b582783e7437437dddc0e2b3c94fde806eecbfc
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-06-28 21:25:33 +0200

    fix: Adjust a.a.a.sB to be compilable
    Removes outdated references, AdMob API usage, and cleans the file up a bit.

--- app/src/main/java/a/a/a/sB.java
+++ app/src/main/java/a/a/a/sB.java
@@ -4,16 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 
-import com.besome.sketch.InitActivity;
-import com.google.ads.consent.ConsentInformation;
+import com.besome.sketch.MainActivity;
+import com.sketchware.remod.R;
 
 import java.io.File;
 import java.util.HashMap;
-import java.util.Iterator;
 
-/* loaded from: classes.dex */
 public class sB {
-    public static void a(Context context, boolean z) {
+    public static void a(Context context, boolean resetOnlySettings) {
         new DB(context, "P2").a();
         new DB(context, "P3").a();
         new DB(context, "P1").a();
@@ -22,32 +20,31 @@ public class sB {
         new DB(context, "P25").a();
         new DB(context, "P26").a();
         new DB(context, "P16").a("P16I0");
-        ConsentInformation a2 = ConsentInformation.a(context);
-        if (a2 != null) {
-            a2.i();
-        }
-        String str = File.separator;
+
         oB oBVar = new oB();
         oBVar.b(wq.r());
         oBVar.b(context.getFilesDir() + File.separator + "libs");
-        if (!z) {
+
+        if (!resetOnlySettings) {
             oBVar.b(wq.q());
             oBVar.b(wq.s());
-            Iterator<HashMap<String, Object>> it = lC.a().iterator();
-            while (it.hasNext()) {
-                String c = yB.c(it.next(), "sc_id");
-                new DB(context, "D01_" + c).a();
-                new DB(context, "D02_" + c).a();
-                new DB(context, "D03_" + c).a();
-                new DB(context, "D04_" + c).a();
-                new DB(context, "D05_" + c).a();
+
+            for (HashMap<String, Object> project : lC.a()) {
+                String sc_id = yB.c(project, "sc_id");
+                new DB(context, "D01_" + sc_id).a();
+                new DB(context, "D02_" + sc_id).a();
+                new DB(context, "D03_" + sc_id).a();
+                new DB(context, "D04_" + sc_id).a();
+                new DB(context, "D05_" + sc_id).a();
             }
             new DB(context, "P15").a();
         }
-        bB.a(context, xB.b().a(context, 2131625762), 0).show();
+
+        bB.a(context, xB.b().a(context, R.string.program_information_reset_system_complete_initialize), bB.TOAST_NORMAL).show();
         lC.a(context, true);
-        Intent intent = new Intent(context, InitActivity.class);
-        intent.setFlags(335544320);
+
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         Activity activity = (Activity) context;
         activity.startActivity(intent);
         activity.finishAffinity();
