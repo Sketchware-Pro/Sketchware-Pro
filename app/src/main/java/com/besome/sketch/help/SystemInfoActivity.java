afb72280b9a285343cfcae4f661fbb92fc9d78a3
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-06-30 21:47:51 +0200

    chore: Clean up SystemInfoActivity.java
    Removes useless fields and renames most of its methods. Cleans them up too.

--- app/src/main/java/com/besome/sketch/help/SystemInfoActivity.java
+++ app/src/main/java/com/besome/sketch/help/SystemInfoActivity.java
@@ -22,54 +22,7 @@ import a.a.a.xB;
 
 public class SystemInfoActivity extends BaseAppCompatActivity {
 
-    private final int m = 0;
-    private final int n = 1;
-    private final int o = 2;
-    private final int p = 3;
-    private final int q = 4;
-    private final int r = 5;
     private LinearLayout content;
-
-    private void a(int key, String name, String description) {
-        PropertyTwoLineItem propertyTwoLineItem = new PropertyTwoLineItem(this);
-        propertyTwoLineItem.setKey(key);
-        propertyTwoLineItem.setName(name);
-        propertyTwoLineItem.setDesc(description);
-        content.addView(propertyTwoLineItem);
-    }
-
-    private void l() {
-        String var1 = xB.b().a(getApplicationContext(), R.string.system_information_title_android_version);
-        String var2 = GB.b() + "(" + VERSION.RELEASE + ")";
-        a(n, var1, var2);
-    }
-
-    private void m() {
-        String var1 = xB.b().a(getApplicationContext(), R.string.system_information_title_android_version);
-        String var2 = "API - " + VERSION.SDK_INT;
-        a(m, var1, var2);
-    }
-
-    private void n() {
-        PropertyOneLineItem propertyOneLineItem = new PropertyOneLineItem(this);
-        propertyOneLineItem.setKey(r);
-        propertyOneLineItem.setName(xB.b().a(getApplicationContext(), R.string.system_information_developer_options));
-        content.addView(propertyOneLineItem);
-        propertyOneLineItem.setOnClickListener(v -> {
-            if (!mB.a()) {
-                try {
-                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
-                    startActivity(intent);
-                } catch (ActivityNotFoundException ignored) {
-                }
-            }
-        });
-    }
-
-    private void o() {
-        float[] var1 = GB.b(this);
-        a(p, xB.b().a(getApplicationContext(), R.string.system_information_dpi), String.valueOf(var1[0]));
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -86,22 +39,60 @@ public class SystemInfoActivity extends BaseAppCompatActivity {
             if (!mB.a()) onBackPressed();
         });
         content = findViewById(R.id.content);
-        m();
-        l();
-        q();
-        o();
-        p();
-        n();
+        addApiLevelInfo();
+        addAndroidVersionNameInfo();
+        addScreenResolutionInfo();
+        addScreenDpiInfo();
+        addModelNameInfo();
+        addDeveloperOptionsShortcut();
     }
 
-    private void p() {
-        a(q, xB.b().a(getApplicationContext(), R.string.system_information_model_name), Build.MODEL);
+    private void addInfo(int key, String name, String description) {
+        PropertyTwoLineItem propertyTwoLineItem = new PropertyTwoLineItem(this);
+        propertyTwoLineItem.setKey(key);
+        propertyTwoLineItem.setName(name);
+        propertyTwoLineItem.setDesc(description);
+        content.addView(propertyTwoLineItem);
     }
 
-    private void q() {
-        int[] var1 = GB.c(this);
-        String var2 = xB.b().a(getApplicationContext(), R.string.system_information_system_resolution);
-        String var3 = var1[0] + " x " + var1[1];
-        a(o, var2, var3);
+    private void addAndroidVersionNameInfo() {
+        addInfo(1, xB.b().a(getApplicationContext(), R.string.system_information_title_android_version),
+                GB.b() + "(" + VERSION.RELEASE + ")");
+    }
+
+    private void addApiLevelInfo() {
+        addInfo(0, xB.b().a(getApplicationContext(), R.string.system_information_title_android_version),
+                "API - " + VERSION.SDK_INT);
+    }
+
+    private void addDeveloperOptionsShortcut() {
+        PropertyOneLineItem propertyOneLineItem = new PropertyOneLineItem(this);
+        propertyOneLineItem.setKey(5);
+        propertyOneLineItem.setName(xB.b().a(getApplicationContext(), R.string.system_information_developer_options));
+        content.addView(propertyOneLineItem);
+        propertyOneLineItem.setOnClickListener(v -> {
+            if (!mB.a()) {
+                try {
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
+                    startActivity(intent);
+                } catch (ActivityNotFoundException ignored) {
+                }
+            }
+        });
+    }
+
+    private void addScreenDpiInfo() {
+        float[] dpiXY = GB.b(this);
+        addInfo(3, xB.b().a(getApplicationContext(), R.string.system_information_dpi), String.valueOf(dpiXY[0]));
+    }
+
+    private void addModelNameInfo() {
+        addInfo(4, xB.b().a(getApplicationContext(), R.string.system_information_model_name), Build.MODEL);
+    }
+
+    private void addScreenResolutionInfo() {
+        int[] widthHeight = GB.c(this);
+        addInfo(2, xB.b().a(getApplicationContext(), R.string.system_information_system_resolution),
+                widthHeight[0] + " x " + widthHeight[1]);
     }
 }
