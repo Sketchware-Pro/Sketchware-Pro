42fefb0df127b1ba38da3fd4d3d722fd3bb1f376
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-07-31 21:22:41 +0530

    fix, refactor: Make qA compile, format code

--- app/src/main/java/a/a/a/qA.java
+++ app/src/main/java/a/a/a/qA.java
@@ -1,21 +1,16 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package a.a.a;
 
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import com.besome.sketch.SketchApplication;
-import com.besome.sketch.lib.base.BaseActivity;
+
 import com.besome.sketch.lib.base.BaseAppCompatActivity;
 import com.google.android.gms.analytics.Tracker;
 
 public class qA extends Fragment {
+
     public Activity a;
     @Deprecated
     public Context b;
@@ -27,51 +22,31 @@ public class qA extends Fragment {
     }
 
     public void a() {
-        FragmentActivity var1 = this.getActivity();
-        if (var1 instanceof BaseActivity) {
-            ((BaseActivity)var1).a();
-        } else if (var1 instanceof BaseAppCompatActivity) {
-            ((BaseAppCompatActivity)var1).h();
+        if (getActivity() instanceof BaseAppCompatActivity) {
+            ((BaseAppCompatActivity) getActivity()).h();
         }
-
     }
 
     public void a(MA var1) {
-        FragmentActivity var2 = this.getActivity();
-        if (var2 instanceof BaseActivity) {
-            ((BaseActivity)var2).a(var1);
-        } else if (var2 instanceof BaseAppCompatActivity) {
-            ((BaseAppCompatActivity)var2).a(var1);
+        if (getActivity() instanceof BaseAppCompatActivity) {
+            ((BaseAppCompatActivity) getActivity()).a(var1);
         }
-
     }
 
     public void b() {
-        FragmentActivity var1 = this.getActivity();
-        if (var1 instanceof BaseActivity) {
-            ((BaseActivity)var1).b();
-        } else if (var1 instanceof BaseAppCompatActivity) {
-            ((BaseAppCompatActivity)var1).k();
+        if (getActivity() instanceof BaseAppCompatActivity) {
+            ((BaseAppCompatActivity) getActivity()).k();
         }
-
     }
 
-    public void onCreate(Bundle var1) {
-        super.onCreate(var1);
-        this.a = this.getActivity();
-        this.b = this.a.getApplicationContext();
-        this.d = new EA(this.b);
-        this.e = new Zo(this.b);
-        this.c = ((SketchApplication)this.getActivity().getApplication()).a();
-        this.c.enableAdvertisingIdCollection(true);
-        this.c.enableExceptionReporting(true);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        a = getActivity();
+        b = a.getApplicationContext();
+        d = new EA(b);
+        e = new Zo(b);
+        c = new Tracker();
     }
 
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    public void onDetach() {
-        super.onDetach();
-    }
 }
