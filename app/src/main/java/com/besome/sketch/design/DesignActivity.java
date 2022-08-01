2665f11e62b7e865c1c5ba28055cc4a81f2c9947
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-08-01 17:27:38 +0200

    chore: Remove not anymore in use ad-related logic from DesignActivity
    That interface and onActivityResult handling was most likely to support showing an interstitial ad before compiling.

--- app/src/main/java/com/besome/sketch/design/DesignActivity.java
+++ app/src/main/java/com/besome/sketch/design/DesignActivity.java
@@ -71,7 +71,6 @@ import a.a.a.kC;
 import a.a.a.lC;
 import a.a.a.mB;
 import a.a.a.rs;
-import a.a.a.uo;
 import a.a.a.wq;
 import a.a.a.yB;
 import a.a.a.yq;
@@ -104,7 +103,7 @@ import mod.jbk.diagnostic.MissingFileException;
 import mod.jbk.util.LogUtil;
 import mod.khaled.logcat.LogReaderActivity;
 
-public class DesignActivity extends BaseAppCompatActivity implements OnClickListener, uo {
+public class DesignActivity extends BaseAppCompatActivity implements OnClickListener {
 
     private static final int REQUEST_CODE_VIEW_MANAGER = 208;
     private static final int REQUEST_CODE_IMAGE_MANAGER = 209;
@@ -191,13 +190,6 @@ public class DesignActivity extends BaseAppCompatActivity implements OnClickList
     }
 
     @Override
-    public void e(int i) {
-        if (i == 188) {
-            new BuildAsyncTask(getApplicationContext()).execute();
-        }
-    }
-
-    @Override
     public void finish() {
         jC.a();
         cC.a();
@@ -239,10 +231,6 @@ public class DesignActivity extends BaseAppCompatActivity implements OnClickList
         super.onActivityResult(requestCode, resultCode, data);
 
         switch (requestCode) {
-            case 188:
-                new BuildAsyncTask(getApplicationContext()).execute();
-                break;
-
             case REQUEST_CODE_VIEW_MANAGER:
                 if (resultCode == RESULT_OK) {
                     if (projectFileSelector != null) {
