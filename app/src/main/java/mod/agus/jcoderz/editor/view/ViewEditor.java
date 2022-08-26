9658180131a614dccd762348c2f96a038c6003d3
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-08-26 21:32:18 +0200

    chore: Remove unused methods in mod.agus.jcoderz.editor.view.ViewEditor
    The rest is used in a.a.a.jr.

--- app/src/main/java/mod/agus/jcoderz/editor/view/ViewEditor.java
+++ app/src/main/java/mod/agus/jcoderz/editor/view/ViewEditor.java
@@ -1,24 +1,14 @@
 package mod.agus.jcoderz.editor.view;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 
 public class ViewEditor {
-
     public static Iterator<?> a() {
         return Collections.emptyIterator();
     }
 
-    public static HashMap<?, ?> b(String unused1, String unused2, String unused3) {
-        return new HashMap<>();
-    }
-
     public static Iterator<?> c() {
         return Collections.emptyIterator();
-    }
-
-    public static HashMap<?, ?> d(String unused1, String unused2) {
-        return new HashMap<>();
     }
 }
