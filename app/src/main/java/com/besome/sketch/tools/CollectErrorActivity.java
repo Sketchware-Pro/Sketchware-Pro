f0c4d3fd291a9e7ddb9b359fcf7f537187847661
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-08-27 21:38:47 +0200

    feat: Improve and add Sketchware Pro version name and others to webhook crash report
    Adds the app version name, base.apk's size, and improved content format to the webhook crash report. Some code blatantly stolen from a.a.a.Dp.

--- app/src/main/java/com/besome/sketch/tools/CollectErrorActivity.java
+++ app/src/main/java/com/besome/sketch/tools/CollectErrorActivity.java
@@ -3,20 +3,26 @@ package com.besome.sketch.tools;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.format.Formatter;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.sketchware.remod.R;
 
+import java.io.File;
 import java.util.HashMap;
 
 import a.a.a.GB;
 import a.a.a.xB;
 import mod.RequestNetwork;
 import mod.RequestNetworkController;
+import mod.SketchwareUtil;
 
 public class CollectErrorActivity extends Activity {
     private final String webUrl = "webhook url here";
@@ -51,29 +57,43 @@ public class CollectErrorActivity extends Activity {
                     .setNeutralButton("Show error", null) // null to set proper onClick listeners later without dismissing the AlertDialog
                     .show();
 
+
+            TextView messageView = dialog.findViewById(android.R.id.message);
+
             dialog.getButton(AlertDialog.BUTTON_NEUTRAL).setOnClickListener(v -> {
-                TextView messageView = dialog.findViewById(android.R.id.message);
                 messageView.setTextIsSelectable(true);
                 messageView.setText(error);
             });
             dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v -> {
-                String content = "```\nSKETCHWARE ver=" + GB.d(getApplicationContext())
-                        + "\nLocale=" + GB.g(getApplicationContext())
-                        + "\nVERSION.RELEASE: " + Build.VERSION.RELEASE
-                        + "\nBRAND: " + Build.BRAND
-                        + "\nMANUFACTURER: " + Build.MANUFACTURER
-                        + "\nMODEL: " + Build.MODEL;
+                PackageInfo info;
+
+                try {
+                    info = getPackageManager().getPackageInfo(getPackageName(), 0);
+                } catch (PackageManager.NameNotFoundException e) {
+                    messageView.setTextIsSelectable(true);
+                    messageView.setText("Somehow couldn't get package info. Stack trace:\n" + Log.getStackTraceString(e));
+                    return;
+                }
+
+                long fileSizeInBytes = new File(info.applicationInfo.sourceDir).length();
+
+                String content = "Sketchware Pro " + info.versionName + " (" + info.versionCode + ")\n"
+                        + "base.apk size: " + Formatter.formatFileSize(this, fileSizeInBytes) + " (" + fileSizeInBytes + " B)\n"
+                        + "Locale: " + GB.g(getApplicationContext()) + "\n"
+                        + "SDK version: " + Build.VERSION.SDK_INT + "\n"
+                        + "Brand: " + Build.BRAND + "\n"
+                        + "Manufacturer: " + Build.MANUFACTURER + "\n"
+                        + "Model: " + Build.MODEL + "\n";
 
                 HashMap<String, Object> map = new HashMap<>();
-                if ((content + "\r\n\n" + error).length() > 2000) {
-                    map.put("content", content + "\n```");
+                if ((content + "\n```\n" + error + "```").length() > 2000) {
+                    map.put("content", content);
                     requestNetwork.setParams(map, RequestNetworkController.REQUEST_BODY);
                     requestNetwork.startRequestNetwork("POST", webUrl, new Gson().toJson(map), listener);
                     map = new HashMap<>();
-                    map.put("content", "```\n" + error + "\n```");
+                    map.put("content", "```\n" + error + "```");
                 } else {
-                    content += "\r\n\n" + error;
-                    content += "\n```";
+                    content += "\n```\n" + error + "```";
                     map.put("content", content);
                     //idk why it's needed every time before starting request, without this the webhook doesn't get sent
                 }
