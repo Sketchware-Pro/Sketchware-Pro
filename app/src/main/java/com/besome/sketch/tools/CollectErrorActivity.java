32c34bf677c6b8e1d18530dfcaa8c7cd38b76b63
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-08-27 20:22:52 +0200

    refactor: Make most fields local variables, clean up code more
    Simplifies the AlertDialog and webhook content code.

--- app/src/main/java/com/besome/sketch/tools/CollectErrorActivity.java
+++ app/src/main/java/com/besome/sketch/tools/CollectErrorActivity.java
@@ -19,15 +19,12 @@ import mod.RequestNetwork;
 import mod.RequestNetworkController;
 
 public class CollectErrorActivity extends Activity {
-    private RequestNetwork reqnet;
-    private RequestNetwork.RequestListener listener;
     private final String webUrl = "webhook url here";
-    private AlertDialog dialog;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        reqnet = new RequestNetwork(this);
-        listener = new RequestNetwork.RequestListener() {
+        RequestNetwork requestNetwork = new RequestNetwork(this);
+        RequestNetwork.RequestListener listener = new RequestNetwork.RequestListener() {
             @Override
             public void onResponse(String tag, String message, HashMap<String, Object> param3) {
                 Toast.makeText(getApplicationContext(), "Report sent!", Toast.LENGTH_LONG).show();
@@ -42,46 +39,46 @@ public class CollectErrorActivity extends Activity {
         Intent intent = getIntent();
         if (intent != null) {
             final String error = intent.getStringExtra("error");
-            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-            dialogBuilder.setTitle(xB.b().a(getApplicationContext(), R.string.common_error_an_error_occurred));
-            dialogBuilder.setMessage("An error occurred while running application.\n\nDo you want to report this error log so that we can fix it?\nNo personal information will be included.");
-            dialogBuilder.setPositiveButton("send", (dialog, which) -> {
-                // left blank intentionally
-            });
-            dialogBuilder.setNegativeButton("no", (dialog, which) -> finish());
-            dialogBuilder.setNeutralButton("Show error", (dialog, which) -> {
-                // do nothing because it's gonna be overrided later to stop dialog to dismiss when button is clicked to show error
-            });
-            dialog = dialogBuilder.create();
-            dialog.show();
+
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle(xB.b().a(getApplicationContext(), R.string.common_error_an_error_occurred))
+                    .setMessage("An error occurred while running application.\n" +
+                            "\n" +
+                            "Do you want to report this error log so that we can fix it?\n" +
+                            "No personal information will be included.")
+                    .setPositiveButton("send", null)
+                    .setNegativeButton("no", (dialogInterface, which) -> finish())
+                    .setNeutralButton("Show error", null) // null to set proper onClick listeners later without dismissing the AlertDialog
+                    .show();
+
             dialog.getButton(AlertDialog.BUTTON_NEUTRAL).setOnClickListener(v -> {
                 TextView messageView = dialog.findViewById(android.R.id.message);
                 messageView.setTextIsSelectable(true);
                 messageView.setText(error);
             });
             dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v -> {
-                StringBuilder content = new StringBuilder("```\nSKETCHWARE ver=" + GB.d(getApplicationContext())
+                String content = "```\nSKETCHWARE ver=" + GB.d(getApplicationContext())
                         + "\nLocale=" + GB.g(getApplicationContext())
                         + "\nVERSION.RELEASE: " + Build.VERSION.RELEASE
                         + "\nBRAND: " + Build.BRAND
                         + "\nMANUFACTURER: " + Build.MANUFACTURER
-                        + "\nMODEL: " + Build.MODEL);
+                        + "\nMODEL: " + Build.MODEL;
 
                 HashMap<String, Object> map = new HashMap<>();
                 if ((content + "\r\n\n" + error).length() > 2000) {
                     map.put("content", content + "\n```");
-                    reqnet.setParams(map, RequestNetworkController.REQUEST_BODY);
-                    reqnet.startRequestNetwork("POST", webUrl, new Gson().toJson(map), listener);
+                    requestNetwork.setParams(map, RequestNetworkController.REQUEST_BODY);
+                    requestNetwork.startRequestNetwork("POST", webUrl, new Gson().toJson(map), listener);
                     map = new HashMap<>();
                     map.put("content", "```\n" + error + "\n```");
                 } else {
-                    content.append("\r\n\n" + error);
-                    content.append("\n```");
-                    map.put("content", content.toString());
+                    content += "\r\n\n" + error;
+                    content += "\n```";
+                    map.put("content", content);
                     //idk why it's needed every time before starting request, without this the webhook doesn't get sent
                 }
-                reqnet.setParams(map, RequestNetworkController.REQUEST_BODY);
-                reqnet.startRequestNetwork("POST", webUrl, new Gson().toJson(map), listener);
+                requestNetwork.setParams(map, RequestNetworkController.REQUEST_BODY);
+                requestNetwork.startRequestNetwork("POST", webUrl, new Gson().toJson(map), listener);
                 Toast.makeText(getApplicationContext(), "Sending crash logs...", Toast.LENGTH_SHORT).show();
             });
         }
