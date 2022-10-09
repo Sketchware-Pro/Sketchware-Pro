505e8c8fb45c5ff5eb800a4b8ac76ac755c6c3c6
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-10-09 12:46:42 +0200

    feat: Make BlocksManagerCreatorActivity use error parsing JSON dialog

--- app/src/main/java/mod/hilal/saif/activities/tools/BlocksManagerCreatorActivity.java
+++ app/src/main/java/mod/hilal/saif/activities/tools/BlocksManagerCreatorActivity.java
@@ -32,6 +32,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
 import com.sketchware.remod.R;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -103,8 +104,6 @@ public class BlocksManagerCreatorActivity extends AppCompatActivity {
         MaterialButton save = findViewById(R.id.save);
         LinearLayout reset = findViewById(R.id.reset);
 
-        AlertDialog.Builder blockTypeDialog = new AlertDialog.Builder(this);
-
         backIcon.setOnClickListener(Helper.getBackPressedClickListener(this));
         Helper.applyRippleToToolbarView(backIcon);
 
@@ -162,7 +161,7 @@ public class BlocksManagerCreatorActivity extends AppCompatActivity {
                     "Header (h)"
             );
             AtomicInteger choice = new AtomicInteger();
-            blockTypeDialog.setTitle("Block type")
+            new AlertDialog.Builder(this).setTitle("Block type")
                     .setSingleChoiceItems(choices.toArray(new String[0]),
                             types.indexOf(type.getText().toString()), (dialog, which) -> choice.set(which))
                     .setPositiveButton(R.string.common_word_save, (dialog, which) -> type.setText(types.get(choice.get())))
@@ -445,9 +444,11 @@ public class BlocksManagerCreatorActivity extends AppCompatActivity {
                 }
                 return;
             }
-        } catch (JsonParseException ignored) {
+            // fall-through to shared handler
+        } catch (JsonParseException e) {
+            // fall-through to shared handler
         }
-        SketchwareUtil.toastError("Invalid blocks file detected!");
+        SketchwareUtil.showFailedToParseJsonDialog(this, new File(path), "Custom Blocks", v -> getBlockList());
         blocksList = new ArrayList<>();
     }
 
