69e1e11d4a5670346916401ef927dc2766a8374d
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-10-23 21:19:24 +0200

    chore: Use constants in IconBadgeView

--- app/src/main/java/dev/aldi/sayuti/editor/view/palette/IconBadgeView.java
+++ app/src/main/java/dev/aldi/sayuti/editor/view/palette/IconBadgeView.java
@@ -3,10 +3,11 @@ package dev.aldi.sayuti.editor.view.palette;
 import android.content.Context;
 import android.view.ViewGroup;
 
-import com.besome.sketch.beans.LayoutBean;
 import com.besome.sketch.beans.ViewBean;
 import com.besome.sketch.editor.view.palette.IconBase;
 import com.sketchware.remod.R;
+
+import mod.agus.jcoderz.beans.ViewBeans;
 
 public class IconBadgeView extends IconBase {
 
@@ -24,10 +25,9 @@ public class IconBadgeView extends IconBase {
     @Override
     public ViewBean getBean() {
         ViewBean viewBean = new ViewBean();
-        viewBean.type = 33;
-        LayoutBean layoutBean = viewBean.layout;
-        layoutBean.width = ViewGroup.LayoutParams.WRAP_CONTENT;
-        layoutBean.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        viewBean.type = ViewBeans.VIEW_TYPE_WIDGET_BADGEVIEW;
+        viewBean.layout.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+        viewBean.layout.height = ViewGroup.LayoutParams.WRAP_CONTENT;
         viewBean.text.text = getName();
         viewBean.convert = "com.allenliu.badgeview.BadgeView";
         return viewBean;
