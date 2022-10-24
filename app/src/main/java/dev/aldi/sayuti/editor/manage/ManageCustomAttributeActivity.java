9efb5fc3ac43fae4162de37441e5311d4ed2ee8f
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-10-24 16:51:11 +0200

    refactor: Make ManageCustomAttributeActivity use R fields

--- app/src/main/java/dev/aldi/sayuti/editor/manage/ManageCustomAttributeActivity.java
+++ app/src/main/java/dev/aldi/sayuti/editor/manage/ManageCustomAttributeActivity.java
@@ -16,6 +16,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sketchware.remod.R;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -38,8 +40,8 @@ public class ManageCustomAttributeActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(2131427794);
-        listview = findViewById(2131232437);
+        setContentView(R.layout.manage_custom_attribute);
+        listview = findViewById(R.id.manage_attr_listview);
         initList();
         initToolbar();
     }
@@ -66,8 +68,8 @@ public class ManageCustomAttributeActivity extends AppCompatActivity {
     }
 
     public void initToolbar() {
-        ((TextView) findViewById(2131232458)).setText("AppCompat Injection Manager");
-        ImageView imageView = findViewById(2131232457);
+        ((TextView) findViewById(R.id.tx_toolbar_title)).setText("AppCompat Injection Manager");
+        ImageView imageView = findViewById(R.id.ig_toolbar_back);
         Helper.applyRippleToToolbarView(imageView);
         imageView.setOnClickListener(Helper.getBackPressedClickListener(this));
     }
@@ -110,12 +112,12 @@ public class ManageCustomAttributeActivity extends AppCompatActivity {
         @Override
         public View getView(final int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
-                convertView = getLayoutInflater().inflate(2131427796, null);
+                convertView = getLayoutInflater().inflate(R.layout.custom_view_attribute, null);
             }
-            LinearLayout linearLayout = convertView.findViewById(2131232440);
+            LinearLayout linearLayout = convertView.findViewById(R.id.cus_attr_layout);
             makeup(linearLayout);
-            ((ImageView) convertView.findViewById(2131232442)).setImageResource(2131166315);
-            ((TextView) convertView.findViewById(2131232441)).setText(_data.get(position).get("type").toString());
+            ((ImageView) convertView.findViewById(R.id.cus_attr_btn)).setImageResource(R.drawable.ic_property_inject);
+            ((TextView) convertView.findViewById(R.id.cus_attr_text)).setText(_data.get(position).get("type").toString());
             linearLayout.setOnClickListener(v -> {
                 i.setClass(getApplicationContext(), AddCustomAttributeActivity.class);
                 i.putExtra("sc_id", str);
