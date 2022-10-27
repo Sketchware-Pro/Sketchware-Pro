57eca6625bf9ec0bea9f292c83b5269c4d9adac7
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-10-27 16:08:50 +0200

    refactor: Use R fields and other constants in AddCustomAttributeActivity

--- app/src/main/java/dev/aldi/sayuti/editor/injection/AddCustomAttributeActivity.java
+++ app/src/main/java/dev/aldi/sayuti/editor/injection/AddCustomAttributeActivity.java
@@ -7,7 +7,9 @@ import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.RippleDrawable;
 import android.os.Bundle;
 import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -22,6 +24,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
+import com.sketchware.remod.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -45,14 +48,14 @@ public class AddCustomAttributeActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(2131427795);
+        setContentView(R.layout.add_custom_attribute);
         initialize(savedInstanceState);
         initializeLogic();
     }
 
     private void initialize(Bundle _savedInstanceState) {
-        fab = findViewById(2131232439);
-        listview = findViewById(2131232438);
+        fab = findViewById(R.id.add_attr_fab);
+        listview = findViewById(R.id.add_attr_listview);
         fab.setOnClickListener(v -> dialog("create", 0));
     }
 
@@ -74,8 +77,8 @@ public class AddCustomAttributeActivity extends AppCompatActivity {
     }
 
     public void initToolbar(String title) {
-        ((TextView) findViewById(2131232458)).setText(title);
-        ImageView imageView = findViewById(2131232457);
+        ((TextView) findViewById(R.id.tx_toolbar_title)).setText(title);
+        ImageView imageView = findViewById(R.id.ig_toolbar_back);
         Helper.applyRippleToToolbarView(imageView);
         imageView.setOnClickListener(Helper.getBackPressedClickListener(this));
     }
@@ -94,16 +97,16 @@ public class AddCustomAttributeActivity extends AppCompatActivity {
 
     private void dialog(final String type, final int position) {
         final AlertDialog dialog = new AlertDialog.Builder(this).create();
-        View inflate = getLayoutInflater().inflate(2131427797, null);
+        View inflate = getLayoutInflater().inflate(R.layout.custom_dialog_attribute, null);
         dialog.setView(inflate);
         dialog.setCanceledOnTouchOutside(false);
         dialog.setCancelable(false);
-        dialog.getWindow().setBackgroundDrawableResource(17170445);
-        TextView save = inflate.findViewById(2131232445);
-        TextView cancel = inflate.findViewById(2131232444);
-        final EditText editText = inflate.findViewById(2131232446);
-        final EditText editText2 = inflate.findViewById(2131232443);
-        final EditText editText3 = inflate.findViewById(2131232447);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        TextView save = inflate.findViewById(R.id.dialog_btn_save);
+        TextView cancel = inflate.findViewById(R.id.dialog_btn_cancel);
+        final EditText editText = inflate.findViewById(R.id.dialog_input_res);
+        final EditText editText2 = inflate.findViewById(R.id.dialog_input_attr);
+        final EditText editText3 = inflate.findViewById(R.id.dialog_input_value);
         if (type.equals("edit")) {
             value = listMap.get(position).get("value").toString();
             editText.setText(value.substring(0, value.indexOf(":")));
@@ -161,11 +164,11 @@ public class AddCustomAttributeActivity extends AppCompatActivity {
         @Override
         public View getView(final int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
-                convertView = getLayoutInflater().inflate(2131427796, null);
+                convertView = getLayoutInflater().inflate(R.layout.custom_view_attribute, null);
             }
-            LinearLayout linearLayout = convertView.findViewById(2131232440);
-            TextView textView = convertView.findViewById(2131232441);
-            final ImageView imageView = convertView.findViewById(2131232442);
+            LinearLayout linearLayout = convertView.findViewById(R.id.cus_attr_layout);
+            TextView textView = convertView.findViewById(R.id.cus_attr_text);
+            final ImageView imageView = convertView.findViewById(R.id.cus_attr_btn);
             imageView.setRotation(90.0f);
             makeup(linearLayout, 10, 5);
             makeup(imageView, 100, 0);
@@ -174,16 +177,16 @@ public class AddCustomAttributeActivity extends AppCompatActivity {
             } else {
                 value = listMap.get(position).get("value").toString();
                 SpannableString spannableString = new SpannableString(value);
-                spannableString.setSpan(new ForegroundColorSpan(-8769908), 0, value.indexOf(":"), 33);
-                spannableString.setSpan(new ForegroundColorSpan(-14606047), value.indexOf(":"), value.indexOf("=") + 1, 33);
-                spannableString.setSpan(new ForegroundColorSpan(-12213691), value.indexOf("\""), value.length(), 33);
+                spannableString.setSpan(new ForegroundColorSpan(0xff7a2e8c), 0, value.indexOf(":"), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                spannableString.setSpan(new ForegroundColorSpan(0xff212121), value.indexOf(":"), value.indexOf("=") + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                spannableString.setSpan(new ForegroundColorSpan(0xff45a245), value.indexOf("\""), value.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                 textView.setText(spannableString);
                 linearLayout.setVisibility(View.VISIBLE);
             }
             imageView.setOnClickListener(v -> {
                 PopupMenu popupMenu = new PopupMenu(getApplicationContext(), imageView);
-                popupMenu.getMenu().add(0, 0, 0, "Edit");
-                popupMenu.getMenu().add(0, 1, 0, "Delete");
+                popupMenu.getMenu().add(Menu.NONE, 0, Menu.NONE, "Edit");
+                popupMenu.getMenu().add(Menu.NONE, 1, Menu.NONE, "Delete");
                 popupMenu.setOnMenuItemClickListener(item -> {
                     if (item.getItemId() == 0) {
                         dialog("edit", position);
