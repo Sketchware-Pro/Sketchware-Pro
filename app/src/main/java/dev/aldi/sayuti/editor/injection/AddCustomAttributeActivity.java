d0634e41ecd705a114ac33fa6fea3820813adf0c
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-10-28 20:22:19 +0200

    chore: Clean up AddCustomAttributeActivity.CustomAdapter
    Renames _data to injections, and gives other local variables better names. Also fixes an unnecessarily duplicated `return true` call.

--- app/src/main/java/dev/aldi/sayuti/editor/injection/AddCustomAttributeActivity.java
+++ app/src/main/java/dev/aldi/sayuti/editor/injection/AddCustomAttributeActivity.java
@@ -138,20 +138,20 @@ public class AddCustomAttributeActivity extends AppCompatActivity {
 
     private class CustomAdapter extends BaseAdapter {
 
-        private final ArrayList<HashMap<String, Object>> _data;
+        private final ArrayList<HashMap<String, Object>> injections;
 
         public CustomAdapter(ArrayList<HashMap<String, Object>> arrayList) {
-            _data = arrayList;
+            injections = arrayList;
         }
 
         @Override
         public int getCount() {
-            return _data.size();
+            return injections.size();
         }
 
         @Override
         public HashMap<String, Object> getItem(int position) {
-            return _data.get(position);
+            return injections.get(position);
         }
 
         @Override
@@ -160,40 +160,43 @@ public class AddCustomAttributeActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
+        public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
                 convertView = getLayoutInflater().inflate(R.layout.custom_view_attribute, null);
             }
-            LinearLayout linearLayout = convertView.findViewById(R.id.cus_attr_layout);
-            TextView textView = convertView.findViewById(R.id.cus_attr_text);
-            final ImageView imageView = convertView.findViewById(R.id.cus_attr_btn);
-            imageView.setRotation(90.0f);
-            makeup(linearLayout, 10, 5);
-            makeup(imageView, 100, 0);
-            if (!_data.get(position).containsKey("type") || !activityInjections.get(position).get("type").toString().equals(widgetType)) {
-                linearLayout.setVisibility(View.GONE);
+
+            LinearLayout root = convertView.findViewById(R.id.cus_attr_layout);
+            TextView injection = convertView.findViewById(R.id.cus_attr_text);
+            ImageView options = convertView.findViewById(R.id.cus_attr_btn);
+
+            options.setRotation(90);
+            makeup(root, 10, 5);
+            makeup(options, 100, 0);
+
+            if (!injections.get(position).containsKey("type") || !activityInjections.get(position).get("type").toString().equals(widgetType)) {
+                root.setVisibility(View.GONE);
             } else {
                 String value = activityInjections.get(position).get("value").toString();
                 SpannableString spannableString = new SpannableString(value);
                 spannableString.setSpan(new ForegroundColorSpan(0xff7a2e8c), 0, value.indexOf(":"), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                 spannableString.setSpan(new ForegroundColorSpan(0xff212121), value.indexOf(":"), value.indexOf("=") + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                 spannableString.setSpan(new ForegroundColorSpan(0xff45a245), value.indexOf("\""), value.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                textView.setText(spannableString);
-                linearLayout.setVisibility(View.VISIBLE);
+                injection.setText(spannableString);
+                root.setVisibility(View.VISIBLE);
             }
-            imageView.setOnClickListener(v -> {
-                PopupMenu popupMenu = new PopupMenu(getApplicationContext(), imageView);
+            options.setOnClickListener(v -> {
+                PopupMenu popupMenu = new PopupMenu(getApplicationContext(), options);
                 popupMenu.getMenu().add(Menu.NONE, 0, Menu.NONE, "Edit");
                 popupMenu.getMenu().add(Menu.NONE, 1, Menu.NONE, "Delete");
                 popupMenu.setOnMenuItemClickListener(item -> {
                     if (item.getItemId() == 0) {
                         dialog("edit", position);
-                        return true;
+                    } else {
+                        activityInjections.remove(position);
+                        FileUtil.writeFile(activityInjectionsFilePath, new Gson().toJson(activityInjections));
+                        notifyDataSetChanged();
+                        SketchwareUtil.toast("Deleted successfully");
                     }
-                    activityInjections.remove(position);
-                    FileUtil.writeFile(activityInjectionsFilePath, new Gson().toJson(activityInjections));
-                    notifyDataSetChanged();
-                    SketchwareUtil.toast("Deleted successfully");
                     return true;
                 });
                 popupMenu.show();
