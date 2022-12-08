ccb04e331394de03b64144d1c81afd885a04a9e6
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-12-09 00:42:41 +0100

    refactor: Give fields in a.a.a.qu better names and make them private, other minor fixes

--- app/src/main/java/a/a/a/qu.java
+++ app/src/main/java/a/a/a/qu.java
@@ -1,7 +1,6 @@
 package a.a.a;
 
 import android.content.Context;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,45 +11,36 @@ import com.besome.sketch.beans.ProjectLibraryBean;
 import com.sketchware.remod.R;
 
 public class qu extends CardView {
+    private final Context context;
+    private ImageView icon;
+    private TextView title;
+    private TextView description;
+    private TextView enabled;
 
-    public Context j;
-    public View k;
-    public ImageView l;
-    public TextView m;
-    public TextView n;
-    public TextView o;
-
-    public qu(Context var1) {
-        super(var1);
-        j = var1;
+    public qu(Context context) {
+        super(context);
+        this.context = context;
     }
 
     public void a(int resLayout) {
-        wB.a(j, this, resLayout);
-        l = findViewById(R.id.lib_icon);
-        m = findViewById(R.id.lib_title);
-        n = findViewById(R.id.lib_desc);
-        o = findViewById(R.id.tv_enable);
+        wB.a(context, this, resLayout);
+        icon = findViewById(R.id.lib_icon);
+        title = findViewById(R.id.lib_title);
+        description = findViewById(R.id.lib_desc);
+        enabled = findViewById(R.id.tv_enable);
         LayoutParams layoutParams = new LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.topMargin = (int) wB.a(j, 4f);
-        layoutParams.bottomMargin = (int) wB.a(j, 4f);
+        layoutParams.topMargin = (int) wB.a(context, 4f);
+        layoutParams.bottomMargin = (int) wB.a(context, 4f);
         setLayoutParams(layoutParams);
     }
 
     public void setData(ProjectLibraryBean projectLibraryBean) {
-        l.setImageResource(ProjectLibraryBean.getLibraryIcon(projectLibraryBean.libType));
-        m.setText(xB.b().a(getContext(), ProjectLibraryBean.getLibraryResName(projectLibraryBean.libType)));
-        n.setText(xB.b().a(getContext(), ProjectLibraryBean.getLibraryResDesc(projectLibraryBean.libType)));
-        String var3;
-        if ("Y".equals(projectLibraryBean.useYn)) {
-            var3 = "ON";
-        } else {
-            var3 = "OFF";
-        }
-
-        o.setText(var3);
-        o.setSelected("Y".equals(projectLibraryBean.useYn));
+        icon.setImageResource(ProjectLibraryBean.getLibraryIcon(projectLibraryBean.libType));
+        title.setText(xB.b().a(getContext(), ProjectLibraryBean.getLibraryResName(projectLibraryBean.libType)));
+        description.setText(xB.b().a(getContext(), ProjectLibraryBean.getLibraryResDesc(projectLibraryBean.libType)));
+        enabled.setText("Y".equals(projectLibraryBean.useYn) ? "ON" : "OFF");
+        enabled.setSelected("Y".equals(projectLibraryBean.useYn));
     }
 }
