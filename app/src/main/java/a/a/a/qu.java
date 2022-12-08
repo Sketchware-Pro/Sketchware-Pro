e26c422cd28d38dd05aa3f303e5c5cf4b0fe7c6d
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-12-09 00:39:15 +0100

    refactor: Make a.a.a.qu use R fields and constants

--- app/src/main/java/a/a/a/qu.java
+++ app/src/main/java/a/a/a/qu.java
@@ -2,12 +2,14 @@ package a.a.a;
 
 import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
 
 import com.besome.sketch.beans.ProjectLibraryBean;
+import com.sketchware.remod.R;
 
 public class qu extends CardView {
 
@@ -25,14 +27,15 @@ public class qu extends CardView {
 
     public void a(int resLayout) {
         wB.a(j, this, resLayout);
-        k = findViewById(2131230931);
-        l = findViewById(2131231428);
-        m = findViewById(2131231430);
-        n = findViewById(2131231427);
-        o = findViewById(2131231965);
-        LayoutParams layoutParams = new LayoutParams(-1, -2);
-        layoutParams.topMargin = (int) wB.a(j, 4.0F);
-        layoutParams.bottomMargin = (int) wB.a(j, 4.0F);
+        l = findViewById(R.id.lib_icon);
+        m = findViewById(R.id.lib_title);
+        n = findViewById(R.id.lib_desc);
+        o = findViewById(R.id.tv_enable);
+        LayoutParams layoutParams = new LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.topMargin = (int) wB.a(j, 4f);
+        layoutParams.bottomMargin = (int) wB.a(j, 4f);
         setLayoutParams(layoutParams);
     }
 
@@ -50,5 +53,4 @@ public class qu extends CardView {
         o.setText(var3);
         o.setSelected("Y".equals(projectLibraryBean.useYn));
     }
-
 }
