518ce6501555f1fb36e9cca20ad6f3238a6383d7
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2022-12-19 00:36:11 +0530

    fix: Fix all missing `Override` annotation, raw use of parameterized class

--- app/src/main/java/a/a/a/Fw.java
+++ app/src/main/java/a/a/a/Fw.java
@@ -1,5 +1,6 @@
 package a.a.a;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -22,6 +23,7 @@ import com.besome.sketch.editor.manage.view.ManageViewActivity;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+@SuppressLint("ResourceType")
 public class Fw extends qA {
     public RecyclerView f;
     public String g;
@@ -44,12 +46,12 @@ public class Fw extends qA {
         var5[var1] = var6;
         var4.append(var6);
         String var9 = var4.toString();
-        ArrayList var12 = jC.a(g).d(var2);
+        ArrayList<ViewBean> var12 = jC.a(g).d(var2);
         var2 = var9;
 
         while (true) {
             boolean var7 = false;
-            Iterator var10 = var12.iterator();
+            Iterator<ViewBean> var10 = var12.iterator();
 
             boolean var13;
             while (true) {
@@ -58,7 +60,7 @@ public class Fw extends qA {
                     break;
                 }
 
-                if (var2.equals(((ViewBean) var10.next()).id)) {
+                if (var2.equals(var10.next().id)) {
                     var13 = true;
                     break;
                 }
@@ -116,19 +118,19 @@ public class Fw extends qA {
 
     public final void c(ProjectFileBean var1) {
         ProjectFileBean var2 = i.get(j.c);
-        ArrayList var3 = jC.a(g).d(var2.getXmlName());
+        ArrayList<ViewBean> var3 = jC.a(g).d(var2.getXmlName());
 
         for (int var4 = var3.size() - 1; var4 >= 0; --var4) {
-            ViewBean var5 = (ViewBean) var3.get(var4);
+            ViewBean var5 = var3.get(var4);
             jC.a(g).a(var2, var5);
         }
 
-        ArrayList var6 = a(var1.presetName);
+        ArrayList<ViewBean> var6 = a(var1.presetName);
         jC.a(g);
-        Iterator var8 = eC.a(var6).iterator();
+        Iterator<ViewBean> var8 = eC.a(var6).iterator();
 
         while (var8.hasNext()) {
-            ViewBean var7 = (ViewBean) var8.next();
+            ViewBean var7 = var8.next();
             var7.id = a(var7.type, var2.getXmlName());
             jC.a(g).a(var2.getXmlName(), var7);
             if (var7.type == 3 && var2.fileType == 0) {
@@ -141,13 +143,13 @@ public class Fw extends qA {
     public void d() {
         g = getActivity().getIntent().getStringExtra("sc_id");
         h = getActivity().getIntent().getStringExtra("compatUseYn");
-        ArrayList var1 = jC.b(g).b();
+        ArrayList<ProjectFileBean> var1 = jC.b(g).b();
         if (var1 != null) {
-            Iterator var2 = var1.iterator();
+            Iterator<ProjectFileBean> var2 = var1.iterator();
             boolean var3 = false;
 
             while (var2.hasNext()) {
-                ProjectFileBean var4 = (ProjectFileBean) var2.next();
+                ProjectFileBean var4 = var2.next();
                 if (var4.fileName.equals("main")) {
                     i.add(0, var4);
                     var3 = true;
@@ -164,7 +166,7 @@ public class Fw extends qA {
     }
 
     public final void e() {
-        for (Iterator var1 = i.iterator(); var1.hasNext(); ((ProjectFileBean) var1.next()).isSelected = false) {
+        for (Iterator<ProjectFileBean> var1 = i.iterator(); var1.hasNext(); var1.next().isSelected = false) {
         }
 
     }
@@ -193,19 +195,18 @@ public class Fw extends qA {
     }
 
     public void g() {
-        ArrayList var1 = i;
-        if (var1 != null) {
-            if (var1.size() == 0) {
-                l.setVisibility(0);
-                f.setVisibility(8);
+        if (i != null) {
+            if (i.size() == 0) {
+                l.setVisibility(View.VISIBLE);
+                f.setVisibility(View.GONE);
             } else {
-                f.setVisibility(0);
-                l.setVisibility(8);
+                f.setVisibility(View.VISIBLE);
+                l.setVisibility(View.GONE);
             }
         }
-
     }
 
+    @Override
     public void onActivityCreated(Bundle var1) {
         super.onActivityCreated(var1);
         if (var1 == null) {
@@ -220,6 +221,7 @@ public class Fw extends qA {
         g();
     }
 
+    @Override
     public void onActivityResult(int var1, int var2, Intent var3) {
         super.onActivityResult(var1, var2, var3);
         Fw.a var4;
@@ -239,9 +241,10 @@ public class Fw extends qA {
 
     }
 
+    @Override
     public View onCreateView(LayoutInflater var1, ViewGroup var2, Bundle var3) {
         ViewGroup var4 = (ViewGroup) var1.inflate(2131427442, var2, false);
-        i = new ArrayList();
+        i = new ArrayList<>();
         f = var4.findViewById(2131231442);
         f.setHasFixedSize(true);
         f.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -252,6 +255,7 @@ public class Fw extends qA {
         return var4;
     }
 
+    @Override
     public void onSaveInstanceState(Bundle var1) {
         var1.putString("sc_id", g);
         var1.putString("compatUseYn", h);
@@ -272,21 +276,23 @@ public class Fw extends qA {
 
         }
 
+        @Override
         public int a() {
             return d.i != null ? d.i.size() : 0;
         }
 
-        public void a(Fw.a.a var1, int var2) {
-            var1.v.setVisibility(0);
-            var1.y.setVisibility(8);
+        @Override
+        public void b(Fw.a.a var1, int var2) {
+            var1.v.setVisibility(View.VISIBLE);
+            var1.y.setVisibility(View.GONE);
             if (var2 == 0) {
-                var1.t.setVisibility(8);
+                var1.t.setVisibility(View.GONE);
             } else if (d.k) {
-                var1.y.setVisibility(0);
-                var1.v.setVisibility(8);
+                var1.y.setVisibility(View.VISIBLE);
+                var1.v.setVisibility(View.GONE);
             } else {
-                var1.y.setVisibility(8);
-                var1.v.setVisibility(0);
+                var1.y.setVisibility(View.GONE);
+                var1.v.setVisibility(View.VISIBLE);
             }
 
             ProjectFileBean var3 = d.i.get(var2);
@@ -301,6 +307,7 @@ public class Fw extends qA {
 
         }
 
+        @Override
         public Fw.a.a b(ViewGroup var1, int var2) {
             return new Fw.a.a(this, LayoutInflater.from(var1.getContext()).inflate(2131427571, var1, false));
         }
@@ -308,10 +315,8 @@ public class Fw extends qA {
         public final int f(int var1) {
             String var2 = String.format("%4s", Integer.toBinaryString(var1)).replace(' ', '0');
             Resources var3 = d.getContext().getResources();
-            StringBuilder var4 = new StringBuilder();
-            var4.append("activity_");
-            var4.append(var2);
-            return var3.getIdentifier(var4.toString(), "drawable", d.getContext().getPackageName());
+            String var4 = "activity_" + var2;
+            return var3.getIdentifier(var4, "drawable", d.getContext().getPackageName());
         }
 
         public class a extends v {
@@ -336,7 +341,7 @@ public class Fw extends qA {
                 y = var2.findViewById(2131230959);
                 z = var2.findViewById(2131231132);
                 A = var2.findViewById(2131231168);
-                t.setVisibility(8);
+                t.setVisibility(View.GONE);
                 u.setOnClickListener(new Cw(this, var1));
                 u.setOnLongClickListener(new Dw(this, var1));
                 A.setOnClickListener(new Ew(this, var1));
