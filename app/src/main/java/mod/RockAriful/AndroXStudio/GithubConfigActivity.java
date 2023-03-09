e56f2f8eeccad203780022564abec0abe8f0eae8
Androx Studio <88816748+AndroXStudiO@users.noreply.github.com>
2023-03-09 09:01:04 +0600

    Update GithubConfigActivity.java

--- app/src/main/java/mod/RockAriful/AndroXStudio/GithubConfigActivity.java
+++ app/src/main/java/mod/RockAriful/AndroXStudio/GithubConfigActivity.java
@@ -42,7 +42,6 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import com.github.angads25.filepicker.*;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -55,6 +54,35 @@ import java.util.HashMap;
 import java.util.regex.*;
 
 import org.json.*;
+import org.slf4j.*;
+import org.eclipse.jgit.*;
+import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.api.errors.*;
+import java.io.BufferedWriter; 
+import java.io.File; 
+import java.io.FileOutputStream; 
+import java.io.OutputStreamWriter; 
+import java.text.MessageFormat; 
+import java.util.Date; 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean; 
+ 
+import org.eclipse.jgit.api.CloneCommand; 
+import org.eclipse.jgit.api.Git; 
+import org.eclipse.jgit.api.ResetCommand.ResetType; 
+import org.eclipse.jgit.api.ListBranchCommand.ListMode;
+import org.eclipse.jgit.api.errors.GitAPIException; 
+import org.eclipse.jgit.lib.Constants; 
+import org.eclipse.jgit.revwalk.RevCommit; 
+import org.eclipse.jgit.transport.CredentialsProvider; 
+import org.eclipse.jgit.transport.PushResult; 
+import org.eclipse.jgit.transport.RefSpec; 
+import org.eclipse.jgit.transport.*;
+import org.eclipse.jgit.transport.RemoteRefUpdate; 
+import org.eclipse.jgit.transport.RemoteRefUpdate.Status; 
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider; 
+import org.eclipse.jgit.lib.*;
+import org.eclipse.jgit.util.FileUtils; 
 
 public class GithubConfigActivity extends AppCompatActivity {
 	
