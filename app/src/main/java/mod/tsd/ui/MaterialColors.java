47996a9ad8159ce4cd02262f8ffa5dcf1343ade0
TechnicalStudioDeveloper <appdeveloper400@gmail.com>
2023-03-27 20:39:32 +0530

    fix: Failed to add new MaterialColors java file

---
+++ app/src/main/java/mod/tsd/ui/MaterialColors.java
@@ -0,0 +1,10 @@
+package mod.tsd.ui;
+
+import java.lang.String;
+
+public class MaterialColors {
+	public static String colorPrimary = "#A13F28";
+	public static String colorOnPrimary = "#FFFFFF";
+	public static String colorPrimaryContainer = "#FFDAD2";
+	public static String colorOnPrimaryContainer = "#3D0600";
+}
