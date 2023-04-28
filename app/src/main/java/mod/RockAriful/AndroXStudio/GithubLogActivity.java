36c341797de419549a44f29737e1ce712a4bc0fb
Androx Studio <88816748+AndroXStudiO@users.noreply.github.com>
2023-04-28 19:22:42 +0800

    Update GithubLogActivity.java

--- app/src/main/java/mod/RockAriful/AndroXStudio/GithubLogActivity.java
+++ app/src/main/java/mod/RockAriful/AndroXStudio/GithubLogActivity.java
@@ -1,43 +1,55 @@
 package mod.RockAriful.AndroXStudio;
 
+import android.animation.*;
+import android.app.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import com.google.android.material.appbar.AppBarLayout;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import org.json.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import a.a.a.yB;
+import a.a.a.lC;
+
+import mod.RockAriful.AndroXStudio.*;
 import com.sketchware.remod.R;
 import mod.SketchwareUtil; 
 import mod.agus.jcoderz.lib.FilePathUtil;
 import mod.agus.jcoderz.lib.FileUtil;
 
-import android.content.*;
-import android.app.*;
-import android.app.Activity;
-import android.graphics.*;
-import android.graphics.drawable.*;
-
-import java.io.*;
-import java.text.*;
-import java.util.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.regex.*;
-import java.util.List;
-
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.util.concurrent.*;
-import android.os.*;
-
-import android.widget.*;
-import android.view.*;
-import android.view.View;
-
-import a.a.a.yB;
-import a.a.a.lC;
+import io.github.rosemoe.sora.langs.java.JavaLanguage;
+import io.github.rosemoe.sora.widget.CodeEditor;
+import io.github.rosemoe.sora.widget.component.Magnifier;
+import io.github.rosemoe.sora.widget.schemes.EditorColorScheme;
 
 import org.eclipse.jgit.api.*;
 import org.eclipse.jgit.api.errors.*;
@@ -67,197 +79,230 @@ import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.eclipse.jgit.lib.*;
 import org.eclipse.jgit.util.FileUtils; 
 
+import org.eclipse.jgit.revwalk.RevCommitList;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.internal.storage.file.*;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.diff.*;
+import org.eclipse.jgit.treewalk.*;
 
-public class PushToGitHub {
-    
-    private ArrayList<HashMap<String, Object>> JsonMAP = new ArrayList<>();
-    private static String Result ="";
-    private static String sc_id ="";
-    private static boolean isSucces = false;
-    private static Activity mContext;
-    private static AlertDialog prog;
-    private static String _FilePATH ="";
-    private static String _RepositoryURL ="";
-    private static String _setRefSpecs ="";
-    private static String _UserName ="";
-    private static String _AccessToken ="";
-    private static String _CommitMessage ="";
-    
-    
-    public PushToGitHub(Activity context,  final String _sc_id, final boolean _Force){
-        mContext = context;
-        sc_id = _sc_id;
-        
-       if(_Force){
-         new Thread(() -> {
-	 	  _Uber_progress(true);
-           HashMap<String, Object> projectInfo = lC.b(sc_id);
-           _FilePATH = FileUtil.getExternalStorageDir()+"/sketchware/.github_src/"+yB.c(projectInfo, "my_ws_name");
-           mContext.runOnUiThread(() ->
-              FatchString()
-            );
-         }).start();
-            
-       }else{
-        new Thread(() -> {
-	 	  _Uber_progress(true);
-           _FilePATH = new ExportForGitHub(mContext,sc_id).exportSrc();
-           mContext.runOnUiThread(() ->
-              FatchString()
-            );
-         }).start();
-       }
-    }
-    
-    public void  FatchString(){
-        try{
-			JsonMAP = new Gson().fromJson(FileUtil.readFile(FileUtil.getExternalStorageDir()+"/.sketchware/data/"+sc_id+"/github_config"), new TypeToken<ArrayList<HashMap<String, Object>>>(){}.getType());
-	    	_RepositoryURL = JsonMAP.get((int)0).get("repository").toString();
-	    	_setRefSpecs = JsonMAP.get((int)0).get("RefSpecs").toString();
-			_UserName = JsonMAP.get((int)0).get("username").toString();
-	    	_AccessToken = JsonMAP.get((int)0).get("token").toString();
-		}catch(Exception e){}
-    }
-    
-    public static boolean pushREPO(final String _setMessage, final String _Fileformat) {
-       
-         if(!FileUtil.isExistFile(_FilePATH)){
-    	   SketchwareUtil.toastError(_FilePATH+" Not Exist!");
-           _Uber_progress(false);
-  	     return false;
-    	 }
-         try(Git git = Git.init().setDirectory(new File(_FilePATH)).call()){
-             
-         }catch(GitAPIException e){
-			 SketchwareUtil.toastError(e.toString());
-             _Uber_progress(false);
-             return false;
-         }
-         
-		ExecutorService executor = Executors.newSingleThreadExecutor();
-		final Handler handler = new Handler(Looper.getMainLooper());
-        
-		 executor.execute(new Runnable() {
-		    @Override
-		   public void run() {
-               SketchwareUtil.toastError("Start");
-		     try(Git git = Git.open(new File(_FilePATH))) {
+public class GithubLogActivity extends AppCompatActivity {
+	
+	private Toolbar _toolbar;
+	private AppBarLayout _app_bar;
+	private CoordinatorLayout _coordinator;
+	private String sc_id = "";
+	private AlertDialog prog;
+    private String _DiffyTEXT ="";
+	private io.github.rosemoe.sora.widget.CodeEditor editor;
+	
 
-		         if (_Fileformat.isEmpty()) {
-                   git.add().addFilepattern(".").call();
-                   SketchwareUtil.toastError("Format file empty");
-                 }else{
-                     
-                  if (_FilePATH.isEmpty()) {
-                   Result = "FilePath not exist!";
-                   isSucces = false;
-                   return;   
-                  }
-                  SketchwareUtil.toastError("Start2");
-                  
-                   List<String> fileNames = Arrays.asList(_Fileformat.split(";\\s*"));
-                   if(!fileNames.isEmpty()){
-  			      for (String fileName : fileNames) {
- 			        try{
-                      boolean fileFound = Files.walk(Paths.get(_FilePATH))
-                      .filter(p -> p.getFileName().toString().equals(fileName))
-                      .findFirst()
-                      .isPresent();
-                        
-                      if (fileFound) {  
-    			       Files.walk(Paths.get(_FilePATH))
-     		          .filter(p -> p.getFileName().toString().equals(fileName))
-         	 	     .forEach(p -> {
-          	          try {
-                         git.add().addFilepattern(p.toString()).call();
-					 	SketchwareUtil.toastError(p.toString());	  
-             	       } catch (Exception e) {
-                          SketchwareUtil.toastError(e.toString());
-                          Result = e.toString();
-                          isSucces = false;
-                          return;
-            	        }
-          	         });
-                      }else{
-                       SketchwareUtil.toastError("Invalid files reference, No files found!");
-                      }
-    			     } catch (IOException e) {
-    		           SketchwareUtil.toastError(e.toString());
-                       Result = e.toString();
-                       isSucces = false;
-                       return;
-     		  	  }     
- 				   }
-                   }else{
-                    SketchwareUtil.toastError("Invalid files reference, No files found!");
-		     	   Result = "Invalid files reference, No files found!";
-                    isSucces = false;
-                    return;
-                   }
-                  
-                 }
-	 	      
-                    
-	 	        git.commit().setMessage(_setMessage).call();
-	 	        
-	 	        PushCommand push = git.push();
-                 push.setCredentialsProvider(new UsernamePasswordCredentialsProvider(_UserName, _AccessToken));
-  		       push.setRemote(_RepositoryURL);
- 	 	       push.setRefSpecs(new RefSpec(_setRefSpecs));
- 		        push.setForce(true);
- 			
-					          
-		  	 Iterable<PushResult> results = push.call();
-	   		for (PushResult r : results) {
-   			  for(RemoteRefUpdate update : r.getRemoteUpdates()) {
-	     	 	  //System.out.println("Having result: " + update);
-    		 	  if(update.getStatus() != RemoteRefUpdate.Status.OK && update.getStatus() != RemoteRefUpdate.Status.UP_TO_DATE) {
-		  	        Result = "Push failed: "+ update.getStatus();
-	 	             isSucces = false;
-	    		     throw new RuntimeException(Result);
-   	 		    }else{
-    			      Result = "Successfully Pushed! Status : " + update.getStatus().toString()+" with :- "+update.getNewObjectId().getName();
-	    		      isSucces = true;
-		    		}
-	   	 	 }
-	   		}
-					             
-	    	 }catch(IOException | GitAPIException | JGitInternalException e) {
-			    Result = e.getMessage();
-			    isSucces = false;
-			    e.printStackTrace();
-			 }
-				        
-		  	handler.post(new Runnable() {
-				 @Override
-				  public void run() {
-                      _Uber_progress(false);
-					  if(isSucces){
-					     SketchwareUtil.toast(Result);
-					  }else{
-					     SketchwareUtil.toastError(Result);
-					  }
-				  }
-			   });
-		   }
-		});
-        return isSucces;
+	private String ProjectNAME = "";
+	private String RepositoryPATH = "";
+	private String GitHubLast_PATH = "";
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.github_log);
+		initialize(_savedInstanceState);
+		initializeLogic();
 	}
-    
-    public static void _Uber_progress(final boolean _ifShow) {
+	
+	private void initialize(Bundle _savedInstanceState) {
+		_app_bar = findViewById(R.id._app_bar);
+		_coordinator = findViewById(R.id._coordinator);
+		_toolbar = findViewById(R.id._toolbar);
+		setSupportActionBar(_toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _v) {
+				onBackPressed();
+			}
+		});
+		
+		editor = findViewById(R.id.editor);
+		
+	}
+	
+	private void initializeLogic() {
+		sc_id = getIntent().getStringExtra("sc_id");
+		
+                editor.setTypefaceText(Typeface.MONOSPACE);
+                editor.setEditable(false);
+                editor.setTextSize(14);                
+                editor.getComponent(Magnifier.class).setWithinEditorForcibly(true);
+				editor.setColorScheme(new EditorColorScheme());
+                editor.setEditorLanguage(new JavaLanguage());
+               
+		HashMap<String, Object> projectInfo = lC.b(sc_id);
+		ProjectNAME = yB.c(projectInfo, "my_ws_name");
+
+		RepositoryPATH = FileUtil.getExternalStorageDir()+"/sketchware/.github_src/"+ProjectNAME+"/.git/";
+		GitHubLast_PATH = FileUtil.getExternalStorageDir()+"/sketchware/.github_src/"+ProjectNAME+"/.git/GiTChangeLog";
+		_Uber_progress(true);
+        
+		if (getIntent().getStringExtra("TYPE").equals("LOG")) {
+		  setTitle("Show commit logs");
+          editor.setText(_FatchCommitLOG());
+          _Uber_progress(false);
+		} else {
+		  setTitle("GitHub Last Changes");
+		  new Thread(() -> {                 
+                _FatchDiff();
+               runOnUiThread(() ->{
+                   _Uber_progress(false);
+                   _DiffyParse(FileUtil.readFile(GitHubLast_PATH));
+                   //_DiffyViewer(editor,FileUtil.readFile(GitHubLast_PATH));
+                });
+           }).start();
+
+		}
+	}
+	public String _FatchCommitLOG() {
+		 try{
+			     
+			     StringBuilder AddList = new  StringBuilder();
+			     
+			    Repository repoo = new FileRepository(RepositoryPATH);
+			    Git gits = new Git(repoo);
+			    RevWalk walk = new RevWalk(repoo);
+			
+			    List<Ref> branches = gits.branchList().call();
+			
+			    for (Ref branch : branches) {
+				        String branchName = branch.getName();
+				
+				        AddList.append("\nCommits of branch:" + branch.getName());
+				        AddList.append("\n<------------------[START]---------------------->");
+				
+				        Iterable<RevCommit> commits = gits.log().all().call();
+				
+				        for (RevCommit commit : commits) {
+					            boolean foundInThisBranch = false;
+					
+					            RevCommit targetCommit = walk.parseCommit(repoo.resolve(
+					                    commit.getName()));
+					            for (Map.Entry<String, Ref> e : repoo.getAllRefs().entrySet()) {
+						                if (e.getKey().startsWith(Constants.R_HEADS)) {
+							                    if (walk.isMergedInto(targetCommit, walk.parseCommit(
+							                            e.getValue().getObjectId()))) {
+								                        String foundInBranch = e.getValue().getName();
+								                        if (branchName.equals(foundInBranch)) {
+									                            foundInThisBranch = true;
+									                            break;
+									                        }
+								                    }
+							                }
+						            }
+					
+					            if (foundInThisBranch) {
+						                AddList.append("\nCommit name: "+commit.getName());
+						                AddList.append("\nCommitAuthorIdent: "+commit.getAuthorIdent().getName());
+						                AddList.append("\nCommit time: "+ new Date(commit.getCommitTime() * 1000L));
+						                AddList.append("\nMessage: "+commit.getFullMessage());
+                                        AddList.append("\n"+"<-----------------------[END]------------------->"+"\n");
+						            }
+					        }
+				    }			     
+			     			     			     			    		
+			    return AddList.toString();
+			 }catch(GitAPIException | IOException | NullPointerException e){
+			     return e.toString();
+			 }
+	}
+	
+	
+	public void _FatchDiff() {
+		   try{
+			      Repository repository = new FileRepositoryBuilder()
+			            .setGitDir(new File(RepositoryPATH)).build();
+			    // Here we get the head commit and it's first parent.
+			    // Adjust to your needs to locate the proper commits.
+			     RevCommit headCommit = getHeadCommit(repository);
+			     RevCommit diffWith = headCommit.getParent(0);
+			     FileOutputStream stdout = new FileOutputStream(GitHubLast_PATH);
+			     try (DiffFormatter diffFormatter = new DiffFormatter(stdout)) {
+				        diffFormatter.setRepository(repository);
+				        for (DiffEntry entry : diffFormatter.scan(diffWith, headCommit)) {
+					            diffFormatter.format(diffFormatter.toFileHeader(entry));
+					        }
+				     }
+			   }catch(Exception e) {
+			       SketchwareUtil.toastError(e.toString());
+			   }
+		 }
+	    private static RevCommit getHeadCommit(Repository repository) throws Exception {
+		    try (Git git = new Git(repository)) {
+			        Iterable<RevCommit> history = git.log().setMaxCount(1).call();
+			        return history.iterator().next();
+			    }
+		
+    	}
+      /*  
+     public void _DiffyViewer(final io.github.rosemoe.sora.widget.CodeEditor _tv_view, final String _DiffyString) {
+		final String[] lines;
+		Spannable spannable1 = new SpannableString(_DiffyString);
+		android.text.style.ForegroundColorSpan fgSpan = new android.text.style.ForegroundColorSpan(Color.parseColor("#ffffff"));
+		android.text.style.BackgroundColorSpan bgSpan = new android.text.style.BackgroundColorSpan(Color.parseColor("#4caf50"));
+		android.text.style.BackgroundColorSpan RbgSpan = new android.text.style.BackgroundColorSpan(Color.parseColor("#d81b69"));
+		
+		lines = _DiffyString.split("\n");
+		int x = 0;
+		for (int i = 0; i < lines.length; i++) {
+		    if (lines[i].substring((int)(0), (int)(1)).replaceAll("\\s","").equals("+")) {
+			   int n = _DiffyString.indexOf(lines[i]);
+			   x = n+1;
+                 try{
+				   spannable1.setSpan(android.text.style.CharacterStyle.wrap(fgSpan),n, n+lines[i].length(), 0);
+				   spannable1.setSpan(android.text.style.CharacterStyle.wrap(bgSpan), n, n+lines[i].length(), 0);
+                 }catch(Exception e){
+                   SketchwareUtil.toastError(e.toString());
+                 }
+			 } else if (lines[i].substring((int)(0), (int)(1)).replaceAll("\\s","").equals("-")) {
+                int n = _DiffyString.indexOf(lines[i]);
+			   x = n+1;
+                try{
+                   spannable1.setSpan(android.text.style.CharacterStyle.wrap(fgSpan),n, n+lines[i].length(), 0);
+				   spannable1.setSpan(android.text.style.CharacterStyle.wrap(RbgSpan), n, n+lines[i].length(), 0);
+                }catch(Exception e){
+                  SketchwareUtil.toastError(e.toString());
+                }
+			 }else{}
+		}   
+		 _tv_view.setText(spannable1);
+ 	}
+     */
+     
+     public void _DiffyParse(final String DiffyTEXT){
+       _DiffyTEXT = DiffyTEXT;
+        new Thread(() -> {                 
+             _DiffyTEXT = _DiffyTEXT.replace("diff --git a/","\n>--------------[START]------------<\n");
+             _DiffyTEXT = _DiffyTEXT.replace("--- a/","\n>--------------[Old Commit File]------------<\n");
+             _DiffyTEXT = _DiffyTEXT.replace("+++ b/","\n>--------------[New Commit File]------------<\n");
+             _DiffyTEXT = _DiffyTEXT.replace("\\ No newline at end of file","\n>--------------[END]------------<\n");
+             _DiffyTEXT = _DiffyTEXT.replace(" b/app/","\n>--------------[]------------<\napp/");
+             _DiffyTEXT = _DiffyTEXT.replace("@@","\n>--------------[@@]------------<\n");
+   	      runOnUiThread(() ->{
+               editor.setText(_DiffyTEXT);
+   	      });
+        }).start();
+     }
+      
+     public  void _Uber_progress(final boolean _ifShow) {
 		if (_ifShow) {
-            mContext.runOnUiThread(new Runnable() {
-              public void run() {
-                prog = new AlertDialog.Builder(mContext).create();
-				prog.setCancelable(false);
-				prog.setCanceledOnTouchOutside(false);
-				prog.getWindow().setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(Color.TRANSPARENT));
-				prog.getWindow().setDimAmount(0.4f);
-				View inflate = mContext.getLayoutInflater().inflate(R.layout.rockariful_github_loading, null);
-				prog.setView(inflate);
-				prog.show();
- 	  	   }
-			});
+           prog = new AlertDialog.Builder(this).create();
+	   	prog.setCancelable(false);
+			prog.setCanceledOnTouchOutside(false);
+			prog.getWindow().setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(Color.TRANSPARENT));
+			prog.getWindow().setDimAmount(0.4f);
+			View inflate = getLayoutInflater().inflate(R.layout.rockariful_github_loading, null);
+			prog.setView(inflate);
+			final TextView title = (TextView) inflate.findViewById(R.id.textview1);
+			title.setText("Now Loading");
+			prog.show();
 		}
 		else {
 			if (prog != null){
@@ -265,4 +310,5 @@ public class PushToGitHub {
 			}
 		}
 	}
+
 }
