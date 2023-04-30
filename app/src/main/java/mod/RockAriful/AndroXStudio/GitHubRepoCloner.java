b6e793410bb78479ab00ce9f1602401009664dc2
Androx Studio <88816748+AndroXStudiO@users.noreply.github.com>
2023-04-30 11:03:26 +0800

    Update GitHubRepoCloner.java

--- app/src/main/java/mod/RockAriful/AndroXStudio/GitHubRepoCloner.java
+++ app/src/main/java/mod/RockAriful/AndroXStudio/GitHubRepoCloner.java
@@ -10,7 +10,7 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-
+import mod.SketchwareUtil;
 import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
