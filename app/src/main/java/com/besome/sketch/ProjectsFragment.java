65df1b242d5b2c47ea2b11bc4239b7c2f418824d
Androx Studio <88816748+AndroXStudiO@users.noreply.github.com>
2023-05-01 20:10:04 +0800

    Update ProjectsFragment.java

--- app/src/main/java/com/besome/sketch/ProjectsFragment.java
+++ app/src/main/java/com/besome/sketch/ProjectsFragment.java
@@ -635,9 +635,8 @@ public class ProjectsFragment extends DA implements View.OnClickListener {
                 
                 @Override
    			 public void onProgress(int progress) {
-                  SketchwareUtil.toast("Cloning "+progress+"Bytes");
-                  no.setText("Cloning "+progress+"Bytes");
- 			   }
+                  
+ 			  }
     
 		  	});
 
