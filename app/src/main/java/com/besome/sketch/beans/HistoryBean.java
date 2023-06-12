d4602bd0824998dde0bfce2db40a35b7cef8904d
elfilibusterismo <113604229+elfilibusterismo@users.noreply.github.com>
2023-06-12 23:45:46 +0800

    fix: changed all the nA to BaseBean in HistoryBean.java

--- app/src/main/java/com/besome/sketch/beans/HistoryBean.java
+++ app/src/main/java/com/besome/sketch/beans/HistoryBean.java
@@ -7,24 +7,24 @@ public class HistoryBean extends BaseBean {
     public static final int ACTION_TYPE_REMOVE = 2;
     public static final int ACTION_TYPE_UPDATE = 1;
     public int actionType;
-    public nA currentData;
-    public nA prevData;
+    public BaseBean currentData;
+    public BaseBean prevData;
 
-    public HistoryBean(int i, nA nAVar, nA nAVar2) {
-        actionType = i;
-        prevData = nAVar;
-        currentData = nAVar2;
+    public HistoryBean(int actionType, BaseBean prevData, BaseBean currentData) {
+        actionType = actionType;
+        prevData = prevData;
+        currentData = currentData;
     }
 
     public int getActionType() {
         return actionType;
     }
 
-    public nA getCurrentData() {
+    public BaseBean getCurrentData() {
         return currentData;
     }
 
-    public nA getPrevData() {
+    public BaseBean getPrevData() {
         return prevData;
     }
 }
