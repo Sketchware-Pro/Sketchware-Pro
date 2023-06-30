3abf0c774c61b0ee335365d17dad5ce0a2418981
Javkhlanbayar Khongorzul <jbk@duck.com>
2023-06-30 09:15:10 +0200

    chore: Drop unnecessary qualifications in CollapsibleEventLayout

--- app/src/main/java/com/besome/sketch/editor/event/CollapsibleEventLayout.java
+++ app/src/main/java/com/besome/sketch/editor/event/CollapsibleEventLayout.java
@@ -38,45 +38,45 @@ public class CollapsibleEventLayout extends FrameLayout {
     private void initialize(Context context) {
         this.context = context;
         wB.a(context, this, R.layout.fr_logic_list_item_buttons);
-        this.projectButtons = findViewById(R.id.project_buttons);
-        this.warning = findViewById(R.id.ll_warning);
-        this.confirm = findViewById(R.id.confirm_layout);
-        this.confirmYes = findViewById(R.id.confirm_yes);
-        this.confirmNo = findViewById(R.id.confirm_no);
+        projectButtons = findViewById(R.id.project_buttons);
+        warning = findViewById(R.id.ll_warning);
+        confirm = findViewById(R.id.confirm_layout);
+        confirmYes = findViewById(R.id.confirm_yes);
+        confirmNo = findViewById(R.id.confirm_no);
         TextView warningMessage = findViewById(R.id.tv_warning_message);
         TextView yes = findViewById(R.id.confirm_yes_text);
         yes.setText(xB.b().a(getContext(), R.string.common_word_continue));
         TextView no = findViewById(R.id.confirm_no_text);
         no.setText(xB.b().a(getContext(), R.string.common_word_cancel));
-        this.confirm.setVisibility(INVISIBLE);
-        this.warning.setVisibility(GONE);
+        confirm.setVisibility(INVISIBLE);
+        warning.setVisibility(GONE);
         warningMessage.setText(xB.b().a(getContext(), R.string.common_message_confirm));
-        this.reset = a(0, R.drawable.ic_reset_color_32dp, xB.b().a(context, R.string.common_word_reset));
-        this.delete = a(1, R.drawable.delete_96, xB.b().a(context, R.string.common_word_delete));
-        this.addToCollection = a(2, R.drawable.ic_bookmark_red_48dp, xB.b().a(context, R.string.logic_list_menu_add_to_collection));
-        this.addToCollection.setVisibility(GONE);
-        this.projectButtons.addView(this.reset);
-        this.projectButtons.addView(this.delete);
-        this.projectButtons.addView(this.addToCollection);
-        this.flipTopIn = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.flip_top_in);
-        this.flipTopOut = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.flip_top_out);
-        this.flipBottomIn = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.flip_bottom_in);
-        this.flipBottomOut = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.flip_bottom_out);
+        reset = a(0, R.drawable.ic_reset_color_32dp, xB.b().a(context, R.string.common_word_reset));
+        delete = a(1, R.drawable.delete_96, xB.b().a(context, R.string.common_word_delete));
+        addToCollection = a(2, R.drawable.ic_bookmark_red_48dp, xB.b().a(context, R.string.logic_list_menu_add_to_collection));
+        addToCollection.setVisibility(GONE);
+        projectButtons.addView(reset);
+        projectButtons.addView(delete);
+        projectButtons.addView(addToCollection);
+        flipTopIn = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.flip_top_in);
+        flipTopOut = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.flip_top_out);
+        flipBottomIn = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.flip_bottom_in);
+        flipBottomOut = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.flip_bottom_out);
     }
 
     public void b() {
-        this.delete.setVisibility(GONE);
+        delete.setVisibility(GONE);
     }
 
     public void c() {
-        this.addToCollection.setVisibility(GONE);
+        addToCollection.setVisibility(GONE);
     }
 
     public void d() {
-        this.flipTopOut.setTarget(this.projectButtons);
-        this.flipTopIn.setTarget(this.confirm);
+        flipTopOut.setTarget(projectButtons);
+        flipTopIn.setTarget(confirm);
         AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.play(this.flipTopOut).with(this.flipTopIn);
+        animatorSet.play(flipTopOut).with(flipTopIn);
         animatorSet.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
@@ -98,23 +98,23 @@ public class CollapsibleEventLayout extends FrameLayout {
     }
 
     public void e() {
-        this.delete.setVisibility(VISIBLE);
+        delete.setVisibility(VISIBLE);
     }
 
     public void f() {
-        this.addToCollection.setVisibility(VISIBLE);
+        addToCollection.setVisibility(VISIBLE);
     }
 
     public void setButtonOnClickListener(View.OnClickListener onClickListener) {
-        this.confirmYes.setOnClickListener(onClickListener);
-        this.confirmNo.setOnClickListener(onClickListener);
-        this.reset.setOnClickListener(onClickListener);
-        this.delete.setOnClickListener(onClickListener);
-        this.addToCollection.setOnClickListener(onClickListener);
+        confirmYes.setOnClickListener(onClickListener);
+        confirmNo.setOnClickListener(onClickListener);
+        reset.setOnClickListener(onClickListener);
+        delete.setOnClickListener(onClickListener);
+        addToCollection.setOnClickListener(onClickListener);
     }
 
     public final CollapsibleButton a(int i, int i2, String str) {
-        CollapsibleButton collapsibleButton = new CollapsibleButton(this.context);
+        CollapsibleButton collapsibleButton = new CollapsibleButton(context);
         collapsibleButton.b = i;
         collapsibleButton.d.setImageResource(i2);
         collapsibleButton.e.setText(str);
@@ -122,10 +122,10 @@ public class CollapsibleEventLayout extends FrameLayout {
     }
 
     public void a() {
-        this.flipBottomIn.setTarget(this.projectButtons);
-        this.flipBottomOut.setTarget(this.confirm);
+        flipBottomIn.setTarget(projectButtons);
+        flipBottomOut.setTarget(confirm);
         AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.play(this.flipBottomIn).with(this.flipBottomOut);
+        animatorSet.play(flipBottomIn).with(flipBottomOut);
         animatorSet.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
