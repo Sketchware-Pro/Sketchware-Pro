a20e24d8d1ad6ce0da5140274ed8df779800e83d
Javkhlanbayar Khongorzul <jbk@duck.com>
2023-07-09 18:06:57 +0200

    refactor, chore: Clean up a.a.a.Tu's methods, fields, and inner classes
    Makes them private and also gives them sensible names.

--- app/src/main/java/a/a/a/Tu.java
+++ app/src/main/java/a/a/a/Tu.java
@@ -27,14 +27,13 @@ import java.util.ArrayList;
 import mod.hey.studios.util.Helper;
 
 public class Tu extends LinearLayout implements Uu, View.OnClickListener {
-    public RecyclerView a;
-    public a b;
-    public ArrayList<AdTestDeviceBean> c;
+    private TestDeviceAdapter adapter;
+    private ArrayList<AdTestDeviceBean> testDevices;
 
     public Tu(Context context) {
         super(context);
-        c = new ArrayList<>();
-        a(context);
+        testDevices = new ArrayList<>();
+        initialize(context);
     }
 
     private String getCurrentDeviceId() {
@@ -52,11 +51,10 @@ public class Tu extends LinearLayout implements Uu, View.OnClickListener {
     }
 
     @Override
-    public void onClick(View view) {
-        if (view.getId() != R.id.layout_set_test_device) {
-            return;
+    public void onClick(View v) {
+        if (v.getId() == R.id.layout_set_test_device) {
+            showAddTestDeviceDialog();
         }
-        a();
     }
 
     @Override
@@ -66,38 +64,33 @@ public class Tu extends LinearLayout implements Uu, View.OnClickListener {
 
     @Override
     public void setData(ProjectLibraryBean projectLibraryBean) {
-        c = projectLibraryBean.testDevices;
-        b.notifyDataSetChanged();
+        testDevices = projectLibraryBean.testDevices;
+        adapter.notifyDataSetChanged();
     }
 
-    class a extends RecyclerView.Adapter<a.ViewHolder> {
-        public int c = -1;
-
-        class ViewHolder extends RecyclerView.ViewHolder {
-            public TextView t;
-            public ImageView u;
+    private class TestDeviceAdapter extends RecyclerView.Adapter<TestDeviceAdapter.ViewHolder> {
+        private class ViewHolder extends RecyclerView.ViewHolder {
+            public final TextView deviceId;
+            public final ImageView delete;
 
             public ViewHolder(@NonNull View itemView) {
                 super(itemView);
-                t = itemView.findViewById(R.id.tv_device_id);
-                u = itemView.findViewById(R.id.img_delete);
-                u.setOnClickListener(v -> {
-                    Tu.this.a(getLayoutPosition());
+                deviceId = itemView.findViewById(R.id.tv_device_id);
+                delete = itemView.findViewById(R.id.img_delete);
+                delete.setOnClickListener(v -> {
+                    showDeleteTestDeviceDialog(getLayoutPosition());
                 });
             }
         }
 
-        public a() {
-        }
-
         @Override
         public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-            holder.t.setText(Tu.this.c.get(position).deviceId);
+            holder.deviceId.setText(testDevices.get(position).deviceId);
         }
 
         @Override
         public int getItemCount() {
-            return Tu.this.c.size();
+            return testDevices.size();
         }
 
         @Override
@@ -107,23 +100,23 @@ public class Tu extends LinearLayout implements Uu, View.OnClickListener {
         }
     }
 
-    public final void a(Context context) {
+    private void initialize(Context context) {
         wB.a(context, this, R.layout.manage_library_admob_test_device);
         gB.b(this, 600, 200, null);
         ((TextView) findViewById(R.id.tv_set_test_device)).setText(xB.b().a(getContext(), R.string.design_library_admob_button_set_test_device));
         findViewById(R.id.layout_set_test_device).setOnClickListener(this);
-        a = findViewById(R.id.list_test_device);
-        a.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-        b = new a();
-        a.setAdapter(b);
+        RecyclerView testDevices = findViewById(R.id.list_test_device);
+        testDevices.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        adapter = new TestDeviceAdapter();
+        testDevices.setAdapter(adapter);
     }
 
     @Override
     public void a(ProjectLibraryBean projectLibraryBean) {
-        projectLibraryBean.testDevices = c;
+        projectLibraryBean.testDevices = testDevices;
     }
 
-    public final String a(String str) {
+    private String a(String str) {
         try {
             MessageDigest messageDigest = MessageDigest.getInstance("MD5");
             messageDigest.update(str.getBytes());
@@ -139,49 +132,49 @@ public class Tu extends LinearLayout implements Uu, View.OnClickListener {
         }
     }
 
-    public final void a() {
-        aB aBVar = new aB((Activity) getContext());
-        aBVar.b(xB.b().a(getContext(), R.string.design_library_admob_dialog_set_test_device_title));
-        aBVar.a(R.drawable.add_96_blue);
-        View a2 = wB.a(getContext(), R.layout.manage_library_setting_admob_test_device_add);
-        EditText editText = a2.findViewById(R.id.ed_device_id);
-        ((TextInputLayout) a2.findViewById(R.id.ti_device_id)).setHint(xB.b().a(getContext(), R.string.design_library_admob_dialog_set_test_device_hint_device_id));
-        SB sb = new SB(getContext(), a2.findViewById(R.id.ti_device_id), 1, 100);
-        editText.setText(getCurrentDeviceId());
-        editText.setPrivateImeOptions("defaultInputmode=english;");
-        aBVar.a(a2);
-        aBVar.b(xB.b().a(getContext(), R.string.common_word_add), v -> {
-            if (sb.b()) {
-                String text = editText.getText().toString();
-                for (AdTestDeviceBean device : c) {
-                    if (device.deviceId.equals(text)) {
+    private void showAddTestDeviceDialog() {
+        aB dialog = new aB((Activity) getContext());
+        dialog.b(xB.b().a(getContext(), R.string.design_library_admob_dialog_set_test_device_title));
+        dialog.a(R.drawable.add_96_blue);
+        View content = wB.a(getContext(), R.layout.manage_library_setting_admob_test_device_add);
+        EditText edDeviceId = content.findViewById(R.id.ed_device_id);
+        ((TextInputLayout) content.findViewById(R.id.ti_device_id)).setHint(xB.b().a(getContext(), R.string.design_library_admob_dialog_set_test_device_hint_device_id));
+        SB validator = new SB(getContext(), content.findViewById(R.id.ti_device_id), 1, 100);
+        edDeviceId.setText(getCurrentDeviceId());
+        edDeviceId.setPrivateImeOptions("defaultInputmode=english;");
+        dialog.a(content);
+        dialog.b(xB.b().a(getContext(), R.string.common_word_add), v -> {
+            if (validator.b()) {
+                String deviceId = edDeviceId.getText().toString();
+                for (AdTestDeviceBean device : testDevices) {
+                    if (device.deviceId.equals(deviceId)) {
                         bB.a(getContext(), xB.b().a(getContext(), R.string.design_library_admob_dialog_set_test_device_warning_duplicated), 0).show();
                         return;
                     }
                 }
-                c.add(new AdTestDeviceBean(text));
-                b.notifyItemInserted(c.size() - 1);
-                aBVar.dismiss();
+                testDevices.add(new AdTestDeviceBean(deviceId));
+                adapter.notifyItemInserted(testDevices.size() - 1);
+                dialog.dismiss();
             } else {
-                editText.requestFocus();
+                edDeviceId.requestFocus();
             }
         });
-        aBVar.a(xB.b().a(getContext(), R.string.common_word_cancel), Helper.getDialogDismissListener(aBVar));
-        aBVar.show();
+        dialog.a(xB.b().a(getContext(), R.string.common_word_cancel), Helper.getDialogDismissListener(dialog));
+        dialog.show();
     }
 
-    public final void a(int i) {
-        aB aBVar = new aB((Activity) getContext());
-        aBVar.b(xB.b().a(getContext(), R.string.design_library_admob_dialog_delete_test_device_title));
-        aBVar.a(R.drawable.delete_96);
-        aBVar.a(xB.b().a(getContext(), R.string.design_library_admob_dialog_confirm_delete_test_device));
-        aBVar.b(xB.b().a(getContext(), R.string.common_word_delete), v -> {
-            c.remove(i);
-            b.notifyItemRemoved(i);
+    private void showDeleteTestDeviceDialog(int index) {
+        aB dialog = new aB((Activity) getContext());
+        dialog.b(xB.b().a(getContext(), R.string.design_library_admob_dialog_delete_test_device_title));
+        dialog.a(R.drawable.delete_96);
+        dialog.a(xB.b().a(getContext(), R.string.design_library_admob_dialog_confirm_delete_test_device));
+        dialog.b(xB.b().a(getContext(), R.string.common_word_delete), v -> {
+            testDevices.remove(index);
+            adapter.notifyItemRemoved(index);
             bB.a(getContext(), xB.b().a(getContext(), R.string.common_message_complete_delete), 0).show();
-            aBVar.dismiss();
+            dialog.dismiss();
         });
-        aBVar.a(xB.b().a(getContext(), R.string.common_word_cancel), Helper.getDialogDismissListener(aBVar));
-        aBVar.show();
+        dialog.a(xB.b().a(getContext(), R.string.common_word_cancel), Helper.getDialogDismissListener(dialog));
+        dialog.show();
     }
 }
