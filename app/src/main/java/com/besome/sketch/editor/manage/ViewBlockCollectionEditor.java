4f7957d50bbaa6a0abc9b46f8e4a2c7bd25d3a80
Javkhlanbayar Khongorzul <jbk@duck.com>
2023-07-21 22:59:16 +0200

    feat: Add ViewBlockCollectionEditor in Java
    Decompiled from Sketchware Pro v6.3.0 fix1's .apk file using jadx v1.4.7.

---
+++ app/src/main/java/com/besome/sketch/editor/manage/ViewBlockCollectionEditor.java
@@ -0,0 +1,50 @@
+package com.besome.sketch.editor.manage;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+import com.besome.sketch.editor.logic.BlockPane;
+import com.besome.sketch.editor.view.LogicEditorScrollView;
+
+public class ViewBlockCollectionEditor extends LogicEditorScrollView {
+    public Context i;
+    public BlockPane j;
+    public boolean k;
+    public int[] l;
+
+    public ViewBlockCollectionEditor(Context context) {
+        super(context);
+        this.k = true;
+        this.l = new int[2];
+        a(context);
+    }
+
+    private void a(Context context) {
+        this.i = context;
+        this.j = new BlockPane(this.i);
+        this.j.setLayoutParams(new FrameLayout.LayoutParams(-2, -2));
+        addView(this.j);
+    }
+
+    public BlockPane getBlockPane() {
+        return this.j;
+    }
+
+    @Override
+    public void onLayout(boolean z, int i, int i2, int i3, int i4) {
+        super.onLayout(z, i, i2, i3, i4);
+        if (this.k) {
+            this.j.getLayoutParams().width = i3 - i;
+            this.j.getLayoutParams().height = i4 - i2;
+            this.j.b();
+            this.k = false;
+        }
+    }
+
+    public ViewBlockCollectionEditor(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        this.k = true;
+        this.l = new int[2];
+        a(context);
+    }
+}
