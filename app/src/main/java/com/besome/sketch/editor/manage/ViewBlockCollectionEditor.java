9059c7b7aeeb0830ec5347e0bb6483899fe988e6
Javkhlanbayar Khongorzul <jbk@duck.com>
2023-07-21 23:05:49 +0200

    chore: Clean up fields in ViewBlockCollectionEditor

--- app/src/main/java/com/besome/sketch/editor/manage/ViewBlockCollectionEditor.java
+++ app/src/main/java/com/besome/sketch/editor/manage/ViewBlockCollectionEditor.java
@@ -2,15 +2,14 @@ package com.besome.sketch.editor.manage;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import com.besome.sketch.editor.logic.BlockPane;
 import com.besome.sketch.editor.view.LogicEditorScrollView;
 
 public class ViewBlockCollectionEditor extends LogicEditorScrollView {
-    public Context i;
-    public BlockPane j;
-    public boolean k;
-    public int[] l;
+    private BlockPane blockPane;
+    private boolean layoutParamsToBeSet;
 
     public ViewBlockCollectionEditor(Context context) {
         this(context, null);
@@ -18,30 +17,29 @@ public class ViewBlockCollectionEditor extends LogicEditorScrollView {
 
     public ViewBlockCollectionEditor(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
-        this.k = true;
-        this.l = new int[2];
+        layoutParamsToBeSet = true;
         initialize(context);
     }
 
     private void initialize(Context context) {
-        this.i = context;
-        this.j = new BlockPane(this.i);
-        this.j.setLayoutParams(new FrameLayout.LayoutParams(-2, -2));
-        addView(this.j);
+        blockPane = new BlockPane(context);
+        blockPane.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        addView(blockPane);
     }
 
     public BlockPane getBlockPane() {
-        return this.j;
+        return blockPane;
     }
 
     @Override
     public void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-        if (this.k) {
-            this.j.getLayoutParams().width = right - left;
-            this.j.getLayoutParams().height = bottom - top;
-            this.j.b();
-            this.k = false;
+        if (layoutParamsToBeSet) {
+            blockPane.getLayoutParams().width = right - left;
+            blockPane.getLayoutParams().height = bottom - top;
+            blockPane.b();
+            layoutParamsToBeSet = false;
         }
     }
 }
