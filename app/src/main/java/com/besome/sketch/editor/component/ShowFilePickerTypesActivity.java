f43eff358657490b9cbd5b659ffd6e9b1be7546f
Javkhlanbayar Khongorzul <jbk@duck.com>
2023-07-21 23:14:16 +0200

    refactor: Use R fields and constants in ShowFilePickerTypesActivity

--- app/src/main/java/com/besome/sketch/editor/component/ShowFilePickerTypesActivity.java
+++ app/src/main/java/com/besome/sketch/editor/component/ShowFilePickerTypesActivity.java
@@ -1,13 +1,17 @@
 package com.besome.sketch.editor.component;
 
-import a.a.a.xB;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
 import com.besome.sketch.lib.base.BaseDialogActivity;
 import com.google.android.gms.analytics.HitBuilders;
+import com.sketchware.remod.R;
+
+import a.a.a.xB;
 
 public class ShowFilePickerTypesActivity extends BaseDialogActivity implements View.OnClickListener {
     public RadioGroup t;
@@ -16,20 +20,20 @@ public class ShowFilePickerTypesActivity extends BaseDialogActivity implements V
     public void onClick(View view) {
         String str;
         int id2 = view.getId();
-        if (id2 == 2131230909) {
+        if (id2 == R.id.common_dialog_cancel_button) {
             finish();
-        } else if (id2 != 2131230914) {
+        } else if (id2 != R.id.common_dialog_ok_button) {
         } else {
-            if (this.t.getCheckedRadioButtonId() == 2131231633) {
+            if (this.t.getCheckedRadioButtonId() == R.id.radio_all) {
                 str = "*/*";
-            } else if (this.t.getCheckedRadioButtonId() == 2131231641) {
+            } else if (this.t.getCheckedRadioButtonId() == R.id.radio_image) {
                 str = "image/*";
             } else {
-                str = this.t.getCheckedRadioButtonId() == 2131231635 ? "audio/*" : "text/*";
+                str = this.t.getCheckedRadioButtonId() == R.id.radio_audio ? "audio/*" : "text/*";
             }
             Intent intent = new Intent();
             intent.putExtra("mime_type", str);
-            setResult(-1, intent);
+            setResult(Activity.RESULT_OK, intent);
             finish();
         }
     }
@@ -37,15 +41,15 @@ public class ShowFilePickerTypesActivity extends BaseDialogActivity implements V
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        e(xB.b().a(this, 2131625118));
-        d(xB.b().a(getApplicationContext(), 2131625035));
-        b(xB.b().a(getApplicationContext(), 2131624974));
-        setContentView(2131427726);
-        this.t = (RadioGroup) findViewById(2131231639);
-        ((RadioButton) findViewById(2131231633)).setText(xB.b().a(this, 2131625119));
-        ((RadioButton) findViewById(2131231641)).setText(xB.b().a(this, 2131625121));
-        ((RadioButton) findViewById(2131231635)).setText(xB.b().a(this, 2131625120));
-        ((RadioButton) findViewById(2131231648)).setText(xB.b().a(this, 2131625122));
+        e(xB.b().a(this, R.string.component_file_picker_title_select_mime_type));
+        d(xB.b().a(getApplicationContext(), R.string.common_word_select));
+        b(xB.b().a(getApplicationContext(), R.string.common_word_cancel));
+        setContentView(R.layout.show_file_picker_types);
+        this.t = (RadioGroup) findViewById(R.id.radio_group);
+        ((RadioButton) findViewById(R.id.radio_all)).setText(xB.b().a(this, R.string.component_file_picker_title_select_mime_type_all_files));
+        ((RadioButton) findViewById(R.id.radio_image)).setText(xB.b().a(this, R.string.component_file_picker_title_select_mime_type_image_files));
+        ((RadioButton) findViewById(R.id.radio_audio)).setText(xB.b().a(this, R.string.component_file_picker_title_select_mime_type_audio_files));
+        ((RadioButton) findViewById(R.id.radio_text)).setText(xB.b().a(this, R.string.component_file_picker_title_select_mime_type_text_files));
         this.r.setOnClickListener(this);
         this.s.setOnClickListener(this);
     }
