34f3f5cbb979039a993fc863eaa16769af7d6f77
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2023-07-24 00:45:58 +0530

    feat: Add CustomViewPager.class to java

---
+++ app/src/main/java/com/besome/sketch/lib/ui/CustomViewPager.java
@@ -0,0 +1,45 @@
+package com.besome.sketch.lib.ui;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.viewpager.widget.ViewPager;
+
+public class CustomViewPager extends ViewPager {
+    private boolean isTouchEnabled = true;
+
+    public CustomViewPager(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    public void k() {
+        isTouchEnabled = false;
+    }
+
+    public void l() {
+        isTouchEnabled = true;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent motionEvent) {
+        if (isTouchEnabled) {
+            try {
+                return super.onInterceptTouchEvent(motionEvent);
+            } catch (Exception var3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        if (isTouchEnabled) {
+            return super.onTouchEvent(motionEvent);
+        }
+        return false;
+    }
+}
