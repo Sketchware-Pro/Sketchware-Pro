7d8069a63a5d1ad18006c3d9f1f913ed52955f60
Elfilibusterismo <113604229+elfilibusterismo@users.noreply.github.com>
2023-08-19 00:40:13 +0530

    feat: Add Actual TabLayout preview in ViewEditor

---
+++ app/src/main/java/com/besome/sketch/editor/view/item/ItemTabLayout.java
@@ -0,0 +1,81 @@
+package com.besome.sketch.editor.view.item;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+import androidx.annotation.NonNull;
+
+import com.besome.sketch.beans.ViewBean;
+import com.google.android.material.tabs.TabLayout;
+import com.sketchware.remod.R;
+
+import a.a.a.sy;
+import a.a.a.wB;
+
+public class ItemTabLayout extends TabLayout implements sy {
+
+    private final Paint paint;
+    private final Rect rect;
+    private final float paddingFactor;
+    private ViewBean viewBean;
+    private boolean hasSelection;
+    private boolean hasFixed;
+
+    public ItemTabLayout(Context context) {
+        super(context);
+        paddingFactor = wB.a(context, 1.0f);
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(0x9599d5d0);
+        rect = new Rect();
+
+        setDrawingCacheEnabled(true);
+        addTab(newTab().setText("Tab 1"), true);
+        addTab(newTab().setText("Tab 2"));
+        addTab(newTab().setText("Tab 3"));
+    }
+
+    @Override
+    public ViewBean getBean() {
+        return viewBean;
+    }
+
+    @Override
+    public void setBean(ViewBean viewBean) {
+        this.viewBean = viewBean;
+    }
+
+    @Override
+    public boolean getFixed() {
+        return hasFixed;
+    }
+
+    public void setFixed(boolean z) {
+        hasFixed = z;
+    }
+
+    public boolean getSelection() {
+        return hasSelection;
+    }
+
+    @Override
+    public void setSelection(boolean z) {
+        hasSelection = z;
+        invalidate();
+    }
+
+    @Override
+    public void onDraw(@NonNull Canvas canvas) {
+        if (hasSelection) {
+            rect.set(0, 0, getMeasuredWidth(), getMeasuredHeight());
+            canvas.drawRect(rect, paint);
+        }
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public void setPadding(int left, int top, int right, int bottom) {
+        super.setPadding((int) (left * paddingFactor), (int) (top * paddingFactor), (int) (right * paddingFactor), (int) (bottom * paddingFactor));
+    }
+}
