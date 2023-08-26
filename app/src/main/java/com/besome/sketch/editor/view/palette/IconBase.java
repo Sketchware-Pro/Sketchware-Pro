dcb5fafde4d578c1e976ae4f5cc9b9964ce9a16d
TechnicalStudioDeveloper <appdeveloper400@gmail.com>
2023-08-26 16:16:03 +0530

    feat: Added IconBase in java

---
+++ app/src/main/java/com/besome/sketch/editor/view/palette/IconBase.java
@@ -0,0 +1,32 @@
+package com.besome.sketch.editor.view.palette;
+
+import android.content.Context;
+import com.besome.sketch.beans.ViewBean;
+import com.besome.sketch.lib.base.BaseWidget;
+
+public class IconBase extends BaseWidget {
+    public String e;
+
+    public IconBase(Context context) {
+        super(context);
+        this.setBackgroundResource(2131165891);
+    }
+
+    public ViewBean getBean() {
+        return null;
+    }
+
+    public String getName() {
+        return this.e;
+    }
+
+    public void setName(String string) {
+        this.e = string;
+    }
+
+    public void setText(String string) {
+        this.b.setTextSize(2, 11.0f);
+        this.setWidgetName(string);
+    }
+}
+
