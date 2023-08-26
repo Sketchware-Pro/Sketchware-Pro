eab115af750c7940e564f3d9b0e7c763ede44f2b
TechnicalStudioDeveloper <appdeveloper400@gmail.com>
2023-08-26 16:09:30 +0530

    chore: Cleanup unnecessary redundant cast in BaseWidget

--- app/src/main/java/com/besome/sketch/lib/base/BaseWidget.java
+++ app/src/main/java/com/besome/sketch/lib/base/BaseWidget.java
@@ -18,43 +18,43 @@ public class BaseWidget extends LinearLayout {
 
     public BaseWidget(Context context) {
         super(context);
-        this.a(context);
+        a(context);
     }
 
     public void a(Context context) {
-        wB.a((Context)context, (ViewGroup)this, (int)R.layout.widget_layout);
-        this.a = (ImageView)this.findViewById(R.id.img_widget);
-        this.b = (TextView)this.findViewById(R.id.tv_widget);
-        this.b.setTextSize(12.0f);
-        this.b.setGravity(3);
-        this.b.setPadding((int)wB.a((Context)this.getContext(), (float)2.0f), 0, 0, 0);
-        this.setDrawingCacheEnabled(true);
+        wB.a(context, this, R.layout.widget_layout);
+        a = findViewById(R.id.img_widget);
+        b = findViewById(R.id.tv_widget);
+        b.setTextSize(12.0f);
+        b.setGravity(3);
+        b.setPadding((int)wB.a(getContext(), 2.0f), 0, 0, 0);
+        setDrawingCacheEnabled(true);
     }
 
     public int getWidgetImageResId() {
-        return this.c;
+        return c;
     }
 
     public String getWidgetName() {
-        return this.b.getText().toString();
+        return b.getText().toString();
     }
 
     public int getWidgetType() {
-        return this.d;
+        return d;
     }
 
     public void setWidgetImage(int n) {
-        this.c = n;
-        this.a.setImageResource(n);
+        c = n;
+        a.setImageResource(n);
     }
 
     public void setWidgetName(String string) {
-        this.b.setText((CharSequence)string);
+        b.setText(string);
     }
 
     public void setWidgetType(a a2) {
-        this.setBackgroundResource(R.drawable.icon_bg);
-        this.d = a2.ordinal();
+        setBackgroundResource(R.drawable.icon_bg);
+        d = a2.ordinal();
     }
 
     public static enum a {
