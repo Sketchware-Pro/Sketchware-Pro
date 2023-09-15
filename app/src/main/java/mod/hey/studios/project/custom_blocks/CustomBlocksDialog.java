5bb66151425e44a2786faf8b54ea7df5cdec9ad5
Ilyasse <ilyassesalama7@gmail.com>
2023-09-15 04:27:51 +0100

    fix: Dismiss button not showing for longer custom blocks list

--- app/src/main/java/mod/hey/studios/project/custom_blocks/CustomBlocksDialog.java
+++ app/src/main/java/mod/hey/studios/project/custom_blocks/CustomBlocksDialog.java
@@ -9,9 +9,10 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
+
 import com.besome.sketch.beans.BlockBean;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.sketchware.remod.R;
 import com.sketchware.remod.databinding.ViewUsedCustomBlocksBinding;
 
 import java.util.ArrayList;
@@ -20,14 +21,6 @@ import a.a.a.Rs;
 import mod.hey.studios.editor.manage.block.v2.BlockLoader;
 
 public class CustomBlocksDialog {
-
-    /* Note by Hey! Studios DEV */
-    /*
-     i had this idea to create a dialog where one can see the custom blocks they used in their project.
-     its actually completed, but for some reason i havent released it publicly. will probably do in 6.3.0.
-     //16.12.2020 note: i implemented it.
-     */
-
     public static void show(Activity context, String sc_id) {
         ViewUsedCustomBlocksBinding dialogBinding = ViewUsedCustomBlocksBinding.inflate(context.getLayoutInflater());
         var blockContainer = dialogBinding.customBlocksContainer;
@@ -43,21 +36,18 @@ public class CustomBlocksDialog {
                 dialogBinding.customBlocksContainer.addView(createBlockInfo(context, block, sc_id));
                 blockContainer.addView(createBlock(context, block));
 
-                if (i != (list.size() - 1)) {
-                    blockContainer.addView(createSpace(context));
-                }
+                if (i != (list.size() - 1)) blockContainer.addView(createSpace(context));
             }
         }
 
-        var dialog = new MaterialAlertDialogBuilder(context)
+        var dialogBuilder = new MaterialAlertDialogBuilder(context)
                 .setTitle("Used Custom Blocks")
-                .setMessage(subtitle)
-                .setPositiveButton(R.string.common_word_close, null);
+                .setMessage(subtitle);
+        if (!list.isEmpty()) dialogBuilder.setView(dialogBinding.getRoot());
 
-        if (!list.isEmpty())
-            dialog.setView(dialogBinding.getRoot());
-
-        dialog.show();
+        AlertDialog alertDialog = dialogBuilder.create();
+        dialogBinding.okayButton.setOnClickListener(view -> alertDialog.dismiss());
+        alertDialog.show();
     }
 
     // Helpers
