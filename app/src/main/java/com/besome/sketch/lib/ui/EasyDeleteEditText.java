ff82f60ce503d2dd645a3e4a79580a3756140531
DevKumar00001 <appdeveloper400@gmail.com>
2023-09-27 18:12:51 +0530

    chore: Cleanup unnecessary qualifier(this) in EasyDeleteEditText.java

--- app/src/main/java/com/besome/sketch/lib/ui/EasyDeleteEditText.java
+++ app/src/main/java/com/besome/sketch/lib/ui/EasyDeleteEditText.java
@@ -25,49 +25,49 @@ public class EasyDeleteEditText extends RelativeLayout {
     
     public EasyDeleteEditText(final Context context) {
         super(context);
-        this.a(context);
+        a(context);
     }
     
     public EasyDeleteEditText(final Context context, final AttributeSet set) {
         super(context, set);
-        this.a(context);
+        a(context);
     }
     
     public EasyDeleteEditText(final Context context, final AttributeSet set, final int n) {
         super(context, set, n);
-        this.a(context);
+        a(context);
     }
     
     public final void a() {
-        if (this.c.isEnabled() && this.c.hasFocus() && this.c.length() > 0) {
-            this.b.setVisibility(0);
+        if (c.isEnabled() && c.hasFocus() && c.length() > 0) {
+            b.setVisibility(0);
         }
         else {
-            this.b.setVisibility(8);
+            b.setVisibility(8);
         }
     }
     
     public final void a(final Context a) {
         wB.a(this.a = a, (ViewGroup)this, 2131427412);
-        this.b = (ImageView)this.findViewById(2131231132);
-        this.c = (EditText)this.findViewById(2131230983);
-        this.d = (TextInputLayout)this.findViewById(2131230984);
-        this.b.setVisibility(8);
-        this.b.setOnClickListener((View$OnClickListener)new VA(this));
-        this.c.addTextChangedListener((TextWatcher)new WA(this));
-        this.c.setOnFocusChangeListener((View$OnFocusChangeListener)new XA(this));
-        this.a();
+        b = (ImageView)findViewById(2131231132);
+        c = (EditText)findViewById(2131230983);
+        d = (TextInputLayout)findViewById(2131230984);
+        b.setVisibility(8);
+        b.setOnClickListener((View$OnClickListener)new VA(this));
+        c.addTextChangedListener((TextWatcher)new WA(this));
+        c.setOnFocusChangeListener((View$OnFocusChangeListener)new XA(this));
+        a();
     }
     
     public EditText getEditText() {
-        return this.c;
+        return c;
     }
     
     public TextInputLayout getTextInputLayout() {
-        return this.d;
+        return d;
     }
     
     public void setHint(final String hint) {
-        this.d.setHint((CharSequence)hint);
+        d.setHint((CharSequence)hint);
     }
 }
