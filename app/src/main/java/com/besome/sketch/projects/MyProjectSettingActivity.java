e0e72de4d424dc54a40c7bd7a0193971eb71b26e
Ilyasse <ilyassesalama7@gmail.com>
2023-10-07 19:19:28 +0100

    refactor: Use AndroidX ExifInterface

--- app/src/main/java/com/besome/sketch/projects/MyProjectSettingActivity.java
+++ app/src/main/java/com/besome/sketch/projects/MyProjectSettingActivity.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.media.ExifInterface;
+import androidx.exifinterface.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
