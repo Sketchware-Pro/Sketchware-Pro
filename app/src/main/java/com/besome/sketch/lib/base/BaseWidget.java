211b381046b453bab1d0039d7a5aa17c8983cf4e
Remaker <farzasz7@gmail.com>
2024-01-13 19:16:52 +0700

    fix(editor): Small UI fixes

--- app/src/main/java/com/besome/sketch/lib/base/BaseWidget.java
+++ app/src/main/java/com/besome/sketch/lib/base/BaseWidget.java
@@ -2,6 +2,7 @@ package com.besome.sketch.lib.base;
 
 import android.content.Context;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,16 +22,19 @@ public class BaseWidget extends LinearLayout {
 
     public BaseWidget(Context context) {
         super(context);
+        View.inflate(context, R.layout.widget_layout, this);
+
+        setOrientation(HORIZONTAL);
+        setGravity(Gravity.CENTER);
+
         a(context);
     }
 
     public void a(Context context) {
-        wB.a(context, this, R.layout.widget_layout);
         img_widget = findViewById(R.id.img_widget);
         tv_widget = findViewById(R.id.tv_widget);
-        tv_widget.setTextSize(12);
-        tv_widget.setGravity(Gravity.LEFT);
-        tv_widget.setPadding((int) wB.a(getContext(), 2.0f), 0, 0, 0);
+
+        setBackgroundResource(R.drawable.icon_bg);
         setDrawingCacheEnabled(true);
     }
 
@@ -60,7 +64,6 @@ public class BaseWidget extends LinearLayout {
     }
 
     public void setWidgetType(a widgetType) {
-        setBackgroundResource(R.drawable.icon_bg);
         this.widgetType = widgetType.ordinal();
     }
 
