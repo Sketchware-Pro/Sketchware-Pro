3a16856fbc47250c700e5167423560ed05699358
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2024-03-05 21:16:06 +0530

    style: Code clean-up

--- app/src/main/java/a/a/a/Pw.java
+++ app/src/main/java/a/a/a/Pw.java
@@ -34,29 +34,29 @@ public class Pw extends RelativeLayout implements View.OnClickListener {
 
     public Pw(Context context, boolean idk) {
         super(context);
-        this.a(idk);
+        a(idk);
     }
 
     private RadioButton a(String fileName) {
-        RadioButton radioButton = new RadioButton(this.getContext());
+        RadioButton radioButton = new RadioButton(getContext());
         radioButton.setText(fileName);
         radioButton.setTag(fileName);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(-1, (int) (wB.a(this.getContext(), 1.0F) * 40.0F));
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(-1, (int) (wB.a(getContext(), 1.0F) * 40.0F));
         radioButton.setGravity(19);
         radioButton.setLayoutParams(layoutParams);
         return radioButton;
     }
 
     private void a() {
-        aB dialog = new aB((Activity) this.getContext());
-        dialog.b(this.c.getText().toString());
-        dialog.a(this.f);
-        View rootView = wB.a(this.getContext(), R.layout.property_popup_selector_single);
+        aB dialog = new aB((Activity) getContext());
+        dialog.b(c.getText().toString());
+        dialog.a(f);
+        View rootView = wB.a(getContext(), R.layout.property_popup_selector_single);
         ViewGroup j = (ViewGroup) rootView.findViewById(R.id.rg_content);
-        j.addView(this.a("none"));
+        j.addView(a("none"));
 
-        for (ProjectFileBean projectFileBean : this.i) {
-            RadioButton var4 = this.a(projectFileBean.fileName);
+        for (ProjectFileBean projectFileBean : i) {
+            RadioButton var4 = a(projectFileBean.fileName);
             j.addView(var4);
         }
 
@@ -70,7 +70,7 @@ public class Pw extends RelativeLayout implements View.OnClickListener {
         }
 
         dialog.a(rootView);
-        dialog.b(xB.b().a(this.getContext(), 2131625035), view -> {
+        dialog.b(xB.b().a(getContext(), 2131625035), view -> {
             for (int i = 0, childCount = j.getChildCount(); i < childCount; i++) {
                 RadioButton radioButton = (RadioButton) j.getChildAt(i);
                 if (radioButton.isChecked()) {
@@ -82,57 +82,57 @@ public class Pw extends RelativeLayout implements View.OnClickListener {
             }
             dialog.dismiss();
         });
-        dialog.a(xB.b().a(this.getContext(), 2131624974), Helper.getDialogDismissListener(dialog));
+        dialog.a(xB.b().a(getContext(), 2131624974), Helper.getDialogDismissListener(dialog));
         dialog.show();
     }
 
     private void a(boolean var2) {
-        wB.a(this.getContext(), this, R.layout.property_selector_item);
-        this.c = (TextView) this.findViewById(R.id.tv_name);
-        this.d = (TextView) this.findViewById(R.id.tv_value);
-        this.g = this.findViewById(R.id.property_item);
-        this.h = this.findViewById(R.id.property_menu_item);
-        this.e = (ImageView) this.findViewById(R.id.img_left_icon);
+        wB.a(getContext(), this, R.layout.property_selector_item);
+        c = (TextView) findViewById(R.id.tv_name);
+        d = (TextView) findViewById(R.id.tv_value);
+        g = findViewById(R.id.property_item);
+        h = findViewById(R.id.property_menu_item);
+        e = (ImageView) findViewById(R.id.img_left_icon);
         if (var2) {
-            this.setOnClickListener(this);
-            this.setSoundEffectsEnabled(true);
+            setOnClickListener(this);
+            setSoundEffectsEnabled(true);
         }
 
     }
 
     public String getKey() {
-        return this.a;
+        return a;
     }
 
     public String getValue() {
-        return this.b;
+        return b;
     }
 
     public void onClick(View var1) {
         if (!mB.a()) {
             if ("property_custom_view_listview".equals(a)) {
-                this.a();
+                a();
             }
         }
     }
 
     public void setCustomView(ArrayList<ProjectFileBean> customView) {
-        this.i = customView;
+        i = customView;
     }
 
     public void setKey(String key) {
-        this.a = key;
-        int var2 = this.getResources().getIdentifier(key, "string", this.getContext().getPackageName());
+        a = key;
+        int var2 = getResources().getIdentifier(key, "string", getContext().getPackageName());
         if (var2 > 0) {
-            this.c.setText(xB.b().a(this.getResources(), var2));
-            this.f = 2131165638;
-            if (this.h.getVisibility() == View.VISIBLE) {
-                ImageView var3 = (ImageView) this.findViewById(R.id.img_icon);
-                TextView var4 = (TextView) this.findViewById(R.id.tv_title);
-                var3.setImageResource(this.f);
-                var4.setText(xB.b().a(this.getContext(), var2));
+            c.setText(xB.b().a(getResources(), var2));
+            f = 2131165638;
+            if (h.getVisibility() == View.VISIBLE) {
+                ImageView var3 = (ImageView) findViewById(R.id.img_icon);
+                TextView var4 = (TextView) findViewById(R.id.tv_title);
+                var3.setImageResource(f);
+                var4.setText(xB.b().a(getContext(), var2));
             } else {
-                this.e.setImageResource(this.f);
+                e.setImageResource(f);
             }
         }
 
@@ -144,11 +144,11 @@ public class Pw extends RelativeLayout implements View.OnClickListener {
 
     public void setOrientationItem(int orientationItem) {
         if (orientationItem == 0) {
-            this.g.setVisibility(View.GONE);
-            this.h.setVisibility(View.VISIBLE);
+            g.setVisibility(View.GONE);
+            h.setVisibility(View.VISIBLE);
         } else {
-            this.g.setVisibility(View.VISIBLE);
-            this.h.setVisibility(View.GONE);
+            g.setVisibility(View.VISIBLE);
+            h.setVisibility(View.GONE);
         }
 
     }
@@ -167,7 +167,7 @@ public class Pw extends RelativeLayout implements View.OnClickListener {
             var2 = "none";
         }
 
-        this.b = var2;
-        this.d.setText(var2);
+        b = var2;
+        d.setText(var2);
     }
 }
