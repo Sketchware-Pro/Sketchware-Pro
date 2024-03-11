060c17c7adbdb7f5ce7c3c77f43788c8543b11a7
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2024-03-12 05:12:53 +0530

    style: Some code clean-ups in QuizBoard.java

--- app/src/main/java/com/besome/sketch/tools/QuizBoard.java
+++ app/src/main/java/com/besome/sketch/tools/QuizBoard.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package com.besome.sketch.tools;
 
 import android.content.Context;
@@ -29,6 +24,7 @@ import a.a.a.tq;
 import a.a.a.wB;
 
 public class QuizBoard extends LinearLayout implements View.OnClickListener {
+
     public final int a = 15000;
     public final int b = 250;
     public final int c = 2000;
@@ -51,36 +47,36 @@ public class QuizBoard extends LinearLayout implements View.OnClickListener {
 
     public QuizBoard(Context var1) {
         super(var1);
-        this.a(var1);
+        a(var1);
     }
 
     private void setData(QuizBean var1) {
-        this.r = var1;
-        this.e.setText(var1.question);
+        r = var1;
+        e.setText(var1.question);
         int var2 = var1.type;
         if (var2 == 1) {
-            this.f.setVisibility(0);
-            this.g.setVisibility(0);
-            this.h.setVisibility(0);
-            mB.a(this.g, 1);
-            mB.a(this.h, 1);
-            this.g.setOnClickListener(this);
-            this.h.setOnClickListener(this);
-            this.i.setVisibility(8);
+            f.setVisibility(0);
+            g.setVisibility(0);
+            h.setVisibility(0);
+            mB.a(g, 1);
+            mB.a(h, 1);
+            g.setOnClickListener(this);
+            h.setOnClickListener(this);
+            i.setVisibility(8);
         } else if (var2 == 2) {
-            this.i.setVisibility(0);
-            this.j.setOnClickListener(this);
-            this.k.setOnClickListener(this);
-            this.l.setText(var1.answerA);
-            this.m.setText(var1.answerB);
-            this.f.setVisibility(8);
+            i.setVisibility(0);
+            j.setOnClickListener(this);
+            k.setOnClickListener(this);
+            l.setText(var1.answerA);
+            m.setText(var1.answerB);
+            f.setVisibility(8);
         }
 
     }
 
     private void setTimeoutProgress(int var1) {
         int var2 = var1 / 250;
-        var1 = this.p.getChildCount();
+        var1 = p.getChildCount();
 
         while (true) {
             --var1;
@@ -88,88 +84,88 @@ public class QuizBoard extends LinearLayout implements View.OnClickListener {
                 return;
             }
 
-            this.p.getChildAt(var1).setBackgroundColor(-1118482);
+            p.getChildAt(var1).setBackgroundColor(-1118482);
         }
     }
 
     public void a() {
-        a var1 = this.s;
+        a var1 = s;
         if (var1 != null) {
             var1.cancel();
-            this.s = null;
+            s = null;
         }
 
     }
 
     public final void a(Context var1) {
         wB.a(var1, this, layout.quiz_board);
-        this.d = (TextView) this.findViewById(id.tv_remaing_time);
-        this.e = (TextView) this.findViewById(id.tv_question);
-        this.p = (LinearLayout) this.findViewById(id.timeout_bar);
-        this.f = (RelativeLayout) this.findViewById(id.layout_answer_ox);
-        this.g = (ImageView) this.findViewById(id.img_answer_o);
-        this.h = (ImageView) this.findViewById(id.img_answer_x);
-        this.i = (LinearLayout) this.findViewById(id.layout_answer_ab);
-        this.j = this.findViewById(id.view_answer_a);
-        this.k = this.findViewById(id.view_answer_b);
-        this.l = (TextView) this.findViewById(id.tv_answer_a);
-        this.m = (TextView) this.findViewById(id.tv_answer_b);
-        this.n = (ImageView) this.findViewById(id.img_answer_a);
-        this.o = (ImageView) this.findViewById(id.img_answer_b);
-        this.g();
+        d = (TextView) findViewById(id.tv_remaing_time);
+        e = (TextView) findViewById(id.tv_question);
+        p = (LinearLayout) findViewById(id.timeout_bar);
+        f = (RelativeLayout) findViewById(id.layout_answer_ox);
+        g = (ImageView) findViewById(id.img_answer_o);
+        h = (ImageView) findViewById(id.img_answer_x);
+        i = (LinearLayout) findViewById(id.layout_answer_ab);
+        j = findViewById(id.view_answer_a);
+        k = findViewById(id.view_answer_b);
+        l = (TextView) findViewById(id.tv_answer_a);
+        m = (TextView) findViewById(id.tv_answer_b);
+        n = (ImageView) findViewById(id.img_answer_a);
+        o = (ImageView) findViewById(id.img_answer_b);
+        g();
     }
 
     public void b() {
-        ArrayList var2 = this.q;
+        ArrayList var2 = q;
         if (var2 == null || var2.size() <= 0) {
-            this.q = tq.a();
+            q = tq.a();
         }
 
-        int var1 = (new Random()).nextInt(this.q.size());
-        this.setData((QuizBean) this.q.remove(var1));
-        this.e();
+        int var1 = (new Random()).nextInt(q.size());
+        setData((QuizBean) q.remove(var1));
+        e();
     }
 
     public final void c() {
-        this.g.setOnClickListener((View.OnClickListener) null);
-        this.h.setOnClickListener((View.OnClickListener) null);
-        this.j.setOnClickListener((View.OnClickListener) null);
-        this.k.setOnClickListener((View.OnClickListener) null);
+        g.setOnClickListener((View.OnClickListener) null);
+        h.setOnClickListener((View.OnClickListener) null);
+        j.setOnClickListener((View.OnClickListener) null);
+        k.setOnClickListener((View.OnClickListener) null);
     }
 
     public final void d() {
-        this.g.setTranslationX(wB.a(this.getContext(), -50.0F));
-        this.g.setAlpha(1.0F);
-        this.g.setScaleX(0.9F);
-        this.g.setScaleY(0.9F);
-        this.h.setTranslationX(wB.a(this.getContext(), 50.0F));
-        this.h.setAlpha(1.0F);
-        this.h.setScaleX(0.9F);
-        this.h.setScaleY(0.9F);
-        this.n.setScaleX(0.0F);
-        this.n.setScaleY(0.0F);
-        this.n.setAlpha(0.0F);
-        this.o.setScaleX(0.0F);
-        this.o.setScaleY(0.0F);
-        this.o.setAlpha(0.0F);
+        g.setTranslationX(wB.a(getContext(), -50.0F));
+        g.setAlpha(1.0F);
+        g.setScaleX(0.9F);
+        g.setScaleY(0.9F);
+        h.setTranslationX(wB.a(getContext(), 50.0F));
+        h.setAlpha(1.0F);
+        h.setScaleX(0.9F);
+        h.setScaleY(0.9F);
+        n.setScaleX(0.0F);
+        n.setScaleY(0.0F);
+        n.setAlpha(0.0F);
+        o.setScaleX(0.0F);
+        o.setScaleY(0.0F);
+        o.setAlpha(0.0F);
     }
 
     public final void e() {
-        a var1 = this.s;
+        a var1 = s;
         if (var1 != null) {
             var1.cancel();
         }
 
-        this.s = null;
+        s = null;
         var1 = new a(this, 15000L, 250L);
-        this.s = var1;
+        s = var1;
         var1.start();
     }
 
     public void f() {
         label32:
         {
-            QuizBean var2 = this.r;
+            QuizBean var2 = r;
             int var1 = var2.type;
             ImageView var3;
             ViewPropertyAnimator var4;
@@ -180,15 +176,15 @@ public class QuizBoard extends LinearLayout implements View.OnClickListener {
                         break label32;
                     }
 
-                    mB.a(this.g, 0);
-                    mB.a(this.h, 1);
-                    this.h.animate().scaleX(1.0F).scaleY(1.0F).translationX(0.0F).alpha(1.0F).start();
-                    var3 = this.g;
+                    mB.a(g, 0);
+                    mB.a(h, 1);
+                    h.animate().scaleX(1.0F).scaleY(1.0F).translationX(0.0F).alpha(1.0F).start();
+                    var3 = g;
                 } else {
-                    mB.a(this.g, 1);
-                    mB.a(this.h, 0);
-                    this.g.animate().scaleX(1.0F).scaleY(1.0F).translationX(0.0F).alpha(1.0F).start();
-                    var3 = this.h;
+                    mB.a(g, 1);
+                    mB.a(h, 0);
+                    g.animate().scaleX(1.0F).scaleY(1.0F).translationX(0.0F).alpha(1.0F).start();
+                    var3 = h;
                 }
 
                 var4 = var3.animate().scaleX(1.0F).scaleY(1.0F).translationX(0.0F).alpha(0.0F);
@@ -203,9 +199,9 @@ public class QuizBoard extends LinearLayout implements View.OnClickListener {
                         break label32;
                     }
 
-                    var3 = this.o;
+                    var3 = o;
                 } else {
-                    var3 = this.n;
+                    var3 = n;
                 }
 
                 var4 = var3.animate().scaleX(1.0F).scaleY(1.0F).alpha(1.0F);
@@ -214,26 +210,26 @@ public class QuizBoard extends LinearLayout implements View.OnClickListener {
             var4.start();
         }
 
-        this.c();
+        c();
         (new Handler()).postDelayed(new lI(this), 2000L);
     }
 
     public void g() {
-        this.d();
-        this.b();
+        d();
+        b();
     }
 
     public void onClick(View var1) {
         if (!mB.a()) {
-            this.a();
+            a();
             switch (var1.getId()) {
                 case 2131231109:
                 case 2131231110:
                 case 2131232314:
                 case 2131232315:
-                    this.f();
+                    f();
                 default:
-                    if (this.r.type == 2) {
+                    if (r.type == 2) {
                         var1.getId();
                     }
 
@@ -246,11 +242,11 @@ public class QuizBoard extends LinearLayout implements View.OnClickListener {
 
         public a(QuizBoard var1, long var2, long var4) {
             super(var2, var4);
-            this.a = var1;
+            a = var1;
         }
 
         public void onFinish() {
-            this.a.f();
+            a.f();
         }
 
         public void onTick(long var1) {
