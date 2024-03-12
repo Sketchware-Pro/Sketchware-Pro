7a2b4e4597b58c2f017ba0d32b6cc400cb9aa732
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2024-03-12 06:25:57 +0530

    style: Some code clean-ups in PropertyActivity.java

--- app/src/main/java/com/besome/sketch/editor/PropertyActivity.java
+++ app/src/main/java/com/besome/sketch/editor/PropertyActivity.java
@@ -50,79 +50,78 @@ import a.a.a.tx;
 import a.a.a.zs;
 
 public class PropertyActivity extends BaseAppCompatActivity implements Kw {
+
     public Toolbar k;
     public LinearLayout l;
     public CustomScrollView m;
     public ProjectFileBean n;
     public ViewBean o;
-    public boolean p = false;
+    public boolean p;
     public String q;
     public LinearLayout r;
     public ArrayList<Integer> s;
     public RecyclerView t;
     public ViewPropertyItems u;
-    public int v = 0;
+    public int v;
     public ro w;
 
-    public PropertyActivity() {
-    }
 
     public void a(String var1, Object var2) {
     }
 
     public void l() {
-        this.u.setProjectFileBean(this.n);
-        this.u.a(this.q, this.o);
-        this.l.addView(this.u);
+        u.setProjectFileBean(n);
+        u.a(q, o);
+        l.addView(u);
     }
 
     public final void m() {
-        ArrayList var1 = jC.d(this.q).m();
+        ArrayList var1 = jC.d(q).m();
         tx var2;
-        if (var1.indexOf(this.o.layout.backgroundResource) < 0) {
-            this.o.layout.backgroundResource = null;
-            var2 = (tx) this.l.findViewWithTag("property_background_resource");
+        if (!var1.contains(o.layout.backgroundResource)) {
+            o.layout.backgroundResource = null;
+            var2 = (tx) l.findViewWithTag("property_background_resource");
             if (var2 != null) {
-                var2.setValue(this.o.layout.backgroundResource);
+                var2.setValue(o.layout.backgroundResource);
             }
         }
 
-        ViewBean var6 = this.o;
-        if (var6.type == 6 && var1.indexOf(var6.image.resName) < 0) {
-            this.o.image.resName = "default_image";
-            var2 = (tx) this.l.findViewWithTag("property_image");
+        ViewBean var6 = o;
+        if (var6.type == 6 && !var1.contains(var6.image.resName)) {
+            o.image.resName = "default_image";
+            var2 = (tx) l.findViewWithTag("property_image");
             if (var2 != null) {
-                var2.setValue(this.o.image.resName);
+                var2.setValue(o.image.resName);
             }
         }
 
-        Iterator var7 = jC.b(this.q).e().iterator();
+        Iterator var7 = jC.b(q).e().iterator();
 
         String var3;
         Iterator var8;
         while (var7.hasNext()) {
             var3 = (String) var7.next();
-            var8 = jC.a(this.q).d(var3).iterator();
+            var8 = jC.a(q).d(var3).iterator();
 
             while (var8.hasNext()) {
                 ViewBean var4 = (ViewBean) var8.next();
-                if (var4.type == 6 && var1.indexOf(var4.image.resName) < 0) {
+                if (var4.type == 6 && !var1.contains(var4.image.resName)) {
                     var4.image.resName = "default_image";
-                    this.p = true;
+                    p = true;
                 }
 
-                if (var1.indexOf(var4.layout.backgroundResource) < 0) {
+                if (!var1.contains(var4.layout.backgroundResource)) {
                     var4.layout.backgroundResource = null;
-                    this.p = true;
+                    p = true;
                 }
             }
         }
 
-        var7 = jC.b(this.q).d().iterator();
+        var7 = jC.b(q).d().iterator();
 
         while (var7.hasNext()) {
             var3 = (String) var7.next();
-            Iterator var5 = jC.a(this.q).b(var3).entrySet().iterator();
+            Iterator var5 = jC.a(q).b(var3).entrySet().iterator();
 
             while (var5.hasNext()) {
                 var8 = ((ArrayList) ((Map.Entry) var5.next()).getValue()).iterator();
@@ -130,10 +129,10 @@ public class PropertyActivity extends BaseAppCompatActivity implements Kw {
                 while (var8.hasNext()) {
                     BlockBean var9 = (BlockBean) var8.next();
                     if ("setImage".equals(var9.opCode)) {
-                        if (var1.indexOf(var9.parameters.get(1)) < 0) {
+                        if (!var1.contains(var9.parameters.get(1))) {
                             var9.parameters.set(1, "default_image");
                         }
-                    } else if ("setBgResource".equals(var9.opCode) && var1.indexOf(var9.parameters.get(1)) < 0) {
+                    } else if ("setBgResource".equals(var9.opCode) && !var1.contains(var9.parameters.get(1))) {
                         var9.parameters.set(1, "NONE");
                     }
                 }
@@ -143,104 +142,104 @@ public class PropertyActivity extends BaseAppCompatActivity implements Kw {
     }
 
     public void n() {
-        this.r.setVisibility(8);
+        r.setVisibility(8);
     }
 
     public void o() {
         ViewBean var1;
-        if (this.o.id.equals("_fab")) {
-            var1 = jC.a(this.q).h(this.n.getXmlName());
+        if (o.id.equals("_fab")) {
+            var1 = jC.a(q).h(n.getXmlName());
         } else {
-            var1 = jC.a(this.q).c(this.n.getXmlName(), this.o.preId);
+            var1 = jC.a(q).c(n.getXmlName(), o.preId);
         }
 
-        cC.c(this.q).a(this.n.getXmlName(), var1.clone(), this.o);
-        var1.copy(this.o);
+        cC.c(q).a(n.getXmlName(), var1.clone(), o);
+        var1.copy(o);
         Intent var2 = new Intent();
-        var2.putExtra("view_id", this.o.id);
-        var2.putExtra("is_edit_image", this.p);
+        var2.putExtra("view_id", o.id);
+        var2.putExtra("is_edit_image", p);
         var2.putExtra("bean", var1);
-        this.setResult(-1, var2);
-        this.finish();
+        setResult(-1, var2);
+        finish();
     }
 
     public void onActivityResult(int var1, int var2, Intent var3) {
         super.onActivityResult(var1, var2, var3);
-        if (var1 == 209 && var2 == -1 && jC.d(this.q) != null && var3 != null) {
+        if (var1 == 209 && var2 == -1 && jC.d(q) != null && var3 != null) {
             String var4 = var3.getStringExtra("sc_id");
             ArrayList var5 = var3.getParcelableArrayListExtra("result");
             if (jC.d(var4) != null) {
                 jC.d(var4).b(var5);
-                this.m();
+                m();
             }
         }
 
     }
 
     public void onBackPressed() {
-        this.u.i(this.o);
-        this.o();
+        u.i(o);
+        o();
     }
 
     public void onCreate(Bundle var1) {
         super.onCreate(var1);
-        this.setContentView(layout.property);
+        setContentView(layout.property);
         if (!super.j()) {
-            this.finish();
+            finish();
         }
 
-        this.w = new ro(this.getApplicationContext());
-        Toolbar var2 = (Toolbar) this.findViewById(id.toolbar);
-        this.k = var2;
-        this.setSupportActionBar(var2);
-        this.findViewById(id.layout_main_logo).setVisibility(8);
-        this.getSupportActionBar().setDisplayShowTitleEnabled(true);
-        this.getSupportActionBar().setDisplayShowHomeEnabled(true);
-        this.k.setNavigationOnClickListener(new zs(this));
-        this.k.setPopupTheme(style.ThemeOverlay_ToolbarMenu);
+        w = new ro(getApplicationContext());
+        Toolbar var2 = (Toolbar) findViewById(id.toolbar);
+        k = var2;
+        setSupportActionBar(var2);
+        findViewById(id.layout_main_logo).setVisibility(8);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        k.setNavigationOnClickListener(new zs(this));
+        k.setPopupTheme(style.ThemeOverlay_ToolbarMenu);
         ArrayList var4 = new ArrayList();
-        this.s = var4;
+        s = var4;
         var4.add(1);
-        this.s.add(2);
-        this.s.add(3);
-        this.s.add(4);
-        RecyclerView var6 = (RecyclerView) this.findViewById(id.property_group_list);
-        this.t = var6;
+        s.add(2);
+        s.add(3);
+        s.add(4);
+        RecyclerView var6 = (RecyclerView) findViewById(id.property_group_list);
+        t = var6;
         var6.setHasFixedSize(true);
-        LinearLayoutManager var7 = new LinearLayoutManager(this.getApplicationContext(), 1, false);
-        this.t.setLayoutManager(var7);
-        this.t.setAdapter(new a(this));
-        this.m = (CustomScrollView) this.findViewById(id.scroll_view);
-        this.l = (LinearLayout) this.findViewById(id.content);
+        LinearLayoutManager var7 = new LinearLayoutManager(getApplicationContext(), 1, false);
+        t.setLayoutManager(var7);
+        t.setAdapter(new a(this));
+        m = (CustomScrollView) findViewById(id.scroll_view);
+        l = (LinearLayout) findViewById(id.content);
         Parcelable var3;
         if (var1 != null) {
-            this.q = var1.getString("sc_id");
-            this.n = (ProjectFileBean) var1.getParcelable("project_file");
+            q = var1.getString("sc_id");
+            n = (ProjectFileBean) var1.getParcelable("project_file");
             var3 = var1.getParcelable("bean");
         } else {
-            this.q = this.getIntent().getStringExtra("sc_id");
-            this.n = (ProjectFileBean) this.getIntent().getParcelableExtra("project_file");
-            var3 = this.getIntent().getParcelableExtra("bean");
+            q = getIntent().getStringExtra("sc_id");
+            n = (ProjectFileBean) getIntent().getParcelableExtra("project_file");
+            var3 = getIntent().getParcelableExtra("bean");
         }
 
-        this.o = (ViewBean) var3;
-        ActionBar var8 = this.getSupportActionBar();
+        o = (ViewBean) var3;
+        ActionBar var8 = getSupportActionBar();
         String var5;
-        if (this.o.id.charAt(0) == '_') {
-            var5 = this.o.id.substring(1);
+        if (o.id.charAt(0) == '_') {
+            var5 = o.id.substring(1);
         } else {
-            var5 = this.o.id;
+            var5 = o.id;
         }
 
         var8.setTitle(var5);
-        LinearLayout var9 = (LinearLayout) this.findViewById(id.layout_ads);
-        this.r = var9;
+        LinearLayout var9 = (LinearLayout) findViewById(id.layout_ads);
+        r = var9;
         var9.setVisibility(8);
         super.j.h();
     }
 
     public boolean onCreateOptionsMenu(Menu var1) {
-        this.getMenuInflater().inflate(menu.property_menu, var1);
+        getMenuInflater().inflate(menu.property_menu, var1);
         return true;
     }
 
@@ -250,7 +249,7 @@ public class PropertyActivity extends BaseAppCompatActivity implements Kw {
 
     public boolean onOptionsItemSelected(MenuItem var1) {
         if (var1.getItemId() == id.menu_add_image_res) {
-            this.p();
+            p();
         }
 
         return super.onOptionsItemSelected(var1);
@@ -259,56 +258,56 @@ public class PropertyActivity extends BaseAppCompatActivity implements Kw {
     public void onPostCreate(Bundle var1) {
         super.onPostCreate(var1);
         ViewPropertyItems var2 = new ViewPropertyItems(this);
-        this.u = var2;
+        u = var2;
         var2.setOrientation(1);
-        this.l();
+        l();
     }
 
     public void onResume() {
         super.onResume();
         if (!super.j()) {
-            this.finish();
+            finish();
         }
 
         if (super.j.h()) {
-            this.n();
+            n();
         }
 
-        super.d.setScreenName(PropertyActivity.class.getSimpleName().toString());
+        super.d.setScreenName(PropertyActivity.class.getSimpleName());
         super.d.send((new HitBuilders.ScreenViewBuilder()).build());
     }
 
     public void onSaveInstanceState(Bundle var1) {
-        var1.putString("sc_id", this.q);
-        var1.putParcelable("project_file", this.n);
-        var1.putParcelable("bean", this.o);
+        var1.putString("sc_id", q);
+        var1.putParcelable("project_file", n);
+        var1.putParcelable("bean", o);
         super.onSaveInstanceState(var1);
     }
 
     public void p() {
-        Intent var1 = new Intent(this.getApplicationContext(), ManageImageActivity.class);
+        Intent var1 = new Intent(getApplicationContext(), ManageImageActivity.class);
         var1.setFlags(536870912);
-        var1.putExtra("sc_id", this.q);
-        this.startActivityForResult(var1, 209);
+        var1.putExtra("sc_id", q);
+        startActivityForResult(var1, 209);
     }
 
     public class a extends RecyclerView.Adapter<a> {
         public final PropertyActivity c;
 
         public a(PropertyActivity var1) {
-            this.c = var1;
+            c = var1;
         }
 
         public int getItemCount() {
-            return this.c.s.size();
+            return c.s.size();
         }
 
         public void onBindViewHolder(a var1, int var2) {
-            (Integer) this.c.s.get(var2);
+            (Integer) c.s.get(var2);
             ViewPropertyAnimatorCompat var3;
             ColorMatrix var4;
             ColorMatrixColorFilter var5;
-            if (this.c.v == var2) {
+            if (c.v == var2) {
                 var3 = ViewCompat.animate(var1.t);
                 var3.scaleX(1.1F);
                 var3.scaleY(1.1F);
@@ -359,41 +358,41 @@ public class PropertyActivity extends BaseAppCompatActivity implements Kw {
 
             public a(a var1, View var2) {
                 super(var2);
-                this.w = var1;
-                this.t = (ImageView) var2.findViewById(id.img_icon);
-                this.u = (TextView) var2.findViewById(id.tv_name);
+                w = var1;
+                t = (ImageView) var2.findViewById(id.img_icon);
+                u = (TextView) var2.findViewById(id.tv_name);
                 View var3 = var2.findViewById(id.pointer_left);
-                this.v = var3;
+                v = var3;
                 var3.setBackgroundResource(drawable.triangle_point_left_primary);
                 var2.setOnClickListener(this);
             }
 
             public void onClick(View var1) {
                 if (!mB.a()) {
-                    if (this.getLayoutPosition() != -1) {
-                        int var2 = this.getLayoutPosition();
-                        a var4 = this.w;
+                    if (getLayoutPosition() != -1) {
+                        int var2 = getLayoutPosition();
+                        a var4 = w;
                         int var3 = var4.c.v;
                         if (var2 != var3) {
                             var4.notifyItemChanged(var3);
-                            this.w.c.v = this.getLayoutPosition();
-                            var4 = this.w;
+                            w.c.v = getLayoutPosition();
+                            var4 = w;
                             var4.notifyItemChanged(var4.c.v);
-                            var2 = (Integer) this.w.c.s.get(this.w.c.v);
+                            var2 = (Integer) w.c.s.get(w.c.v);
                             if (var2 != 1) {
                                 if (var2 != 2) {
                                     if (var2 != 3) {
                                         if (var2 == 4) {
-                                            this.w.c.u.f(this.w.c.o);
+                                            w.c.u.f(w.c.o);
                                         }
                                     } else {
-                                        this.w.c.u.h(this.w.c.o);
+                                        w.c.u.h(w.c.o);
                                     }
                                 } else {
-                                    this.w.c.u.g(this.w.c.o);
+                                    w.c.u.g(w.c.o);
                                 }
                             } else {
-                                this.w.c.u.d(this.w.c.o);
+                                w.c.u.d(w.c.o);
                             }
                         }
                     }
