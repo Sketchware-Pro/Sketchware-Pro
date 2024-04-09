4747a7ac0596912283d67678148054464949d466
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2024-04-09 15:00:16 +0530

    refactor: Rename some fields/methods and local variables

--- app/src/main/java/com/besome/sketch/editor/manage/font/ManageFontImportActivity.java
+++ app/src/main/java/com/besome/sketch/editor/manage/font/ManageFontImportActivity.java
@@ -1,6 +1,6 @@
 package com.besome.sketch.editor.manage.font;
 
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -27,31 +28,25 @@ import java.util.Iterator;
 
 import a.a.a.QB;
 import a.a.a.bB;
+import a.a.a.mB;
 import a.a.a.uq;
 import a.a.a.xB;
 
+@SuppressLint("NotifyDataSetChanged")
 public class ManageFontImportActivity extends BaseAppCompatActivity implements View.OnClickListener {
 
-    public TextView A;
-    public ImageView k;
-    public TextView l;
-    public TextView m;
-    public TextView n;
-    public TextView o;
-    public RecyclerView p;
-    public EasyDeleteEditText q;
-    public EditText r;
-    public CheckBox s;
-    public a t;
-    public ArrayList<ProjectResourceBean> u;
-    public ArrayList<ProjectResourceBean> v;
-    public int w;
-    public int x;
-    public Button y;
-    public QB z;
+    private TextView textFont;
+    private TextView tvCurrentnum;
+    private EditText r;
+    private CheckBox chkSamename;
+    private ItemAdapter itemAdapter;
+    private ArrayList<ProjectResourceBean> projectFonts;
+    private ArrayList<ProjectResourceBean> selectedCollection;
+    private int x;
+    private QB nameValidator;
 
-    public final boolean b(String var1) {
-        for (ProjectResourceBean resourceBean : u) {
+    private boolean b(String var1) {
+        for (ProjectResourceBean resourceBean : projectFonts) {
             if (!resourceBean.resName.equals(var1)) {
                 return false;
             }
@@ -59,34 +54,33 @@ public class ManageFontImportActivity extends BaseAppCompatActivity implements V
         return true;
     }
 
-    public final void f(int var1) {
-        String var2 = v.get(var1).resFullName;
-        A.setTypeface(Typeface.createFromFile(var2));
-        A.setText(xB.b().a(getApplicationContext(), R.string.design_manager_font_description_example_sentence));
+    private void f(int position) {
+        String resFullName = selectedCollection.get(position).resFullName;
+        textFont.setTypeface(Typeface.createFromFile(resFullName));
+        textFont.setText(xB.b().a(getApplicationContext(), R.string.design_manager_font_description_example_sentence));
     }
 
-    public final ArrayList<String> l() {
-        ArrayList<String> var1 = new ArrayList<>();
-        var1.add("app_icon");
-        for (ProjectResourceBean projectResourceBean : v) {
-            var1.add(projectResourceBean.resName);
+    private ArrayList<String> getProjectResources() {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("app_icon");
+        for (ProjectResourceBean projectResourceBean : selectedCollection) {
+            names.add(projectResourceBean.resName);
         }
-        return var1;
+        return names;
     }
 
-    public final ArrayList<String> m() {
-        ArrayList<String> var1 = new ArrayList<>();
-        var1.add("app_icon");
-
-        for (ProjectResourceBean projectResourceBean : u) {
-            var1.add(projectResourceBean.resName);
+    private ArrayList<String> m() {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("app_icon");
+        for (ProjectResourceBean projectResourceBean : projectFonts) {
+            names.add(projectResourceBean.resName);
         }
-        return var1;
+        return names;
     }
 
-    public final boolean n() {
+    private boolean n() {
         ArrayList<String> var1 = new ArrayList<>();
-        for (ProjectResourceBean var3 : v) {
+        for (ProjectResourceBean var3 : selectedCollection) {
             if (var3.isDuplicateCollection) {
                 var1.add(var3.resName);
             }
@@ -110,126 +104,121 @@ public class ManageFontImportActivity extends BaseAppCompatActivity implements V
                 var7.append(var5);
             }
 
-            Context var12 = getApplicationContext();
-            String var11 = var8 + "\n[" + var6 + "]";
-            bB.a(var12, var11, 1).show();
+            String message = var8 + "\n[" + var6 + "]";
+            bB.a(getApplicationContext(), message, 1).show();
             return true;
         } else {
             return false;
         }
     }
 
-    public final boolean o() {
-        return z.b();
+    private boolean o() {
+        return nameValidator.b();
     }
 
+    @Override
     public void onBackPressed() {
         setResult(-1);
         super.onBackPressed();
     }
 
-    public void onClick(View var1) {
-        int var2 = var1.getId();
-        if (var2 != R.id.btn_decide) {
-            if (var2 != R.id.img_backbtn) {
-                if (var2 == R.id.tv_sendbtn && !n()) {
-                    Intent var5 = new Intent();
-                    var5.putParcelableArrayListExtra("results", v);
-                    setResult(-1, var5);
-                    finish();
-                }
-            } else {
-                onBackPressed();
-            }
-        } else {
-            String var6 = r.getText().toString();
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.btn_decide) {
+            String resName = r.getText().toString();
             if (!o()) {
-                ProjectResourceBean var7 = v.get(x);
+                ProjectResourceBean var7 = selectedCollection.get(x);
                 r.setText(var7.resName);
                 return;
             }
 
-            if (!s.isChecked()) {
-                ProjectResourceBean var3 = v.get(x);
-                var3.resName = var6;
-                var3.isDuplicateCollection = false;
-                z.a(l());
-                t.notifyDataSetChanged();
+            if (!chkSamename.isChecked()) {
+                ProjectResourceBean resourceBean = selectedCollection.get(x);
+                resourceBean.resName = resName;
+                resourceBean.isDuplicateCollection = false;
+                nameValidator.a(getProjectResources());
+                itemAdapter.notifyDataSetChanged();
             } else {
-                ProjectResourceBean var4;
-                for (var2 = 0; var2 < v.size(); var4.isDuplicateCollection = false) {
-                    var4 = v.get(var2);
-                    StringBuilder var8 = new StringBuilder();
-                    var8.append(var6);
-                    var8.append("_");
-                    ++var2;
-                    var8.append(var2);
-                    var4.resName = var8.toString();
+                ProjectResourceBean projectResourceBean;
+                for (i = 0; i < selectedCollection.size(); projectResourceBean.isDuplicateCollection = false) {
+                    projectResourceBean = selectedCollection.get(i);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append(resName);
+                    stringBuilder.append("_");
+                    i++;
+                    stringBuilder.append(i);
+                    projectResourceBean.resName = stringBuilder.toString();
                 }
 
-                z.a(l());
-                t.notifyDataSetChanged();
+                nameValidator.a(getProjectResources());
+                itemAdapter.notifyDataSetChanged();
             }
+        } else if (i == R.id.img_backbtn) {
+            onBackPressed();
+        } else if (i == R.id.tv_sendbtn && !n()) {
+            Intent var5 = new Intent();
+            var5.putParcelableArrayListExtra("results", selectedCollection);
+            setResult(-1, var5);
+            finish();
         }
-
     }
 
-    public void onCreate(Bundle var1) {
-        super.onCreate(var1);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         if (!super.j()) {
             finish();
         }
-
         setContentView(R.layout.manage_font_import);
-        k = findViewById(R.id.img_backbtn);
-        k.setOnClickListener(this);
-        l = findViewById(R.id.tv_currentnum);
-        m = findViewById(R.id.tv_totalnum);
-        n = findViewById(R.id.tv_sendbtn);
-        n.setText(xB.b().a(getApplicationContext(), R.string.common_word_import).toUpperCase());
-        n.setOnClickListener(this);
-        o = findViewById(R.id.tv_samename);
-        o.setText(xB.b().a(getApplicationContext(), R.string.design_manager_font_title_apply_same_naming));
-        t = new a(this);
-        p = findViewById(R.id.recycler_list);
-        p.setHasFixedSize(true);
-        p.setAdapter(t);
-        LinearLayoutManager var2 = new LinearLayoutManager(getApplicationContext(), RecyclerView.HORIZONTAL, false);
-        p.setLayoutManager(var2);
-        u = getIntent().getParcelableArrayListExtra("project_fonts");
-        v = getIntent().getParcelableArrayListExtra("selected_collections");
-        w = v.size();
-        l.setText(String.valueOf(1));
-        m.setText(String.valueOf(w));
-        q = findViewById(R.id.ed_input);
-        r = q.getEditText();
-        r.setText(v.get(0).resName);
+        findViewById(R.id.img_backbtn).setOnClickListener(this);
+        tvCurrentnum = findViewById(R.id.tv_currentnum);
+        TextView tvTotalnum = findViewById(R.id.tv_totalnum);
+        TextView tvSendbtn = findViewById(R.id.tv_sendbtn);
+        tvSendbtn.setText(xB.b().a(getApplicationContext(), R.string.common_word_import).toUpperCase());
+        tvSendbtn.setOnClickListener(this);
+        TextView tvSamename = findViewById(R.id.tv_samename);
+        tvSamename.setText(xB.b().a(getApplicationContext(), R.string.design_manager_font_title_apply_same_naming));
+        itemAdapter = new ItemAdapter();
+        RecyclerView recyclerList = findViewById(R.id.recycler_list);
+        recyclerList.setHasFixedSize(true);
+        recyclerList.setAdapter(itemAdapter);
+        recyclerList.setLayoutManager(new LinearLayoutManager(getApplicationContext(), RecyclerView.HORIZONTAL, false));
+        projectFonts = getIntent().getParcelableArrayListExtra("project_fonts");
+        selectedCollection = getIntent().getParcelableArrayListExtra("selected_collections");
+        tvCurrentnum.setText(String.valueOf(1));
+        tvTotalnum.setText(String.valueOf(selectedCollection.size()));
+        EasyDeleteEditText edInput = findViewById(R.id.ed_input);
+        r = edInput.getEditText();
+        r.setText(selectedCollection.get(0).resName);
         r.setPrivateImeOptions("defaultInputmode=english;");
-        q.setHint(xB.b().a(this, R.string.design_manager_font_hint_enter_font_name));
-        z = new QB(getApplicationContext(), q.getTextInputLayout(), uq.b, m(), l());
-        s = findViewById(R.id.chk_samename);
-        s.setOnCheckedChangeListener((buttonView, isChecked) -> {
+        edInput.setHint(xB.b().a(this, R.string.design_manager_font_hint_enter_font_name));
+        nameValidator = new QB(getApplicationContext(), edInput.getTextInputLayout(), uq.b, m(), getProjectResources());
+        chkSamename = findViewById(R.id.chk_samename);
+        chkSamename.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
-                z.c(null);
-                z.a(v.size());
+                nameValidator.c(null);
+                nameValidator.a(selectedCollection.size());
             } else {
-                z.c(v.get(x).resName);
-                z.a(1);
+                nameValidator.c(selectedCollection.get(x).resName);
+                nameValidator.a(1);
             }
         });
-        y = findViewById(R.id.btn_decide);
-        y.setText(xB.b().a(getApplicationContext(), R.string.design_manager_change_name_button));
-        y.setOnClickListener(this);
-        A = findViewById(R.id.text_font);
+        Button btnDecide = findViewById(R.id.btn_decide);
+        btnDecide.setText(xB.b().a(getApplicationContext(), R.string.design_manager_change_name_button));
+        btnDecide.setOnClickListener(this);
+        textFont = findViewById(R.id.text_font);
     }
 
-    public void onPostCreate(Bundle var1) {
-        super.onPostCreate(var1);
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
         p();
         f(0);
-        t.notifyDataSetChanged();
+        itemAdapter.notifyDataSetChanged();
     }
 
+    @Override
     public void onResume() {
         super.onResume();
         if (!super.j()) {
@@ -237,10 +226,10 @@ public class ManageFontImportActivity extends BaseAppCompatActivity implements V
         }
     }
 
-    public final void p() {
+    private void p() {
         ArrayList<ProjectResourceBean> var1 = new ArrayList<>();
         ArrayList<ProjectResourceBean> var2 = new ArrayList<>();
-        Iterator<ProjectResourceBean> var3 = v.iterator();
+        Iterator<ProjectResourceBean> var3 = selectedCollection.iterator();
 
         ProjectResourceBean var4;
         while (var3.hasNext()) {
@@ -260,78 +249,76 @@ public class ManageFontImportActivity extends BaseAppCompatActivity implements V
             bB.a(getApplicationContext(), xB.b().a(getApplicationContext(), R.string.design_manager_message_collection_name_no_conflict), 0).show();
         }
 
-        v = new ArrayList<>();
+        selectedCollection = new ArrayList<>();
 
         for (ProjectResourceBean projectResourceBean : var1) {
             var4 = projectResourceBean;
-            v.add(var4);
+            selectedCollection.add(var4);
         }
 
-        for (ProjectResourceBean var6 : var2) {
-            v.add(var6);
-        }
+        selectedCollection.addAll(var2);
     }
 
-    public class a extends RecyclerView.Adapter<a> {
-        public final ManageFontImportActivity c;
+    public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
 
-        public a(ManageFontImportActivity var1) {
-            c = var1;
+        public ItemAdapter() {
         }
 
+        @Override
         public int getItemCount() {
-            return c.v.size();
+            return selectedCollection.size();
         }
 
-        public void onBindViewHolder(a var1, int var2) {
-            if (c.v.get(var2).isDuplicateCollection) {
-                var1.u.setImageResource(R.drawable.ic_cancel_48dp);
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+            if (selectedCollection.get(position).isDuplicateCollection) {
+                viewHolder.imgConflict.setImageResource(R.drawable.ic_cancel_48dp);
             } else {
-                var1.u.setImageResource(R.drawable.ic_ok_48dp);
+                viewHolder.imgConflict.setImageResource(R.drawable.ic_ok_48dp);
             }
 
-            if (var2 == c.x) {
-                var1.v.setBackgroundResource(R.drawable.bg_outline_dark_yellow);
+            if (position == x) {
+                viewHolder.img.setBackgroundResource(R.drawable.bg_outline_dark_yellow);
             } else {
-                var1.v.setBackgroundResource(R.drawable.bg_outline);
+                viewHolder.img.setBackgroundResource(R.drawable.bg_outline);
             }
-            var1.v.setImageResource(R.drawable.ic_font_48dp);
-            var1.w.setText(c.v.get(var2).resName);
+            viewHolder.img.setImageResource(R.drawable.ic_font_48dp);
+            viewHolder.tvName.setText(selectedCollection.get(position).resName);
         }
 
-        public a onCreateViewHolder(ViewGroup var1, int var2) {
-            return new a(this, LayoutInflater.from(var1.getContext()).inflate(R.layout.manage_import_list_item, var1, false));
+        @Override
+        @NonNull
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.manage_import_list_item, parent, false));
         }
 
-        public class a extends RecyclerView.ViewHolder {
-            public LinearLayout t;
-            public ImageView u;
-            public ImageView v;
-            public TextView w;
-            public final a x;
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public LinearLayout layoutItem;
+            public ImageView imgConflict;
+            public ImageView img;
+            public TextView tvName;
 
-            public a(a var1, View var2) {
-                super(var2);
-                x = var1;
-                t = var2.findViewById(R.id.layout_item);
-                u = var2.findViewById(R.id.img_conflict);
-                v = var2.findViewById(R.id.img);
-                w = var2.findViewById(R.id.tv_name);
-                v.setOnClickListener(view -> {
-                    if (!a.a.a.mB.a()) {
-                        ManageFontImportActivity.this.x = getLayoutPosition();
-                        f(ManageFontImportActivity.this.x);
-                        l.setText(String.valueOf(getLayoutPosition() + 1));
-                        r.setText(ManageFontImportActivity.this.v.get(getLayoutPosition()).resName);
-                        if (s.isChecked()) {
-                            z.c(null);
-                            z.a(ManageFontImportActivity.this.v.size());
+            public ViewHolder(View itemView) {
+                super(itemView);
+                layoutItem = itemView.findViewById(R.id.layout_item);
+                imgConflict = itemView.findViewById(R.id.img_conflict);
+                img = itemView.findViewById(R.id.img);
+                tvName = itemView.findViewById(R.id.tv_name);
+                img.setOnClickListener(view -> {
+                    if (!mB.a()) {
+                        x = getLayoutPosition();
+                        f(x);
+                        tvCurrentnum.setText(String.valueOf(getLayoutPosition() + 1));
+                        r.setText(selectedCollection.get(getLayoutPosition()).resName);
+                        if (chkSamename.isChecked()) {
+                            nameValidator.c(null);
+                            nameValidator.a(selectedCollection.size());
                         } else {
-                            z.c(ManageFontImportActivity.this.v.get(getLayoutPosition()).resName);
-                            z.a(1);
+                            nameValidator.c(selectedCollection.get(getLayoutPosition()).resName);
+                            nameValidator.a(1);
                         }
 
-                        ManageFontImportActivity.this.t.notifyDataSetChanged();
+                        itemAdapter.notifyDataSetChanged();
                     }
                 });
             }
