d0bf64257304a62fa8ca604191749929fb62241e
remaker17 <farzasz7@gmail.com>
2024-06-07 21:51:44 +0700

    feat: Show immediately root settings

--- app/src/main/java/mod/remaker/settings/ExperimentalSettingsActivity.java
+++ app/src/main/java/mod/remaker/settings/ExperimentalSettingsActivity.java
@@ -3,13 +3,31 @@ package mod.remaker.settings;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.sketchware.remod.R;
+
+import mod.remaker.settings.fragment.RootSettingsFragment;
 
 public class ExperimentalSettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_experimental_settings);
+        switchFragment(new RootSettingsFragment(), false);
+    }
+
+    public void switchFragment(PreferenceFragment fragment, boolean addToBackStack) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Fragment activeFragment = getSupportFragmentManager().findFragmentById(R.id.settings_fragment_container);
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+        if (activeFragment != null) {
+            transaction.hide(activeFragment);
+        }
+        transaction.add(R.id.settings_fragment_container, fragment);
+        transaction.commit();
     }
 }
