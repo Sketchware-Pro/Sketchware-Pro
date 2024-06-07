7d975af1f3157e20163e6de34e8f9c2716d07578
remaker17 <farzasz7@gmail.com>
2024-06-07 21:58:23 +0700

    fix: Preference content fragment was transparent if we are use `FragmentTransaction#add` to navigate between fragments

--- app/src/main/java/mod/remaker/settings/PreferenceContentFragment.java
+++ app/src/main/java/mod/remaker/settings/PreferenceContentFragment.java
@@ -1,6 +1,18 @@
 package mod.remaker.settings;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.preference.PreferenceFragmentCompat;
 
+import mod.remaker.util.ThemeUtils;
+
 public abstract class PreferenceContentFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.setBackgroundColor(ThemeUtils.getColor(view, android.R.attr.colorBackground));
+    }
 }
