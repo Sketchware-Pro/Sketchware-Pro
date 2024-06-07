71cee05fd9c54a1724bd4ac7675034daf9b864d6
remaker17 <farzasz7@gmail.com>
2024-06-07 20:36:05 +0700

    feat: Introduce experimental settings

---
+++ app/src/main/java/mod/remaker/settings/ExperimentalSettingsActivity.java
@@ -0,0 +1,15 @@
+package mod.remaker.settings;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sketchware.remod.R;
+
+public class ExperimentalSettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_experimental_settings);
+    }
+}
