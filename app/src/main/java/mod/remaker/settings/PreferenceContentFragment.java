3198348d2962aeba07dffa7ee47e94d646131c2e
remaker17 <farzasz7@gmail.com>
2024-06-08 10:52:16 +0700

    feat: Add feature to track every preference

--- app/src/main/java/mod/remaker/settings/PreferenceContentFragment.java
+++ app/src/main/java/mod/remaker/settings/PreferenceContentFragment.java
@@ -5,7 +5,10 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 import mod.remaker.util.ThemeUtils;
 
@@ -14,5 +17,26 @@ public abstract class PreferenceContentFragment extends PreferenceFragmentCompat
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         view.setBackgroundColor(ThemeUtils.getColor(view, android.R.attr.colorBackground));
+
+        PreferenceScreen screen = getPreferenceScreen();
+        for (int i = 0; i < screen.getPreferenceCount(); i++) {
+            Preference preference = screen.getPreference(i);
+            setupPreference(preference);
+        }
+    }
+
+    private void setupPreference(Preference preference) {
+        if (!preference.isVisible()) return;
+        if (preference instanceof PreferenceCategory category) {
+            for (int i = 0; i < category.getPreferenceCount(); i++) {
+                Preference pref = category.getPreference(i);
+                setupPreference(pref);
+            }
+            return;
+        }
+        onSetupPreference(preference);
+    }
+
+    protected void onSetupPreference(Preference preference) {
     }
 }
