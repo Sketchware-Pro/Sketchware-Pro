230f38eae9d0861071018c3021bb1b4f9d6a1929
Pranav Purwar <purwarpranav80@gmail.com>
2024-06-29 00:22:24 +0530

    feat: upgrade kotlinc to v2.0.0

--- kotlinc/src/main/java/javax/management/NotificationEmitter.java
+++
@@ -1,5 +0,0 @@
-package javax.management;
-
-public interface NotificationEmitter extends NotificationBroadcaster {
-    void removeNotificationListener(NotificationListener var1, NotificationFilter var2, Object var3) throws ListenerNotFoundException;
-}
--- kotlinc/src/main/java/org/jetbrains/kotlin/com/intellij/psi/augment/AugmentProvider.java
+++
@@ -1,8 +0,0 @@
-package org.jetbrains.kotlin.com.intellij.psi.augment;
-
-public class AugmentProvider extends PsiAugmentProvider {
-
-    public AugmentProvider() {
-
-    }
-}
