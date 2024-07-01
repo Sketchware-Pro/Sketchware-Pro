43ac933f6362e724b81c189ed0dfe2089a6ea39f
Ilyasse <46769766+ilyassesalama@users.noreply.github.com>
2024-07-02 00:13:22 +0100

    fix, chore: Back button not working, code improvements

--- app/src/main/java/mod/trindadedev/settings/appearance/AppearanceFragment.java
+++ app/src/main/java/mod/trindadedev/settings/appearance/AppearanceFragment.java
@@ -1,33 +1,35 @@
 package mod.trindadedev.settings.appearance;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import mod.trindadedev.settings.appearance.theme.manage.ThemeManager;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
-import com.sketchware.remod.databinding.SettingsAppearanceBinding;
 import com.sketchware.remod.R;
+import com.sketchware.remod.databinding.SettingsAppearanceBinding;
+
+import mod.hey.studios.util.Helper;
+import mod.trindadedev.settings.appearance.theme.manage.ThemeManager;
 
 public class AppearanceFragment extends Fragment {
     private SettingsAppearanceBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = SettingsAppearanceBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        switch(ThemeManager.getCurrentTheme(requireContext())) {
-            case ThemeManager.THEME_SYSTEM:
-                binding.toggleThemes.check(R.id.themeSystem);
-                break;
+        binding.topAppBar.setNavigationOnClickListener(Helper.getBackPressedClickListener(getActivity()));
+
+        switch (ThemeManager.getCurrentTheme(requireContext())) {
             case ThemeManager.THEME_LIGHT:
                 binding.toggleThemes.check(R.id.themeLight);
                 break;
@@ -41,19 +43,14 @@ public class AppearanceFragment extends Fragment {
 
         binding.toggleThemes.setOnCheckedChangeListener((group, checkedId) -> {
             if (checkedId != View.NO_ID) {
-                switch (checkedId) {
-                    case R.id.themeLight:
-                        ThemeManager.applyTheme(requireContext(), ThemeManager.THEME_LIGHT);
-                        break;
-                    case R.id.themeSystem:
-                        ThemeManager.applyTheme(requireContext(), ThemeManager.THEME_SYSTEM);
-                        break;
-                    case R.id.themeDark:
-                        ThemeManager.applyTheme(requireContext(), ThemeManager.THEME_DARK);
-                        break;
-                    default:
-                        ThemeManager.applyTheme(requireContext(), ThemeManager.THEME_SYSTEM);
-                        break;
+                if (checkedId == R.id.themeLight) {
+                    ThemeManager.applyTheme(requireContext(), ThemeManager.THEME_LIGHT);
+                } else if (checkedId == R.id.themeSystem) {
+                    ThemeManager.applyTheme(requireContext(), ThemeManager.THEME_SYSTEM);
+                } else if (checkedId == R.id.themeDark) {
+                    ThemeManager.applyTheme(requireContext(), ThemeManager.THEME_DARK);
+                } else {
+                    ThemeManager.applyTheme(requireContext(), ThemeManager.THEME_SYSTEM);
                 }
             }
         });
