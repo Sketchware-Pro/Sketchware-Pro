e7059a7950f139f6c3451bb05cc82b805c362a4d
Yamenher <164387611+Yamenher@users.noreply.github.com>
2024-07-01 06:58:10 +0100

    Update SketchApplication.java
    Improvements for better Unhandled errors catching on newer android versions

--- app/src/main/java/com/besome/sketch/SketchApplication.java
+++ app/src/main/java/com/besome/sketch/SketchApplication.java
@@ -1,14 +1,10 @@
 package com.besome.sketch;
 
-import android.app.AlarmManager;
 import android.app.Application;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Process;
 import android.util.Log;
-
-import com.besome.sketch.tools.CollectErrorActivity;
 
 import com.google.android.gms.analytics.Tracker;
 
@@ -27,24 +23,20 @@ public class SketchApplication extends Application {
     @Override
     public void onCreate() {
         mApplicationContext = getApplicationContext();
-        Thread.UncaughtExceptionHandler uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
-        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
-            Log.e("SketchApplication", "Uncaught exception on thread " + thread.getName(), throwable);
 
-            Intent intent = new Intent(getApplicationContext(), CollectErrorActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            intent.putExtra("error", Log.getStackTraceString(throwable));
-            ((AlarmManager) getSystemService(Context.ALARM_SERVICE))
-                    .set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                            1000,
-                            PendingIntent.getActivity(getApplicationContext(), 11111, intent,
-                                    PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE));
-            Process.killProcess(Process.myPid());
-            System.exit(1);
-            if (uncaughtExceptionHandler != null) {
-                uncaughtExceptionHandler.uncaughtException(thread, throwable);
-            }
-        });
+        Thread.setDefaultUncaughtExceptionHandler(
+                new Thread.UncaughtExceptionHandler() {
+                    @Override
+                    public void uncaughtException(Thread thread, Throwable throwable) {
+                        Intent intent = new Intent(getApplicationContext(), DebugActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        intent.putExtra("error", Log.getStackTraceString(throwable));
+                        startActivity(intent);
+                        Process.killProcess(Process.myPid());
+                        System.exit(1);
+                    }
+                });
+
         super.onCreate();
     }
 }
