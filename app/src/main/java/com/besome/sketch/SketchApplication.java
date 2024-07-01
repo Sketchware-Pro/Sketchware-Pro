1009513072404213d7df75dc40992fcacf3c484e
Yamenher <164387611+Yamenher@users.noreply.github.com>
2024-07-01 06:45:26 +0100

    feat : Update SketchApplication.java
    Make Debug screen activity more responsive in newer android versions by removing AlarmManager component and directly using Intent instead to redirect to the error screen

--- app/src/main/java/com/besome/sketch/SketchApplication.java
+++ app/src/main/java/com/besome/sketch/SketchApplication.java
@@ -41,8 +41,6 @@ public class SketchApplication extends Application {
                         System.exit(1);
                     }
                 });
-            if (uncaughtExceptionHandler != null) {
-                uncaughtExceptionHandler.uncaughtException(thread, throwable);
             }
         });
         super.onCreate();
