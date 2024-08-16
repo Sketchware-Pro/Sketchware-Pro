4e4900e1b73c0b09bd0e815ddab50bc9818cec36
MOHAMMAD SHIBILY.M <shibisulthan25@gmail.com>
2024-08-16 05:12:25 +0000

    update

---
+++ app/src/main/java/mod/codeware/AIViewGeneratorActivity.java
@@ -0,0 +1,16 @@
+package mod.codeware;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sketchware.remod.R;
+
+public class AIViewGeneratorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aiview_generator);
+    }
+}
