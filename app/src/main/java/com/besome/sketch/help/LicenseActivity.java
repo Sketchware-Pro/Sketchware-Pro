46aeecce37b0008eecfe0aa4d842d86c72e60720
Ant Summer <devsuay@gmail.com>
2024-08-21 15:44:22 -0300

    feat: Redesign `LicenseActivity`

--- app/src/main/java/com/besome/sketch/help/LicenseActivity.java
+++ app/src/main/java/com/besome/sketch/help/LicenseActivity.java
@@ -3,38 +3,32 @@ package com.besome.sketch.help;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
-import android.view.View;
-import android.widget.TextView;
 
-import androidx.appcompat.widget.Toolbar;
+import androidx.activity.EdgeToEdge;
+
+import com.sketchware.remod.R;
+import com.sketchware.remod.databinding.ActivityOssLibrariesBinding;
 
 import com.besome.sketch.lib.base.BaseAppCompatActivity;
-import com.sketchware.remod.R;
 
-import a.a.a.mB;
 import a.a.a.oB;
 import mod.hey.studios.util.Helper;
 
 public class LicenseActivity extends BaseAppCompatActivity {
 
+    private ActivityOssLibrariesBinding binding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.oss);
+         EdgeToEdge.enable(this);
+         super.onCreate(savedInstanceState);
+         binding = ActivityOssLibrariesBinding.inflate(getLayoutInflater());
+         setContentView(binding.getRoot());
+         
+         binding.toolbar.setNavigationOnClickListener(Helper.getBackPressedClickListener(this));
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        findViewById(R.id.layout_main_logo).setVisibility(View.GONE);
-        getSupportActionBar().setTitle(Helper.getResString(R.string.program_information_title_open_source_license));
-        toolbar.setNavigationOnClickListener(view -> {
-            if (!mB.a()) onBackPressed();
-        });
-
-        TextView licensesText = findViewById(R.id.tv_oss);
-        licensesText.setText(new oB().b(getApplicationContext(), "oss.txt"));
-        licensesText.setAutoLinkMask(Linkify.WEB_URLS);
-        licensesText.setMovementMethod(LinkMovementMethod.getInstance());
+         binding.licensesText.setText(new oB().b(getApplicationContext(), "oss.txt"));
+         binding.licensesText.setAutoLinkMask(Linkify.WEB_URLS);
+         binding.licensesText.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
