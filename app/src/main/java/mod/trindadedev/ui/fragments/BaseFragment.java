82ce99064c2599f3f35b95d4fbcf366871f45561
trindadedev13 <devsuay@gmail.com>
2024-08-29 17:01:23 -0300

    feat: add MaterialSharedAxis Transition on BaseFragment

--- app/src/main/java/mod/trindadedev/ui/fragments/BaseFragment.java
+++ app/src/main/java/mod/trindadedev/ui/fragments/BaseFragment.java
@@ -1,9 +1,13 @@
 package mod.trindadedev.ui.fragments;
 
+import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.annotation.Nullable;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.transition.MaterialSharedAxis;
 
 import com.sketchware.remod.R;
 
@@ -11,15 +15,25 @@ import mod.hey.studios.util.Helper;
 
 public class BaseFragment extends Fragment {
 
-    public void configureToolbar(MaterialToolbar view) {
-        view.setNavigationOnClickListener(Helper.getBackPressedClickListener(getActivity()));
-    }
 
-    public void openFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getParentFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.settings_fragment_container, fragment)
-                .addToBackStack(null)
-                .commit();
-    }
+     @Override
+     public void onCreate(@Nullable Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          setEnterTransition(new MaterialSharedAxis(MaterialSharedAxis.X, true));
+          setReturnTransition(new MaterialSharedAxis(MaterialSharedAxis.X, false));
+          setExitTransition(new MaterialSharedAxis(MaterialSharedAxis.X, true));
+          setReenterTransition(new MaterialSharedAxis(MaterialSharedAxis.X, false));
+     }
+
+     public void configureToolbar(MaterialToolbar view) {
+          view.setNavigationOnClickListener(Helper.getBackPressedClickListener(getActivity()));
+     }
+
+     public void openFragment(Fragment fragment) {
+          FragmentManager fragmentManager = getParentFragmentManager();
+          fragmentManager.beginTransaction()
+                  .replace(R.id.settings_fragment_container, fragment)
+                  .addToBackStack(null)
+                  .commit();
+     }
 }
