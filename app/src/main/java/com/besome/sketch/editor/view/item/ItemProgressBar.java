1ef13fecb3d7ea5f0550844136bb5fdb8bb3015b
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2024-08-31 01:06:53 +0530

    style, refactor: Reformat file, code clean-up

--- app/src/main/java/com/besome/sketch/editor/view/item/ItemProgressBar.java
+++ app/src/main/java/com/besome/sketch/editor/view/item/ItemProgressBar.java
@@ -1,22 +1,20 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package com.besome.sketch.editor.view.item;
 
-import a.a.a.sy;
-import a.a.a.wB;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+
 import com.besome.sketch.beans.ViewBean;
 
+import a.a.a.sy;
+import a.a.a.wB;
+
 public class ItemProgressBar extends LinearLayout implements sy {
+
     public ViewBean a;
     public boolean b;
     public boolean c;
@@ -26,69 +24,64 @@ public class ItemProgressBar extends LinearLayout implements sy {
 
     public ItemProgressBar(Context var1) {
         super(var1);
-        this.a(var1);
+        a(var1);
     }
 
     public void a(Context var1) {
-        this.e = wB.a(var1, 1.0F);
-        this.d = new Paint(1);
-        this.d.setColor(-1785080368);
-        ((LinearLayout)this).setDrawingCacheEnabled(true);
-        this.f = new ImageView(((LinearLayout)this).getContext());
+        e = wB.a(var1, 1.0F);
+        d = new Paint(1);
+        d.setColor(-1785080368);
+        setDrawingCacheEnabled(true);
+        f = new ImageView(getContext());
         LinearLayout.LayoutParams var2 = new LinearLayout.LayoutParams(-1, -2);
-        this.f.setLayoutParams(var2);
-        this.f.setImageResource(2131166032);
-        this.f.setScaleType(ScaleType.FIT_XY);
-        this.f.setPadding(0, 0, 0, 0);
-        ((LinearLayout)this).addView(this.f);
-        ((LinearLayout)this).setGravity(17);
+        f.setLayoutParams(var2);
+        f.setImageResource(2131166032);
+        f.setScaleType(ScaleType.FIT_XY);
+        f.setPadding(0, 0, 0, 0);
+        addView(f);
+        setGravity(17);
     }
 
     public ViewBean getBean() {
-        return this.a;
+        return a;
     }
 
     public boolean getFixed() {
-        return this.c;
+        return c;
     }
 
     public boolean getSelection() {
-        return this.b;
+        return b;
     }
 
     public void onDraw(Canvas var1) {
-        synchronized(this){}
-
-        try {
-            if (this.b) {
-                Rect var2 = new Rect(0, 0, ((LinearLayout)this).getMeasuredWidth(), ((LinearLayout)this).getMeasuredHeight());
-                var1.drawRect(var2, this.d);
-            }
-
-            super.onDraw(var1);
-        } finally {
-            ;
+        if (b) {
+            Rect var2 = new Rect(0, 0, getMeasuredWidth(), getMeasuredHeight());
+            var1.drawRect(var2, d);
         }
+
+        super.onDraw(var1);
 
     }
 
     public void setBean(ViewBean var1) {
-        this.a = var1;
+        a = var1;
     }
 
     public void setFixed(boolean var1) {
-        this.c = var1;
+        c = var1;
     }
 
     public void setPadding(int var1, int var2, int var3, int var4) {
-        float var5 = (float)var1;
-        float var6 = this.e;
-        super.setPadding((int)(var5 * var6), (int)((float)var2 * var6), (int)((float)var3 * var6), (int)((float)var4 * var6));
+        float var5 = (float) var1;
+        float var6 = e;
+        super.setPadding((int) (var5 * var6), (int) ((float) var2 * var6), (int) ((float) var3 * var6), (int) ((float) var4 * var6));
     }
 
     public void setProgressBarStyle(String var1) {
         byte var3;
-        label26: {
+        label26:
+        {
             int var2 = var1.hashCode();
             if (var2 != -1631599723) {
                 if (var2 == 583759257 && var1.equals("?android:progressBarStyleHorizontal")) {
@@ -105,20 +98,20 @@ public class ItemProgressBar extends LinearLayout implements sy {
 
         if (var3 != 0) {
             if (var3 == 1) {
-                this.f.setImageResource(2131166033);
-                this.f.getLayoutParams().width = (int)(this.e * 320.0F);
-                this.f.getLayoutParams().height = (int)(this.e * 30.0F);
+                f.setImageResource(2131166033);
+                f.getLayoutParams().width = (int) (e * 320.0F);
+                f.getLayoutParams().height = (int) (e * 30.0F);
             }
         } else {
-            this.f.setImageResource(2131166032);
-            this.f.getLayoutParams().width = (int)(this.e * 30.0F);
-            this.f.getLayoutParams().height = (int)(this.e * 30.0F);
+            f.setImageResource(2131166032);
+            f.getLayoutParams().width = (int) (e * 30.0F);
+            f.getLayoutParams().height = (int) (e * 30.0F);
         }
 
     }
 
     public void setSelection(boolean var1) {
-        this.b = var1;
-        ((LinearLayout)this).invalidate();
+        b = var1;
+        invalidate();
     }
 }
