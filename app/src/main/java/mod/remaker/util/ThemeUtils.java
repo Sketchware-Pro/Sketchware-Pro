ac02e518f19fb3956dfc84deca2ca4d1e8db84df
remaker17 <farzasz7@gmail.com>
2024-09-04 19:27:55 +0700

    feat: Improve UI for import icon activity

--- app/src/main/java/mod/remaker/util/ThemeUtils.java
+++ app/src/main/java/mod/remaker/util/ThemeUtils.java
@@ -1,5 +1,7 @@
 package mod.remaker.util;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.view.View;
 
 import androidx.annotation.AttrRes;
@@ -14,5 +16,11 @@ public class ThemeUtils {
     @ColorInt
     public static int getColor(View view, @AttrRes int resourceId) {
         return MaterialColors.getColor(view, resourceId);
+    }
+
+    public static boolean isDarkThemeEnabled(Context context) {
+        Configuration configuration = context.getResources().getConfiguration();
+        int nightMode = configuration.uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        return nightMode == Configuration.UI_MODE_NIGHT_YES;
     }
 }
