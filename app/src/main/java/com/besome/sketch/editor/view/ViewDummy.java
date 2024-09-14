f4939c35f0a22c67926328247a5c1efe80a55461
Javkhlanbayar Khongorzul <jbk@duck.com>
2024-09-14 11:42:04 +0200

    chore: Clean up ViewDummy
    Removes unnecessary casts and qualifiers.

--- app/src/main/java/com/besome/sketch/editor/view/ViewDummy.java
+++ app/src/main/java/com/besome/sketch/editor/view/ViewDummy.java
@@ -24,41 +24,41 @@ public class ViewDummy extends RelativeLayout {
 
     public ViewDummy(Context context) {
         super(context);
-        this.d = new int[2];
-        this.e = new int[2];
-        this.allowed = false;
+        d = new int[2];
+        e = new int[2];
+        allowed = false;
         initialize(context);
     }
 
     public final void initialize(Context context) {
         wB.a(context, this, R.layout.dummy);
-        this.img_notallowed = (ImageView) findViewById(R.id.img_notallowed);
-        this.img_dummy = (ImageView) findViewById(R.id.img_dummy);
-        this.layout_dummy = (LinearLayout) findViewById(R.id.layout_dummy);
+        img_notallowed = findViewById(R.id.img_notallowed);
+        img_dummy = findViewById(R.id.img_dummy);
+        layout_dummy = findViewById(R.id.layout_dummy);
     }
 
     public void b(View view) {
         Bitmap bitmap = createBitmapOfView(view);
-        view.getLocationOnScreen(this.d);
-        this.img_dummy.setImageBitmap(bitmap);
-        this.img_dummy.setAlpha(0.5f);
+        view.getLocationOnScreen(d);
+        img_dummy.setImageBitmap(bitmap);
+        img_dummy.setAlpha(0.5f);
     }
 
     public boolean getAllow() {
-        return this.allowed;
+        return allowed;
     }
 
     public void setAllow(boolean allowed) {
         this.allowed = allowed;
         if (allowed) {
-            this.img_notallowed.setVisibility(View.INVISIBLE);
+            img_notallowed.setVisibility(View.INVISIBLE);
         } else {
-            this.img_notallowed.setVisibility(View.VISIBLE);
+            img_notallowed.setVisibility(View.VISIBLE);
         }
     }
 
     public void setDummyVisibility(int visibility) {
-        this.layout_dummy.setVisibility(visibility);
+        layout_dummy.setVisibility(visibility);
     }
 
     public final Bitmap createBitmapOfView(View view) {
@@ -69,54 +69,54 @@ public class ViewDummy extends RelativeLayout {
 
     public ViewDummy(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.d = new int[2];
-        this.e = new int[2];
-        this.allowed = false;
+        d = new int[2];
+        e = new int[2];
+        allowed = false;
         initialize(context);
     }
 
     public void a(Rs rs) {
         switch (rs.b) {
             case "b":
-                this.img_dummy.setImageResource(R.drawable.selected_block_boolean);
+                img_dummy.setImageResource(R.drawable.selected_block_boolean);
                 break;
             case "d":
             case "n":
-                this.img_dummy.setImageResource(R.drawable.selected_block_integer);
+                img_dummy.setImageResource(R.drawable.selected_block_integer);
                 break;
             case "s":
-                this.img_dummy.setImageResource(R.drawable.selected_block_string);
+                img_dummy.setImageResource(R.drawable.selected_block_string);
                 break;
             case "c":
-                this.img_dummy.setImageResource(R.drawable.selected_block_loop);
+                img_dummy.setImageResource(R.drawable.selected_block_loop);
                 break;
             case "e":
-                this.img_dummy.setImageResource(R.drawable.selected_block_ifelse);
+                img_dummy.setImageResource(R.drawable.selected_block_ifelse);
                 break;
             case "f":
-                this.img_dummy.setImageResource(R.drawable.selected_block_final);
+                img_dummy.setImageResource(R.drawable.selected_block_final);
                 break;
             case "v":
             case "p":
             case "l":
             case "a":
-                this.img_dummy.setImageResource(R.drawable.selected_block_string);
+                img_dummy.setImageResource(R.drawable.selected_block_string);
                 break;
             default:
-                this.img_dummy.setImageResource(R.drawable.selected_block_command);
+                img_dummy.setImageResource(R.drawable.selected_block_command);
                 break;
         }
-        this.img_dummy.setAlpha(0.5f);
-        rs.getLocationOnScreen(this.d);
+        img_dummy.setAlpha(0.5f);
+        rs.getLocationOnScreen(d);
     }
 
     public void a(View view, float f, float f2, float f3, float f4, float f5, float f6) {
-        if (this.layout_dummy.getVisibility() != View.VISIBLE) {
+        if (layout_dummy.getVisibility() != View.VISIBLE) {
             setDummyVisibility(View.VISIBLE);
         }
-        getLocationOnScreen(this.e);
-        this.layout_dummy.setX(((((this.d[0] - this.e[0]) + f) - f3) - this.img_notallowed.getWidth()) + f5);
-        this.layout_dummy.setY((((this.d[1] - this.e[1]) + f2) - f4) + f6);
+        getLocationOnScreen(e);
+        layout_dummy.setX(((((d[0] - e[0]) + f) - f3) - img_notallowed.getWidth()) + f5);
+        layout_dummy.setY((((d[1] - e[1]) + f2) - f4) + f6);
     }
 
     public void a(View view, float f, float f2, float f3, float f4) {
@@ -124,6 +124,6 @@ public class ViewDummy extends RelativeLayout {
     }
 
     public void a(int[] iArr) {
-        this.img_dummy.getLocationOnScreen(iArr);
+        img_dummy.getLocationOnScreen(iArr);
     }
 }
