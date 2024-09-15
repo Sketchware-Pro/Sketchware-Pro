80626dd929e70b2d6d9e9fc536ed40092e837d84
Javkhlanbayar Khongorzul <jbk@duck.com>
2024-09-15 14:02:28 +0200

    refactor: Use AndroidX PreferenceFragment in System Settings
    This depends on commits b24bde211732fa107cc0b5b0ba062c745feaca6e and be291fcf5d73fbdb735776b95caee08171f274fd. Is it technically still a refactor?

--- app/src/main/java/com/besome/sketch/help/SystemSettingActivity.java
+++ app/src/main/java/com/besome/sketch/help/SystemSettingActivity.java
@@ -1,62 +1,32 @@
 package com.besome.sketch.help;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
 
-import androidx.core.widget.NestedScrollView;
+import androidx.annotation.Nullable;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.besome.sketch.lib.base.BaseAppCompatActivity;
-import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.appbar.MaterialToolbar;
 import com.sketchware.remod.R;
-
-import mod.hey.studios.util.Helper;
+import com.sketchware.remod.databinding.PreferenceActivityBinding;
 
 public class SystemSettingActivity extends BaseAppCompatActivity {
-    private SharedPreferences.Editor preferenceEditor;
-
-    private LinearLayout content;
-    private NestedScrollView contentLayout;
-    private AppBarLayout appBarLayout;
-    private MaterialToolbar topAppBar;
-    private CollapsingToolbarLayout collapsingToolbar;
-
-    @Override
-    public void onBackPressed() {
-        if (saveSettings()) {
-            setResult(Activity.RESULT_OK, new Intent());
-            finish();
-        }
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.prefences_content_appbar);
+        var binding = PreferenceActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        content = findViewById(R.id.content);
-        topAppBar = findViewById(R.id.topAppBar);
-        appBarLayout = findViewById(R.id.appBarLayout);
-        contentLayout = findViewById(R.id.contentLayout);
-        collapsingToolbar = findViewById(R.id.collapsingToolbar);
-
-        topAppBar.setTitle(R.string.main_drawer_title_system_settings);
-        topAppBar.setNavigationOnClickListener(view -> onBackPressed());
-        loadPreferences();
+        binding.topAppBar.setTitle(R.string.main_drawer_title_system_settings);
+        getSupportFragmentManager().beginTransaction()
+                .replace(binding.fragmentContainer.getId(), new PreferenceFragment())
+                .commit();
     }
 
-    private void loadPreferences() {
-        SharedPreferences preferences = getSharedPreferences("P12", Context.MODE_PRIVATE);
-        preferenceEditor = preferences.edit();
-    }
-
-    private boolean saveSettings() {
-        return preferenceEditor.commit();
+    public static class PreferenceFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+            getPreferenceManager().setSharedPreferencesName("P12");
+            setPreferencesFromResource(R.xml.preferences_system_settings, rootKey);
+        }
     }
 }
