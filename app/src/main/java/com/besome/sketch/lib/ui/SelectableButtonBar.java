a4cac7fc6fdc099050dec94a3ddd135bcd8f18fc
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2024-10-04 00:30:54 +0530

    refactor: Remove unnecessary qualifiers

--- app/src/main/java/com/besome/sketch/lib/ui/SelectableButtonBar.java
+++ app/src/main/java/com/besome/sketch/lib/ui/SelectableButtonBar.java
@@ -13,77 +13,78 @@ import a.a.a.cB;
 import a.a.a.wB;
 
 public class SelectableButtonBar extends LinearLayout {
+
     public ArrayList<Integer> a;
     public ArrayList<String> b;
     public YA c;
-    public int d = 0;
+    public int d;
 
     public SelectableButtonBar(Context var1) {
         super(var1);
-        this.a(var1);
+        a(var1);
     }
 
     public SelectableButtonBar(Context var1, AttributeSet var2) {
         super(var1, var2);
-        this.a(var1);
+        a(var1);
     }
 
     public void a() {
-        ((LinearLayout) this).removeAllViews();
-        int var1 = this.b.size();
+        removeAllViews();
+        int var1 = b.size();
 
         for (int var2 = 0; var2 < var1; ++var2) {
-            LinearLayout var3 = this.b((Integer) this.a.get(var2), (String) this.b.get(var2));
+            LinearLayout var3 = b(a.get(var2), b.get(var2));
             if (var2 == 0) {
-                ((View) var3).setBackgroundResource(2131166076);
+                var3.setBackgroundResource(2131166076);
             } else if (var2 == var1 - 1) {
-                ((View) var3).setBackgroundResource(2131166077);
+                var3.setBackgroundResource(2131166077);
             } else {
-                ((View) var3).setBackgroundResource(2131166075);
+                var3.setBackgroundResource(2131166075);
             }
 
-            ((LinearLayout) this).addView(var3);
+            addView(var3);
         }
 
-        this.setSelectedItemByIndex(this.d);
+        setSelectedItemByIndex(d);
     }
 
     public void a(int var1, String var2) {
-        this.a.add(var1);
-        this.b.add(var2);
+        a.add(var1);
+        b.add(var2);
     }
 
     public final void a(Context var1) {
-        this.a = new ArrayList();
-        this.b = new ArrayList();
+        a = new ArrayList<>();
+        b = new ArrayList<>();
     }
 
     public final LinearLayout b(int var1, String var2) {
-        a var3 = new a(this, ((LinearLayout) this).getContext(), var1, var2);
+        a var3 = new a(this, getContext(), var1, var2);
         LinearLayout.LayoutParams var4 = new LinearLayout.LayoutParams(0, -2);
         var4.weight = 1.0F;
-        ((LinearLayout) var3).setLayoutParams(var4);
+        var3.setLayoutParams(var4);
         ((LinearLayout) var3).setOnClickListener(new cB(this));
         return var3;
     }
 
     public YA getListener() {
-        return this.c;
+        return c;
     }
 
     public int getSelectedItemKey() {
-        return ((a) ((LinearLayout) this).getChildAt(this.d)).b;
+        return ((a) getChildAt(d)).b;
     }
 
     public void setListener(YA var1) {
-        this.c = var1;
+        c = var1;
     }
 
     public void setSelectedItemByIndex(int var1) {
-        this.d = var1;
+        d = var1;
 
-        for (int var2 = 0; var2 < ((LinearLayout) this).getChildCount(); ++var2) {
-            a var3 = (a) ((LinearLayout) this).getChildAt(var2);
+        for (int var2 = 0; var2 < getChildCount(); ++var2) {
+            a var3 = (a) getChildAt(var2);
             var3.setSelected(false);
             if (var2 == var1) {
                 var3.setSelected(true);
@@ -93,13 +94,13 @@ public class SelectableButtonBar extends LinearLayout {
     }
 
     public void setSelectedItemByKey(int var1) {
-        for (int var2 = 0; var2 < ((LinearLayout) this).getChildCount(); ++var2) {
-            a var3 = (a) ((LinearLayout) this).getChildAt(var2);
+        for (int var2 = 0; var2 < getChildCount(); ++var2) {
+            a var3 = (a) getChildAt(var2);
             var3.setSelected(false);
             if (var3.b == var1) {
                 var3.setSelected(true);
-                this.d = var2;
-                YA var4 = this.c;
+                d = var2;
+                YA var4 = c;
                 if (var4 != null) {
                     var4.a(var1);
                 }
@@ -117,8 +118,8 @@ public class SelectableButtonBar extends LinearLayout {
 
         public a(SelectableButtonBar var1, Context var2, int var3, int var4, String var5) {
             super(var2);
-            this.e = var1;
-            this.a(var2, var3, var4, var5);
+            e = var1;
+            a(var2, var3, var4, var5);
         }
 
         public a(SelectableButtonBar var1, Context var2, int var3, String var4) {
@@ -126,29 +127,29 @@ public class SelectableButtonBar extends LinearLayout {
         }
 
         public final void a(Context var1, int var2, int var3, String var4) {
-            this.a = var2;
-            this.b = var3;
-            this.c = var4;
-            ((LinearLayout) this).setOrientation(0);
-            ((LinearLayout) this).setGravity(17);
-            var2 = (int) wB.a(((LinearLayout) this).getContext(), 4.0F);
-            ((LinearLayout) this).setPadding(var2, var2, var2, var2);
-            this.d = new TextView(var1);
+            a = var2;
+            b = var3;
+            c = var4;
+            setOrientation(0);
+            setGravity(17);
+            var2 = (int) wB.a(getContext(), 4.0F);
+            setPadding(var2, var2, var2, var2);
+            d = new TextView(var1);
             LinearLayout.LayoutParams var5 = new LinearLayout.LayoutParams(-2, -2);
-            this.d.setLayoutParams(var5);
-            this.d.setText(var4);
-            this.d.setTextColor(-16740915);
-            this.d.setLines(1);
-            this.d.setTextSize(2, 12.0F);
-            ((LinearLayout) this).addView(this.d);
+            d.setLayoutParams(var5);
+            d.setText(var4);
+            d.setTextColor(-16740915);
+            d.setLines(1);
+            d.setTextSize(2, 12.0F);
+            addView(d);
         }
 
         public void setSelected(boolean var1) {
             super.setSelected(var1);
             if (var1) {
-                this.d.setTextColor(-1);
+                d.setTextColor(-1);
             } else {
-                this.d.setTextColor(-16740915);
+                d.setTextColor(-16740915);
             }
 
         }
