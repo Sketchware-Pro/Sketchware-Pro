7a902d2d7785e5af69ff55af0b67de49edc6dc6b
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2024-10-04 08:13:27 +0530

    refactor: Rename some fields/method, make some private

--- app/src/main/java/com/besome/sketch/lib/ui/SelectableButtonBar.java
+++ app/src/main/java/com/besome/sketch/lib/ui/SelectableButtonBar.java
@@ -1,7 +1,9 @@
 package com.besome.sketch.lib.ui;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -9,151 +11,134 @@ import android.widget.TextView;
 import com.sketchware.remod.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import a.a.a.YA;
 import a.a.a.wB;
 
 public class SelectableButtonBar extends LinearLayout {
 
-    public ArrayList<Integer> a;
-    public ArrayList<String> b;
-    public YA c;
-    public int d;
+    private final List<Integer> keys = new ArrayList<>();
+    private final List<String> values = new ArrayList<>();
+    private YA selectionListener;
+    private int selectedItemIndex;
 
-    public SelectableButtonBar(Context var1) {
-        super(var1);
-        a(var1);
+    public SelectableButtonBar(Context context) {
+        super(context);
     }
 
-    public SelectableButtonBar(Context var1, AttributeSet var2) {
-        super(var1, var2);
-        a(var1);
+    public SelectableButtonBar(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
     }
 
     public void a() {
         removeAllViews();
-        int var1 = b.size();
+        int valuesSize = values.size();
 
-        for (int var2 = 0; var2 < var1; ++var2) {
-            LinearLayout var3 = b(a.get(var2), b.get(var2));
-            if (var2 == 0) {
-                var3.setBackgroundResource(R.drawable.selector_btnbar_left);
-            } else if (var2 == var1 - 1) {
-                var3.setBackgroundResource(R.drawable.selector_btnbar_right);
+        for (int i = 0; i < valuesSize; ++i) {
+            LinearLayout layout = createButton(keys.get(i), values.get(i));
+            if (i == 0) {
+                layout.setBackgroundResource(R.drawable.selector_btnbar_left);
+            } else if (i == valuesSize - 1) {
+                layout.setBackgroundResource(R.drawable.selector_btnbar_right);
             } else {
-                var3.setBackgroundResource(R.drawable.selector_btnbar_center);
+                layout.setBackgroundResource(R.drawable.selector_btnbar_center);
             }
 
-            addView(var3);
+            addView(layout);
         }
 
-        setSelectedItemByIndex(d);
+        setSelectedItemByIndex(selectedItemIndex);
     }
 
-    public void a(int var1, String var2) {
-        a.add(var1);
-        b.add(var2);
+    public void a(int key, String value) {
+        keys.add(key);
+        values.add(value);
     }
 
-    public final void a(Context var1) {
-        a = new ArrayList<>();
-        b = new ArrayList<>();
-    }
-
-    public final LinearLayout b(int var1, String var2) {
-        a var3 = new a(this, getContext(), var1, var2);
-        LinearLayout.LayoutParams var4 = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
-        var4.weight = 1.0F;
-        var3.setLayoutParams(var4);
-        var3.setOnClickListener(view -> setSelectedItemByKey(var3.b));
-        return var3;
+    private LinearLayout createButton(int key, String value) {
+        SelectableButton selectableButton = new SelectableButton(getContext(), key, value);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.weight = 1.0F;
+        selectableButton.setLayoutParams(layoutParams);
+        selectableButton.setOnClickListener(view -> setSelectedItemByKey(selectableButton.key));
+        return selectableButton;
     }
 
     public YA getListener() {
-        return c;
-    }
-
-    public int getSelectedItemKey() {
-        return ((a) getChildAt(d)).b;
+        return selectionListener;
     }
 
     public void setListener(YA var1) {
-        c = var1;
+        selectionListener = var1;
     }
 
-    public void setSelectedItemByIndex(int var1) {
-        d = var1;
+    public int getSelectedItemKey() {
+        return ((SelectableButton) getChildAt(selectedItemIndex)).key;
+    }
 
-        for (int var2 = 0; var2 < getChildCount(); ++var2) {
-            a var3 = (a) getChildAt(var2);
-            var3.setSelected(false);
-            if (var2 == var1) {
-                var3.setSelected(true);
+    public void setSelectedItemByIndex(int selectedIndex) {
+        selectedItemIndex = selectedIndex;
+
+        for (int i = 0; i < getChildCount(); ++i) {
+            SelectableButton selectableButton = (SelectableButton) getChildAt(i);
+            selectableButton.setSelected(false);
+            if (i == selectedIndex) {
+                selectableButton.setSelected(true);
             }
         }
-
     }
 
-    public void setSelectedItemByKey(int var1) {
-        for (int var2 = 0; var2 < getChildCount(); ++var2) {
-            a var3 = (a) getChildAt(var2);
-            var3.setSelected(false);
-            if (var3.b == var1) {
-                var3.setSelected(true);
-                d = var2;
-                YA var4 = c;
-                if (var4 != null) {
-                    var4.a(var1);
+    public void setSelectedItemByKey(int key) {
+        for (int i = 0; i < getChildCount(); ++i) {
+            SelectableButton selectableButton = (SelectableButton) getChildAt(i);
+            selectableButton.setSelected(false);
+            if (selectableButton.key == key) {
+                selectableButton.setSelected(true);
+                selectedItemIndex = i;
+                if (selectionListener != null) {
+                    selectionListener.a(key);
                 }
             }
         }
-
     }
 
-    public class a extends LinearLayout {
-        public int a;
-        public int b;
-        public String c;
-        public TextView d;
-        public final SelectableButtonBar e;
+    @SuppressLint("ViewConstructor")
+    public static class SelectableButton extends LinearLayout {
 
-        public a(SelectableButtonBar var1, Context var2, int var3, int var4, String var5) {
-            super(var2);
-            e = var1;
-            a(var2, var3, var4, var5);
+        private int key;
+        private TextView textView;
+
+        public SelectableButton(Context context, int key, String value) {
+            super(context);
+            initialize(context, key, value);
         }
 
-        public a(SelectableButtonBar var1, Context var2, int var3, String var4) {
-            this(var1, var2, 0, var3, var4);
-        }
-
-        public final void a(Context var1, int var2, int var3, String var4) {
-            a = var2;
-            b = var3;
-            c = var4;
+        private void initialize(Context context, int key, String value) {
+            this.key = key;
             setOrientation(LinearLayout.HORIZONTAL);
-            setGravity(17);
-            var2 = (int) wB.a(getContext(), 4.0F);
-            setPadding(var2, var2, var2, var2);
-            d = new TextView(var1);
-            LinearLayout.LayoutParams var5 = new LinearLayout.LayoutParams(
+            setGravity(Gravity.CENTER);
+            int padding = (int) wB.a(getContext(), 4.0F);
+            setPadding(padding, padding, padding, padding);
+            textView = new TextView(context);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
                     ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            d.setLayoutParams(var5);
-            d.setText(var4);
-            d.setTextColor(0xff008dcd);
-            d.setLines(1);
-            d.setTextSize(2, 12.0F);
-            addView(d);
+            textView.setLayoutParams(layoutParams);
+            textView.setText(value);
+            textView.setTextColor(0xff008dcd);
+            textView.setLines(1);
+            textView.setTextSize(2, 12.0F);
+            addView(textView);
         }
 
-        public void setSelected(boolean var1) {
-            super.setSelected(var1);
-            if (var1) {
-                d.setTextColor(0xffffffff);
+        @Override
+        public void setSelected(boolean selected) {
+            super.setSelected(selected);
+            if (selected) {
+                textView.setTextColor(0xffffffff);
             } else {
-                d.setTextColor(0xff008dcd);
+                textView.setTextColor(0xff008dcd);
             }
-
         }
     }
 }
