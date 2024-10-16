52de02cb6ab48af3a29007f6c6134d3fac89fefc
elfilibusterismo <elfilibusterismo.contact@gmail.com>
2024-10-17 04:45:17 +0800

    refactor: directly use fab of navigation rail view header

--- app/src/main/java/a/a/a/rs.java
+++ app/src/main/java/a/a/a/rs.java
@@ -235,7 +235,7 @@ public class rs extends qA implements View.OnClickListener, MoreblockImporterDia
         RecyclerView eventList = parent.findViewById(R.id.event_list);
         paletteView = parent.findViewById(R.id.palette);
         RecyclerView categoryList = parent.findViewById(R.id.category_list);
-        fab = parent.findViewById(R.id.fab);
+        fab = paletteView.getHeaderView().findViewById(R.id.fab);
         noEvents.setVisibility(View.GONE);
         noEvents.setText(xB.b().a(requireContext(), R.string.event_message_no_events));
         eventList.setLayoutManager(new LinearLayoutManager(null, RecyclerView.VERTICAL, false));
