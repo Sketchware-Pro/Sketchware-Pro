18ec202e3e51df8edcece53a8dfb8defb686a250
Pranav Purwar <purwarpranav80@gmail.com>
2024-10-22 19:57:20 +0530

    Convert some file to java

---
+++ app/src/main/java/com/besome/sketch/fragments/projects_store/ProjectsStoreFragment.java
@@ -0,0 +1,87 @@
+package com.besome.sketch.fragments.projects_store;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
+
+import com.besome.sketch.fragments.projects_store.adapters.StorePagerProjectsAdapter;
+import com.besome.sketch.fragments.projects_store.adapters.StoreProjectsAdapter;
+import com.besome.sketch.fragments.projects_store.api.SketchHubAPI;
+import com.besome.sketch.fragments.projects_store.classes.CenterZoomListener;
+import com.besome.sketch.fragments.projects_store.classes.HorizontalItemDecoration;
+import com.sketchware.remod.BuildConfig;
+import com.sketchware.remod.R;
+import com.sketchware.remod.databinding.FragmentProjectsStoreBinding;
+
+public class ProjectsStoreFragment extends Fragment {
+    private FragmentProjectsStoreBinding binding;
+    private SketchHubAPI sketchHubAPI;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentProjectsStoreBinding.inflate(inflater, container, false);
+        sketchHubAPI = new SketchHubAPI(BuildConfig.SKETCHUB_API_KEY);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.storeSideNote.setSelected(true);
+        setupRecyclerView(binding.editorsChoiceProjectsRecyclerView);
+        fetchData();
+    }
+
+    private void setupRecyclerView(RecyclerView recyclerView) {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(recyclerView);
+
+        recyclerView.addOnScrollListener(new CenterZoomListener());
+
+        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.recycler_view_item_spacing);
+        recyclerView.addItemDecoration(new HorizontalItemDecoration(spacingInPixels));
+
+        recyclerView.setClipToPadding(false);
+        recyclerView.setClipChildren(false);
+
+        ViewParent parent = recyclerView.getParent();
+        if (parent instanceof ViewGroup) {
+            ((ViewGroup) parent).setClipChildren(false);
+            ((ViewGroup) parent).setClipToPadding(false);
+        }
+
+        int sidePadding = getResources().getDimensionPixelSize(R.dimen.recycler_view_item_spacing);
+        recyclerView.setPadding(sidePadding, 0, sidePadding, 0);
+    }
+
+    private void fetchData() {
+        sketchHubAPI.getEditorsChoicerProjects(1, projectModel -> {
+            if (projectModel != null) {
+                binding.editorsChoiceProjectsRecyclerView.setAdapter(new StorePagerProjectsAdapter(projectModel.getProjects(), requireActivity()));
+            }
+        });
+        sketchHubAPI.getMostDownloadedProjects(1, projectModel -> {
+            if (projectModel != null) {
+                binding.mostDownloadedProjectsRecyclerView.setAdapter(new StoreProjectsAdapter(projectModel.getProjects(), requireActivity()));
+            }
+        });
+        sketchHubAPI.getRecentProjects(1, projectModel -> {
+            if (projectModel != null) {
+                binding.recentProjectsRecyclerView.setAdapter(new StoreProjectsAdapter(projectModel.getProjects(), requireActivity()));
+            }
+        });
+    }
+}
