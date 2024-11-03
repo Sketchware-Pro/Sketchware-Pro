62ee13746fa9ebd2ed2b0381c0f5624b21632e00
aikrq <95296093+aikrq@users.noreply.github.com>
2024-11-03 13:26:14 +0700

    fix: Build settings cannot be saved Fixes #1336

--- app/src/main/java/mod/hey/studios/build/BuildSettingsDialog.java
+++ app/src/main/java/mod/hey/studios/build/BuildSettingsDialog.java
@@ -1,79 +1,123 @@
 package mod.hey.studios.build;
 
+import static mod.hey.studios.build.BuildSettings.SETTING_ANDROID_JAR_PATH;
+import static mod.hey.studios.build.BuildSettings.SETTING_CLASSPATH;
+import static mod.hey.studios.build.BuildSettings.SETTING_DEXER;
+import static mod.hey.studios.build.BuildSettings.SETTING_ENABLE_LOGCAT;
+import static mod.hey.studios.build.BuildSettings.SETTING_JAVA_VERSION;
+import static mod.hey.studios.build.BuildSettings.SETTING_NO_HTTP_LEGACY;
+import static mod.hey.studios.build.BuildSettings.SETTING_NO_WARNINGS;
+
 import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 import pro.sketchware.R;
 import pro.sketchware.databinding.ProjectConfigLayoutBinding;
-
 import pro.sketchware.utility.SketchwareUtil;
 
 public class BuildSettingsDialog {
+    private static int totalViews = 0;
+
+    private static final int VIEW_ANDROIR_JAR_PATH = totalViews++;
+    private static final int VIEW_CLASS_PATH = totalViews++;
+    private static final int VIEW_DEXER = totalViews++;
+    private static final int VIEW_JAVA_VERSION = totalViews++;
+    private static final int VIEW_NO_WARNINGS = totalViews++;
+    private static final int VIEW_NO_HTTP_LEGACY = totalViews++;
+    private static final int VIEW_ENABLE_LOGCAT = totalViews++;
 
     private final Activity activity;
     private final BuildSettings settings;
+    private final ProjectConfigLayoutBinding binding;
+    private View[] views;
 
     public BuildSettingsDialog(Activity activity, String sc_id) {
         this.activity = activity;
+
         settings = new BuildSettings(sc_id);
+        views = new View[totalViews];
+        binding = ProjectConfigLayoutBinding.inflate(activity.getLayoutInflater());
+
+        views[VIEW_ANDROIR_JAR_PATH] = binding.tilAndroidJar.getEditText();
+        views[VIEW_CLASS_PATH] = binding.tilClasspath.getEditText();
+        views[VIEW_DEXER] = binding.rgDexer;
+        views[VIEW_ENABLE_LOGCAT] = binding.cbEnableLogcat;
+        views[VIEW_JAVA_VERSION] = binding.rgJavaVersion;
+        views[VIEW_NO_HTTP_LEGACY] = binding.cbNoHttpLegacy;
+        views[VIEW_NO_WARNINGS] = binding.cbNoWarnings;
+
+        // necessary for mod.hey.studios.project.ProjectSettings, since it checks
+        // for the presence of a tag if missing it cannot save the data
+        addTagsForViews();
+    }
+
+    private void addTagsForViews() {
+        views[VIEW_ANDROIR_JAR_PATH].setTag(SETTING_ANDROID_JAR_PATH);
+        views[VIEW_CLASS_PATH].setTag(SETTING_CLASSPATH);
+        views[VIEW_DEXER].setTag(SETTING_DEXER);
+        views[VIEW_ENABLE_LOGCAT].setTag(SETTING_ENABLE_LOGCAT);
+        views[VIEW_JAVA_VERSION].setTag(SETTING_JAVA_VERSION);
+        views[VIEW_NO_HTTP_LEGACY].setTag(SETTING_NO_HTTP_LEGACY);
+        views[VIEW_NO_WARNINGS].setTag(SETTING_NO_WARNINGS);
     }
 
     public void show() {
-        ProjectConfigLayoutBinding binding = ProjectConfigLayoutBinding.inflate(activity.getLayoutInflater());
+        assert views != null || views.length != 0;
 
-        View[] viewArr = {
-                binding.edittextAndroidJarPath,
-                binding.edittextClasspath,
-                binding.radiogroupDexer,
-                binding.radiogroupJavaVersion,
-                binding.checkboxNoWarnings,
-                binding.checkboxNoHttpLegacy,
-                binding.checkboxEnableLogcat
-        };
+        getEditText(VIEW_ANDROIR_JAR_PATH).setText(settings.getValue(SETTING_ANDROID_JAR_PATH, ""));
+        getEditText(VIEW_CLASS_PATH).setText(settings.getValue(SETTING_CLASSPATH, ""));
 
-        binding.edittextAndroidJarPath.setText(settings.getValue(BuildSettings.SETTING_ANDROID_JAR_PATH, ""));
-        binding.edittextClasspath.setText(settings.getValue(BuildSettings.SETTING_CLASSPATH, ""));
+        setRadioGroupOptions(getRadioGroup(VIEW_DEXER), new String[]{"Dx", "D8"}, SETTING_DEXER, "Dx");
+        setRadioGroupOptions(getRadioGroup(VIEW_JAVA_VERSION), BuildSettingsDialogBridge.getAvailableJavaVersions(), SETTING_JAVA_VERSION, "1.7");
 
-        setRadioGroupOptions(binding.radiogroupDexer, new String[]{"Dx", "D8"}, BuildSettings.SETTING_DEXER, "Dx");
-        setRadioGroupOptions(binding.radiogroupJavaVersion, BuildSettingsDialogBridge.getAvailableJavaVersions(), BuildSettings.SETTING_JAVA_VERSION, "1.7");
+        setCheckboxValue(getCheckbox(VIEW_NO_WARNINGS), SETTING_NO_WARNINGS, true);
+        setCheckboxValue(getCheckbox(VIEW_NO_HTTP_LEGACY), SETTING_NO_HTTP_LEGACY, false);
+        setCheckboxValue(getCheckbox(VIEW_ENABLE_LOGCAT), SETTING_ENABLE_LOGCAT, true);
 
-        setCheckBox(binding.checkboxNoWarnings, BuildSettings.SETTING_NO_WARNINGS, true);
-        setCheckBox(binding.checkboxNoHttpLegacy, BuildSettings.SETTING_NO_HTTP_LEGACY, false);
-        setCheckBox(binding.checkboxEnableLogcat, BuildSettings.SETTING_ENABLE_LOGCAT, true);
-
-        var builder = new MaterialAlertDialogBuilder(activity)
-                .setTitle("Build Settings")
-                .setIcon(R.drawable.ic_mtrl_tune)
-                .setPositiveButton("Save", (dialogInterface, i) -> settings.setValues(viewArr))
-                .setNegativeButton("Cancel", null);
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(activity);
+        builder.setIcon(R.drawable.ic_mtrl_tune);
+        builder.setTitle("Build Settings");
+        builder.setPositiveButton("Save", (d, which) -> settings.setValues(views));
+        builder.setNegativeButton("Cancel", null);
         builder.setView(binding.getRoot());
         builder.show();
+    }
+
+    private EditText getEditText(int index) {
+        return (EditText) views[index];
+    }
+
+    private RadioGroup getRadioGroup(int index) {
+        return (RadioGroup) views[index];
+    }
+
+    private CheckBox getCheckbox(int index) {
+        return (CheckBox) views[index];
     }
 
     private void setRadioGroupOptions(RadioGroup radioGroup, String[] options, String key, String defaultValue) {
         radioGroup.removeAllViews();
         String value = settings.getValue(key, defaultValue);
         for (String option : options) {
-            RadioButton radioButton = new RadioButton(activity);
+            RadioButton radioButton = new RadioButton(radioGroup.getContext());
             radioButton.setText(option);
             radioButton.setId(View.generateViewId());
-            radioButton.setLayoutParams(new RadioGroup.LayoutParams(
-                    0,
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    1.0f));
+            radioButton.setLayoutParams(new RadioGroup.LayoutParams(0, -2, 1f));
             if (value.equals(option)) {
                 radioButton.setChecked(true);
             }
             radioButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
                 if (!isChecked) return;
-                if (key.equals(BuildSettings.SETTING_JAVA_VERSION)) {
+                if (key.equals(SETTING_JAVA_VERSION)) {
                     BuildSettingsDialogBridge.handleJavaVersionChange(option);
-                } else if (key.equals(BuildSettings.SETTING_DEXER)) {
+                } else if (key.equals(SETTING_DEXER)) {
                     BuildSettingsDialogBridge.handleDexerChange(option);
                 }
             });
@@ -82,12 +126,12 @@ public class BuildSettingsDialog {
         radioGroup.setTag(key);
     }
 
-    private void setCheckBox(CheckBox checkBox, String key, boolean defaultValue) {
+    private void setCheckboxValue(CheckBox checkBox, String key, boolean defaultValue) {
         String value = settings.getValue(key, defaultValue ? "true" : "false");
         checkBox.setChecked(value.equals("true"));
         checkBox.setTag(key);
 
-        if (key.equals(BuildSettings.SETTING_NO_HTTP_LEGACY)) {
+        if (key.equals(SETTING_NO_HTTP_LEGACY)) {
             checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
                 if (isChecked) {
                     SketchwareUtil.toast("Note that this option may cause issues if RequestNetwork component is used");
