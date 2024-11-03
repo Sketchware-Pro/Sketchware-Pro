a4a56a73ee260e9b2f8e9b6b6e9e0679575d68ad
aikrq <95296093+aikrq@users.noreply.github.com>
2024-11-03 17:20:14 +0700

    feat: Do not use AlarmManager for uncaught exception handler

--- app/src/main/java/pro/sketchware/SketchApplication.java
+++ app/src/main/java/pro/sketchware/SketchApplication.java
@@ -1,8 +1,6 @@
 package pro.sketchware;
 
-import android.app.AlarmManager;
 import android.app.Application;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Process;
@@ -13,7 +11,6 @@ import com.besome.sketch.tools.CollectErrorActivity;
 import mod.trindadedev.manage.theme.ThemeManager;
 
 public class SketchApplication extends Application {
-
     private static Context mApplicationContext;
 
     public static Context getContext() {
@@ -23,22 +20,15 @@ public class SketchApplication extends Application {
     @Override
     public void onCreate() {
         mApplicationContext = getApplicationContext();
-        Thread.UncaughtExceptionHandler uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
-        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
-            Log.e("SketchApplication", "Uncaught exception on thread " + thread.getName(), throwable);
-
-            Intent intent = new Intent(getApplicationContext(), CollectErrorActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            intent.putExtra("error", Log.getStackTraceString(throwable));
-            ((AlarmManager) getSystemService(Context.ALARM_SERVICE))
-                    .set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                            1000,
-                            PendingIntent.getActivity(getApplicationContext(), 11111, intent,
-                                    PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE));
-            Process.killProcess(Process.myPid());
-            System.exit(1);
-            if (uncaughtExceptionHandler != null) {
-                uncaughtExceptionHandler.uncaughtException(thread, throwable);
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread thread, Throwable throwable) {
+                Intent intent = new Intent(getApplicationContext(), CollectErrorActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.putExtra("error", Log.getStackTraceString(throwable));
+                startActivity(intent);
+                Process.killProcess(Process.myPid());
+                System.exit(1);
             }
         });
         super.onCreate();
