ed6d7d62edfe696984a3535f3f3c37832ca25449
Ilyasse <ilyassesalama7@gmail.com>
2024-11-21 01:03:33 +0100

    chore: Reformat code

--- app/src/main/java/mod/hilal/saif/activities/tools/IconSelectorDialog.java
+++ app/src/main/java/mod/hilal/saif/activities/tools/IconSelectorDialog.java
@@ -5,22 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.TextView;
 import android.widget.GridView;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import mod.jbk.util.OldResourceIdMapper;
-import pro.sketchware.databinding.DialogIconSelectorBinding;
-import pro.sketchware.databinding.ItemIconSelectorBinding;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class IconSelectorDialog {
+import mod.jbk.util.OldResourceIdMapper;
+import pro.sketchware.databinding.DialogIconSelectorBinding;
+import pro.sketchware.databinding.ItemIconSelectorBinding;
 
+public class IconSelectorDialog {
     private final Activity activity;
     private final List<Integer> data;
     private final TextView toSetText;
@@ -52,34 +51,18 @@ public class IconSelectorDialog {
         }
 
         builder = new MaterialAlertDialogBuilder(activity)
-            .setTitle("Select an icon")
-            .setView(dialogBinding.getRoot())
-            .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
-            .create();
+                .setTitle("Select an icon")
+                .setView(dialogBinding.getRoot())
+                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                .create();
         builder.show();
     }
 
-    private class IconListAdapter extends BaseAdapter {
-
+    private static class IconListAdapter extends BaseAdapter {
         private final List<Integer> iconsList;
 
         public IconListAdapter(List<Integer> arrayList) {
             iconsList = arrayList;
-        }
-
-        @Override
-        public int getCount() {
-            return iconsList.size();
-        }
-
-        @Override
-        public Integer getItem(int index) {
-            return iconsList.get(index);
-        }
-
-        @Override
-        public long getItemId(int index) {
-            return index;
         }
 
         @Override
@@ -95,10 +78,25 @@ public class IconSelectorDialog {
 
             int oldResourceId = getItem(position);
             itemBinding.iconImage.setImageResource(
-                OldResourceIdMapper.getDrawableFromOldResourceId(oldResourceId)
+                    OldResourceIdMapper.getDrawableFromOldResourceId(oldResourceId)
             );
 
             return convertView;
+        }
+
+        @Override
+        public int getCount() {
+            return iconsList.size();
+        }
+
+        @Override
+        public Integer getItem(int index) {
+            return iconsList.get(index);
+        }
+
+        @Override
+        public long getItemId(int index) {
+            return index;
         }
     }
 }
