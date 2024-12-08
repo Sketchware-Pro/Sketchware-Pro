8dcf1832618ed54eec6f1cb8bbeae96382b862c5
bobur-777 <bobur777rakhmonov@gmail.com>
2024-12-08 18:20:02 +0500

    fix: Remove vector images when open when close Image activity

--- app/src/main/java/a/a/a/pu.java
+++ app/src/main/java/a/a/a/pu.java
@@ -231,7 +231,10 @@ public class pu extends qA implements View.OnClickListener {
         }
         jC.d(sc_id).b(images);
         jC.d(sc_id).y();
-        jC.a(sc_id).b(jC.d(sc_id));
+
+        // This method is replaces not exist images to default_image, I removed it because it changes vector images to default_images
+        // jC.a(sc_id).b(jC.d(sc_id));
+
         jC.a(sc_id).k();
     }
 
