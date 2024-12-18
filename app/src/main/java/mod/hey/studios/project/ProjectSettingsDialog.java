066eebd574c2bfc3f81e0e11912655a0a523e18e
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-18 14:35:21 -0300

    refactor: make Project Settings a BottomSheet

--- app/src/main/java/mod/hey/studios/project/ProjectSettingsDialog.java
+++ app/src/main/java/mod/hey/studios/project/ProjectSettingsDialog.java
@@ -5,15 +5,18 @@ import static com.besome.sketch.Config.VAR_DEFAULT_TARGET_SDK_VERSION;
 
 import android.app.Activity;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.besome.sketch.beans.ProjectLibraryBean;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import mod.hey.studios.util.Helper;
 
 import pro.sketchware.R;
 import pro.sketchware.databinding.DialogProjectSettingsBinding;
 
-import a.a.a.aB;
 import a.a.a.jC;
 
 public class ProjectSettingsDialog {
@@ -29,28 +32,34 @@ public class ProjectSettingsDialog {
     }
 
     public void show() {
-        aB dialog = new aB(activity);
-        dialog.a(R.drawable.services_48);
-        dialog.b("Project configurations");
-
+        BottomSheetDialog dialog = new BottomSheetDialog(activity);
         DialogProjectSettingsBinding binding = DialogProjectSettingsBinding.inflate(activity.getLayoutInflater());
+        
+        dialog.setOnShowListener(bsd -> {
+            var b = (BottomSheetDialog) bsd;
+            var parent = b.findViewById(com.google.android.material.R.id.design_bottom_sheet);
+            if (parent != null) {
+                BottomSheetBehavior<View> behavior = BottomSheetBehavior.from(parent);
+                behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                behavior.setSkipCollapsed(true);
+            }
+        });
 
         binding.etMinimumSdkVersion.setText(settings.getValue(ProjectSettings.SETTING_MINIMUM_SDK_VERSION, String.valueOf(VAR_DEFAULT_MIN_SDK_VERSION)));
         binding.etTargetSdkVersion.setText(settings.getValue(ProjectSettings.SETTING_TARGET_SDK_VERSION, String.valueOf(VAR_DEFAULT_TARGET_SDK_VERSION)));
         binding.etApplicationClassName.setText(settings.getValue(ProjectSettings.SETTING_APPLICATION_CLASS, ".SketchApplication"));
 
         binding.enableViewbinding.setChecked(
-                settings.getValue(ProjectSettings.SETTING_ENABLE_VIEWBINDING, "false").equals("true"));
+            settings.getValue(ProjectSettings.SETTING_ENABLE_VIEWBINDING, "false").equals("true"));
         binding.cbRemoveOldMethods.setChecked(
-                settings.getValue(ProjectSettings.SETTING_DISABLE_OLD_METHODS, "false").equals("true"));
+            settings.getValue(ProjectSettings.SETTING_DISABLE_OLD_METHODS, "false").equals("true"));
         binding.cbUseNewMaterial3AppTheme.setChecked(
-                settings.getValue(ProjectSettings.SETTING_ENABLE_MATERIAL3, "false").equals("true"));
+            settings.getValue(ProjectSettings.SETTING_ENABLE_MATERIAL3, "false").equals("true"));
         binding.cbUseDynamicColors.setChecked(settings.isDynamicColorsEnable());
-       
-        // dont enable if app compat is off
+
         binding.cbUseNewMaterial3AppTheme.setEnabled(projectLibrary.isEnabled());
         binding.cbUseDynamicColors.setEnabled(projectLibrary.isEnabled());
-       
+
         binding.cbUseNewMaterial3AppTheme.setOnCheckedChangeListener((sw, isChecked) -> {
             binding.cbUseDynamicColors.setOnCheckedChangeListener(null);
             if (!isChecked) {
@@ -62,7 +71,7 @@ public class ProjectSettingsDialog {
                 }
             });
         });
-        
+
         binding.cbUseDynamicColors.setOnCheckedChangeListener((sw, isChecked) -> {
             binding.cbUseNewMaterial3AppTheme.setOnCheckedChangeListener(null);
             if (isChecked) {
@@ -74,7 +83,7 @@ public class ProjectSettingsDialog {
                 }
             });
         });
-        
+
         binding.enableViewbinding.setTag(ProjectSettings.SETTING_ENABLE_VIEWBINDING);
         binding.etMinimumSdkVersion.setTag(ProjectSettings.SETTING_MINIMUM_SDK_VERSION);
         binding.etTargetSdkVersion.setTag(ProjectSettings.SETTING_TARGET_SDK_VERSION);
@@ -82,24 +91,25 @@ public class ProjectSettingsDialog {
         binding.cbRemoveOldMethods.setTag(ProjectSettings.SETTING_DISABLE_OLD_METHODS);
         binding.cbUseNewMaterial3AppTheme.setTag(ProjectSettings.SETTING_ENABLE_MATERIAL3);
         binding.cbUseDynamicColors.setTag(ProjectSettings.SETTING_ENABLE_DYNAMIC_COLORS);
-        
-        dialog.a(binding.getRoot());
+
+        dialog.setContentView(binding.getRoot());
 
         final View[] preferences = {
-                binding.etMinimumSdkVersion,
-                binding.etTargetSdkVersion,
-                binding.etApplicationClassName,
-                binding.enableViewbinding,
-                binding.cbRemoveOldMethods,
-                binding.cbUseNewMaterial3AppTheme,
-                binding.cbUseDynamicColors
+              binding.etMinimumSdkVersion,
+              binding.etTargetSdkVersion,
+              binding.etApplicationClassName,
+              binding.enableViewbinding,
+              binding.cbRemoveOldMethods,
+              binding.cbUseNewMaterial3AppTheme,
+              binding.cbUseDynamicColors
         };
 
-        dialog.a(Helper.getResString(R.string.common_word_cancel), Helper.getDialogDismissListener(dialog));
-        dialog.b(Helper.getResString(R.string.common_word_save), v -> {
+        binding.btnCancel.setOnClickListener(v -> dialog.dismiss());
+        binding.btnSave.setOnClickListener(v -> {
             settings.setValues(preferences);
             dialog.dismiss();
         });
+
         dialog.show();
     }
 }
