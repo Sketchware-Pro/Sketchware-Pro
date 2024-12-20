a70573506c1fe993b562551492e490beb254b254
Abolo <107351062+TanTai97o@users.noreply.github.com>
2024-12-21 02:21:45 +0700

    chore: remove unused import

--- app/src/main/java/pro/sketchware/SketchApplication.java
+++ app/src/main/java/pro/sketchware/SketchApplication.java
@@ -3,7 +3,6 @@ package pro.sketchware;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Process;
 import android.util.Log;
