597e5f06949348c4a917cc3294cb74025c0def5b
waelchateur <waelchateur@gmail.com>
2024-12-31 03:53:14 +0100

    feat: Enhanced xml highlighting in editor

--- app/src/main/java/mod/jbk/code/CodeEditorLanguages.java
+++ app/src/main/java/mod/jbk/code/CodeEditorLanguages.java
@@ -15,27 +15,14 @@ import mod.jbk.util.LogUtil;
 public class CodeEditorLanguages {
     private static final String TAG = "CodeEditorLanguages";
 
-    public static final String[] LANGUAGES = {"kotlin.tmLanguage", "xml.tmLanguage.json"};
     public static final String SCOPE_NAME_KOTLIN = "source.kotlin";
     public static final String SCOPE_NAME_XML = "text.xml";
 
     static {
-        FileProviderRegistry.getInstance().addFileProvider(
-                new AssetsFileResolver(SketchApplication.getContext().getAssets()));
-
-        for (String language : LANGUAGES) {
-            LanguageDefinitionListBuilder builder = new LanguageDefinitionListBuilder();
-            String languageName = language.substring(0, language.indexOf('.'));
-            builder.language(languageName, languageDefinitionBuilder -> {
-                languageDefinitionBuilder.grammar = "textmate/" + language;
-                languageDefinitionBuilder.defaultScopeName(language.equals(LANGUAGES[1]) ? "text" : "source");
-                return Unit.INSTANCE;
-            });
-
+        FileProviderRegistry.getInstance().addFileProvider(new AssetsFileResolver(SketchApplication.getContext().getAssets()));
             Throwable t;
             try {
-                GrammarRegistry.getInstance().loadGrammars(builder);
-                continue;
+                GrammarRegistry.getInstance().loadGrammars("textmate/languages.json");
             } catch (Exception e) {
                 t = e;
                 // fall-through
@@ -44,8 +31,7 @@ public class CodeEditorLanguages {
                 LogUtil.e(TAG, "Probably running on a low API device");
                 // fall-through
             }
-            LogUtil.e(TAG, "Failed to load language '" + language + "'", t);
-        }
+        
     }
 
     public static Language loadTextMateLanguage(String scopeName) {
