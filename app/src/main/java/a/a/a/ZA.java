f8b36bb557942e5d0ff958faca0b159cefc7d6f0
Mustiy0 <87241434+Mustiy0@users.noreply.github.com>
2025-01-13 02:52:13 +0300

    changing sketchware logo loading colors

--- app/src/main/java/a/a/a/ZA.java
+++ app/src/main/java/a/a/a/ZA.java
@@ -1,13 +1,24 @@
 package a.a.a;
 
+import static pro.sketchware.utility.theme.ThemeManager.getColorFromTheme;
+import static pro.sketchware.utility.theme.ThemeManager.getCurrentTheme;
+
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.view.Window;
 import android.widget.TextView;
 
 import androidx.core.view.WindowCompat;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieProperty;
+import com.airbnb.lottie.model.KeyPath;
+import com.airbnb.lottie.value.LottieFrameInfo;
+import com.airbnb.lottie.value.SimpleLottieValueCallback;
+
 import pro.sketchware.R;
 
 import mod.hey.studios.util.Helper;
@@ -21,6 +32,7 @@ public class ZA extends Dialog {
         setContentView(R.layout.progress);
         animationView = findViewById(R.id.anim_sketchware);
         TextView tvProgress = findViewById(R.id.tv_progress);
+        initColor(context);
         tvProgress.setText(Helper.getResString(R.string.common_message_loading));
         super.setCancelable(false);
 
@@ -47,6 +59,20 @@ public class ZA extends Dialog {
         if (animationView != null && !animationView.isAnimating()) {
             animationView.resumeAnimation();
         }
+    }
+
+    private void initColor(Context context){
+        animationView.addValueCallback(
+                new KeyPath("group1", "**"),
+                LottieProperty.COLOR_FILTER,
+                frameInfo -> new PorterDuffColorFilter(getColorFromTheme(animationView.getContext(), getCurrentTheme(context), com.google.android.material.R.attr.colorPrimary), PorterDuff.Mode.SRC_ATOP)
+        );
+
+        animationView.addValueCallback(
+                new KeyPath("group2", "**"),
+                LottieProperty.COLOR_FILTER,
+                frameInfo -> new PorterDuffColorFilter(getColorFromTheme(animationView.getContext(), getCurrentTheme(context), com.google.android.material.R.attr.colorOnSecondary), PorterDuff.Mode.SRC_ATOP)
+        );
     }
 
 }
