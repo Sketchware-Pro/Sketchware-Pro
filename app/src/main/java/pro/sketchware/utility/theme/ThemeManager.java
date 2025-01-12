d4d8311f8cd1779e986c53394457bcb754da48cf
Mustiy0 <87241434+Mustiy0@users.noreply.github.com>
2025-01-12 22:52:57 +0300

    feat: adding necessary methods to ThemeManager

--- app/src/main/java/pro/sketchware/utility/theme/ThemeManager.java
+++ app/src/main/java/pro/sketchware/utility/theme/ThemeManager.java
@@ -3,20 +3,32 @@ package pro.sketchware.utility.theme;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.content.res.TypedArray;
+import android.util.TypedValue;
 
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.view.ContextThemeWrapper;
+
+import java.util.ArrayList;
+
+import pro.sketchware.R;
 
 public class ThemeManager {
 
     private static final String THEME_PREF = "themedata";
-    private static final String THEME_KEY = "idetheme";
+    private static final String MODE_KEY = "idetheme";// theme mode : light , dark and follow system
+    private static final String THEME_KEY = "idethemecolor"; // eg : green apple , sakura
+    private static final String AMOLED_KEY = "ideisamoled";
 
     public static final int THEME_SYSTEM = 0;
     public static final int THEME_LIGHT = 1;
     public static final int THEME_DARK = 2;
 
-    public static void applyTheme(Context context, int type) {
-        saveTheme(context, type);
+    public static void applyMode(Context context, int type) {
+        saveThemeMode(context, type);
+        saveThemeMode(context, type);
+        applyTheme(context, getCurrentTheme(context));
+        if (isAmoledEnabled(context)) applyAmoled(context);
 
         switch (type) {
             case THEME_LIGHT:
@@ -30,26 +42,92 @@ public class ThemeManager {
         }
     }
 
+    public static void applyTheme(Context context, int theme) {
+        switch (theme) {
+            case (0):
+                context.setTheme(R.style.Theme_Dynamic);
+                break;
+            case (1):
+                context.setTheme(R.style.Theme_Lavender);
+                break;
+            case (2):
+                context.setTheme(R.style.Theme_yogNesh);
+                break;
+            case (3):
+                context.setTheme(R.style.Theme_YinYang);
+                break;
+            case (4):
+                context.setTheme(R.style.Theme_sketchwareOg);
+                break;
+            case (5):
+                context.setTheme(R.style.Theme_GreenApple);
+                break;
+            case (6):
+                context.setTheme(R.style.Theme_Sakura);
+                break;
+            default:
+                context.setTheme(R.style.Theme_Dynamic);
+        }
+        saveTheme(context,theme);
+    }
+
+    public static boolean isAmoledEnabled(Context context) {
+        return getPreferences(context).getBoolean(AMOLED_KEY, false);
+    }
+
+    public static int getSystemAppliedTheme(Context context){
+        TypedValue outValue = new TypedValue();
+        context.getTheme().resolveAttribute(R.attr.themeId, outValue, true);
+        return outValue.data;
+    }
+
+    public static void applyAmoled(Context context) {
+        context.getTheme().applyStyle(R.style.Theme_Amoled, true);
+    }
+
+    public static void setAmoled(Context context, boolean bool) {
+        getPreferences(context).edit().putBoolean(AMOLED_KEY, bool).apply();
+        if (bool) applyAmoled(context);
+    }
+
     public static int getCurrentTheme(Context context) {
         return getPreferences(context).getInt(THEME_KEY, THEME_SYSTEM);
     }
 
-    public static boolean isSystemTheme(Context context) {
-        return getCurrentTheme(context) == THEME_SYSTEM;
+    public static int getCurrentMode(Context context) {
+        return getPreferences(context).getInt(MODE_KEY, THEME_SYSTEM);
     }
 
-    public static int getSystemAppliedTheme(Context context) {
-        int nightModeFlags = context.getResources().getConfiguration().uiMode
-                & Configuration.UI_MODE_NIGHT_MASK;
+    public static ArrayList<ThemeItem> getThemesList(){
+        ArrayList<ThemeItem> themeList = new ArrayList<>();
 
-        return switch (nightModeFlags) {
-            case Configuration.UI_MODE_NIGHT_NO -> THEME_LIGHT;
-            case Configuration.UI_MODE_NIGHT_YES -> THEME_DARK;
-            default -> THEME_SYSTEM;
-        };
+        themeList.add(new ThemeItem("Dynamic", R.style.Theme_Dynamic, 0));
+        themeList.add(new ThemeItem("Lavender", R.style.Theme_Lavender, 1));
+        themeList.add(new ThemeItem("Yog & esh", R.style.Theme_yogNesh, 2));
+        themeList.add(new ThemeItem("Yin & Yang", R.style.Theme_YinYang, 3));
+        themeList.add(new ThemeItem("Sketchware original", R.style.Theme_sketchwareOg, 4));
+        themeList.add(new ThemeItem("Green Apple", R.style.Theme_GreenApple, 5));
+        themeList.add(new ThemeItem("Sakura", R.style.Theme_Sakura, 6));
+
+
+        return themeList;
     }
 
-    private static void saveTheme(Context context, int theme) {
+    public static int getColorFromTheme(Context context, int themeResId, int attrResId) {
+        ContextThemeWrapper themeWrapper = new ContextThemeWrapper(context, themeResId);
+        TypedArray typedArray = themeWrapper.obtainStyledAttributes(new int[]{attrResId});
+        int color = typedArray.getColor(0, 0xbdbdbd);
+        typedArray.recycle();
+        return color;
+    }
+
+
+    private static void saveThemeMode(Context context, int theme) {
+        getPreferences(context).edit().putInt(MODE_KEY, theme).apply();
+    }
+
+
+    public static void saveTheme(Context context, int theme) {
         getPreferences(context).edit().putInt(THEME_KEY, theme).apply();
     }
 
