9c2ee84ee15e3906ce2a589421224d9228ef0ed6
Elmo <113604229+elfilibusterismo@users.noreply.github.com>
2025-03-11 23:42:15 +0800

    feat: Ensure root view is a ViewGroup in ViewBeanParser

--- app/src/main/java/pro/sketchware/tools/ViewBeanParser.java
+++ app/src/main/java/pro/sketchware/tools/ViewBeanParser.java
@@ -5,6 +5,7 @@ import static pro.sketchware.utility.PropertiesUtil.parseReferName;
 
 import android.util.Pair;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 
@@ -81,6 +82,10 @@ public class ViewBeanParser {
                 case XmlPullParser.START_TAG -> {
                     var name = parser.getName();
                     if (!isRootSkipped) {
+                        var view = InvokeUtil.createView(getContext(), name);
+                        if (!(view instanceof ViewGroup)) {
+                            throw new IOException("Root view must be a ViewGroup");
+                        }
                         Map<String, String> attributes = new LinkedHashMap<>();
                         for (int i = 0; i < parser.getAttributeCount(); i++) {
                             if (!parser.getAttributeName(i).startsWith("xmlns")) {
