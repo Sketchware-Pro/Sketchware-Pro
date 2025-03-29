260aa44d217c4748d2083bd218740d8f71f7ef6e
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-03-30 00:52:30 +0530

    refactor: Improve code readability in _B.java

--- app/src/main/java/a/a/a/_B.java
+++ app/src/main/java/a/a/a/_B.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package a.a.a;
 
 import android.content.Context;
@@ -14,6 +9,7 @@ import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class _B extends MB {
+
     public String[] f;
     public String[] g;
     public ArrayList<String> h;
@@ -22,37 +18,33 @@ public class _B extends MB {
 
     public _B(Context var1, TextInputLayout var2, String[] var3, String[] var4, ArrayList<String> var5, String var6) {
         super(var1, var2);
-        this.f = var3;
-        this.g = var4;
-        this.h = var5;
-        this.i = var6;
-    }
-
-    public CharSequence filter(CharSequence var1, int var2, int var3, Spanned var4, int var5, int var6) {
-        return null;
+        f = var3;
+        g = var4;
+        h = var5;
+        i = var6;
     }
 
     public void onTextChanged(CharSequence var1, int var2, int var3, int var4) {
         String var5 = var1.toString().trim().toLowerCase();
         if (var5.length() < 1) {
-            super.b.setErrorEnabled(true);
-            super.b.setError(xB.b().a(super.a, 2131625433, new Object[]{1}));
-            super.d = false;
+            b.setErrorEnabled(true);
+            b.setError(xB.b().a(a, 2131625433, 1));
+            d = false;
         } else if (var5.length() > 100) {
-            super.b.setErrorEnabled(true);
-            super.b.setError(xB.b().a(super.a, 2131625432, new Object[]{100}));
-            super.d = false;
+            b.setErrorEnabled(true);
+            b.setError(xB.b().a(a, 2131625432, 100));
+            d = false;
         } else {
-            String var6 = this.i;
-            if (var6 != null && var6.length() > 0 && var5.equals(this.i.toLowerCase())) {
-                super.b.setErrorEnabled(false);
-                super.d = true;
-            } else if (this.h.indexOf(var5) >= 0) {
-                super.b.setErrorEnabled(true);
-                super.b.setError(xB.b().a(super.a, 2131624950));
-                super.d = false;
+            String var6 = i;
+            if (var6 != null && var6.length() > 0 && var5.equals(i.toLowerCase())) {
+                b.setErrorEnabled(false);
+                d = true;
+            } else if (h.contains(var5)) {
+                b.setErrorEnabled(true);
+                b.setError(xB.b().a(a, 2131624950));
+                d = false;
             } else {
-                var6 = this.g;
+                var6 = g;
                 var3 = ((Object[]) var6).length;
                 var2 = 0;
 
@@ -71,11 +63,11 @@ public class _B extends MB {
                 }
 
                 if (var2) {
-                    super.b.setErrorEnabled(true);
-                    super.b.setError(xB.b().a(super.a, 2131624950));
-                    super.d = false;
+                    b.setErrorEnabled(true);
+                    b.setError(xB.b().a(a, 2131624950));
+                    d = false;
                 } else {
-                    var6 = this.f;
+                    var6 = f;
                     var3 = ((Object[]) var6).length;
                     var2 = 0;
 
@@ -94,21 +86,21 @@ public class _B extends MB {
                     }
 
                     if (var2) {
-                        super.b.setErrorEnabled(true);
-                        super.b.setError(xB.b().a(super.a, 2131625495));
-                        super.d = false;
+                        b.setErrorEnabled(true);
+                        b.setError(xB.b().a(a, 2131625495));
+                        d = false;
                     } else if (!Character.isLetter(var5.charAt(0))) {
-                        super.b.setErrorEnabled(true);
-                        super.b.setError(xB.b().a(super.a, 2131625497));
-                        super.d = false;
+                        b.setErrorEnabled(true);
+                        b.setError(xB.b().a(a, 2131625497));
+                        d = false;
                     } else {
-                        if (this.j.matcher(var1).matches()) {
-                            super.b.setErrorEnabled(false);
-                            super.d = true;
+                        if (j.matcher(var1).matches()) {
+                            b.setErrorEnabled(false);
+                            d = true;
                         } else {
-                            super.b.setErrorEnabled(true);
-                            super.b.setError(xB.b().a(super.a, 2131625436));
-                            super.d = false;
+                            b.setErrorEnabled(true);
+                            b.setError(xB.b().a(a, 2131625436));
+                            d = false;
                         }
 
                     }
