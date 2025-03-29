fe6ce04f43e5ab2bacb91dfd0398192f9033704f
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-03-30 03:06:06 +0530

    refactor: Improve code readability in NB class

--- app/src/main/java/a/a/a/NB.java
+++ app/src/main/java/a/a/a/NB.java
@@ -1,43 +1,31 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package a.a.a;
 
 import android.content.Context;
-import android.text.Spanned;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 
+import pro.sketchware.R;
+
 public class NB extends MB {
-    public ArrayList<String> f;
-    public String g;
 
-    public NB(Context var1, TextInputLayout var2, ArrayList<String> var3) {
+    private final ArrayList<String> preDefNames;
+
+    public NB(Context var1, TextInputLayout var2, ArrayList<String> names) {
         super(var1, var2);
-        this.f = var3;
+        preDefNames = names;
     }
 
-    public CharSequence filter(CharSequence var1, int var2, int var3, Spanned var4, int var5, int var6) {
-        return null;
-    }
-
-    public void onTextChanged(CharSequence var1, int var2, int var3, int var4) {
-        String var5 = var1.toString();
-        String var6 = this.g;
-        if (var6 != null && var6.length() > 0 && var5.equals(this.g)) {
-            super.b.setErrorEnabled(false);
-            super.d = true;
-        } else if (this.f.indexOf(var5) >= 0) {
-            super.b.setErrorEnabled(true);
-            super.b.setError(xB.b().a(super.a, 2131624950));
-            super.d = false;
+    public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+        String inputValue = charSequence.toString();
+        if (preDefNames.contains(inputValue)) {
+            b.setErrorEnabled(true);
+            b.setError(xB.b().a(a, R.string.common_message_name_unavailable));
+            d = false;
         } else {
-            super.b.setErrorEnabled(false);
-            super.d = true;
+            b.setErrorEnabled(false);
+            d = true;
         }
     }
 }
