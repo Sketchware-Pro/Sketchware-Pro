4d1ca74d26fd6cfe6e04465f4c65c7cf408bf621
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-03-30 14:35:10 +0530

    refactor: Simplify and clarify code in UB.java

--- app/src/main/java/a/a/a/UB.java
+++ app/src/main/java/a/a/a/UB.java
@@ -1,88 +1,81 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package a.a.a;
 
 import android.content.Context;
-import android.text.Spanned;
+
 import com.google.android.material.textfield.TextInputLayout;
+
 import java.util.regex.Pattern;
 
 public class UB extends MB {
+
     public Pattern f = Pattern.compile("([a-zA-Z][a-zA-Z\\d]*\\.)*[a-zA-Z][a-zA-Z\\d]*");
 
-    public UB(Context var1, TextInputLayout var2) {
-        super(var1, var2);
+    public UB(Context conte, TextInputLayout textInputLayout) {
+        super(conte, textInputLayout);
     }
 
-    public CharSequence filter(CharSequence var1, int var2, int var3, Spanned var4, int var5, int var6) {
-        return null;
-    }
-
-    public void onTextChanged(CharSequence var1, int var2, int var3, int var4) {
-        var2 = var1.toString().trim().length();
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        start = s.toString().trim().length();
         Integer var5 = 50;
-        if (var2 > 50) {
-            super.b.setErrorEnabled(true);
-            if (super.e == 0) {
-                super.b.setError(xB.b().a(super.a, 2131625432, new Object[]{var5}));
+        if (start > 50) {
+            b.setErrorEnabled(true);
+            if (e == 0) {
+                b.setError(xB.b().a(a, 2131625432, var5));
             } else {
-                super.b.setError(xB.b().a(super.a, super.e, new Object[]{var5}));
+                b.setError(xB.b().a(a, e, var5));
             }
 
-            super.d = false;
+            d = false;
         } else {
-            super.b.setErrorEnabled(false);
-            super.d = true;
-            if (this.f.matcher(var1.toString()).matches()) {
-                if (var1.toString().indexOf(".") < 0) {
-                    super.b.setErrorEnabled(true);
-                    super.b.setError(xB.b().a(super.a, 2131625683));
-                    super.d = false;
+            b.setErrorEnabled(false);
+            d = true;
+            if (f.matcher(s.toString()).matches()) {
+                if (s.toString().indexOf(".") < 0) {
+                    b.setErrorEnabled(true);
+                    b.setError(xB.b().a(a, 2131625683));
+                    d = false;
                     return;
                 }
 
-                super.b.setErrorEnabled(false);
-                super.d = true;
+                b.setErrorEnabled(false);
+                d = true;
             } else {
-                super.b.setErrorEnabled(true);
-                super.b.setError(xB.b().a(super.a, 2131625435));
-                super.d = false;
+                b.setErrorEnabled(true);
+                b.setError(xB.b().a(a, 2131625435));
+                d = false;
             }
 
-            String[] var15 = var1.toString().split("\\.");
+            String[] var15 = s.toString().split("\\.");
             int var6 = var15.length;
-            var2 = 0;
+            start = 0;
 
-            for(var3 = 0; var2 < var6; var3 = var4) {
-                String var7 = var15[var2];
+            for (before = 0; start < var6; before = count) {
+                String var7 = var15[start];
                 String[] var10 = uq.b;
                 int var8 = var10.length;
                 int var9 = 0;
 
-                while(true) {
-                    var4 = var3;
+                while (true) {
+                    count = before;
                     if (var9 >= var8) {
                         break;
                     }
 
                     if (var10[var9].equals(var7)) {
-                        var4 = 1;
+                        count = 1;
                         break;
                     }
 
                     ++var9;
                 }
 
-                ++var2;
+                ++start;
             }
 
-            if (var3) {
-                super.b.setErrorEnabled(true);
-                super.b.setError(xB.b().a(super.a, 2131625495));
-                super.d = false;
+            if (before) {
+                b.setErrorEnabled(true);
+                b.setError(xB.b().a(a, 2131625495));
+                d = false;
             }
 
         }
