72743dd956ba24f4fc0ce69103a6ef77aaf07f16
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-03-30 14:43:00 +0530

    refactor: Rename some local variables and a field

--- app/src/main/java/a/a/a/UB.java
+++ app/src/main/java/a/a/a/UB.java
@@ -8,76 +8,59 @@ import java.util.regex.Pattern;
 
 public class UB extends MB {
 
-    public Pattern f = Pattern.compile("([a-zA-Z][a-zA-Z\\d]*\\.)*[a-zA-Z][a-zA-Z\\d]*");
+    private static final Pattern packagePattern = Pattern.compile("([a-zA-Z][a-zA-Z\\d]*\\.)*[a-zA-Z][a-zA-Z\\d]*");
 
-    public UB(Context conte, TextInputLayout textInputLayout) {
-        super(conte, textInputLayout);
+    public UB(Context context, TextInputLayout textInputLayout) {
+        super(context, textInputLayout);
     }
 
     public void onTextChanged(CharSequence s, int start, int before, int count) {
-        start = s.toString().trim().length();
-        Integer var5 = 50;
-        if (start > 50) {
+        if (s.toString().trim().length() > 50) {
             b.setErrorEnabled(true);
             if (e == 0) {
-                b.setError(xB.b().a(a, 2131625432, var5));
+                b.setError(xB.b().a(a, 0x7f0e05d8, 50));
             } else {
-                b.setError(xB.b().a(a, e, var5));
+                b.setError(xB.b().a(a, e, 50));
             }
-
+            d = false;
+            return;
+        }
+        b.setErrorEnabled(false);
+        d = true;
+        if (!packagePattern.matcher(s.toString()).matches()) {
+            b.setErrorEnabled(true);
+            b.setError(xB.b().a(a, 0x7f0e05db));
             d = false;
         } else {
+            if (!s.toString().contains(".")) {
+                b.setErrorEnabled(true);
+                b.setError(xB.b().a(a, 0x7f0e06d3));
+                d = false;
+                return;
+            }
             b.setErrorEnabled(false);
             d = true;
-            if (f.matcher(s.toString()).matches()) {
-                if (s.toString().indexOf(".") < 0) {
-                    b.setErrorEnabled(true);
-                    b.setError(xB.b().a(a, 2131625683));
-                    d = false;
-                    return;
+        }
+        boolean containsReservedWord = false;
+        for (String packagePart : s.toString().split("\\.")) {
+            String[] reservedWords = uq.b;
+            int length = reservedWords.length;
+            int reservedWordIndex = 0;
+            while (true) {
+                if (reservedWordIndex >= length) {
+                    break;
                 }
-
-                b.setErrorEnabled(false);
-                d = true;
-            } else {
-                b.setErrorEnabled(true);
-                b.setError(xB.b().a(a, 2131625435));
-                d = false;
-            }
-
-            String[] var15 = s.toString().split("\\.");
-            int var6 = var15.length;
-            start = 0;
-
-            for (before = 0; start < var6; before = count) {
-                String var7 = var15[start];
-                String[] var10 = uq.b;
-                int var8 = var10.length;
-                int var9 = 0;
-
-                while (true) {
-                    count = before;
-                    if (var9 >= var8) {
-                        break;
-                    }
-
-                    if (var10[var9].equals(var7)) {
-                        count = 1;
-                        break;
-                    }
-
-                    ++var9;
+                if (reservedWords[reservedWordIndex].equals(packagePart)) {
+                    containsReservedWord = true;
+                    break;
                 }
-
-                ++start;
+                reservedWordIndex++;
             }
-
-            if (before) {
-                b.setErrorEnabled(true);
-                b.setError(xB.b().a(a, 2131625495));
-                d = false;
-            }
-
+        }
+        if (containsReservedWord) {
+            b.setErrorEnabled(true);
+            b.setError(xB.b().a(a, 0x7f0e0617));
+            d = false;
         }
     }
 }
