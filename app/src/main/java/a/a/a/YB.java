33038c6b6e0cad78f58ff94bef2f3864c6f4f383
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-04-08 02:37:22 +0530

    refactor: Update YB class for improved readability and efficiency

--- app/src/main/java/a/a/a/YB.java
+++ app/src/main/java/a/a/a/YB.java
@@ -1,99 +1,74 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package a.a.a;
 
 import android.content.Context;
-import android.text.Spanned;
+
 import com.google.android.material.textfield.TextInputLayout;
+
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class YB extends MB {
-    public String[] f;
-    public ArrayList<String> g;
-    public String h;
-    public Pattern i = Pattern.compile("^[a-z][a-z0-9_]*");
 
-    public YB(Context var1, TextInputLayout var2, String[] var3, ArrayList<String> var4) {
-        super(var1, var2);
-        this.f = var3;
-        this.g = var4;
+    private String[] f;
+    private ArrayList<String> g;
+    private String h;
+    private Pattern i = Pattern.compile("^[a-z][a-z0-9_]*");
+
+    public YB(Context context, TextInputLayout textInputLayout, String[] strArr, ArrayList<String> arrayList) {
+        super(context, textInputLayout);
+        f = strArr;
+        g = arrayList;
     }
 
-    public YB(Context var1, TextInputLayout var2, String[] var3, ArrayList<String> var4, String var5) {
-        super(var1, var2);
-        this.f = var3;
-        this.g = var4;
-        this.h = var5;
+    public YB(Context context, TextInputLayout textInputLayout, String[] strArr, ArrayList<String> arrayList, String str) {
+        super(context, textInputLayout);
+        f = strArr;
+        g = arrayList;
+        h = str;
     }
 
-    public CharSequence filter(CharSequence var1, int var2, int var3, Spanned var4, int var5, int var6) {
-        return null;
-    }
-
-    public void onTextChanged(CharSequence var1, int var2, int var3, int var4) {
-        if (var1.toString().trim().length() < 3) {
-            super.b.setErrorEnabled(true);
-            super.b.setError(xB.b().a(super.a, 2131625433, new Object[]{3}));
-            super.d = false;
-        } else if (var1.toString().trim().length() > 100) {
-            super.b.setErrorEnabled(true);
-            super.b.setError(xB.b().a(super.a, 2131625432, new Object[]{100}));
-            super.d = false;
-        } else {
-            String var5 = this.h;
-            if (var5 != null && var5.length() > 0 && var1.toString().equals(this.h)) {
-                super.b.setErrorEnabled(false);
-                super.d = true;
-            } else {
-                String[] var6 = this.f;
-                var3 = var6.length;
-                var2 = 0;
-
-                while(true) {
-                    if (var2 >= var3) {
-                        var2 = 0;
-                        break;
-                    }
-
-                    var5 = var6[var2];
-                    if (var1.toString().equals(var5)) {
-                        var2 = 1;
-                        break;
-                    }
-
-                    ++var2;
-                }
-
-                if (var2) {
-                    super.b.setErrorEnabled(true);
-                    super.b.setError(xB.b().a(super.a, 2131625495));
-                    super.d = false;
-                } else if (!"main".equals(var1.toString()) && this.g.indexOf(var1.toString()) < 0) {
-                    if (!Character.isLetter(var1.charAt(0))) {
-                        super.b.setErrorEnabled(true);
-                        super.b.setError(xB.b().a(super.a, 2131625497));
-                        super.d = false;
-                    } else {
-                        if (this.i.matcher(var1.toString()).matches()) {
-                            super.b.setErrorEnabled(false);
-                            super.d = true;
-                        } else {
-                            super.b.setErrorEnabled(true);
-                            super.b.setError(xB.b().a(super.a, 2131625437));
-                            super.d = false;
-                        }
-
-                    }
-                } else {
-                    super.b.setErrorEnabled(true);
-                    super.b.setError(xB.b().a(super.a, 2131624950));
-                    super.d = false;
-                }
+    public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        if (charSequence.toString().trim().length() < 3) {
+            b.setErrorEnabled(true);
+            b.setError(xB.b().a(a, 0x7f0e05d9, 3));
+            d = false;
+            return;
+        }
+        if (charSequence.toString().trim().length() > 100) {
+            b.setErrorEnabled(true);
+            b.setError(xB.b().a(a, 0x7f0e05d8, 100));
+            d = false;
+            return;
+        }
+        String str = h;
+        if (str != null && str.length() > 0 && charSequence.toString().equals(h)) {
+            b.setErrorEnabled(false);
+            d = true;
+            return;
+        }
+        for (String str2 : f) {
+            if (charSequence.toString().equals(str2)) {
+                b.setErrorEnabled(true);
+                b.setError(xB.b().a(a, 0x7f0e0617));
+                d = false;
+                return;
             }
+        }
+        if ("main".equals(charSequence.toString()) || g.contains(charSequence.toString())) {
+            b.setErrorEnabled(true);
+            b.setError(xB.b().a(a, 0x7f0e03f6));
+            d = false;
+        } else if (!Character.isLetter(charSequence.charAt(0))) {
+            b.setErrorEnabled(true);
+            b.setError(xB.b().a(a, 0x7f0e0619));
+            d = false;
+        } else if (this.i.matcher(charSequence.toString()).matches()) {
+            b.setErrorEnabled(false);
+            d = true;
+        } else {
+            b.setErrorEnabled(true);
+            b.setError(xB.b().a(a, 0x7f0e05dd));
+            d = false;
         }
     }
 }
