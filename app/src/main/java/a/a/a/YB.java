b2a2b34ec813e8854272ef28190a91f3efdd0f4d
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-04-08 02:41:26 +0530

    refactor: Give fields, local variable and parameters better name

--- app/src/main/java/a/a/a/YB.java
+++ app/src/main/java/a/a/a/YB.java
@@ -9,25 +9,25 @@ import java.util.regex.Pattern;
 
 public class YB extends MB {
 
-    private String[] f;
-    private ArrayList<String> g;
-    private String h;
-    private Pattern i = Pattern.compile("^[a-z][a-z0-9_]*");
+    private static final Pattern namePattern = Pattern.compile("^[a-z][a-z0-9_]*");
+    private final String[] reservedNames;
+    private final ArrayList<String> existingNames;
+    private String originalName;
 
-    public YB(Context context, TextInputLayout textInputLayout, String[] strArr, ArrayList<String> arrayList) {
+    public YB(Context context, TextInputLayout textInputLayout, String[] reservedNames, ArrayList<String> existingNames) {
         super(context, textInputLayout);
-        f = strArr;
-        g = arrayList;
+        this.reservedNames = reservedNames;
+        this.existingNames = existingNames;
     }
 
-    public YB(Context context, TextInputLayout textInputLayout, String[] strArr, ArrayList<String> arrayList, String str) {
+    public YB(Context context, TextInputLayout textInputLayout, String[] reservedNames, ArrayList<String> existingNames, String originalName) {
         super(context, textInputLayout);
-        f = strArr;
-        g = arrayList;
-        h = str;
+        this.reservedNames = reservedNames;
+        this.existingNames = existingNames;
+        this.originalName = originalName;
     }
 
-    public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+    public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
         if (charSequence.toString().trim().length() < 3) {
             b.setErrorEnabled(true);
             b.setError(xB.b().a(a, 0x7f0e05d9, 3));
@@ -40,21 +40,21 @@ public class YB extends MB {
             d = false;
             return;
         }
-        String str = h;
-        if (str != null && str.length() > 0 && charSequence.toString().equals(h)) {
+        String str = originalName;
+        if (str != null && !str.isEmpty() && charSequence.toString().equals(originalName)) {
             b.setErrorEnabled(false);
             d = true;
             return;
         }
-        for (String str2 : f) {
-            if (charSequence.toString().equals(str2)) {
+        for (String reservedName : reservedNames) {
+            if (charSequence.toString().equals(reservedName)) {
                 b.setErrorEnabled(true);
                 b.setError(xB.b().a(a, 0x7f0e0617));
                 d = false;
                 return;
             }
         }
-        if ("main".equals(charSequence.toString()) || g.contains(charSequence.toString())) {
+        if ("main".equals(charSequence.toString()) || existingNames.contains(charSequence.toString())) {
             b.setErrorEnabled(true);
             b.setError(xB.b().a(a, 0x7f0e03f6));
             d = false;
@@ -62,7 +62,7 @@ public class YB extends MB {
             b.setErrorEnabled(true);
             b.setError(xB.b().a(a, 0x7f0e0619));
             d = false;
-        } else if (this.i.matcher(charSequence.toString()).matches()) {
+        } else if (YB.namePattern.matcher(charSequence.toString()).matches()) {
             b.setErrorEnabled(false);
             d = true;
         } else {
