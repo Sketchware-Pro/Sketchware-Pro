ea737b85f586a6cb816474634a3572c8d9a03582
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-04-08 02:43:48 +0530

    fix: Replace magic numbers with constants

--- app/src/main/java/a/a/a/YB.java
+++ app/src/main/java/a/a/a/YB.java
@@ -7,6 +7,8 @@ import com.google.android.material.textfield.TextInputLayout;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
+import pro.sketchware.R;
+
 public class YB extends MB {
 
     private static final Pattern namePattern = Pattern.compile("^[a-z][a-z0-9_]*");
@@ -30,13 +32,13 @@ public class YB extends MB {
     public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
         if (charSequence.toString().trim().length() < 3) {
             b.setErrorEnabled(true);
-            b.setError(xB.b().a(a, 0x7f0e05d9, 3));
+            b.setError(xB.b().a(a, R.string.invalid_value_min_lenth, 3));
             d = false;
             return;
         }
         if (charSequence.toString().trim().length() > 100) {
             b.setErrorEnabled(true);
-            b.setError(xB.b().a(a, 0x7f0e05d8, 100));
+            b.setError(xB.b().a(a, R.string.invalid_value_max_lenth, 100));
             d = false;
             return;
         }
@@ -49,25 +51,25 @@ public class YB extends MB {
         for (String reservedName : reservedNames) {
             if (charSequence.toString().equals(reservedName)) {
                 b.setErrorEnabled(true);
-                b.setError(xB.b().a(a, 0x7f0e0617));
+                b.setError(xB.b().a(a, R.string.logic_editor_message_reserved_keywords));
                 d = false;
                 return;
             }
         }
         if ("main".equals(charSequence.toString()) || existingNames.contains(charSequence.toString())) {
             b.setErrorEnabled(true);
-            b.setError(xB.b().a(a, 0x7f0e03f6));
+            b.setError(xB.b().a(a, R.string.common_message_name_unavailable));
             d = false;
         } else if (!Character.isLetter(charSequence.charAt(0))) {
             b.setErrorEnabled(true);
-            b.setError(xB.b().a(a, 0x7f0e0619));
+            b.setError(xB.b().a(a, R.string.logic_editor_message_variable_name_must_start_letter));
             d = false;
         } else if (YB.namePattern.matcher(charSequence.toString()).matches()) {
             b.setErrorEnabled(false);
             d = true;
         } else {
             b.setErrorEnabled(true);
-            b.setError(xB.b().a(a, 0x7f0e05dd));
+            b.setError(xB.b().a(a, R.string.invalid_value_rule_4));
             d = false;
         }
     }
