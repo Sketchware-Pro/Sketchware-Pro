05b0e3f499641dcbf676fb31ac39c78acc668eda
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-04-08 02:49:14 +0530

    refactor: Some code cleanup

--- app/src/main/java/a/a/a/XB.java
+++ app/src/main/java/a/a/a/XB.java
@@ -1,13 +1,7 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package a.a.a;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.text.Spanned;
 import android.view.View;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -15,43 +9,40 @@ import com.google.android.material.textfield.TextInputLayout;
 import java.util.regex.Pattern;
 
 public class XB extends MB {
-    public Pattern f = Pattern.compile("[A-Fa-f0-9]*");
-    public View g;
+
+    private final Pattern f = Pattern.compile("[A-Fa-f0-9]*");
+    private final View g;
 
     public XB(Context var1, TextInputLayout var2, View var3) {
         super(var1, var2);
-        this.g = var3;
-    }
-
-    public CharSequence filter(CharSequence var1, int var2, int var3, Spanned var4, int var5, int var6) {
-        return null;
+        g = var3;
     }
 
     public void onTextChanged(CharSequence var1, int var2, int var3, int var4) {
         String var6 = var1.toString().trim();
         if (var6.length() > 8) {
-            super.b.setErrorEnabled(true);
-            super.b.setError(xB.b().a(super.a, 2131625432, new Object[]{8}));
-            super.d = false;
+            b.setErrorEnabled(true);
+            b.setError(xB.b().a(a, 2131625432, 8));
+            d = false;
         } else {
-            if (this.f.matcher(var6).matches()) {
+            if (f.matcher(var6).matches()) {
                 try {
                     var6 = String.format("#%8s", var6).replaceAll(" ", "F");
-                    this.g.setBackgroundColor(Color.parseColor(var6));
+                    g.setBackgroundColor(Color.parseColor(var6));
                 } catch (Exception var5) {
-                    super.b.setErrorEnabled(true);
-                    super.b.setError(xB.b().a(super.a, 2131625431));
-                    super.d = false;
-                    this.g.setBackgroundColor(-592138);
+                    b.setErrorEnabled(true);
+                    b.setError(xB.b().a(a, 2131625431));
+                    d = false;
+                    g.setBackgroundColor(-592138);
                 }
 
-                super.b.setErrorEnabled(false);
-                super.d = true;
+                b.setErrorEnabled(false);
+                d = true;
             } else {
-                super.b.setErrorEnabled(true);
-                super.b.setError(xB.b().a(super.a, 2131625431));
-                this.g.setBackgroundColor(-592138);
-                super.d = false;
+                b.setErrorEnabled(true);
+                b.setError(xB.b().a(a, 2131625431));
+                g.setBackgroundColor(-592138);
+                d = false;
             }
 
         }
