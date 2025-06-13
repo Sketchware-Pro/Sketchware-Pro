503264717f20264814cb46ce672305fd53e57091
Edward-NewGate-KOC <mechri.ishak.dev@gmail.com>
2025-06-13 15:07:45 +0100

    refactor: group main libs and sub-dependencies in DependencyBundle record instead of separate String fields
    Signed-off-by: Edward-NewGate-KOC <mechri.ishak.dev@gmail.com>

--- buildSrc/src/main/java/com/sketchware/LibraryVersionsClassBuilder.java
+++ buildSrc/src/main/java/com/sketchware/LibraryVersionsClassBuilder.java
@@ -3,29 +3,56 @@ package com.sketchware;
 import java.io.*;
 import java.util.*;
 import java.nio.file.*;
-import java.io.IOException;
+import java.util.stream.Collectors;
 
 public class LibraryVersionsClassBuilder {
 
-    private static String buildLibraryFields(Set<String> uniqueLibs) {
+    private static String buildLibraryFields(Map<String, ArrayList<String>> libsMap) {
         StringBuilder sb = new StringBuilder();
 
-        for (String lib : uniqueLibs) {
-            String[] parts = lib.split(":");
+        for (Map.Entry<String, ArrayList<String>> entry : libsMap.entrySet()) {
+            String mainLib = entry.getKey();
+            List<String> deps = entry.getValue();
 
-            String group = parts[0].substring(0, parts[0].indexOf(".")).toUpperCase().replace('.', '_');
-            String artifact = parts[1].toUpperCase().replace('-', '_');
-            String fieldName = group + "_" + artifact;
+            String fieldName = getFieldName(mainLib);
 
-            sb.append(String.format("""
-                                public static String %s = "%s:%s";
-                            """,
-                    fieldName, parts[1], parts[2]));
+            String mainVersion = deps.getFirst();
+            String subDepsList = deps.stream()
+                    .skip(1)
+                    .map(s -> "\"" + s + "\"")
+                    .collect(Collectors.joining(", "));
+
+            if (!subDepsList.isEmpty()) {
+                sb.append(String.format(
+                        """
+                                    public static final DependencyBundle %s = new DependencyBundle("%s", %s);
+                                """,
+                        fieldName, mainVersion, subDepsList
+                ));
+            } else {
+                sb.append(String.format(
+                        """
+                                    public static final DependencyBundle %s = new DependencyBundle("%s");
+                                """,
+                        fieldName, mainVersion
+                ));
+            }
         }
         return sb.toString();
     }
 
-    public static void generate(File outputDir, List<String> libs) throws IOException {
+    private static String getFieldName(String mainLib) {
+        String[] parts = mainLib.split(":");
+        String group = parts[0];
+        String artifact = parts[1];
+        String groupPart = group.contains(".")
+                ? group.substring(0, group.indexOf(".")).toUpperCase().replace('.', '_')
+                : group.toUpperCase().replace('.', '_');
+        String artifactPart = artifact.toUpperCase().replace('-', '_');
+        return groupPart + "_" + artifactPart;
+    }
+
+    public static void generate(File outputDir, Map<String, ArrayList<String>> libsMap) throws IOException {
         File javaDir = new File(outputDir, "pro/sketchware");
 
         if (!javaDir.exists()) {
@@ -33,8 +60,7 @@ public class LibraryVersionsClassBuilder {
         }
         File javaFile = new File(javaDir, "LibraryVersions.java");
 
-        Set<String> uniqueLibs = new LinkedHashSet<>(libs);
-        String fields = buildLibraryFields(uniqueLibs);
+        String fields = buildLibraryFields(libsMap);
 
         String content = String.format("""
                 /**
@@ -42,15 +68,32 @@ public class LibraryVersionsClassBuilder {
                  */
                 package pro.sketchware;
                 
+                import java.util.ArrayList;
+                import java.util.Arrays;
+                
                 public final class LibraryVersions {
                 
                     %s
                 
+                    public static record DependencyBundle(String libraryVersion, String... subDependencies) {
+                
+                        public DependencyBundle(String libraryVersion) {
+                            this(libraryVersion, new String[0]);
+                        }
+                
+                        public ArrayList<String> getAllDependencies() {
+                            ArrayList<String> allDependencies = new ArrayList<>();
+                            allDependencies.add(libraryVersion);
+                            allDependencies.addAll(Arrays.asList(subDependencies));
+                            return allDependencies;
+                        }
+                
+                    }
+
                 }
                 """, fields.trim());
 
         Files.writeString(javaFile.toPath(), content);
     }
-
 
 }
--- buildSrc/src/main/java/com/sketchware/SketchwareLibsPlugin.java
+++ buildSrc/src/main/java/com/sketchware/SketchwareLibsPlugin.java
@@ -1,15 +1,30 @@
 package com.sketchware;
 
-import org.gradle.api.*;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.ResolvedArtifact;
+import org.gradle.api.artifacts.ResolvedDependency;
 
-import java.io.*;
-import java.util.*;
-import java.util.stream.Stream;
-import java.util.zip.*;
-import java.nio.file.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UncheckedIOException;
+
 import java.nio.file.StandardCopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class SketchwareLibsPlugin implements Plugin<Project> {
 
@@ -34,20 +49,24 @@ public class SketchwareLibsPlugin implements Plugin<Project> {
                     Path libsFolder = buildDir.resolve("libs");
                     Files.createDirectories(libsFolder);
 
-                    List<String> gradleLibs = new ArrayList<>();
-                    for (org.gradle.api.artifacts.ResolvedArtifact dep : sketchwareLibs.getResolvedConfiguration().getResolvedArtifacts()) {
-                        String group = dep.getModuleVersion().getId().getGroup();
+                    Map<String, ArrayList<String>> libsMap = new LinkedHashMap<>();
+                    Set<ResolvedDependency> firstLevelDeps = sketchwareLibs.getResolvedConfiguration()
+                            .getFirstLevelModuleDependencies();
+                    for (ResolvedDependency dep : firstLevelDeps) {
+                        LinkedHashSet<String> artifacts = new LinkedHashSet<>();
+                        collectDependencyArtifacts(dep, artifacts);
+                        libsMap.put(
+                                dep.getModuleGroup() + ":" + dep.getModuleName() + ":" + dep.getModuleVersion(),
+                                new ArrayList<>(artifacts)
+                        );
+                    }
+
+                    for (ResolvedArtifact dep : sketchwareLibs.getResolvedConfiguration().getResolvedArtifacts()) {
                         String name = dep.getModuleVersion().getId().getName();
-                        String version = dep.getModuleVersion().getId().getVersion();
-
-                        String gradleId = group + ":" + name + ":" + version;
-                        gradleLibs.add(gradleId);
-
                         Path libFolder = libsFolder.resolve(name);
                         Files.createDirectories(libFolder);
 
                         File depFile = dep.getFile();
-
                         if (depFile.getName().endsWith(".aar")) {
                             unzipAAR(depFile, libFolder);
                         } else if (depFile.getName().endsWith(".jar")) {
@@ -70,7 +89,7 @@ public class SketchwareLibsPlugin implements Plugin<Project> {
 
                     Files.createDirectories(generatedDir);
 
-                    LibraryVersionsClassBuilder.generate(generatedDir.toFile(), gradleLibs);
+                    LibraryVersionsClassBuilder.generate(generatedDir.toFile(), libsMap);
 
                 } catch (Exception e) {
                     throw new RuntimeException("Error building sketchware libs", e);
@@ -79,6 +98,13 @@ public class SketchwareLibsPlugin implements Plugin<Project> {
 
             project.getTasks().named("preBuild").configure(t -> t.dependsOn("buildSketchwareLibs"));
         });
+    }
+
+    private void collectDependencyArtifacts(ResolvedDependency dep, Set<String> acc) {
+        acc.add(dep.getModuleName() + ":" + dep.getModuleVersion());
+        for (ResolvedDependency child : dep.getChildren()) {
+            collectDependencyArtifacts(child, acc);
+        }
     }
 
     private void unzipAAR(File aar, Path destDir) throws IOException {
@@ -99,7 +125,8 @@ public class SketchwareLibsPlugin implements Plugin<Project> {
     }
 
     private void zipFolder(File sourceDir, File zipFile) throws IOException {
-        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile)); Stream<Path> files = Files.walk(sourceDir.toPath())) {
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile));
+             Stream<Path> files = Files.walk(sourceDir.toPath())) {
             files.filter(Files::isRegularFile).forEach(path -> {
                 try {
                     ZipEntry entry = new ZipEntry(sourceDir.toPath().relativize(path).toString());
