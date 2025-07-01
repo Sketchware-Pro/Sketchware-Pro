2e8faf95028ca630db47b462c50c1cdb87e35adf
aikr <farzasz7@gmail.com>
2025-07-01 20:37:07 +0700

    feat: Harmonize block background color with primary color
    Signed-off-by: aikr <farzasz7@gmail.com>

--- app/src/main/java/mod/hey/studios/project/custom_blocks/CustomBlocksManager.java
+++ app/src/main/java/mod/hey/studios/project/custom_blocks/CustomBlocksManager.java
@@ -1,5 +1,6 @@
 package mod.hey.studios.project.custom_blocks;
 
+import android.content.Context;
 import android.os.Environment;
 
 import com.besome.sketch.beans.BlockBean;
@@ -25,11 +26,13 @@ import pro.sketchware.utility.FileUtil;
 import pro.sketchware.utility.SketchwareUtil;
 
 public class CustomBlocksManager {
+    private final Context context;
     final String sc_id;
     ArrayList<BlockBean> blocks;
     ArrayList<ExtraBlockInfo> custom_blocks;
 
-    public CustomBlocksManager(String sc_id) {
+    public CustomBlocksManager(Context context, String sc_id) {
+        this.context = context;
         this.sc_id = sc_id;
 
         load();
@@ -90,7 +93,7 @@ public class CustomBlocksManager {
                     if (!(block.opCode.equals("definedFunc")
                             || block.opCode.equals("getVar")
                             || block.opCode.equals("getArg"))) {
-                        if (kq.a(block.opCode, block.type) == -7711273) {
+                        if (kq.a(context, block.opCode, block.type) == 0xff8a55d7) {
                             if (!usedBlocks.contains(block.opCode)) {
                                 usedBlocks.add(block.opCode);
 
