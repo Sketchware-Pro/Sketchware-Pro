b24b7ec737228d916f7c2c8ddb44380189bb7cae
aikr <farzasz7@gmail.com>
2025-07-02 12:37:27 +0700

    feat: Use ViewBinding in AsdDialog.java
    Signed-off-by: aikr <farzasz7@gmail.com>

--- app/src/main/java/mod/hilal/saif/asd/AsdDialog.java
+++ app/src/main/java/mod/hilal/saif/asd/AsdDialog.java
@@ -11,29 +11,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.Button;
-
-import androidx.appcompat.widget.Toolbar;
 
 import com.besome.sketch.editor.LogicEditorActivity;
 
 import a.a.a.Lx;
 import a.a.a.Ss;
-import io.github.rosemoe.sora.widget.CodeEditor;
 import io.github.rosemoe.sora.widget.component.EditorAutoCompletion;
 import mod.hey.studios.code.SrcCodeEditor;
 import mod.hey.studios.util.Helper;
 import pro.sketchware.R;
+import pro.sketchware.databinding.CodeEditorHsAsdBinding;
 import pro.sketchware.utility.EditorUtils;
 import pro.sketchware.utility.SketchwareUtil;
 
 public class AsdDialog extends Dialog implements DialogInterface.OnDismissListener {
     private static SharedPreferences pref;
     private Activity act;
-    private CodeEditor codeEditor;
+    private CodeEditorHsAsdBinding binding;
     private String str;
-    private Button cancel;
-    private Button save;
 
     public AsdDialog(Activity activity) {
         super(activity);
@@ -43,7 +38,9 @@ public class AsdDialog extends Dialog implements DialogInterface.OnDismissListen
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.code_editor_hs_asd);
+
+        binding = CodeEditorHsAsdBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         Window window = getWindow();
         if (window != null) {
@@ -51,21 +48,15 @@ public class AsdDialog extends Dialog implements DialogInterface.OnDismissListen
             window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         }
 
-        cancel = findViewById(R.id.btn_cancel);
-        save = findViewById(R.id.btn_save);
+        binding.editor.setTypefaceText(EditorUtils.getTypeface(act));
+        binding.editor.setText(Lx.j(str, false));
+        binding.editor.setWordwrap(false);
 
-        codeEditor = findViewById(R.id.editor);
-        codeEditor.setTypefaceText(EditorUtils.getTypeface(act));
-        codeEditor.setText(Lx.j(str, false));
-        codeEditor.setWordwrap(false);
-
-        EditorUtils.loadJavaConfig(codeEditor);
-        SrcCodeEditor.loadCESettings(act, codeEditor, "dlg");
+        EditorUtils.loadJavaConfig(binding.editor);
+        SrcCodeEditor.loadCESettings(act, binding.editor, "dlg");
         pref = SrcCodeEditor.pref;
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-
-        Menu menu = toolbar.getMenu();
+        Menu menu = binding.toolbar.getMenu();
         MenuItem itemWordwrap = menu.findItem(R.id.action_word_wrap);
         MenuItem itemAutocomplete = menu.findItem(R.id.action_autocomplete);
         MenuItem itemAutocompleteSymbolPair = menu.findItem(R.id.action_autocomplete_symbol_pair);
@@ -74,15 +65,15 @@ public class AsdDialog extends Dialog implements DialogInterface.OnDismissListen
         itemAutocomplete.setChecked(pref.getBoolean("dlg_ac", false));
         itemAutocompleteSymbolPair.setChecked(pref.getBoolean("dlg_acsp", true));
 
-        toolbar.setOnMenuItemClickListener(item -> {
+        binding.toolbar.setOnMenuItemClickListener(item -> {
             int id = item.getItemId();
             if (id == R.id.action_undo) {
-                codeEditor.undo();
+                binding.editor.undo();
             } else if (id == R.id.action_redo) {
-                codeEditor.redo();
+                binding.editor.redo();
             } else if (id == R.id.action_pretty_print) {
                 StringBuilder sb = new StringBuilder();
-                String[] split = codeEditor.getText().toString().split("\n");
+                String[] split = binding.editor.getText().toString().split("\n");
                 for (String s : split) {
                     String trim = (s + "X").trim();
                     sb.append(trim.substring(0, trim.length() - 1));
@@ -97,25 +88,25 @@ public class AsdDialog extends Dialog implements DialogInterface.OnDismissListen
                     SketchwareUtil.toastError("Your code contains incorrectly nested parentheses");
                 }
                 if (!failed) {
-                    codeEditor.setText(code);
+                    binding.editor.setText(code);
                 }
             } else if (id == R.id.action_word_wrap) {
                 item.setChecked(!item.isChecked());
-                codeEditor.setWordwrap(item.isChecked());
+                binding.editor.setWordwrap(item.isChecked());
                 pref.edit().putBoolean("dlg_ww", item.isChecked()).apply();
             } else if (id == R.id.action_autocomplete_symbol_pair) {
                 item.setChecked(!item.isChecked());
-                codeEditor.getProps().symbolPairAutoCompletion = item.isChecked();
+                binding.editor.getProps().symbolPairAutoCompletion = item.isChecked();
                 pref.edit().putBoolean("dlg_acsp", item.isChecked()).apply();
             } else if (id == R.id.action_autocomplete) {
                 item.setChecked(!item.isChecked());
-                codeEditor.getComponent(EditorAutoCompletion.class).setEnabled(item.isChecked());
+                binding.editor.getComponent(EditorAutoCompletion.class).setEnabled(item.isChecked());
                 pref.edit().putBoolean("dlg_ac", item.isChecked()).apply();
             } else if (id == R.id.action_paste) {
-                codeEditor.pasteText();
+                binding.editor.pasteText();
             } else if (id == R.id.action_find_replace) {
-                codeEditor.getSearcher().stopSearch();
-                codeEditor.beginSearchMode();
+                binding.editor.getSearcher().stopSearch();
+                binding.editor.beginSearchMode();
             }
             return true;
         });
@@ -125,17 +116,17 @@ public class AsdDialog extends Dialog implements DialogInterface.OnDismissListen
 
     @Override
     public void onDismiss(DialogInterface dialog) {
-        pref.edit().putInt("dlg_ts", (int) (codeEditor.getTextSizePx() / act.getResources().getDisplayMetrics().scaledDensity)).apply();
+        pref.edit().putInt("dlg_ts", (int) (binding.editor.getTextSizePx() / act.getResources().getDisplayMetrics().scaledDensity)).apply();
         pref = null;
         act = null;
     }
 
     public void saveLis(LogicEditorActivity logicEditorActivity, boolean z, Ss ss, AsdDialog asdDialog) {
-        save.setOnClickListener(new AsdHandlerCodeEditor(logicEditorActivity, z, ss, asdDialog, codeEditor));
+        binding.btnSave.setOnClickListener(new AsdHandlerCodeEditor(logicEditorActivity, z, ss, asdDialog, binding.editor));
     }
 
     public void cancelLis(AsdDialog asdDialog) {
-        cancel.setOnClickListener(Helper.getDialogDismissListener(asdDialog));
+        binding.btnCancel.setOnClickListener(Helper.getDialogDismissListener(asdDialog));
     }
 
     public void setCon(String str2) {
