c96ea910b6169966a30171785b055182c8a30be4
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-08-04 01:22:02 +0530

    Refactor: Improve code in `lv.java`
    - Removed unnecessary casts.
    - Added `@Override` annotations.
    - Simplified variable names.
    - Used `findViewById` directly instead of from a `LinearLayout` instance.
    - Used `getContext()` instead of casting to `LinearLayout` to get the context.
    - Changed an if condition to check for empty string instead of length less than or equal to zero.

--- app/src/main/java/a/a/a/lv.java
+++ app/src/main/java/a/a/a/lv.java
@@ -5,11 +5,11 @@
 
 package a.a.a;
 
-import android.animation.Animator;
 import android.content.Context;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.besome.sketch.beans.ProjectLibraryBean;
 import com.besome.sketch.editor.manage.library.firebase.FirebaseActivity;
 
@@ -20,69 +20,73 @@ public class lv extends LinearLayout implements nv {
 
     public lv(FirebaseActivity var1) {
         super(var1);
-        this.a((Context)var1);
+        a(var1);
     }
 
+    @Override
     public void a() {
     }
 
-    public final void a(Context var1) {
+    private void a(Context var1) {
         wB.a(var1, this, 2131427542);
-        gB.b(this, 600, 200, (Animator.AnimatorListener)null);
-        ((TextView)((LinearLayout)this).findViewById(2131232232)).setText(xB.b().a(var1, 2131625238));
-        ((TextView)((LinearLayout)this).findViewById(2131232199)).setText(xB.b().a(var1, 2131625232));
-        ((TextView)((LinearLayout)this).findViewById(2131232197)).setText(xB.b().a(var1, 2131625231));
-        this.a = (EditText)((LinearLayout)this).findViewById(2131230999);
-        this.b = (EditText)((LinearLayout)this).findViewById(2131230992);
-        this.c = (EditText)((LinearLayout)this).findViewById(2131230991);
+        gB.b(this, 600, 200, null);
+        ((TextView) findViewById(2131232232)).setText(xB.b().a(var1, 2131625238));
+        ((TextView) findViewById(2131232199)).setText(xB.b().a(var1, 2131625232));
+        ((TextView) findViewById(2131232197)).setText(xB.b().a(var1, 2131625231));
+        a = findViewById(2131230999);
+        b = findViewById(2131230992);
+        c = findViewById(2131230991);
     }
 
-    public void a(ProjectLibraryBean var1) {
-        var1.data = this.a.getText().toString().trim();
-        var1.reserved1 = this.b.getText().toString().trim();
-        var1.reserved2 = this.c.getText().toString().trim();
+    @Override
+    public void a(ProjectLibraryBean libraryBean) {
+        libraryBean.data = a.getText().toString().trim();
+        libraryBean.reserved1 = b.getText().toString().trim();
+        libraryBean.reserved2 = c.getText().toString().trim();
     }
 
     public void b() {
-        bB.a(((LinearLayout)this).getContext(), xB.b().a(((LinearLayout)this).getContext(), 2131625224), 1).show();
+        bB.a(getContext(), xB.b().a(getContext(), 2131625224), 1).show();
     }
 
+    @Override
     public String getDocUrl() {
         return "https://docs.sketchware.io/docs/firebase-project-settings.html";
     }
 
+    @Override
     public boolean isValid() {
-        if (this.a.getText().toString().trim().length() <= 0) {
-            this.a.requestFocus();
-            this.b();
+        if (a.getText().toString().trim().length() == 0) {
+            a.requestFocus();
+            b();
             return false;
-        } else if (this.b.getText().toString().trim().length() <= 0) {
-            this.b.requestFocus();
-            this.b();
+        } else if (b.getText().toString().trim().length() == 0) {
+            b.requestFocus();
+            b();
             return false;
-        } else if (this.c.getText().toString().trim().length() <= 0) {
-            this.c.requestFocus();
-            this.b();
+        } else if (c.getText().toString().trim().length() == 0) {
+            c.requestFocus();
+            b();
             return false;
         } else {
             return true;
         }
     }
 
-    public void setData(ProjectLibraryBean var1) {
-        String var2 = var1.data;
-        if (var2 != null && var2.length() > 0) {
-            this.a.setText(var1.data);
+    public void setData(ProjectLibraryBean libraryBean) {
+        String data = libraryBean.data;
+        if (data != null && data.length() > 0) {
+            a.setText(data);
         }
 
-        var2 = var1.reserved1;
-        if (var2 != null && var2.length() > 0) {
-            this.b.setText(var1.reserved1);
+        String reserved1 = libraryBean.reserved1;
+        if (reserved1 != null && reserved1.length() > 0) {
+            b.setText(reserved1);
         }
 
-        var2 = var1.reserved2;
-        if (var2 != null && var2.length() > 0) {
-            this.c.setText(var1.reserved2);
+        String reserved2 = libraryBean.reserved2;
+        if (reserved2 != null && reserved2.length() > 0) {
+            c.setText(reserved2);
         }
 
     }
