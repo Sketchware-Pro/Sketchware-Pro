2c31c5fb54821413dd36a50d5858dc4165aeebb6
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-08-04 00:53:45 +0530

    refactor: Clean up `mv.java`
    This commit refactors `mv.java` by:
    - Removing unnecessary casts.
    - Using `this.findViewById` instead of `((LinearLayout)this).findViewById`.
    - Using `getContext()` instead of `((LinearLayout)this).getContext()`.
    - Adding `@Override` annotations where appropriate.
    - Improving parameter names for clarity.
    - Removing commented-out code.

--- app/src/main/java/a/a/a/mv.java
+++ app/src/main/java/a/a/a/mv.java
@@ -1,39 +1,37 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package a.a.a;
 
-import android.animation.Animator;
 import android.content.Context;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.besome.sketch.beans.ProjectLibraryBean;
 import com.besome.sketch.editor.manage.library.firebase.FirebaseActivity;
 
 public class mv extends LinearLayout implements nv {
+
     public EditText a;
 
-    public mv(FirebaseActivity var1) {
-        super(var1);
-        this.a((Context)var1);
+    public mv(FirebaseActivity firebaseActivity) {
+        super(firebaseActivity);
+        a(firebaseActivity);
     }
 
+    @Override
     public void a() {
-        mB.a(((LinearLayout)this).getContext(), this.a);
+        mB.a(getContext(), a);
     }
 
-    public final void a(Context var1) {
-        wB.a(var1, this, 2131427544);
-        gB.b(this, 600, 200, (Animator.AnimatorListener)null);
-        ((TextView)((LinearLayout)this).findViewById(2131232241)).setText(xB.b().a(var1, 2131625239));
-        this.a = (EditText)((LinearLayout)this).findViewById(2131231003);
+    private void a(Context context) {
+        wB.a(context, this, 2131427544);
+        gB.b(this, 600, 200, null);
+        ((TextView) findViewById(2131232241)).setText(xB.b().a(context, 2131625239));
+        a = findViewById(2131231003);
     }
 
+    @Override
     public void a(ProjectLibraryBean var1) {
-        String var2 = this.a.getText().toString().trim();
+        String var2 = a.getText().toString().trim();
         if (!var2.isEmpty()) {
             var1.reserved3 = var2;
             if (var2.startsWith("gs://")) {
@@ -47,19 +45,20 @@ public class mv extends LinearLayout implements nv {
 
     }
 
+    @Override
     public String getDocUrl() {
         return "https://docs.sketchware.io/docs/firebase-storage.html";
     }
 
+    @Override
     public boolean isValid() {
         return true;
     }
 
-    public void setData(ProjectLibraryBean var1) {
-        String var2 = var1.reserved3;
-        if (var2 != null && var2.length() > 0) {
-            this.a.setText(var1.reserved3);
+    public void setData(ProjectLibraryBean libraryBean) {
+        String reserved3 = libraryBean.reserved3;
+        if (reserved3 != null && reserved3.length() > 0) {
+            a.setText(libraryBean.reserved3);
         }
-
     }
 }
