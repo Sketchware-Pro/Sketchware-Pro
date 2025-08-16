7e19686dd852a5804809229d156930b7ffa14a3a
Edward-NewGate-KOC <mechri.ishak.dev@gmail.com>
2025-08-16 18:21:16 +0100

    fix(NotifyTelegram): apply escape_parentheses only on commit message to keep markdown formatting intact

--- .github/workflows/notify_telegram.py
+++ .github/workflows/notify_telegram.py
@@ -11,24 +11,27 @@ def get_git_commit_info():
     return commit_author, commit_message, commit_hash, commit_hash_short
 
 def escape_markdown_v2(text):
-    escape_chars = r'_*[\]()~`>#+-=|{}.!'
+    escape_chars = r'_~`#+-=|{}.!'
     return re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text)
+
+def escape_parentheses(text):
+    return re.sub(r'([()])', r'\\\1', text)
 
 def main():
     bot_token = os.environ['BOT_TOKEN']
     chat_id = os.environ['CHAT_ID']
     topic_id = os.environ.get('TOPIC_ID')
-    
+
     commit_author, commit_message, commit_hash, commit_hash_short = get_git_commit_info()
 
     message = (
         f"A new [commit](https://github.com/Sketchware-Pro/Sketchware-Pro/commit/{commit_hash}) has been merged to the repository by *{commit_author}*.\n\n"
-        f"*What has changed:*\n>{commit_message}\n\n"
+        f"*What has changed:*\n>{escape_parentheses(commit_message)}\n\n"
         f"I'm currently building it and will send you the APKs here within ~6 mins if the build is successful.\n\n#{commit_hash_short}"
     )
-    
+
     escaped_message = escape_markdown_v2(message)
-    
+
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     payload = {
         "chat_id": chat_id,
