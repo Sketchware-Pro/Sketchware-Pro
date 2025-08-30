28cbbb091e6e24b0868789d9839435ddb7f8e524
aikr <farzasz7@gmail.com>
2025-08-30 13:08:23 +0700

    refactor: Rename variable names to more clear in ItemListView

--- app/src/main/java/com/besome/sketch/editor/view/item/ItemListView.java
+++ app/src/main/java/com/besome/sketch/editor/view/item/ItemListView.java
@@ -6,6 +6,8 @@ import android.graphics.Paint;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+
 import com.besome.sketch.beans.ViewBean;
 import com.besome.sketch.editor.view.ItemView;
 
@@ -23,25 +25,25 @@ public class ItemListView extends ListView implements ItemView {
 
     public Paint paint;
 
-    public float oneDp;
+    public float dip;
 
-    public ArrayList<String> items;
+    public ArrayList<String> exampleData;
 
     public ItemListView(Context context) {
         super(context);
-        items = new ArrayList<>();
+        exampleData = new ArrayList<>();
         initialize(context);
     }
 
     public void initialize(Context context) {
-        oneDp = wB.a(context, 1.0f);
+        dip = wB.a(context, 1.0f);
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setStrokeWidth(wB.a(getContext(), 2.0f));
         setDrawingCacheEnabled(true);
-        items.add("List item 1");
-        items.add("List item 2");
-        items.add("List item 3");
-        setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, items));
+        exampleData.add("List item 1");
+        exampleData.add("List item 2");
+        exampleData.add("List item 3");
+        setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, exampleData));
     }
 
     @Override
@@ -75,27 +77,24 @@ public class ItemListView extends ListView implements ItemView {
     }
 
     @Override
-    public void onDraw(Canvas canvas) {
+    public void onDraw(@NonNull Canvas canvas) {
         if (selected) {
             paint.setColor(0x9599d5d0);
             canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), paint);
         } else {
             paint.setColor(0x60000000);
-            int measuredWidth2 = getMeasuredWidth();
-            int measuredHeight2 = getMeasuredHeight();
-            float measuredWidth = measuredWidth2;
-            canvas.drawLine(0.0f, 0.0f, measuredWidth, 0.0f, paint);
-            float measuredHeight = measuredHeight2;
-            canvas.drawLine(0.0f, 0.0f, 0.0f, measuredHeight, paint);
-            canvas.drawLine(measuredWidth, 0.0f, measuredWidth, measuredHeight, paint);
-            canvas.drawLine(0.0f, measuredHeight, measuredWidth, measuredHeight, paint);
+            int measuredWidth = getMeasuredWidth();
+            int measuredHeight = getMeasuredHeight();
+            canvas.drawLine(0.0f, 0.0f, (float) measuredWidth, 0.0f, paint);
+            canvas.drawLine(0.0f, 0.0f, 0.0f, (float) measuredHeight, paint);
+            canvas.drawLine((float) measuredWidth, 0.0f, (float) measuredWidth, (float) measuredHeight, paint);
+            canvas.drawLine(0.0f, (float) measuredHeight, (float) measuredWidth, (float) measuredHeight, paint);
         }
         super.onDraw(canvas);
     }
 
     @Override
     public void setPadding(int left, int top, int right, int bottom) {
-        float oneDp = this.oneDp;
-        super.setPadding((int) (left * oneDp), (int) (top * oneDp), (int) (right * oneDp), (int) (bottom * oneDp));
+        super.setPadding((int) (left * dip), (int) (top * dip), (int) (right * dip), (int) (bottom * dip));
     }
 }
