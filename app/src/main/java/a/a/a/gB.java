245cfdb494c51a0558b909d3a79e67b239c8280e
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-09-21 01:06:03 +0530

    refactor: Rename local variable to better name

--- app/src/main/java/a/a/a/gB.java
+++ app/src/main/java/a/a/a/gB.java
@@ -9,60 +9,73 @@ import android.view.ViewGroup;
 import android.view.animation.BounceInterpolator;
 
 public class gB {
-    public static void a(View var0, float var1, int var2, int var3, Animator.AnimatorListener var4) {
-        var0.animate().setListener(var4).rotation(var1).setStartDelay((long) var3).setDuration((long) var2).start();
+    public static void a(View view, float rotation, int duration, int delay, Animator.AnimatorListener listener) {
+        view.animate().setListener(listener).rotation(rotation)
+                .setStartDelay(delay).setDuration(duration).start();
     }
 
-    public static void a(View var0, float var1, Animator.AnimatorListener var2) {
-        var0.animate().setListener(var2).rotation(var1).start();
+    public static void a(View view, float rotation, Animator.AnimatorListener listener) {
+        view.animate().setListener(listener).rotation(rotation).start();
     }
 
-    public static void a(View var0, int var1) {
-        int var2 = Color.parseColor("#b2000000");
-        ValueAnimator var3 = ValueAnimator.ofObject(new ArgbEvaluator(), new Object[]{0, var2});
-        var3.setDuration((long) var1);
-        var3.addUpdateListener(new fB(var0));
-        var3.start();
+    public static void a(View view, int duration) {
+        int color = Color.parseColor("#b2000000");
+        ValueAnimator valueAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), 0, color);
+        valueAnimator.setDuration(duration);
+        valueAnimator.addUpdateListener(new fB(view));
+        valueAnimator.start();
     }
 
-    public static void a(View var0, int var1, int var2, Animator.AnimatorListener var3) {
-        var0.setTranslationY((float) (-var0.getMeasuredHeight() * 5));
-        var0.animate().translationY(0.0F).setInterpolator(new BounceInterpolator()).setDuration((long) var1).setStartDelay((long) var2).setListener((Animator.AnimatorListener) null).start();
+    public static void a(View view, int duration, int startDelay, Animator.AnimatorListener listener) {
+        view.setTranslationY((float) (-view.getMeasuredHeight() * 5));
+        view.animate().translationY(0.0F)
+                .setInterpolator(new BounceInterpolator())
+                .setDuration(duration)
+                .setStartDelay(startDelay)
+                .setListener(null).start();
     }
 
-    public static void a(ViewGroup var0, int var1, Animator.AnimatorListener var2) {
-        ValueAnimator var3 = ValueAnimator.ofInt(new int[]{var0.getHeight(), 0});
-        var3.addUpdateListener(new eB(var0));
-        var3.addListener(var2);
-        var3.setDuration((long) var1);
-        var3.start();
+    public static void a(ViewGroup viewGroup, int duration, Animator.AnimatorListener listener) {
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(viewGroup.getHeight(), 0);
+        valueAnimator.addUpdateListener(new eB(viewGroup));
+        valueAnimator.addListener(listener);
+        valueAnimator.setDuration(duration);
+        valueAnimator.start();
     }
 
-    public static void b(View var0, int var1, int var2, Animator.AnimatorListener var3) {
-        var0.setAlpha(0.0F);
-        var0.setTranslationY(140.0F);
-        var0.animate().setListener(var3).alpha(1.0F).translationY(0.0F).setStartDelay((long) var2).setDuration((long) var1).start();
+    public static void b(View view, int duration, int startDelay, Animator.AnimatorListener listener) {
+        view.setAlpha(0.0F);
+        view.setTranslationY(140.0F);
+        view.animate().setListener(listener)
+                .alpha(1.0F)
+                .translationY(0.0F)
+                .setStartDelay(startDelay)
+                .setDuration(duration).start();
     }
 
-    public static void b(ViewGroup var0, int var1, Animator.AnimatorListener var2) {
-        var0.measure(-1, -2);
-        ValueAnimator var3 = ValueAnimator.ofInt(new int[]{0, var0.getMeasuredHeight()});
-        var3.addUpdateListener(new dB(var0));
-        if (var2 != null) {
-            var3.addListener(var2);
+    public static void b(ViewGroup viewGroup, int duration, Animator.AnimatorListener listener) {
+        viewGroup.measure(-1, -2);
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, viewGroup.getMeasuredHeight());
+        valueAnimator.addUpdateListener(new dB(viewGroup));
+        if (listener != null) {
+            valueAnimator.addListener(listener);
         }
 
-        var3.setDuration((long) var1);
-        var3.start();
+        valueAnimator.setDuration(duration);
+        valueAnimator.start();
     }
 
-    public static void c(View var0, int var1, int var2, Animator.AnimatorListener var3) {
-        var0.setTranslationX((float) (-var0.getMeasuredWidth()));
-        var0.animate().translationX(0.0F).setStartDelay((long) var2).setDuration((long) var1).setListener(var3).start();
+    public static void c(View view, int duration, int startDelay, Animator.AnimatorListener listener) {
+        view.setTranslationX((float) (-view.getMeasuredWidth()));
+        view.animate().translationX(0.0F).setStartDelay(startDelay).setDuration(duration).setListener(listener).start();
     }
 
-    public static void d(View var0, int var1, int var2, Animator.AnimatorListener var3) {
-        var0.setTranslationX((float) var0.getMeasuredWidth());
-        var0.animate().translationX(0.0F).setStartDelay((long) var2).setDuration((long) var1).setListener(var3).start();
+    public static void d(View view, int duration, int startDelay, Animator.AnimatorListener listener) {
+        view.setTranslationX((float) view.getMeasuredWidth());
+        view.animate().translationX(0.0F)
+                .setStartDelay(startDelay)
+                .setDuration(duration)
+                .setListener(listener)
+                .start();
     }
 }
